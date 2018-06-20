684d29ca5229aa6078cf1a8563b31ff97e10e0b1
Luke Camilleri <luke.camilleri@casumo.com>
2018-06-20 15:38:33 +0200

    Change DummyImage tag to img

--- src/components/DummyImage.js
+++ src/components/DummyImage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default ({ src, alt, ...rest }) => (
-  <div
+  <img
     src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
     alt={alt}
     {...rest}
