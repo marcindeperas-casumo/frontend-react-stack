b97e0cc663047a637f1f6ded269b1bdf0100f1e0
Cho <choishere@gmail.com>
2018-07-12 11:51:57 +0200

    handle possible updates from websockets into state

--- src/containers/LiveCasinoLobbyContainer/LiveCasinoLobbyContainer.js
+++ src/containers/LiveCasinoLobbyContainer/LiveCasinoLobbyContainer.js
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom";
 import { getHostElement } from "../../utils";
 import lobbyWS from './ws';
 
+
 export default class LiveCasinoLobbyContainer extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.hostElement = getHostElement("liveCasinoLobby");
     this.el = document.createElement("div");
@@ -16,15 +17,105 @@ export default class LiveCasinoLobbyContainer extends React.Component {
     };
   }
 
+  processType (data) {
+    const index = this.state.data.findIndex(e => e.id === data.tableId);
+    const types = {
+      // all tables
+      'State': () => {
+        const tables = Object.keys(data.tables)
+          .map(k => ({...data.tables[k], id: k}));
+        this.setState({ ...this.state, data: tables });
+      },
+      // tables status
+      'TableAssigned': () => {
+        this.setState(prevState => {
+          const newTable = data.table;
+          newTable.id = data.tableId;
+          return {data: [...prevState.data, newTable]}
+        });
+      },
+      'TableUnassigned': () => {
+        this.setState({
+          data: this.state.data.filter((_, i) => i !== index)
+        });
+      },
+      'TableUpdated': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index] = data.table;
+            return {data: newData};
+        });
+      },
+      'TableOpened': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index].open = true;
+            return {data: newData};
+        });
+      },
+      'TableClosed': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index].open = false;
+            return {data: newData};
+        });
+      },
+      // seats
+      'SeatsUpdated': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index].seatsTaken = data.seatsTaken;
+            return {data: newData};
+        });
+      },
+      // numbers
+      'RouletteNumbersUpdated': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index].results = data.results;
+            return {data: newData};
+        });
+      },
+      'BaccaratRoadUpdated': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index].road = data.road;
+            return {data: newData};
+        });
+      },
+      'MoneyWheelNumbersUpdated': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index].results = data.results;
+            return {data: newData};
+        });
+      },
+      // players
+      'PlayersUpdated': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            console.log('index', index, newData[index]);
+            newData[index].players = data.players;
+            return {data: newData};
+        });
+      },
+      // hours
+      'OperationHoursUpdated': () => {
+        this.setState(prevState => {
+            const newData = [...prevState.data];
+            newData[index].operationHours = data.operationHours;
+            return {data: newData};
+        });
+      },
+
+      'default': () => null
+    };
+    return (types[data.type] || types['default'])();
+  }
+
   onmessage (data) {
-    console.log('onmessage data', data);
-
-    if (data.type === 'State') {
-      const tables = Object.keys(data.tables).map(k => ({...data.tables[k], id: k}));
-      this.setState({ ...this.state, data: tables });
-
-      console.log('onmessage tables', tables);
-    }
+    console.log('onmessage data', data, this.state);
+    this.processType(data);
   }
 
   componentWillMount () {
@@ -33,6 +124,7 @@ export default class LiveCasinoLobbyContainer extends React.Component {
     const ws = lobbyWS.connect();
     ws.onopen = e => console.log('ws onopen');
     ws.onmessage = e => this.onmessage(JSON.parse(e.data));
+    // TODO: handle onclose and reconnection
   }
 
   componentDidMount () {
@@ -48,7 +140,7 @@ export default class LiveCasinoLobbyContainer extends React.Component {
 
     return ReactDOM.createPortal(
       <React.Fragment>
-        {data.map(x => <div key={x.id}>{x.name}</div>)}
+        {data.map(x => <div key={x.id}>{x.name}, {x.players}</div>)}
       </React.Fragment>,
       this.el
     );
