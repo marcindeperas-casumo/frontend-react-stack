1cb56a751afef895cfcf8bfad9fc970a8e972baa
Jack Westbrook <jack.westbrook@casumo.com>
2018-07-18 16:53:22 +0200

    Update skeleton list to match latest design

--- src/containers/GamesLists/GamesListsSkeleton.js
+++ src/containers/GamesLists/GamesListsSkeleton.js
@@ -6,9 +6,12 @@ export default () => (
     {Array.from(Array(4).keys()).map(i => (
       <SkeletonGameTiles
         key={i}
-        tileWidth={170}
-        tileHeight={204}
-        preserveAspectRatio="none"
+        tileWidth={175}
+        tileGap={10}
+        tileRadius={8}
+        preserveAspectRatio="xMinYMin"
+        colorLow="#eff6f6"
+        colorHi="#ffffff"
         className="u-padding-top--normal u-padding-top--semi@tablet u-padding-top--semi@desktop
         u-padding-left--small u-padding-left--xlarge@tablet u-padding-left--xlarge@desktop"
       />
