7b8fb90388d98397c495310e8e93ec2b86ebba46
Luke Camilleri <luke.camilleri@casumo.com>
2018-07-27 19:21:22 +0200

    feat(game-tile): Refactor maintenance mode to match the newest design iteration
    Casumo/Home#21650

---
+++ src/components/GameTile/index.js
@@ -0,0 +1,70 @@
+import Heading from "@casumo/cmp-heading";
+import { MoreIcon } from "@casumo/cmp-icons";
+import classNames from "classnames";
+import React from "react";
+import { decodeString } from "../../utils";
+import LazyImage from "../LazyImage";
+import PlayAction from "./PlayAction";
+import TemporaryUnavailable from "./TemporaryUnavailable";
+
+const GameTile = ({
+  logoBackground,
+  logo,
+  name,
+  slug,
+  className,
+  inMaintenanceMode,
+  launchGame,
+}) => (
+  <div
+    className={classNames(
+      className,
+      "c-game-tile",
+      "o-ratio",
+      "o-ratio--game-tile",
+      "c-scrollable-game",
+      "t-border-r--8",
+      inMaintenanceMode && "c-game-tile--maintenance"
+    )}
+    tabIndex={0}
+  >
+    <LazyImage
+      className="o-ratio__content"
+      src={logoBackground}
+      mark={logo}
+      alt={name}
+      dpr={3}
+    />
+    <div
+      className={classNames(
+        "flex-vertical",
+        "o-ratio__content",
+        "c-game-tile__overlay",
+        inMaintenanceMode && "c-game-tile__overlay--maintenance",
+        "u-padding-vert--normal",
+        "u-padding-horiz--small"
+      )}
+    >
+      <Heading
+        className="t-color-white"
+        size="milli"
+        text={decodeString(name)}
+      />
+
+      {inMaintenanceMode ? (
+        <TemporaryUnavailable />
+      ) : (
+        <PlayAction launchGame={launchGame} />
+      )}
+
+      <a href={`/en/play/${slug}`} onMouseDown={e => e.preventDefault()}>
+        <MoreIcon
+          size="med"
+          className="t-background-white t-color-grey-dark-3 t-border-r--circle u-padding--micro"
+        />
+      </a>
+    </div>
+  </div>
+);
+
+export default GameTile;
