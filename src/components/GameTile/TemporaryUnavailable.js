d1866de7247a2587259e9a646ee50dd6828e6bb4
Luke Camilleri <luke.camilleri@casumo.com>
2018-08-22 12:05:56 +0200

    refactor(component): Remove not needed React.Fragment element
    Casumo/Home#21828

--- src/components/GameTile/TemporaryUnavailable.js
+++ src/components/GameTile/TemporaryUnavailable.js
@@ -2,9 +2,7 @@ import React from "react";
 import CMSField from "../../containers/CMSField";
 
 const TemporaryUnavailable = () => (
-  <React.Fragment>
-    {<CMSField slug="mobile.game-details" field="game_in_maintenance_text" />}
-  </React.Fragment>
+  <CMSField slug="mobile.game-details" field="game_in_maintenance_text" />
 );
 
 export default TemporaryUnavailable;
