e57cd061d7aaa4366a66ea50ecdd240f1c44f53e
Luke Camilleri <luke.camilleri@casumo.com>
2018-09-03 13:23:24 +0200

    refactor(precommit): Skip all the pre-commit checks if the current commit is a merge
    Casumo/Home#22430

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,15 +1,24 @@
-# source: https://github.com/okonet/lint-staged/issues/62#issuecomment-411964870
-# precommit.sh
-message="precommit_on_$(git log -1 --format=%H)"
-git stash -k -m$message
+git merge HEAD &> /dev/null
+result=$?
 
-# because git stash always success
-# we need to check if we have stashed successfully
-lastStash=$(git stash list -1)
-if [[ $lastStash == *$message* ]]; then
-	# If stashed, git stash pop when exit
-	trap 'git stash pop' EXIT
+if [ $result -ne 0 ]
+then
+    echo "Merge in progress. Skipping all pre-commit checks."
+else
+    # source: https://github.com/okonet/lint-staged/issues/62#issuecomment-411964870
+    # precommit.sh
+    message="precommit_on_$(git log -1 --format=%H)"
+    git stash -k -m$message
+
+    # because git stash always success
+    # we need to check if we have stashed successfully
+    lastStash=$(git stash list -1)
+    if [[ $lastStash == *$message* ]]; then
+        # If stashed, git stash pop when exit
+        trap 'git stash pop' EXIT
+    fi
+
+    # Here's what we intend to run
+    lint-staged
 fi
 
-# Here's what we intend to run
-lint-staged
