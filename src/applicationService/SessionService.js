d488d8066c45c37451298ea5efcdb398f929552a
Charles Oliver <charles.oliver@casumo.com>
2018-09-13 17:52:17 +0100

    use "prop" not "prop as property". tweak formatting for consistency

--- src/applicationService/SessionService.js
+++ src/applicationService/SessionService.js
@@ -1,10 +1,10 @@
-import { compose, complement, isNil, prop as property } from "ramda";
+import { complement, compose, isNil, prop } from "ramda";
 
 import { composePromises } from "../lib/utils";
 import commonService from "./CommonService";
 import countryGuesserService from "./CountryGuesserService";
 
-const fromCommonHandshake = k => property(`common/composition/${k}`);
+const fromCommonHandshake = k => prop(`common/composition/${k}`);
 
 const pullSession = fromCommonHandshake("session");
 const pullPlayers = fromCommonHandshake("players");
@@ -13,23 +13,23 @@ const currentPlayerFromHandshake = compose(
   x => [pullSession(x), pullPlayers(x)]
 );
 const languageFromPlayer = compose(
-  property("language"),
-  property("configuration")
+  prop("language"),
+  prop("configuration")
 );
 const currencyFromPlayer = compose(
-  property("iso4217CurrencyCode"),
-  property("balance"),
-  property("wallet")
+  prop("iso4217CurrencyCode"),
+  prop("balance"),
+  prop("wallet")
 );
 const countryFromPlayer = compose(
-  property("country"),
-  property("primaryAddress"),
-  property("contactInfo")
+  prop("country"),
+  prop("primaryAddress"),
+  prop("contactInfo")
 );
 const currencyFromPlayerWallet = compose(
-  property("iso4217CurrencyCode"),
-  property("balance"),
-  property("wallet")
+  prop("iso4217CurrencyCode"),
+  prop("balance"),
+  prop("wallet")
 );
 
 export const SessionServiceFactory = ({
@@ -40,9 +40,8 @@ export const SessionServiceFactory = ({
 
   const isAuthenticated = composePromises(complement(isNil), getSession);
   const country = async () => {
-    if (!(await isAuthenticated())) {
-      return countryGuesserService.guess();
-    }
+    if (!(await isAuthenticated())) return countryGuesserService.guess();
+
     const handshake = await commonService.handshake();
     const currentPlayer = currentPlayerFromHandshake(handshake);
 
@@ -54,38 +53,38 @@ export const SessionServiceFactory = ({
       // This should be refactored as language guesser
       return countryGuesserService.guess();
     }
+
     const handshake = await commonService.handshake();
     const currentPlayer = currentPlayerFromHandshake(handshake);
+
     return languageFromPlayer(currentPlayer);
   };
 
   const market = async () => {
     const defaultMarket = "___en";
-    if (!(await isAuthenticated())) {
-      return defaultMarket;
-    }
+
+    if (!(await isAuthenticated())) return defaultMarket;
+
     const handshake = await commonService.handshake();
     const currentPlayer = currentPlayerFromHandshake(handshake);
+
     return currentPlayer.market;
   };
 
   const currencyCode = async () => {
     const defaultCurrency = "EUR";
-    if (!(await isAuthenticated())) {
-      return defaultCurrency;
-    }
+
+    if (!(await isAuthenticated())) return defaultCurrency;
 
     const handshake = await commonService.handshake();
     const currentPlayer = currentPlayerFromHandshake(handshake);
+
     return currencyFromPlayer(currentPlayer);
   };
 
   const playerId = async () => {
-    if (!(await isAuthenticated())) {
-      return null;
-    }
-
-    return composePromises(property("id"), getSession)();
+    if (!(await isAuthenticated())) return null;
+    return composePromises(prop("id"), getSession)();
   };
 
   const iso4217CurrencyCode = async () => {
