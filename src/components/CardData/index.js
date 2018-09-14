f330a52d96bb630b407f8e1ba9ade454981a9478
Cho <choishere@gmail.com>
2018-09-14 11:31:52 +0200

    :sparkles: add recent letters for Live Football

--- src/components/CardData/index.js
+++ src/components/CardData/index.js
@@ -5,8 +5,9 @@ import Badge from "@casumo/cmp-badge";
 import Text from "@casumo/cmp-text";
 import CMSField from "../CMSField";
 import Matcher from "../Matcher";
+import { reject } from "ramda";
 
-import { getDreamCatcherColor, getRouletteColor } from "./utils";
+import { getBadgeColor, topCardLetters } from "./utils";
 
 import "./CardData.scss";
 
@@ -18,67 +19,54 @@ type Props = {
   max: number,
 };
 
-const getNumberColor = (type, n) => {
-  if (type === "MoneyWheel") return getDreamCatcherColor(n);
-  if (type === "Roulette") return getRouletteColor(n);
+const getText = field => (
+  <CMSField
+    slug="mobile.live-casino-cards-content"
+    field={field}
+    view={text => (
+      <Text size="xs" tag="span">
+        {text}
+      </Text>
+    )}
+  />
+);
+
+const renderResults = ({ game }) => {
+  // rejects `Suited Tie` from results and limit to 5
+  const results = reject(v => v === "S", game.results.slice(0, 5));
+  return (
+    <React.Fragment>
+      <div className="o-layout o-layout--gap u-margin-bottom">
+        {results.map((n, i) => {
+          const color = getBadgeColor(game.type, n);
+          return (
+            <Badge
+              key={i}
+              tag="div"
+              bgColor={color}
+              txtColor={color === "yellow" ? "grey-dark-3" : "white"}
+              circle={true}
+            >
+              {game.type === "TopCard"
+                ? topCardLetters[n]
+                : isNaN(parseInt(n, 10))
+                  ? n
+                  : parseInt(n, 10)}
+            </Badge>
+          );
+        })}
+      </div>
+      <Text
+        size="xs"
+        className="t-color-white u-margin-bottom--small u-font-weight-bold u-text-transform-uppercase"
+      >
+        {game.type === "TopCard"
+          ? getText("recent_letters")
+          : getText("recent_numbers")}
+      </Text>
+    </React.Fragment>
+  );
 };
-
-const renderResults = ({ game }) => (
-  <React.Fragment>
-    <div className="o-layout o-layout--gap u-margin-bottom">
-      {game.results.slice(0, 5).map((n, i) => {
-        const color = getNumberColor(game.type, n);
-        return (
-          <Badge
-            key={i}
-            tag="div"
-            bgColor={color}
-            txtColor={color === "yellow" ? "grey-dark-3" : "white"}
-            circle={true}
-          >
-            {isNaN(parseInt(n, 10)) ? n : parseInt(n, 10)}
-          </Badge>
-        );
-      })}
-    </div>
-    <CMSField
-      slug="mobile.live-casino-cards-content"
-      field="recent_numbers"
-      view={text => (
-        <Text
-          size="xs"
-          className="t-color-white u-margin-bottom--small u-font-weight-bold u-text-transform-uppercase"
-        >
-          {text}
-        </Text>
-      )}
-    />
-  </React.Fragment>
-);
-
-const OpenSeats = () => (
-  <CMSField
-    slug="mobile.live-casino-cards-content"
-    field="open_seats"
-    view={text => (
-      <Text size="xs" tag="span">
-        {text}
-      </Text>
-    )}
-  />
-);
-
-const TableFull = () => (
-  <CMSField
-    slug="mobile.live-casino-cards-content"
-    field="table_full"
-    view={text => (
-      <Text size="xs" tag="span">
-        {text}
-      </Text>
-    )}
-  />
-);
 
 const renderSeats = ({ game }) => (
   <React.Fragment>
@@ -105,7 +93,7 @@ const renderSeats = ({ game }) => (
       size="xs"
       className="t-color-white u-margin-bottom--small u-font-weight-bold u-text-transform-uppercase"
     >
-      {game.seats ? <OpenSeats /> : <TableFull />}
+      {game.seats ? getText("open_seats") : getText("table_full")}
     </Text>
   </React.Fragment>
 );
@@ -123,15 +111,15 @@ const Type = props => (
 );
 
 const CardData = ({ className, game, ...props }: Props) => {
-  const condition = game.results
-    ? "results"
-    : game.type === "Blackjack"
-      ? "seats"
-      : null;
+  let renderType = null;
+  if (game.type === "Blackjack") renderType = "seats";
+  if (["MoneyWheel", "Roulette", "TopCard"].includes(game.type))
+    renderType = "results";
+
   return (
-    condition && (
+    renderType && (
       <div className="c-card-data o-flex--vertical o-flex-align--center o-flex-justify--end u-width--1/1 u-font-weight-bold">
-        <Type condition={condition} game={game} />
+        <Type condition={renderType} game={game} />
       </div>
     )
   );
--- src/components/CardData/utils.test.js
+++ src/components/CardData/utils.test.js
@@ -1,8 +1,4 @@
-import {
-  getRouletteColor,
-  getDreamCatcherColor,
-  rouletteNumbers,
-} from "./utils";
+import { getBadgeColor, rouletteResults } from "./utils";
 
 const rouletteRed = [
   "1",
@@ -48,46 +44,66 @@ const rouletteBlack = [
 
 const rouletteGreen = ["0", "00"];
 
-describe("getRouletteColor()", () => {
+describe("getBadgeColor() for `Roulette`", () => {
+  const type = "Roulette";
   test("should return `red` for red numbers", () => {
-    expect(rouletteRed).toEqual(rouletteNumbers["red"]);
-    rouletteRed.forEach(n => expect(getRouletteColor(n)).toBe("red"));
+    expect(rouletteRed).toEqual(rouletteResults["red"]);
+    rouletteRed.forEach(n => expect(getBadgeColor(type, n)).toBe("red"));
   });
 
   test("should return `grey-dark-2` for black numbers", () => {
-    expect(rouletteBlack).toEqual(rouletteNumbers["grey-dark-2"]);
-    rouletteBlack.forEach(n => expect(getRouletteColor(n)).toBe("grey-dark-2"));
+    expect(rouletteBlack).toEqual(rouletteResults["grey-dark-2"]);
+    rouletteBlack.forEach(n =>
+      expect(getBadgeColor(type, n)).toBe("grey-dark-2")
+    );
   });
 
   test("should return `green-light-1` for 0 and 00", () => {
-    expect(rouletteGreen).toEqual(rouletteNumbers["green-light-1"]);
+    expect(rouletteGreen).toEqual(rouletteResults["green-light-1"]);
     rouletteGreen.forEach(n =>
-      expect(getRouletteColor(n)).toBe("green-light-1")
+      expect(getBadgeColor(type, n)).toBe("green-light-1")
     );
   });
 });
 
-describe("getDreamCatcherColor()", () => {
+describe("getBadgeColor() for `MoneyWheel`", () => {
+  const type = "MoneyWheel";
   test("should return `yellow` colour for `01`", () =>
-    expect(getDreamCatcherColor("01")).toBe("yellow"));
+    expect(getBadgeColor(type, "01")).toBe("yellow"));
 
   test("should return `blue-light-1` colour for `02`", () =>
-    expect(getDreamCatcherColor("02")).toBe("blue-light-1"));
+    expect(getBadgeColor(type, "02")).toBe("blue-light-1"));
 
   test("should return `purple` colour for `05`", () =>
-    expect(getDreamCatcherColor("05")).toBe("purple"));
+    expect(getBadgeColor(type, "05")).toBe("purple"));
 
   test("should return `green-light-1` colour for `10`", () =>
-    expect(getDreamCatcherColor("10")).toBe("green-light-1"));
+    expect(getBadgeColor(type, "10")).toBe("green-light-1"));
 
   test("should return `orange` colour for `20`", () =>
-    expect(getDreamCatcherColor("20")).toBe("orange"));
+    expect(getBadgeColor(type, "20")).toBe("orange"));
 
   test("should return `red` colour for `40`", () =>
-    expect(getDreamCatcherColor("40")).toBe("red"));
+    expect(getBadgeColor(type, "40")).toBe("red"));
 
-  test("should return `grey-dark-2` colour for multipliers X2 and X7", () => {
-    expect(getDreamCatcherColor("X2")).toBe("grey-dark-2");
-    expect(getDreamCatcherColor("X7")).toBe("grey-dark-2");
+  test("should return `grey-dark-2` colour for multipliers X2, X7 and by default", () => {
+    expect(getBadgeColor(type, "X2")).toBe("grey-dark-2");
+    expect(getBadgeColor(type, "X7")).toBe("grey-dark-2");
+    expect(getBadgeColor(type, "XX")).toBe("grey-dark-2");
   });
+});
+
+describe("getBadgeColor() for `TopCard`", () => {
+  const type = "TopCard";
+  test("should return `red` colour for `L`", () =>
+    expect(getBadgeColor(type, "L")).toBe("red"));
+
+  test("should return `grey-dark-2` colour for `T`", () =>
+    expect(getBadgeColor(type, "T")).toBe("grey-dark-2"));
+
+  test("should return `red` colour for `R`", () =>
+    expect(getBadgeColor(type, "R")).toBe("blue-light-1"));
+
+  test("should return `grey-dark-2` colour by default", () =>
+    expect(getBadgeColor(type, "W")).toBe("grey-dark-2"));
 });
