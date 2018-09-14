e92f7773c7e9a4b83a2bb9052a4b070b60ca65d9
Michele Settepani <michele.settepani@casumo.com>
2018-09-14 12:20:18 +0200

    JackpotTicker: fix small screen view 💊

--- src/components/JackpotTicker/JackpotTicker.js
+++ src/components/JackpotTicker/JackpotTicker.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Badge from "@casumo/cmp-badge";
 
-export default function JackpotTicker({ formattedJackpotAmount, className }) {
+export default function JackpotTicker({
+  formattedJackpotAmount,
+  className,
+  size = "sm",
+}) {
   if (!formattedJackpotAmount) {
     return null;
   }
 
-  return <Badge className={className}>{formattedJackpotAmount}</Badge>;
+  return (
+    <Badge className={className} size={size}>
+      {formattedJackpotAmount}
+    </Badge>
+  );
 }
