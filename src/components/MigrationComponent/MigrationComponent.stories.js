586924b2f795e0b2e7008e4fe57d9dbd550d1d13
Luke Camilleri <luke.camilleri@casumo.com>
2018-09-18 18:26:18 +0200

    refactor(migration-component-manager): Add component sotries
    Casumo/Home#22783

--- src/components/MigrationComponent/MigrationComponent.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import MigrationComponent from "./MigrationComponent";
-
-storiesOf("MigrationComponent", module)
-  .add("Default", () => <MigrationComponent />)
-  .add("With a twist! 👀", () => <MigrationComponent />);
