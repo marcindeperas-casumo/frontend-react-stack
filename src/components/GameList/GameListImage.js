f417f6f06dc579fdf56735dbdb3153dff9b2f73b
Michele Settepani <michele.settepani@casumo.com>
2018-09-19 11:35:19 +0200

    GameListImage: remove obsolete class 🔪

--- src/components/GameList/GameListImage.js
+++ src/components/GameList/GameListImage.js
@@ -2,12 +2,5 @@ import React from "react";
 import LazyImage from "../LazyImage";
 
 export default function GameListImage({ src }) {
-  return (
-    <LazyImage
-      className="c-card__img-pic"
-      style={{ width: "100%" }}
-      src={src}
-      dpr={3}
-    />
-  );
+  return <LazyImage style={{ width: "100%" }} src={src} dpr={3} />;
 }
