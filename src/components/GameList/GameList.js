9a6ea2ada5540b457a3151fb36f2d2871cdb7a95
Cho <choishere@gmail.com>
2018-09-19 14:44:14 +0200

    add carddata tests, update Card stories, take 2 refactor

--- src/components/GameList/GameList.js
+++ src/components/GameList/GameList.js
@@ -1,68 +1,79 @@
 import React from "react";
-import GameListImage from "./GameListImage";
-import GameListPlayers from "../LiveCasinoCard/CardPlayers";
-import GameListCards from "../LiveCasinoCard/LiveCasinoCard";
-import GameListTiles from "./GameListTiles";
-import GameListTitle from "./GameListTitle";
-import GameListExclusiveTiles from "./GameListExclusiveTiles";
+import Text from "@casumo/cmp-text";
+import Flex from "@casumo/cmp-flex";
 import ScrollingContainer from "@casumo/cmp-scrollable";
+import classNames from "classnames";
+
 import { KO_APP_EVENT_LAUNCH_GAME } from "../../constants";
 import legacyBridge from "../../legacyBridge";
-import Matcher from "../Matcher";
-import CardData from "../LiveCasinoCard";
+
 import GameListSkeleton from "./GameListSkeleton";
+import GameTile from "../GameTile";
+import LiveCasinoCard from "../LiveCasinoCard";
+import Matcher from "../Matcher";
 
-const renderImage = src => <GameListImage src={src} />;
-
-const renderPlayers = number => <GameListPlayers number={number} />;
-
-export const emitLaunchGame = slug => {
+const emitLaunchGame = slug => {
   legacyBridge.emit(KO_APP_EVENT_LAUNCH_GAME, {
     slug,
     playForFun: false,
   });
 };
 
-const renderCardData = game => <CardData game={game} />;
-
-const renderCards = ({ games }) =>
+const renderLiveCasinoCards = ({ games }) =>
   games.map(game => (
-    <GameListCards
-      game={game}
-      key={game.slug}
-      renderImage={renderImage}
-      renderCardData={renderCardData}
-      renderPlayers={renderPlayers}
-    />
+    <Flex.Item className="o-flex__item-fixed-size o-flex" key={game.slug}>
+      <LiveCasinoCard {...game} launchGame={() => emitLaunchGame(game.slug)} />
+    </Flex.Item>
   ));
 
 const renderTiles = ({ games }) =>
-  games.map(game => <GameListTiles game={game} key={game.slug} />);
+  games.map(game => (
+    <Flex.Item
+      className="o-flex__item-fixed-size o-flex c-top-game"
+      key={game.slug}
+    >
+      <GameTile {...game} launchGame={() => emitLaunchGame(game.slug)} />
+    </Flex.Item>
+  ));
 
 const renderExclusiveTiles = ({ games }) =>
-  games.map(game => <GameListExclusiveTiles game={game} key={game.slug} />);
+  games.map(game => (
+    <Flex.Item
+      className="o-flex__item-fixed-size o-flex c-exclusive-game"
+      key={game.slug}
+    >
+      <GameTile
+        {...game}
+        ratio="game-tile-exclusive"
+        imgixOpts={{
+          w: 188,
+          h: 280,
+          fit: "crop",
+        }}
+        launchGame={() => emitLaunchGame(game.slug)}
+      />
+    </Flex.Item>
+  ));
 
 const CardsOrTiles = props => (
   <Matcher
     getKey={({ display }) => display}
     matchers={{
-      cards: renderCards,
       tiles: renderTiles,
+      liveCasinoCards: renderLiveCasinoCards,
       exclusiveTiles: renderExclusiveTiles,
     }}
     {...props}
   />
 );
 
-const paddingPerDevice = {
-  default: "small",
-  tablet: "xlarge",
-  desktop: "xlarge",
-};
-
 const renderList = ({ display, games }) => (
   <ScrollingContainer
-    padding={paddingPerDevice}
+    padding={{
+      default: "small",
+      tablet: "xlarge",
+      desktop: "xlarge",
+    }}
     itemSpacing={display === "cards" ? "small" : "default"}
   >
     <CardsOrTiles display={display} games={games} />
@@ -75,6 +86,7 @@ const renderSkeleton = ({ display }) => (
     itemRatio={display === "cards" ? 0.98 : 1.2}
     itemGap={display === "cards" ? 16 : 8}
     display={display}
+    title={false}
     preserveAspectRatio="xMinYMin"
     colorLow="#eff6f6"
     colorHi="#ffffff"
@@ -99,7 +111,21 @@ const GameList = props => {
   return (
     <div className="u-padding-top--semi">
       <div className="u-display--flex">
-        <GameListTitle title={title} link={link} />
+        <Text
+          className={classNames(
+            "u-padding-bottom--small",
+            "u-padding-bottom--normal@tablet",
+            "u-padding-bottom--normal@desktop",
+            "u-padding-left--small",
+            "u-padding-left--xlarge@tablet",
+            "u-padding-left--xlarge@desktop",
+            "u-font-weight-bold",
+            link && "flex-1"
+          )}
+          tag="h3"
+        >
+          {title}
+        </Text>
       </div>
 
       <LoadingOrList condition={loading} {...props} />
---
+++ src/components/LiveCasinoCard/Card.stories.js
@@ -0,0 +1,40 @@
+/* @flow */
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
+
+import LiveCasinoCard from "./Card";
+
+import gameRoulette from "./__mocks__/Roulette.json";
+import gameTopCard from "./__mocks__/TopCard.json";
+import gameMoneyWheel from "./__mocks__/MoneyWheel.json";
+import gameBlackjack from "./__mocks__/Blackjack.json";
+import gameBlackjackFull from "./__mocks__/BlackjackFull.json";
+
+const stories = storiesOf("Card", module);
+
+stories.add(
+  "Card MoneyWheel",
+  withInfo("Card")(() => <LiveCasinoCard {...gameMoneyWheel} />)
+);
+
+stories.add(
+  "Card Roulette",
+  withInfo("Card")(() => <LiveCasinoCard {...gameRoulette} />)
+);
+
+stories.add(
+  "Card Blackjack Open Seats",
+  withInfo("Card")(() => <LiveCasinoCard {...gameBlackjack} />)
+);
+
+stories.add(
+  "Card Blackjack Full",
+  withInfo("Card")(() => <LiveCasinoCard {...gameBlackjackFull} />)
+);
+
+stories.add(
+  "Card TopCard (Football)",
+  withInfo("Card")(() => <LiveCasinoCard {...gameTopCard} />)
+);
