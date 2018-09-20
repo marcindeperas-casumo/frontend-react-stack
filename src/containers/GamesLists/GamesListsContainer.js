5ef7fb3d5a0f22ad629bb1a9ce6348c2e949d66a
Michele Settepani <michele.settepani@casumo.com>
2018-09-20 14:04:24 +0200

    GamesListsContainer: remove nested ternary operator ☠️

--- src/containers/GamesLists/GamesListsContainer.js
+++ src/containers/GamesLists/GamesListsContainer.js
@@ -9,15 +9,11 @@ import GameList from "Components/GameList";
 import { arrayToObject } from "Utils/index";
 import GamesListsSkeleton from "Containers/GamesLists/GamesListsSkeleton";
 
-const ifLiveCasinoId = id => ["liveCasinoGames", "liveCasino"].includes(id);
-
 const gamesNotInMaintenance = compose(
   not,
   gameInMaintenanceMode
 );
 const removeGamesInMaintenance = games => games.filter(gamesNotInMaintenance);
-
-const exclusiveGamesList = gameListId => gameListId === "exclusiveGames";
 
 export default class GamesListsContainer extends React.Component {
   constructor(props) {
@@ -94,13 +90,7 @@ export default class GamesListsContainer extends React.Component {
           filteredList.map(gameList => (
             <GameList
               key={gameList.title}
-              display={
-                ifLiveCasinoId(gameList.id)
-                  ? "liveCasinoCards"
-                  : exclusiveGamesList(gameList.id)
-                    ? "exclusiveTiles"
-                    : "tiles"
-              }
+              display={listTypeByListId(gameList.id)}
               {...gameList}
             />
           ))}
@@ -108,3 +98,15 @@ export default class GamesListsContainer extends React.Component {
     );
   }
 }
+
+const listIdToGamesList = {
+  exclusiveGames: "exclusiveTiles",
+  liveCasinoGames: "liveCasinoCards",
+  liveCasino: "liveCasinoCards",
+  casumoJackpotGames: "jackpotTiles",
+  default: "tiles",
+};
+
+const listTypeByListId = listId => {
+  return listIdToGamesList[listId] || listIdToGamesList.default;
+};
