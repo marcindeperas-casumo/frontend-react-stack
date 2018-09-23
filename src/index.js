75e58ce79d3c7777d05b85b8fa5eb5075dd7c9f9
Luke Camilleri <luke.camilleri@casumo.com>
2018-09-23 12:15:34 +0200

    refactor(index): Add renderApp method in preparation for HMR
    Casumo/Home#22820

--- src/index.js
+++ src/index.js
@@ -11,18 +11,18 @@ import { Provider } from "react-redux";
 import configureStore from "./configureStore";
 
 window.bridge = legacyBridge;
-const root = document.getElementById("root");
-
 legacyBridge.on(REACT_APP_EVENT_ROUTE_CHANGE, data => {
   legacyBridge.emit(REACT_APP_EVENT_ALL_PORTALS_CLEAR);
   legacyBridge.emit(data.config.id);
 });
 
 const store = configureStore();
+const renderApp = Component =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.getElementById("root")
+  );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  root
-);
+renderApp(App);
