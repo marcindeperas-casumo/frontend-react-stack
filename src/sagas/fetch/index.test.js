ce561328381e2ea33f79cc77443ed97a06b02df7
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-01 13:56:08 +0200

    refactor(fetch-saga): Add asyncCall test
    Casumo/Home#22820

--- src/sagas/fetch/index.test.js
+++ src/sagas/fetch/index.test.js
@@ -52,6 +52,27 @@ describe("fetch saga", () => {
     expect(generator.next().value).toEqual(put(actions.requestComplete(name)));
   });
 
+  test("success flow with asyncCall", () => {
+    const name = "actionName";
+    const asyncCall = jest.fn();
+    const postFetch = "postFetchActionName";
+    const returnData = { foo: { bar: "baz" } };
+
+    const generator = fetchSaga({
+      name,
+      asyncCall,
+      postFetch,
+    });
+
+    expect(generator.next().value).toEqual(put(actions.clearError(name)));
+    expect(generator.next().value).toEqual(put(actions.sendRequest(name)));
+    expect(generator.next().value).toEqual(call(asyncCall));
+    expect(generator.next(returnData).value).toEqual(
+      put(actions.postFetch(postFetch, returnData))
+    );
+    expect(generator.next().value).toEqual(put(actions.requestComplete(name)));
+  });
+
   test("erroneous flow", () => {
     const name = "actionName";
     const method = "GET";
