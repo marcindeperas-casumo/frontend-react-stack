cfd533ccb7036cb2ae799281bb26c7b28bae0893
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-02 20:48:14 +0200

    refactor(scrollable-list): Move the render data map for a given list id inside the component
    Casumo/Home#22820

--- src/components/ScrollableList/ScrollableList.js
+++ src/components/ScrollableList/ScrollableList.js
@@ -1,6 +1,9 @@
 import React, { PureComponent } from "react";
 import ScrollingContainer from "@casumo/cmp-scrollable";
 import GameListTitle from "Components/GameList/GameListTitle";
+import GameTileContainer from "Containers/GameTileContainer";
+import ExclusiveGameTileContainer from "Containers/ExclusiveGameTileContainer";
+import LiveCasinoCardContainer from "Containers/LiveCasinoCardContainer";
 
 const paddingPerDevice = {
   default: "md",
@@ -8,16 +11,31 @@ const paddingPerDevice = {
   desktop: "2xlg",
 };
 
+// Question: Should this map live in here or in the container component?
+//
+// Initially the `Component` property was defined in the container
+// (listIdByComponentMap), but then we had the spacing property defined in here
+// (listIdBySpacingMap). The issue with this is that the keys of the map
+// (listIds) need to be maintained in two different places.
+const listIdToRenderDataMap = {
+  exclusiveGames: { Component: ExclusiveGameTileContainer },
+  liveCasinoGames: { Component: LiveCasinoCardContainer, spacing: "md" },
+  liveCasino: { Component: LiveCasinoCardContainer, spacing: "md" },
+};
+
 export class ScrollableList extends PureComponent {
   render() {
-    const { title, games, gameComponent: Component } = this.props;
+    const { id, title, games } = this.props;
+    const { Component = GameTileContainer, spacing = "default" } =
+      listIdToRenderDataMap[id] || {};
+
     return (
       <div className="u-padding-top--xlg">
         <div className="u-display--flex">
           <GameListTitle title={title} />
         </div>
 
-        <ScrollingContainer padding={paddingPerDevice}>
+        <ScrollingContainer padding={paddingPerDevice} itemSpacing={spacing}>
           {games.map(gameId => (
             <Component key={gameId} id={gameId} />
           ))}
