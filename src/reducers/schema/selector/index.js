d3c9deeffb1ecc30a125499ee4a41d42176eae6a
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-03 11:21:27 +0200

    feat(schema-selector): Add schema selector
    Casumo/Home#22820

---
+++ src/reducers/schema/selector/index.js
@@ -0,0 +1,49 @@
+import { createSelector } from "reselect";
+
+export const schemaSelector = state => state.schema;
+
+export const gameListEntitiesSelector = createSelector(
+  schemaSelector,
+  state => state.gameList
+);
+
+export const gameEntitiesSelector = createSelector(
+  schemaSelector,
+  state => state.game
+);
+
+export const liveTableEntitiesSelector = createSelector(
+  schemaSelector,
+  state => state.liveTable
+);
+
+export const liveCasinoEntitiesSelector = createSelector(
+  schemaSelector,
+  state => state.liveCasino
+);
+
+export const jackpotEntitiesSelector = createSelector(
+  schemaSelector,
+  state => state.jackpot
+);
+
+export const topListIds = createSelector(gameListEntitiesSelector, state => ({
+  listIds: Object.keys(state),
+}));
+
+export const gameSelector = id =>
+  createSelector(
+    gameEntitiesSelector,
+    jackpotEntitiesSelector,
+    liveTableEntitiesSelector,
+    (gameEntities, jackpotEntities, liveTableEntities) => {
+      const game = gameEntities[id];
+      const jackpot = jackpotEntities[game.jackpotId];
+      const liveTable = liveTableEntities[game.tableId];
+      return {
+        ...game,
+        jackpotInfo: jackpot,
+        lobby: liveTable,
+      };
+    }
+  );
