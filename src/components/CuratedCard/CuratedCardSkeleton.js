c9a38afe4382dc03fc2e5acbb53153ae6fc45746
Jack Westbrook <jack.westbrook@casumo.com>
2018-10-04 11:56:40 +0200

    CuratedCardSkeleton obeys responsive ratios

--- src/components/CuratedCard/CuratedCardSkeleton.js
+++ src/components/CuratedCard/CuratedCardSkeleton.js
@@ -5,7 +5,12 @@ export default class CuratedCardSkeleton extends PureComponent {
   render() {
     return (
       <div className="c-curated-card o-ratio o-ratio--curated-card">
-        <Skeleton className="o-ratio__content" width="100" height="100">
+        <Skeleton
+          viewBox={null}
+          className="o-ratio__content"
+          width="100%"
+          height="100%"
+        >
           <rect x="0" y="0" rx="8" ry="8" width="100%" height="100%" />
         </Skeleton>
       </div>
