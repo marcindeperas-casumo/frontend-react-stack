968b5a7864a179641cb829705eb2bfe8267113d6
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-08 18:15:13 +0200

    refactor(card-stories): Ignore card stories from chromatic
    Until we move a mock store into storybook
    
    Casumo/Home#22820

--- src/components/LiveCasinoCard/Card.stories.js
+++ src/components/LiveCasinoCard/Card.stories.js
@@ -3,6 +3,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import info from "../../../.storybook/storybookInfo";
+import isNotChromatic from "../../../.storybook/isNotChromatic";
 
 import LiveCasinoCard from "Components/LiveCasinoCard/Card";
 
@@ -14,52 +15,54 @@ import gameBlackjackFull from "./__mocks__/BlackjackFull.json";
 
 const stories = storiesOf("Card", module);
 
-stories.add(
-  "Card MoneyWheel",
-  () => (
-    <div style={{ maxWidth: "320px" }}>
-      <LiveCasinoCard game={gameMoneyWheel} />
-    </div>
-  ),
-  info({ text: "Card MoneyWheel" })
-);
+if (isNotChromatic) {
+  stories.add(
+    "Card MoneyWheel",
+    () => (
+      <div style={{ maxWidth: "320px" }}>
+        <LiveCasinoCard game={gameMoneyWheel} />
+      </div>
+    ),
+    info({ text: "Card MoneyWheel" })
+  );
 
-stories.add(
-  "Card Roulette",
-  () => (
-    <div style={{ maxWidth: "320px" }}>
-      <LiveCasinoCard game={gameRoulette} />
-    </div>
-  ),
-  info({ text: "Card Roulette" })
-);
+  stories.add(
+    "Card Roulette",
+    () => (
+      <div style={{ maxWidth: "320px" }}>
+        <LiveCasinoCard game={gameRoulette} />
+      </div>
+    ),
+    info({ text: "Card Roulette" })
+  );
 
-stories.add(
-  "Card Blackjack Open Seats",
-  () => (
-    <div style={{ maxWidth: "320px" }}>
-      <LiveCasinoCard game={gameBlackjack} />
-    </div>
-  ),
-  info({ text: "Card Blackjack Open Seats" })
-);
+  stories.add(
+    "Card Blackjack Open Seats",
+    () => (
+      <div style={{ maxWidth: "320px" }}>
+        <LiveCasinoCard game={gameBlackjack} />
+      </div>
+    ),
+    info({ text: "Card Blackjack Open Seats" })
+  );
 
-stories.add(
-  "Card Blackjack Full",
-  () => (
-    <div style={{ maxWidth: "320px" }}>
-      <LiveCasinoCard game={gameBlackjackFull} />
-    </div>
-  ),
-  info({ text: "Card Blackjack Full" })
-);
+  stories.add(
+    "Card Blackjack Full",
+    () => (
+      <div style={{ maxWidth: "320px" }}>
+        <LiveCasinoCard game={gameBlackjackFull} />
+      </div>
+    ),
+    info({ text: "Card Blackjack Full" })
+  );
 
-stories.add(
-  "Card TopCard (Football)",
-  () => (
-    <div style={{ maxWidth: "320px" }}>
-      <LiveCasinoCard game={gameTopCard} />
-    </div>
-  ),
-  info({ text: "Card TopCard (Football)" })
-);
+  stories.add(
+    "Card TopCard (Football)",
+    () => (
+      <div style={{ maxWidth: "320px" }}>
+        <LiveCasinoCard game={gameTopCard} />
+      </div>
+    ),
+    info({ text: "Card TopCard (Football)" })
+  );
+}
