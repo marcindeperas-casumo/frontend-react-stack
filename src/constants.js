ec7c09a554e29f82a3697f5c94a2dc4f4075593b
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-09 12:57:52 +0200

    docs(constants): Add a note to keep the constants file in sync
    This will also be reflected in the casumo-frontend repo
    
    Casumo/Home#22820

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,18 @@
+/* eslint-disable max-len */
+// 📣📣📣 NOTE 📣📣📣
+//
+// The contents of this file are meant to be reflected and kept in sync between
+// the react-stack-poc and casumo-frontend repositories.
+//
+// react-stack-poc
+// (https://github.com/Casumo/mobile-react-stack-poc/blob/master/src/constants.js)
+//
+// casumo-frontend
+// (https://github.com/Casumo/casumo-frontend/tree/master/web/common-frontend/src/js/utils/react-stack/constants.es6)
+//
+// If we could keep the same var names across repositories, that would avoid
+// unnecessary confusion.
+
 // REACT_APP* events are events that the react app will be responsible to react
 // to.
 export const REACT_APP_EVENT_ON_LOGIN = "REACT_APP_EVENT/onLogin";
