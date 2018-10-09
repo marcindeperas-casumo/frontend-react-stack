7d9babd50e6293ce0c2ca3ec5b0d3f15ffda5236
Jack Westbrook <jack.westbrook@casumo.com>
2018-10-09 16:56:09 +0200

    Wip

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Clock extends React.Component {
+  render() {
+    return <div>{"Clock"}</div>;
+  }
+}
+
+export default Clock;
---
+++ src/components/Clock/Clock.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Clock from "./Clock";
+
+describe("Clock", () => {
+  test("should do something", () => {
+    expect(1).toBe(2);
+  });
+});
