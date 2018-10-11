f0de29415cd3ef94726dfee7269772979854ebdb
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-11 13:53:55 +0200

    refactor(app-container): Remove appContainerSelector since it does not enough value at this point
    Casumo/Home#22820

--- src/containers/AppContainer/selectors/index.js
+++
@@ -1,8 +0,0 @@
-import { isAuthenticated } from "Reducers/handshake/selectors";
-import { createStructuredSelector } from "reselect";
-import { activeComponents } from "Reducers/migrationComponents/selector";
-
-export const appContainerSelector = createStructuredSelector({
-  isAuthenticated,
-  activeComponents,
-});
