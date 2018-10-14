36ba2db90c3fc7201ed919199f8fee9527426fd9
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-14 12:20:55 +0200

    refactor(top-lists): Add CuratedCard componenet to the TopLists component
    Casumo/Home#22820

--- src/components/TopLists/TopLists.js
+++ src/components/TopLists/TopLists.js
@@ -1,12 +1,28 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, Fragment } from "react";
+import classNames from "classnames";
 import TopListContainer from "Containers/TopListContainer";
+import CuratedCard from "Components/CuratedCard";
 
 export class TopLists extends PureComponent {
   render() {
     const { listIds } = this.props;
-    return listIds.map(listId => {
-      return <TopListContainer key={listId} listId={listId} />;
-    });
+    return (
+      <Fragment>
+        <CuratedCard
+          className={classNames(
+            "u-margin-top--md",
+            "u-margin-top--lg@tablet",
+            "u-margin-top--lg@desktop",
+            "u-margin-horiz--md",
+            "u-margin-horiz--2xlg@tablet",
+            "u-margin-horiz--2xlg@desktop"
+          )}
+        />
+        {listIds.map(listId => {
+          return <TopListContainer key={listId} listId={listId} />;
+        })}
+      </Fragment>
+    );
   }
 }
 
--- src/containers/GamesListsPortalContainer/selectors/index.js
+++ src/containers/GamesListsPortalContainer/selectors/index.js
@@ -2,12 +2,26 @@ import { createSelector } from "reselect";
 import { fetchStatusFactory } from "Reducers/fetch/selectors";
 import { types as gameTypes } from "Reducers/games";
 import { types as handshakeTypes } from "Reducers/handshake";
+import {
+  isGamesHandshakeLoaded,
+  isApplicationHandshakeLoaded,
+} from "Reducers/handshake/selectors";
 
 export const gamesListsPortalFetchStatusSelector = createSelector(
   fetchStatusFactory(gameTypes.FETCH_TOP_LISTS_START),
   fetchStatusFactory(handshakeTypes.FETCH_GAMES_HANDSHAKE),
-  (fetchTopListsStatus, fetchGamesHandshakeStatus) => ({
+  isGamesHandshakeLoaded,
+  isApplicationHandshakeLoaded,
+  (
+    fetchTopListsStatus,
+    fetchGamesHandshakeStatus,
+    isGamesHandshakeLoaded,
+    isApplicationHandshakeLoaded
+  ) => ({
     isFetching:
-      fetchTopListsStatus.isFetching || fetchGamesHandshakeStatus.isFetching,
+      !isApplicationHandshakeLoaded ||
+      !isGamesHandshakeLoaded ||
+      (!!fetchTopListsStatus.isFetching &&
+        !!fetchGamesHandshakeStatus.isFetching),
   })
 );
