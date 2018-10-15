0fcb311798331b13220ad020a92062e536aec6c4
Luke Camilleri <luke.camilleri@casumo.com>
2018-10-15 15:02:30 +0200

    refactor(game-tile-index): Remove code from GameTile/index.js
    Casumo/Home#22820

--- src/components/GameTile/index.js
+++ src/components/GameTile/index.js
@@ -1,15 +1,1 @@
-import React, { PureComponent } from "react";
-import Flex from "@casumo/cmp-flex";
-import GameTile from "Components/GameTile/GameTile";
-
-export class GenericGameTile extends PureComponent {
-  render() {
-    return (
-      <Flex.Item className="o-flex__item-fixed-size c-top-game">
-        <GameTile {...this.props} />
-      </Flex.Item>
-    );
-  }
-}
-
-export default GenericGameTile;
+export { default } from "./GameTileWrapper";
