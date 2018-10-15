b995b51b470d04d5be15b9cf741baa3268d918a9
Jack Westbrook <jack.westbrook@casumo.com>
2018-10-15 17:15:42 +0200

    Delete clock. This is a timer.

--- src/components/Clock/Clock.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export class Clock extends React.Component {
-  render() {
-    return <div>{"Clock"}</div>;
-  }
-}
-
-export default Clock;
--- src/components/Clock/Clock.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Clock from "./Clock";
-
-describe("Clock", () => {
-  test("should do something", () => {
-    expect(1).toBe(2);
-  });
-});
