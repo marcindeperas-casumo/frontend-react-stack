4b5a51f5381be7509d1f919aaf4210d81b024400
Levente Balogh <balogh.levente.hu@gmail.com>
2018-10-17 03:10:35 +0200

    feat(CMS): add actions

---
+++ src/reducers/cms/cms.actions.js
@@ -0,0 +1,19 @@
+import { types as fetchTypes } from "Reducers/fetch";
+import { getPageBySlug } from "Reducers/cms/api";
+import { types } from "./cms.constants";
+import { getFetchTypeBySlug, getFetchCompleteTypeBySlug } from "./cms.utils";
+
+export const fetchPageBySlug = slug => ({
+  type: types.FETCH_PAGE_BY_SLUG,
+  slug,
+});
+
+export const initiateFetch = ({ slug, hash, lang }) => {
+  return {
+    type: fetchTypes.FETCH,
+    name: getFetchTypeBySlug(slug),
+    postFetch: getFetchCompleteTypeBySlug(slug),
+    asyncCall: getPageBySlug,
+    asyncCallData: { slug, hash, lang },
+  };
+};
