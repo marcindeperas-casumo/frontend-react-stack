c493bdfa947003e04ace5b8c6d4b362d06b23c82
Levente Balogh <balogh.levente.hu@gmail.com>
2018-10-19 14:53:34 +0200

    refactor(Jackpots): remove unnecessary before hook

--- src/components/Jackpots/JackpotsTile/JackpotsTile.test.js
+++ src/components/Jackpots/JackpotsTile/JackpotsTile.test.js
@@ -3,15 +3,10 @@ import { shallow } from "enzyme";
 import JackpotsTile from "./JackpotsTile";
 
 describe("<JackpotsTile />", () => {
-  let rendered;
-  let ids;
-
-  beforeEach(() => {
-    ids = ["one", "two", "three"];
-    rendered = shallow(<JackpotsTile ids={ids} />);
-  });
-
   test("renders a <JackpotsTileRow /> for each id", () => {
+    const ids = ["one", "two", "three"];
+    const rendered = shallow(<JackpotsTile ids={ids} />);
+
     expect(rendered.find("Connect(JackpotsTileRow)").length).toBe(ids.length);
   });
 });
