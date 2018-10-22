d956d06a59e14032c845dfe0a72ca5d849402cf0
Cho <choishere@gmail.com>
2018-10-22 16:42:58 +0200

    wip curated component data

--- src/components/CuratedCard/CuratedCardContainer.js
+++ src/components/CuratedCard/CuratedCardContainer.js
@@ -1,12 +1,17 @@
 // @flow
 import { connect } from "react-redux";
 import type { Connector } from "react-redux";
-import CuratedCard from "Components/CuratedCard";
-import type { Props } from "Components/CuratedCard";
-import { fetchPage, isPageLoadedFactory } from "Reducers/curated";
+import CuratedCard from "Components/CuratedCard/CuratedCard";
+import type { Props } from "Components/CuratedCard/CuratedCard";
+import {
+  fetchPage,
+  curatedSelector,
+  isPageLoadedFactory,
+} from "Reducers/curated";
 
 const connector: Connector<Props> = connect(
   state => ({
+    data: curatedSelector()(state),
     isFetched: isPageLoadedFactory()(state),
   }),
   dispatch => ({
