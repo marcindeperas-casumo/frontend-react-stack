549e8d66bb74618fef1ed8a0f9b8feb400508e57
Michele Settepani <michele.settepani@casumo.com>
2018-10-24 17:58:05 +0200

    PromotionCard: refactor component 👻

--- src/components/PromotionCard/PromotionCard.js
+++ src/components/PromotionCard/PromotionCard.js
@@ -14,63 +14,56 @@ export type Props = {
   slug: string,
 };
 
-export default class PromotionCard extends PureComponent<Props> {
-  renderHeader = () => {
-    // const { slug } = this.props;
+const PromotionCardHeader = slug => (
+  <Text tag="strong" className="t-color-red" size="xs">
+    30 NOV - 1 JAN
+    {/* <CMSField slug={slug} field="" /> */}
+  </Text>
+);
 
-    return (
-      <Text tag="strong" className="t-color-red" size="xs">
-        30 NOV - 1 JAN
-        {/* <CMSField slug="" field="" /> */}
-      </Text>
-    );
-  };
+const PromotionCardContent = slug => (
+  <Text tag="strong" className="t-color-grey-dark-3" size="lg">
+    Slots <br /> Mystery <br /> Prizes
+    {/* <CMSField slug={slug} field="" /> */}
+  </Text>
+);
 
-  renderContent = () => {
-    // const { slug } = this.props;
+const PromotionCardImage = () => (
+  <ImageLazy
+    className="u-display--block c-promotion-card__img"
+    src="https://cms.casumo.com/wp-content/uploads/2018/10/testxmaspromo.png"
+    imgixOpts={{ w: 280 }}
+    dpr={3}
+  />
+);
 
-    return (
-      <Text tag="strong" className="t-color-grey-dark-3" size="lg">
-        Slots <br /> Mystery <br /> Prizes
-        {/* <CMSField slug="" field="" /> */}
-      </Text>
-    );
-  };
-
-  renderImage = () => {
-    return (
-      <ImageLazy
-        className="u-display--block c-promotion-card__img"
-        src="https://cms.casumo.com/wp-content/uploads/2018/10/testxmaspromo.png"
-        imgixOpts={{ w: 280 }}
-        dpr={3}
+const PromotionCardWrapper = ({ promotionCardURL, slug }) => {
+  return (
+    <a
+      href={promotionCardURL}
+      className="c-promotion-card__link o-flex__item-fixed-size"
+      onMouseDown={e => e.preventDefault()}
+    >
+      <Card
+        className="t-border-r--8 u-padding t-background-grey-light-2 "
+        header={() => <PromotionCardHeader slug={slug} />}
+        content={() => <PromotionCardContent slug={slug} />}
+        footer={() => <PromotionCardImage />}
       />
-    );
-  };
+    </a>
+  );
+};
 
-  renderCard = () => {
-    const { slug } = this.props;
+export default class PromotionCard extends PureComponent<Props> {
+  render() {
+    const { isFetched, slug } = this.props;
     const promotionCardURL = `${PROMOTIONS_URL}/${slug}`;
 
-    return (
-      <a
-        href={promotionCardURL}
-        className="c-promotion-card__link o-flex__item-fixed-size"
-        onMouseDown={e => e.preventDefault()}
-      >
-        <Card
-          className="t-border-r--8 u-padding t-background-grey-light-2 "
-          header={this.renderHeader}
-          content={this.renderContent}
-          footer={this.renderImage}
-        />
-      </a>
-    );
-  };
-
-  render() {
-    const { isFetched } = this.props;
     // switch from true to isFetched 👇🏻
-    return true ? this.renderCard() : <PromotionCardSkeleton />;
+    return true ? (
+      <PromotionCardWrapper slug={slug} promotionCardURL={promotionCardURL} />
+    ) : (
+      <PromotionCardSkeleton />
+    );
   }
 }
