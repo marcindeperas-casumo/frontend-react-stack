825bc6bf0df386c793832a0e80ccf5ddbbd4ba1c
Jack Westbrook <jack.westbrook@casumo.com>
2018-10-26 13:33:31 +0200

    test: Update Counter tests
    Casumo/Home#23553

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -5,20 +5,8 @@ import Counter, { REFRESH_RATE } from "./Counter";
 jest.useFakeTimers();
 
 describe("Counter", () => {
-  beforeEach(() => {});
-
   afterEach(() => {
     jest.clearAllTimers();
-  });
-
-  test("should update state.value", () => {
-    const renderProp = jest.fn();
-    const rendered = shallow(
-      <Counter start={0} end={100} render={renderProp} />
-    );
-    expect(rendered.state("value")).toEqual("0");
-    jest.advanceTimersByTime(REFRESH_RATE);
-    expect(rendered.state("value")).toEqual("1");
   });
 
   test("should respect decimals", () => {
@@ -29,11 +17,16 @@ describe("Counter", () => {
     expect(rendered.state("value")).toEqual("0.00");
   });
 
-  test("should call renderProp", () => {
-    const renderProp = jest.fn();
+  test("should call renderProp with props", () => {
+    const renderProp = jest.fn(state => <div>{state.value}</div>);
     expect(renderProp).toHaveBeenCalledTimes(0);
-    shallow(<Counter start={0} end={100} render={renderProp} />);
+    const rendered = shallow(
+      <Counter start={0} end={100} duration={3} render={renderProp} />
+    );
     expect(renderProp).toHaveBeenCalledTimes(1);
+    expect(rendered.text()).toEqual("0");
+    jest.runAllTimers();
+    expect(rendered.text()).toEqual("100");
   });
 
   test("should call countUp by REFRESH_RATE", () => {
