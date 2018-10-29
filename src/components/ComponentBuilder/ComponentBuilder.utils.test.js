040f78bac6d1f9afbd5517357a46bb9ac49db1a9
Levente Balogh <balogh.levente.hu@gmail.com>
2018-10-29 14:03:35 +0100

    tests(ComponentBuilder): test mapping thoroughly

--- src/components/ComponentBuilder/ComponentBuilder.utils.test.js
+++ src/components/ComponentBuilder/ComponentBuilder.utils.test.js
@@ -1,20 +1,41 @@
-import { mount } from "enzyme";
-import { mapContentDefinitionToComponent } from "Components/ComponentBuilder/ComponentBuilder.utils";
+import {
+  mapContentDefinitionToComponent,
+  COMPONENT_MAPPING,
+} from "Components/ComponentBuilder/ComponentBuilder.utils";
 
 describe("ComponentBuilder Utils", () => {
   describe("mapContentDefinitionToComponent()", () => {
     test("maps a content definition to a component", () => {
-      const contentDefinition = {
-        acf_fc_layout: "HTML_CONTENT",
-        html: "<span>Foo.</span>",
-      };
-      const i = 1;
-      const component = mapContentDefinitionToComponent(contentDefinition, i);
-      const rendered = mount(component);
+      const supportedComponents = Object.keys(COMPONENT_MAPPING);
+      const getComponentName = c => c.displayName || c.name;
 
-      expect(rendered.exists()).toBe(true);
-      expect(rendered.find("DangerousHtml")).toHaveLength(1);
-      expect(rendered.html()).toMatch("Foo");
+      supportedComponents.forEach((id, i) => {
+        const contentDefinition = { acf_fc_layout: id };
+        const component = mapContentDefinitionToComponent(contentDefinition, i);
+        const originalComponent = COMPONENT_MAPPING[id];
+
+        expect(getComponentName(component.type)).toEqual(
+          getComponentName(originalComponent)
+        );
+      });
+    });
+
+    test("passes props to every component correctly", () => {
+      const supportedComponents = Object.keys(COMPONENT_MAPPING);
+
+      supportedComponents.forEach((id, i) => {
+        const expectedProps = {
+          prop1: `prop1-${i}`,
+          prop2: `prop2-${i}`,
+        };
+        const contentDefinition = {
+          acf_fc_layout: id,
+          ...expectedProps,
+        };
+        const { props } = mapContentDefinitionToComponent(contentDefinition, i);
+
+        expect(props).toEqual(expectedProps);
+      });
     });
 
     test("returns null if the mapping does not exist", () => {
