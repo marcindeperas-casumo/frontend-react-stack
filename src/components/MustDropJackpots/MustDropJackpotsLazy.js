1a018a03418a51343394aa4b04d631c66f7a2178
Michele Settepani <michele.settepani@casumo.com>
2018-10-31 11:38:32 +0100

    MustDropJackpotsPortal: get rid of React.lazy() in favour of react-loadable 🚀

--- src/components/MustDropJackpots/MustDropJackpotsLazy.js
+++ src/components/MustDropJackpots/MustDropJackpotsLazy.js
@@ -1,14 +1,11 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
+import Loadable from "react-loadable";
 // add a skeleton bae
 
-const MustDropJackpotsContainer = lazy(() =>
-  import("Components/MustDropJackpots/MustDropJackpotsContainer")
-);
+const MustDropJackpotsContainer =
+  "Components/MustDropJackpots/MustDropJackpotsContainer";
 
-export default function MustDropJackpotsLazy() {
-  return (
-    <Suspense fallback={<div>I'm a cute skeleton</div>}>
-      <MustDropJackpotsContainer />
-    </Suspense>
-  );
-}
+export default Loadable({
+  loader: () => import(MustDropJackpotsContainer),
+  loading: () => <div>I'm a cute skeleton</div>,
+});
