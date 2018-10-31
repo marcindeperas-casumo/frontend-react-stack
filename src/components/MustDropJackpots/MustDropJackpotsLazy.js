22630b7e8da31d4c1a357a8ca3228a2254a47cfe
Michele Settepani <michele.settepani@casumo.com>
2018-10-31 12:13:07 +0100

    MustDropJackpotsLazy: refactor

--- src/components/MustDropJackpots/MustDropJackpotsLazy.js
+++ src/components/MustDropJackpots/MustDropJackpotsLazy.js
@@ -2,10 +2,7 @@ import React from "react";
 import Loadable from "react-loadable";
 // add a skeleton bae
 
-const MustDropJackpotsContainer =
-  "Components/MustDropJackpots/MustDropJackpotsContainer";
-
 export default Loadable({
-  loader: () => import(MustDropJackpotsContainer),
+  loader: () => import("Components/MustDropJackpots/MustDropJackpotsContainer"),
   loading: () => <div>I'm a cute skeleton</div>,
 });
