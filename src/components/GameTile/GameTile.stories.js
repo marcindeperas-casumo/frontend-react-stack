a502785265ffbc657593dc11c43f63df96bcd389
Jack Westbrook <jack.westbrook@casumo.com>
2018-11-08 11:59:06 +0100

    test: Update GameTile Stories to include connected.
    Casumo/Home#23964

--- src/components/GameTile/GameTile.stories.js
+++ src/components/GameTile/GameTile.stories.js
@@ -1,29 +1,41 @@
-import { action } from "@storybook/addon-actions";
-import { boolean, withKnobs } from "@storybook/addon-knobs/react";
-import { storiesOf } from "@storybook/react";
 import React from "react";
-import MockStore from "Components/MockStore";
+import { storiesOf } from "@storybook/react";
+import { boolean, withKnobs } from "@storybook/addon-knobs/react";
 import info from "Storybook/storybookInfo";
-import GameTile from "Components/GameTile";
-import GameTileExclusive from "Components/GameTileExclusive";
+import MockStore from "Components/MockStore";
+import GameTileConnected from "Components/GameTile";
+import GameTile from "Components/GameTile/GameTile";
 import game from "./__mocks__/Game.json";
 import jackpotInfo from "./__mocks__/JackpotGameInfo.json";
-import exclusiveGame from "./__mocks__/ExclusiveGame.json";
 
 const stories = storiesOf("GameTile", module);
 stories.addDecorator(withKnobs);
 
 stories.add(
-  "Default Tile",
+  "Default (Connected)",
+  () => {
+    return (
+      <MockStore>
+        <GameTileConnected id="starburst" />
+      </MockStore>
+    );
+  },
+  info({ text: "Default Tile" })
+);
+
+stories.add(
+  "Default",
   () => {
     const inMaintenanceMode = boolean(
       "In maintenance mode",
       game.inMaintenanceMode
     );
     return (
-      <MockStore>
-        <GameTile {...game} inMaintenanceMode={inMaintenanceMode} />
-      </MockStore>
+      <div style={{ maxWidth: "170px" }}>
+        <MockStore>
+          <GameTile {...game} inMaintenanceMode={inMaintenanceMode} />
+        </MockStore>
+      </div>
     );
   },
   info({ text: "Default Tile" })
@@ -37,32 +49,15 @@ stories.add(
       game.inMaintenanceMode
     );
     return (
-      <MockStore>
-        <GameTile
-          {...{ ...game, jackpotInfo }}
-          inMaintenanceMode={inMaintenanceMode}
-        />
-      </MockStore>
+      <div style={{ maxWidth: "170px" }}>
+        <MockStore>
+          <GameTile
+            {...{ ...game, jackpotInfo }}
+            inMaintenanceMode={inMaintenanceMode}
+          />
+        </MockStore>
+      </div>
     );
   },
   info({ text: "With Jackpot" })
-);
-
-stories.add(
-  "Tall Tile",
-  () => {
-    const inMaintenanceMode = boolean(
-      "In maintenance mode",
-      exclusiveGame.inMaintenanceMode
-    );
-    return (
-      <MockStore>
-        <GameTileExclusive
-          {...exclusiveGame}
-          inMaintenanceMode={inMaintenanceMode}
-        />
-      </MockStore>
-    );
-  },
-  info({ text: "Tall Tile" })
 );
