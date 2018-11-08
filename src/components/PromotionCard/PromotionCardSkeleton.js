acc8aa1334f071d55ff0cbe5207910943d80c983
Michele Settepani <michele.settepani@casumo.com>
2018-11-08 16:34:21 +0100

    PromotionCardSkeleton: keep component dimensions 📐

--- src/components/PromotionCard/PromotionCardSkeleton.js
+++ src/components/PromotionCard/PromotionCardSkeleton.js
@@ -4,7 +4,7 @@ import Skeleton from "@casumo/cmp-skeleton";
 export default class PromotionCardSkeleton extends PureComponent {
   render() {
     return (
-      <div className="o-ratio o-ratio--promotion-card o-flex__item-fixed-size">
+      <div className="c-promotion-card o-ratio o-ratio--promotion-card o-flex__item o-flex__item-fixed-size">
         {/* use right ratio ☝🏻 I want this skeleton to have the same size of the real one, but how? :P
             In this case I created a ratio only for the skeleton, as Idon't use it on the component itself,
             but height looks already different. Could you save me @jack?
