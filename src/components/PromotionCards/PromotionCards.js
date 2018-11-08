23265f2027c92b5052acf29e00d51d87a5967cb6
Michele Settepani <michele.settepani@casumo.com>
2018-11-08 09:29:00 +0100

    PromotionCards: receive title, background color and title font color as a prop 🚀

--- src/components/PromotionCards/PromotionCards.js
+++ src/components/PromotionCards/PromotionCards.js
@@ -1,12 +1,23 @@
 // @flow
 import React, { PureComponent } from "react";
+import classNames from "classnames";
 import PromotionCardContainer from "Components/PromotionCard";
 import Scrollable from "@casumo/cmp-scrollable";
+import ScrollableListTitle from "Components/ScrollableListTitle";
 
 export type Props = {
   promotionsSlugs: Array<string>,
   isFetched: boolean,
   startFetch: () => void,
+  title?: string,
+  titleColor?: string,
+  backgroundColor?: string,
+};
+
+const paddingPerDevice = {
+  default: "md",
+  tablet: "2xlg",
+  desktop: "2xlg",
 };
 
 export class PromotionCards extends PureComponent<Props> {
@@ -20,18 +31,38 @@ export class PromotionCards extends PureComponent<Props> {
 
   render() {
     // const { promotionsSlugs } = this.props;
-    const promotionsSlugs = ["dai", "cazzo"]; // SWITCH THIS ONE WITH THE ABOVE ☝🏻
+    const { title, titleColor, backgroundColor } = this.props;
+    const promotionsSlugs = [
+      "dai",
+      "cazzo",
+      "test",
+      "another test",
+      "make",
+      "it",
+      "scrollable",
+    ]; // SWITCH THIS ONE WITH THE ABOVE ☝🏻
     const hasNoPromotionSlugs = !promotionsSlugs || !promotionsSlugs.length;
 
     if (hasNoPromotionSlugs) {
       return null;
     }
+
     return (
-      <Scrollable gap="none" padding="lg">
-        {promotionsSlugs.map(slug => (
-          <PromotionCardContainer slug={slug} key={slug} />
-        ))}
-      </Scrollable>
+      <div
+        className={classNames(
+          backgroundColor && `${backgroundColor}`,
+          titleColor && `${titleColor}`,
+          "u-padding-top--lg u-padding-bottom--lg"
+        )}
+      >
+        {/* find the right padding top */}
+        <ScrollableListTitle title={title} />
+        <Scrollable padding={paddingPerDevice} itemSpacing="md">
+          {promotionsSlugs.map(slug => (
+            <PromotionCardContainer slug={slug} key={slug} />
+          ))}
+        </Scrollable>
+      </div>
     );
   }
 }
