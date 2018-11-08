6fd05ac469475ceba455e2d3045eec4e7db36881
Jack Westbrook <jack.westbrook@casumo.com>
2018-11-08 16:54:27 +0100

    refactor: GameTileContainer to work with new GameTile component.
    Casumo/Home#23964

--- src/components/GameTile/GameTileContainer.js
+++ src/components/GameTile/GameTileContainer.js
@@ -5,13 +5,15 @@ import GameTile from "Components/GameTile/GameTileWrapper";
 import { gameSelector } from "Reducers/schema/selector";
 import { actions as gameActions } from "Reducers/games";
 
-const getGameData = (state, props) => gameSelector(props.id)(state);
+const mapStateToProps = (state, props) => ({
+  game: gameSelector(props.id)(state),
+});
 const mapDispatchToProps = (dispatch, props) => ({
   onLaunchGame: () => dispatch(gameActions.launchGame(props.id)),
 });
 
 const GameTileConnected = connect(
-  getGameData,
+  mapStateToProps,
   mapDispatchToProps
 )(GameTile);
 
