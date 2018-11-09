a534a2bfb463a113c970d3242341a1e37d403cfd
Michele Settepani <michele.settepani@casumo.com>
2018-11-09 12:59:53 +0100

    PromotionCardSkeleton: remove comment

--- src/components/PromotionCard/PromotionCardSkeleton.js
+++ src/components/PromotionCard/PromotionCardSkeleton.js
@@ -5,10 +5,6 @@ export default class PromotionCardSkeleton extends PureComponent {
   render() {
     return (
       <div className="c-promotion-card o-ratio o-ratio--promotion-card o-flex__item o-flex__item-fixed-size">
-        {/* use right ratio ☝🏻 I want this skeleton to have the same size of the real one, but how? :P
-            In this case I created a ratio only for the skeleton, as Idon't use it on the component itself,
-            but height looks already different. Could you save me @jack?
-        */}
         <Skeleton
           viewBox={null}
           className="o-ratio__content"
