ba5939bf67741c13b1b1206dc18f0a352076c249
Jack Westbrook <jack.westbrook@casumo.com>
2018-11-12 16:49:01 +0100

    test: Add tests for PromotionCardTeaser component.
    Casumo/Home#24279

--- src/components/PromotionCardTeaser/PromotionCardTeaser.test.js
+++ src/components/PromotionCardTeaser/PromotionCardTeaser.test.js
@@ -3,7 +3,37 @@ import { shallow } from "enzyme";
 import PromotionCardTeaser from "./PromotionCardTeaser";
 
 describe("PromotionCardTeaser", () => {
-  test("should do something", () => {
-    expect(1).toBe(2);
+  const data = {
+    date: "30 Nov 2018 - 6 Jan 2019",
+    imageSrc:
+      "https://cms.casumo.com/wp-content/uploads/2018/11/promotions-bonus-cards.svg",
+    title: "Boosted<br /> Reel<br /> Races",
+  };
+  test("should render a date", () => {
+    const rendered = shallow(<PromotionCardTeaser {...data} />);
+    expect(
+      rendered
+        .find("Text")
+        .first()
+        .children()
+        .text()
+    ).toBe(data.date);
+  });
+
+  test("should render a title", () => {
+    const rendered = shallow(<PromotionCardTeaser {...data} />);
+    expect(
+      rendered
+        .find("Text")
+        .last()
+        .children()
+        .prop("html")
+    ).toBe(data.title);
+  });
+
+  test("should render an image", () => {
+    const rendered = shallow(<PromotionCardTeaser {...data} />);
+    expect(rendered.find("ImageLazy").length).toBe(1);
+    expect(rendered.find("ImageLazy").prop("src")).toBe(data.imageSrc);
   });
 });
