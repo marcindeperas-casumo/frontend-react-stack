0eb847b83b7aa5b2c2baf85a6f27c8ea83dfaf89
Michele Settepani <michele.settepani@casumo.com>
2018-11-16 14:47:24 +0100

    PromotionCardTeaser: add link to each and every card; date always capital case

--- src/components/PromotionCardTeaser/PromotionCardTeaser.js
+++ src/components/PromotionCardTeaser/PromotionCardTeaser.js
@@ -4,6 +4,7 @@ import Flex from "@casumo/cmp-flex";
 import Text from "@casumo/cmp-text";
 import ImageLazy from "Components/Image/ImageLazy";
 import DangerousHtml from "Components/DangerousHtml";
+import "./PromotionCardTeaser.scss";
 
 type Props = {
   /** The date range the promotion will run for. */
@@ -12,28 +13,38 @@ type Props = {
   title: String,
   /** The src of the image to show on the right. */
   imageSrc: String,
+  /** The link where the card is leading to */
+  link: String,
 };
 
 export class PromotionCardTeaser extends PureComponent<Props> {
   render() {
-    const { date, title, imageSrc } = this.props;
+    const { date, title, imageSrc, link } = this.props;
     return (
-      <Flex
-        align="center"
-        className="t-background-white t-border-r--16 u-padding--lg u-line-height--1"
-      >
-        <Flex.Block>
-          <Text size="xs" className="t-color-red u-margin-bottom">
-            {date}
-          </Text>
-          <Text className="u-font-weight-bold u-margin-bottom--none" size="xlg">
-            <DangerousHtml html={title} />
-          </Text>
-        </Flex.Block>
-        <Flex.Item>
-          <ImageLazy src={imageSrc} />
-        </Flex.Item>
-      </Flex>
+      <a href={link} className="c-promotion-card-teaser t-color-grey-3">
+        <Flex
+          align="center"
+          className="t-background-white t-border-r--16 u-padding--lg u-line-height--1"
+        >
+          <Flex.Block>
+            <Text
+              size="xs"
+              className="t-color-red u-margin-bottom u-text-transform-uppercase"
+            >
+              {date}
+            </Text>
+            <Text
+              className="u-font-weight-bold u-margin-bottom--none"
+              size="xlg"
+            >
+              <DangerousHtml html={title} />
+            </Text>
+          </Flex.Block>
+          <Flex.Item>
+            <ImageLazy src={imageSrc} />
+          </Flex.Item>
+        </Flex>
+      </a>
     );
   }
 }
