d9fb265469139ecefaf6d675c77c14753ee7d1ae
Levente Balogh <balogh.levente.hu@gmail.com>
2018-11-16 16:08:47 +0100

    feat(ContentBuilder): add ContentImage

--- src/components/ComponentBuilder/ComponentBuilder.mapping.js
+++ src/components/ComponentBuilder/ComponentBuilder.mapping.js
@@ -3,10 +3,12 @@ import DangerousHtml from "Components/DangerousHtml";
 import CuratedCard from "Components/CuratedCard";
 import Jackpots from "Components/Jackpots";
 import GameList from "Components/GameList";
+import ContentImage from "Components/ContentImage";
 
 export const COMPONENT_MAPPING = {
   HTML_CONTENT: DangerousHtml,
   GAMES_LIST: GameList,
   CURATED_CARD: CuratedCard,
   JACKPOTS: Jackpots,
+  CONTENT_IMAGE: ContentImage,
 };
