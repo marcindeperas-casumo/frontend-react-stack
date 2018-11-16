4ced5fe41eefd9730b3ce1416fd28d716e72e008
Levente Balogh <balogh.levente.hu@gmail.com>
2018-11-16 17:00:18 +0100

    refactor(ContentSeparator): try Jack's magic!

--- src/components/ContentSeparator/ContentSeparator.js
+++ src/components/ContentSeparator/ContentSeparator.js
@@ -1,10 +1,10 @@
 import React, { PureComponent } from "react";
-import "./ContentSeparator.scss";
 
 export default class ContentSeparator extends PureComponent {
   render() {
     const className = `
-      c-content-separator
+      u-width--1/1
+      t-border-bottom
       u-margin-vert--xlg
       u-padding-horiz--xlg
       u-padding-horiz--lg@mobile
