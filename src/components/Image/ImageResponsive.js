570e233848c733536d3dfd326e4d0955f7051746
Jack Westbrook <jack.westbrook@casumo.com>
2018-11-18 18:22:58 +0100

    revert: Revert svg change to ImageResponsive
    Casumo/Home#24276

--- src/components/Image/ImageResponsive.js
+++ src/components/Image/ImageResponsive.js
@@ -4,14 +4,6 @@ import { LOW_RES_IMAGE_SETTINGS } from "../../constants";
 export default class ImageResponsive extends React.Component {
   render() {
     const { isIntersecting, src, ...rest } = this.props;
-    // Make sure SVGs aren't loaded twice due to low res image settings query string.
-    if (src.match(/\.svg$/)) {
-      return isIntersecting ? (
-        <ResponsiveImage src={src} {...rest} />
-      ) : (
-        <span />
-      );
-    }
 
     return isIntersecting ? (
       <ResponsiveImage imgixOpts={{ w: 170 }} src={src} {...rest} />
