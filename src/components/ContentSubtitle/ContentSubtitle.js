8153f2467b71d5744ee163a1c9fcd7db65679256
Cho <choishere@gmail.com>
2018-11-19 10:37:10 +0100

    rm ContentSubtitle className prop, add margin bottom

--- src/components/ContentSubtitle/ContentSubtitle.js
+++ src/components/ContentSubtitle/ContentSubtitle.js
@@ -3,16 +3,15 @@ import React, { PureComponent } from "react";
 import Text from "@casumo/cmp-text";
 
 type Props = {
-  className?: string,
   subtitle: string,
 };
 
 export default class ContentSubtitle extends PureComponent<Props> {
   render() {
-    const { className, subtitle } = this.props;
+    const { subtitle } = this.props;
 
     return (
-      <Text tag="h2" className={className}>
+      <Text className="u-margin-bottom--xlg" tag="h2">
         {subtitle}
       </Text>
     );
