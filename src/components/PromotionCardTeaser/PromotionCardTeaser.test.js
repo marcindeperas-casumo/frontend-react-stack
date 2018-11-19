dc1934c373eddd5d0233dfa7b31245ab53cc3d5d
Michele Settepani <michele.settepani@casumo.com>
2018-11-19 09:31:25 +0100

    PromotionCardTeaser.test: use promotions data from mocks

--- src/components/PromotionCardTeaser/PromotionCardTeaser.test.js
+++ src/components/PromotionCardTeaser/PromotionCardTeaser.test.js
@@ -1,45 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
 import PromotionCardTeaser from "Components/PromotionCardTeaser/PromotionCardTeaser";
+import promotions from "Components/PromotionCard/__mocks__/promotions.json";
 
-const promotion = {
-  id: "87088",
-  slug: "promotions.boosted-reelraces",
-  title: "Boosted Reel Races",
-  content: "",
-  attachments: [
-    {
-      url:
-        "https://cms.casumo.com/wp-content/uploads/2018/10/promotion-boostedreelraces.png",
-      title: "promotion-boostedreelraces",
-    },
-    {
-      url:
-        "https://cms.casumo.com/wp-content/uploads/2018/10/promobadge-boostedreelraces.png",
-      title: "promobadge-boostedreelraces",
-    },
-  ],
-  custom_fields: {},
-  fields: {
-    critical_for_compliance: false,
-    dates: "30 Nov 2018 - 6 Jan 2019",
-    "": false,
-    title: "Boosted Reel Races",
-    image:
-      "https://cms.casumo.com/wp-content/uploads/2018/10/promotion-boostedreelraces.png",
-    teaser_text:
-      "Compete against other players for the top spot in our fast-paced tournaments.",
-    content_builder: false,
-    badge:
-      "https://cms.casumo.com/wp-content/uploads/2018/10/promobadge-boostedreelraces.png",
-    campaign_badge:
-      "https://cms.casumo.com/wp-content/uploads/2018/10/badge-wintergames.png",
-    cta_text: "Tell me more",
-    terms_and_conditions: "",
-  },
-  children: [],
-  childSlugs: [],
-};
+const promotion = promotions["promotions.boosted-reelraces"];
 
 describe("PromotionCardTeaser", () => {
   test("should render a date", () => {
