33ad27729761aae07dd5687fe463b73aef263f0e
Jack Westbrook <jack.westbrook@casumo.com>
2018-11-20 16:59:33 +0100

    refactor(MustDropJackpotDetailList): Delete overly specific components.
    Casumo/Home#24549

--- src/components/MustDropJackpotDetailList/MustDropJackpotDetailList.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import MustDropJackpotDetailList from "Components/MustDropJackpotDetailList";
-
-describe("MustDropJackpotDetailList", () => {
-  test("should do something", () => {
-    expect(1).toBe(2);
-  });
-});
