9ff9b9866306c60b07317d359bd41d89eccd1993
Levente Balogh <balogh.levente.hu@gmail.com>
2018-11-20 09:42:33 +0100

    feat(ComponentBuilder): add GameListVertical

--- src/components/ComponentBuilder/ComponentBuilder.mapping.js
+++ src/components/ComponentBuilder/ComponentBuilder.mapping.js
@@ -3,6 +3,7 @@ import DangerousHtml from "Components/DangerousHtml";
 import CuratedCard from "Components/CuratedCard";
 import Jackpots from "Components/Jackpots";
 import GameListHorizontal from "Components/GameListHorizontal";
+import GameListVertical from "Components/GameListVertical";
 import ContentImage from "Components/ContentImage";
 import ContentSeparator from "Components/ContentSeparator";
 import ContentSubtitle from "Components/ContentSubtitle";
@@ -13,6 +14,8 @@ import PromotionCardList from "Components/PromotionCardList";
 export const COMPONENT_MAPPING = {
   HTML_CONTENT: DangerousHtml,
   GAMES_LIST: GameListHorizontal,
+  GAMES_LIST_HORIZONTAL: GameListVertical,
+  GAMES_LIST_VERTICAL: GameListVertical,
   CURATED_CARD: CuratedCard,
   JACKPOTS: Jackpots,
   CONTENT_IMAGE: ContentImage,
