79bec7f584abb4282ea17e68fc4b240b26acfcaa
Levente Balogh <balogh.levente.hu@gmail.com>
2018-11-21 08:26:12 +0100

    tests(PageCampaign): remove unnecessary test
    Right now we don't really have any kind of logic
    in this component, only wrapping the <ComponentBuilder />,
    so a test to check if that is used wouldn't give us too much benefit.
    (The test would have to be changed every time the code changes).
    The rendering is checked by the Storybook.
    Will add tests as soon as we have any kind of business logic.

--- src/components/PageCampaign/PageCampaign.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import PageCampaign from "Components/PageCampaign";
-
-describe("PageCampaign", () => {
-  test("should do something", () => {
-    expect(1).toBe(2);
-  });
-});
