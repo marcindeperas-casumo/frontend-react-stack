44c48dac93a14b80fc3a7023a9b535a506a2262f
Levente Balogh <balogh.levente.hu@gmail.com>
2018-11-25 17:21:05 +0100

    feat(CMS): add utility functions

--- src/models/cms/cms.utils.js
+++ src/models/cms/cms.utils.js
@@ -4,3 +4,26 @@ export const getFetchTypeBySlug = slug => `${types.FETCH_PAGE_BY_SLUG}-${slug}`;
 
 export const getFetchCompleteTypeBySlug = slug =>
   `${types.FETCH_PAGE_BY_SLUG_COMPLETE}-${slug}`;
+
+export const getChildren = page => {
+  const { children = [] } = page;
+  const parentSlug = page.slug.replace(".*", "");
+
+  return children.map(child => setSlug(child, `${parentSlug}.${child.slug}`));
+};
+
+export const getChildrenAndParent = page => {
+  const children = getChildren(page);
+  const parent = { ...page, children: [] };
+
+  return [parent, ...children];
+};
+
+export const setSlug = (page, slug) => {
+  slug = slug.replace(".*", "");
+
+  return {
+    ...page,
+    slug,
+  };
+};
