18947fb99cf4c91a8e5d46f0e94350d98e8dcb8b
Jack Westbrook <jack.westbrook@casumo.com>
2018-11-26 18:40:19 +0100

    feat(MustDropJackpots|Jackpots): Update MustDropJackpotsList and Jackpots to make use of JackpotsLis
    Casumo/Home#24513

--- src/components/MustDropJackpotsList/MustDropJackpotsList.js
+++ src/components/MustDropJackpotsList/MustDropJackpotsList.js
@@ -2,9 +2,9 @@
 import React, { PureComponent } from "react";
 import Scrollable from "@casumo/cmp-scrollable";
 import { generateColumns } from "Utils/utils";
-import MustDropJackpotsListTile from "./MustDropJackpotsListTile";
 import ScrollableListTitle from "Components/ScrollableListTitle";
-import "./MustDropJackpotsList.scss";
+import JackpotsListTile from "Components/JackpotsListTile";
+import MustDropJackpotsWidget from "Components/MustDropJackpotsWidget";
 
 const PADDING_PER_DEVICE = {
   default: "md",
@@ -27,8 +27,11 @@ export default class MustDropJackpotsList extends PureComponent<Props> {
       <div className="u-padding-top--xlg">
         <ScrollableListTitle title={title} />
         <Scrollable padding={PADDING_PER_DEVICE} itemSpacing="md">
+          <div className="c-jackpots-list-tile o-flex__item o-flex__item-fixed-size u-padding-bottom--md">
+            <MustDropJackpotsWidget />
+          </div>
           {idsByColumns.map((columnIds, i) => (
-            <MustDropJackpotsListTile
+            <JackpotsListTile
               ids={columnIds}
               key={`must-drop-jackpots-tile-${i}`}
             />
