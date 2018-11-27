8d9fa1b0443aad32df7488b32d3aabda899d3232
Levente Balogh <balogh.levente.hu@gmail.com>
2018-11-27 17:07:30 +0100

    feat(MustDropJackpots): add saga for live updates

--- src/models/jackpotsMustDrop/index.js
+++ src/models/jackpotsMustDrop/index.js
@@ -1,5 +1,6 @@
-export * from "./jackpotsMustDrop.fetch.saga";
 export * from "./jackpotsMustDrop.api";
 export * from "./jackpotsMustDrop.constants";
 export * from "./jackpotsMustDrop.actions";
 export * from "./jackpotsMustDrop.selectors";
+export * from "./jackpotsMustDrop.saga.fetch";
+export * from "./jackpotsMustDrop.saga.update";
