2c045c8da7a64908cd564d178796d49d09c42a20
Michele Settepani <michele.settepani@casumo.com>
2018-11-27 15:55:42 +0100

    remove unnecessary imports

--- src/components/GameListVertical/GameListVerticalContainer.js
+++ src/components/GameListVertical/GameListVerticalContainer.js
@@ -2,8 +2,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import GameListVertical from "Components/GameListVertical/GameListVertical";
-import { gameListSelector } from "Models/schema/selector";
-import { isGameListLoaded } from "Models/games/games.selectors";
 import { fetchThemGames } from "Models/curated/curated.actions";
 
 type Props = {
