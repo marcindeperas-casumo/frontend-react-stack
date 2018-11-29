eccc9b50344beaf468afb075dd5d21419ca1802d
Michele Settepani <michele.settepani@casumo.com>
2018-11-29 15:44:16 +0200

    PromotionPageSkeleton: set viewBox to null 🔪

--- src/components/PromotionPageSkeletons/PromotionPageSkeleton.js
+++ src/components/PromotionPageSkeletons/PromotionPageSkeleton.js
@@ -4,7 +4,7 @@ import Skeleton from "@casumo/cmp-skeleton";
 export default class PromotionPageSkeleton extends PureComponent {
   render() {
     return (
-      <Skeleton width="100%" height="600px">
+      <Skeleton viewBox={null} width="100%" height="600px">
         <rect x="0" y="0" rx="8" ry="8" width="100%" height="192" />
         <rect x="24" y="226" rx="3" ry="3" width="60%" height="24" />
         <rect x="24" y="258" rx="3" ry="3" width="35%" height="16" />
