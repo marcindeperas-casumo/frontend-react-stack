cf0667ad447825aa2bb425296497678545487f9c
Michele Settepani <michele.settepani@casumo.com>
2018-12-04 14:53:59 +0100

    MustDropJackpotsList: use mock store instead 💊

--- src/components/MustDropJackpotsList/MustDropJackpotsList.stories.js
+++ src/components/MustDropJackpotsList/MustDropJackpotsList.stories.js
@@ -3,18 +3,11 @@ import { storiesOf } from "@storybook/react";
 import info from "Storybook/storybookInfo";
 import MustDropJackpotsList from "Components/MustDropJackpotsList";
 import MockStore from "Components/MockStore";
-import toplist from "Components/MustDropJackpotsList/__mocks__/toplist.json";
 
 const stories = storiesOf("MustDropJackpotsList", module);
 
-const state = {
-  schema: {
-    cms: toplist,
-  },
-};
-
 const DefaultStory = () => (
-  <MockStore state={state}>
+  <MockStore>
     <MustDropJackpotsList />
   </MockStore>
 );
