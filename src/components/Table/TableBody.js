1874ea653fb49c50b987dac0c8f335631f9d32cf
Jack Westbrook <jack.westbrook@casumo.com>
2018-12-06 11:15:19 +0100

    fix(ContentSeparator TableBody): Align border colors with designs.
    Casumo/Home#24805

--- src/components/Table/TableBody.js
+++ src/components/Table/TableBody.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from "react";
+import classNames from "classnames";
 import { createModifierClasses } from "@casumo/cudl-react-utils";
 import type { spacerSizes } from "@casumo/cudl-react-prop-types";
 
@@ -19,13 +20,14 @@ type TableBodyProps = {
 const TableCell = ({ column, padding, row, rest }: TableCellsProps) => {
   const renderProp = rest[column];
   const value = row[column];
+  const cellClassNames = classNames("t-color-grey-dark-2", padding);
 
   if (value) {
     if (renderProp) {
-      return <td className={padding}>{renderProp(value)}</td>;
+      return <td className={cellClassNames}>{renderProp(value)}</td>;
     }
 
-    return <td className={padding}>{value}</td>;
+    return <td className={cellClassNames}>{value}</td>;
   }
 
   return <td />;
@@ -46,7 +48,10 @@ const TableBody = ({
   return (
     <tbody>
       {rows.map((row: Object, i: number) => (
-        <tr className="t-border-bottom" key={`${row[columns[0]]}-${i}`}>
+        <tr
+          className="t-border-bottom t-border--current-color t-color-grey-light-2"
+          key={`${row[columns[0]]}-${i}`}
+        >
           {columns.map((column: string) => (
             <TableCell
               key={column}
