72e4587a058547acdcc7bbd08c740ec4715c43c4
Jack Westbrook <jack.westbrook@casumo.com>
2018-12-11 17:02:20 +0100

    test(PromotionGallery): Add tests.
    Casumo/Home#24751

--- src/components/PromotionGallery/PromotionGallery.test.js
+++ src/components/PromotionGallery/PromotionGallery.test.js
@@ -1,9 +1,95 @@
 import React from "react";
 import { shallow } from "enzyme";
-import PromotionGalleryCard from "Components/PromotionGalleryCard";
+import PromotionGallery from "./PromotionGallery";
 
-describe("PromotionCardGallery", () => {
-  test("should do something", () => {
-    expect(1).toBe(2);
+describe("PromotionGallery", () => {
+  let fetchCampaign;
+  let fetchPromotions;
+
+  beforeEach(() => {
+    fetchCampaign = jest.fn();
+    fetchPromotions = jest.fn();
+  });
+
+  test("should fetch the page on component mount", () => {
+    shallow(
+      <PromotionGallery
+        slug="foo"
+        fetchCampaign={fetchCampaign}
+        fetchPromotions={fetchPromotions}
+        promotionsSlugs={["page-1", "page-2"]}
+      />
+    );
+
+    expect(fetchCampaign).toHaveBeenCalledTimes(1);
+  });
+
+  test("should set a background color if backgroundColor is coming down as a prop", () => {
+    const rendered = shallow(
+      <PromotionGallery
+        slug="foo"
+        fetchCampaign={fetchCampaign}
+        fetchPromotions={fetchPromotions}
+        backgroundColor="blue"
+        promotionsSlugs={["page-1", "page-2"]}
+      />
+    );
+
+    expect(rendered.first().hasClass("t-background-blue")).toBe(true);
+  });
+
+  test("should not set a background color if backgroundColor is not coming down as a prop", () => {
+    const rendered = shallow(
+      <PromotionGallery
+        slug="foo"
+        fetchCampaign={fetchCampaign}
+        fetchPromotions={fetchPromotions}
+        promotionsSlugs={["page-1", "page-2"]}
+      />
+    );
+
+    expect(rendered.first().props().className).not.toMatch("t-background-");
+  });
+
+  test("should set a font color if titleColor is coming down as a prop", () => {
+    const rendered = shallow(
+      <PromotionGallery
+        slug="foo"
+        fetchCampaign={fetchCampaign}
+        fetchPromotions={fetchPromotions}
+        titleColor="white"
+        promotionsSlugs={["page-1", "page-2"]}
+      />
+    );
+
+    expect(rendered.first().hasClass("t-color-white")).toBe(true);
+  });
+
+  test("should not set a font color if titleColor is not coming down as a prop", () => {
+    const rendered = shallow(
+      <PromotionGallery
+        slug="foo"
+        fetchCampaign={fetchCampaign}
+        fetchPromotions={fetchPromotions}
+        promotionsSlugs={["page-1", "page-2"]}
+      />
+    );
+
+    expect(rendered.first().props().className).not.toMatch("t-color-");
+  });
+
+  test("should render a see more link", () => {
+    const rendered = shallow(
+      <PromotionGallery
+        slug="foo"
+        fetchCampaign={fetchCampaign}
+        fetchPromotions={fetchPromotions}
+        promotionsSlugs={["page-1", "page-2"]}
+        seeMore="👀"
+      />
+    );
+
+    expect(rendered.find("a").prop("href")).toBe("/promotions");
+    expect(rendered.find("a").html()).toContain("👀");
   });
 });
