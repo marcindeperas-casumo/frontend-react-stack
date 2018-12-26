61d72f1923ce7ccecb8d918a7fe16b09523467d1
Levente Balogh <balogh.levente.hu@gmail.com>
2018-12-26 18:20:50 +0100

    feat(Services/tracker): add trackers per environment

--- src/applicationService/tracker.js
+++ src/applicationService/tracker.js
@@ -1,15 +1,39 @@
 import config from "Src/config";
+import { getEnv } from "Utils";
+import { ENVS } from "Src/constants";
 import createTracker from "Lib/tracker";
 import createAdapterMixpanel from "Lib/tracker.adapter.mixpanel";
+import craeteAdapterNull from "Lib/tracker.adapter.null";
 
-const { mixpanelToken, mixpanelProjectName } = config;
-const mixpanelConfig = { mixpanelToken, mixpanelProjectName };
-const adapterMixpanel = createAdapterMixpanel(mixpanelConfig);
-const adapters = [adapterMixpanel];
-const tracker = createTracker(adapters);
+const tracker = createTracker(getAdapters());
 
 export default tracker;
 
 export const track = tracker.track;
 
 export const setState = tracker.setState;
+
+function getAdapters() {
+  const env = getEnv();
+  const adapterGetterByEnv = {
+    [ENVS.TEST]: getAdaptersNull,
+    [ENVS.DEVELOPMENT]: getAdaptersNull,
+    [ENVS.PRODUCTION]: getAdaptersProd,
+  };
+
+  return adapterGetterByEnv[env]();
+}
+
+function getAdaptersNull() {
+  const adapterNull = craeteAdapterNull();
+
+  return [adapterNull];
+}
+
+function getAdaptersProd() {
+  const { mixpanelToken, mixpanelProjectName } = config;
+  const mixpanelConfig = { mixpanelToken, mixpanelProjectName };
+  const adapterMixpanel = createAdapterMixpanel(mixpanelConfig);
+
+  return [adapterMixpanel];
+}
