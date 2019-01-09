f68f19191c93dd2bb082c120972c054b822d29e0
Levente Balogh <balogh.levente.hu@gmail.com>
2019-01-09 15:52:54 +0100

    feat(Lib): add a http module

---
+++ src/lib/http.js
@@ -0,0 +1,36 @@
+// @flow
+const errorHandler = response => {
+  if (!response.ok) {
+    // eslint-disable-next-line fp/no-throw
+    throw new Error(response.statusText);
+  }
+
+  return response;
+};
+
+const defaultFetch = (url: string, options: ?Object) => {
+  return fetch(url, {
+    credentials: "same-origin",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then(errorHandler)
+    .then(response => response.json());
+};
+
+const get = defaultFetch;
+
+const post = (url, options) =>
+  defaultFetch(url, {
+    method: "POST",
+    ...options,
+  });
+
+const http = {
+  get,
+  post,
+  fetch: defaultFetch,
+};
+
+export default http;
