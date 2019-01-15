18e4b150ddb0ec5b059c5b78b17deeb833b67867
Levente Balogh <balogh.levente.hu@gmail.com>
2019-01-15 10:29:11 +0100

    test(Lib/http): add tests http.post()

--- src/lib/http.test.js
+++ src/lib/http.test.js
@@ -6,13 +6,63 @@ describe("Lib/http", () => {
   const getFetchCallOptionsArg = () => fetch.mock.calls[0][1];
   let responseMock;
 
-  beforeEach(() => {
+  const mockFetch = responseMockOverride => {
     responseMock = {
       ok: true,
       json: jest.fn().mockReturnValue(responseObject),
+      ...responseMockOverride,
     };
+
     // eslint-disable-next-line no-native-reassign
     fetch = jest.fn().mockResolvedValue(responseMock);
+  };
+
+  beforeEach(() => {
+    mockFetch();
+  });
+
+  describe(".post()", () => {
+    test("parses the JSON response", async () => {
+      const response = await http.post("/foo/bar");
+
+      expect(responseMock.json).toHaveBeenCalledTimes(1);
+      expect(response).toEqual(responseObject);
+    });
+
+    test("rejects the promise if the request fails", async () => {
+      mockFetch({ ok: false, statusText: "Some error." });
+
+      expect(http.post("/foo/bar")).rejects.toMatchObject({
+        message: "Some error.",
+      });
+    });
+
+    test("posts to the right URL", async () => {
+      await http.post("/foo/bar");
+
+      expect(getFetchCallUrlArg()).toMatch("/foo/bar");
+    });
+
+    test("uses the default options", async () => {
+      await http.post("/foo/bar");
+
+      expect(getFetchCallOptionsArg()).toMatchObject(DEFAULT_FETCH_OPTIONS);
+    });
+
+    test("extends the fetch options if specified", async () => {
+      const optionsOverride = { foo: "bar", bar: "foo" };
+      const data = null;
+
+      await http.post("/foo/bar", data, optionsOverride);
+
+      expect(getFetchCallOptionsArg()).toMatchObject(optionsOverride);
+    });
+
+    test("uses the POST method", async () => {
+      await http.post("/foo/bar");
+
+      expect(getFetchCallOptionsArg()).toMatchObject({ method: "POST" });
+    });
   });
 
   describe(".get()", () => {
@@ -24,19 +74,10 @@ describe("Lib/http", () => {
     });
 
     test("rejects the promise if the request fails", async () => {
-      const errorMessage = "Some error.";
-
-      responseMock = {
-        ...responseMock,
-        ok: false,
-        statusText: errorMessage,
-      };
-
-      // eslint-disable-next-line no-native-reassign
-      fetch = jest.fn().mockResolvedValue(responseMock);
+      mockFetch({ ok: false, statusText: "Some error." });
 
       expect(http.get("/foo/bar")).rejects.toMatchObject({
-        message: errorMessage,
+        message: "Some error.",
       });
     });
 
