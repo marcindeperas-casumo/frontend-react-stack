050a337fb952622734dd98b58c8474e5b40144dc
Levente Balogh <balogh.levente.hu@gmail.com>
2019-01-16 12:41:23 +0100

    feat(Services): add a domain specific storage service

---
+++ src/applicationService/storage.js
@@ -0,0 +1,21 @@
+// @flow
+import storage from "Lib/storage";
+
+export const NAMESPACE = "react-stack";
+
+export const get = (key: string, defaultValue: ?any) =>
+  storage.get(key, defaultValue, NAMESPACE);
+
+export const set = (key: string, value: any) =>
+  storage.set(key, value, NAMESPACE);
+
+export const remove = (key: string) => storage.remove(key, NAMESPACE);
+
+export const clearAll = () => storage.clearAll(NAMESPACE);
+
+export default {
+  get,
+  set,
+  remove,
+  clearAll,
+};
