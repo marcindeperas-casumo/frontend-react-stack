00bd6044cdb7e142ff0c0aa6768cd18ca24f2ab0
Levente Balogh <balogh.levente.hu@gmail.com>
2019-01-16 14:57:39 +0100

    feat(pre-commit): remove stashing logic
    This logic is already included in lint-staged
    from version > 8.0.0

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,20 +5,7 @@ if [ $result -ne 0 ]
 then
     echo "Merge in progress. Skipping all pre-commit checks."
 else
-    # source: https://github.com/okonet/lint-staged/issues/62#issuecomment-411964870
-    # precommit.sh
-    message="precommit_on_$(git log -1 --format=%H)"
-    git stash save --keep-index $message
-
-    # because git stash always succeeds
-    # we need to check if we have stashed successfully
-    lastStash=$(git stash list -1)
-    if [[ $lastStash == *$message* ]]; then
-        # If stashed, git stash pop when exit
-        trap 'git stash pop' EXIT
-    fi
-
-    # Here's what we intend to run
+    # Run linters on staged files only
     lint-staged
 fi
 
