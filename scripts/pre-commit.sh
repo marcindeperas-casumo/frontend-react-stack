77a3d29f1bd5786a990ef08dc57d8680958e62de
Levente Balogh <balogh.levente.hu@gmail.com>
2019-01-16 15:09:02 +0100

    docs(pre-commit): add a short comment

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,6 +5,11 @@ if [ $result -ne 0 ]
 then
     echo "Merge in progress. Skipping all pre-commit checks."
 else
+    # NOTE!
+    # If you are running any automatic changes on the code here
+    # bear in mind that it will run on staged, unstaged and untracked
+    # code as well.
+
     # Run linters on staged files only
     lint-staged
 fi
