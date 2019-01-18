09493eb24c3e49f4fe5eb406dee582dd33dadd51
Michele Settepani <michele.settepani@casumo.com>
2019-01-18 14:12:15 +0100

    GameSearch: add SectionsList

--- src/components/GameSearch/GameSearch.js
+++ src/components/GameSearch/GameSearch.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from "react";
 
 import Flex from "@casumo/cmp-flex";
 import SearchInput from "Components/SearchInput";
+import SectionsList from "Components/SectionsList";
 
 type Props = {
   isLoaded: boolean,
@@ -18,15 +19,21 @@ export default class GameSearch extends PureComponent<Props> {
   }
 
   render() {
+    const { games } = this.props;
     return (
       <Flex direction="vertical" spacing="none">
-        <Flex.Block className="t-background-grey-light-2">
-          <Flex className="u-padding--md" align="stretch">
+        <Flex.Block>
+          <Flex className="" align="stretch">
             <Flex.Block>
-              <SearchInput
-                autoFocus={true}
-                placeholder="Eg. game title, provider"
-              />
+              <div className="t-background-grey-light-2 u-padding--md">
+                <SearchInput
+                  autoFocus={true}
+                  placeholder="Eg. game title, provider"
+                />
+              </div>
+              <div className="u-padding-horiz--md">
+                <SectionsList items={games} />
+              </div>
             </Flex.Block>
           </Flex>
         </Flex.Block>
