92d7803795270486968eab74ace0a782d0f7455f
Michele Settepani <michele.settepani@casumo.com>
2019-01-21 16:55:19 +0100

    GameRowSearch: create GameRow component for Search

---
+++ src/components/GameRowSearch/GameRowSearch.js
@@ -0,0 +1,83 @@
+// @flow
+import React, { PureComponent } from "react";
+import Flex from "@casumo/cmp-flex";
+import Text from "@casumo/cmp-text";
+import { MoreIcon, PlayIcon } from "@casumo/cmp-icons";
+import { EVENTS, EVENT_PROPS } from "Src/constants";
+import type { Game } from "Types/game";
+import GameThumb from "Components/GameThumb";
+import DangerousHtml from "Components/DangerousHtml";
+import TrackClick from "Components/TrackClick";
+
+type Props = {
+  game: Game,
+  onLaunchGame: () => void,
+  id: string,
+};
+
+export default class GameRowSearch extends PureComponent<Props> {
+  render() {
+    const { game = {}, onLaunchGame, id } = this.props;
+    const { name, logo, logoBackground } = game;
+    const iconStyle =
+      "t-background-white t-color-grey-light-1 t-border-r--circle u-padding--md";
+
+    const RenderPlayIcon = () => (
+      <TrackClick
+        eventName={EVENTS.GAME_LAUNCH}
+        data={{ [EVENT_PROPS.GAME_NAME]: name }}
+      >
+        <Flex.Item onClick={onLaunchGame}>
+          {/* Play Icon */}
+          <PlayIcon size="med" className={iconStyle} />
+        </Flex.Item>
+      </TrackClick>
+    );
+
+    const RenderMoreIcon = () => (
+      <Flex.Item>
+        <TrackClick
+          eventName={EVENTS.GAME_DETAILS}
+          data={{ [EVENT_PROPS.GAME_NAME]: name }}
+        >
+          {/* More Icon */}
+          <a href={`/en/play/${id}`}>
+            <MoreIcon size="med" className={iconStyle} />
+          </a>
+        </TrackClick>
+      </Flex.Item>
+    );
+
+    return (
+      <Flex align="center" className="u-padding-vert">
+        <Flex.Block onClick={onLaunchGame}>
+          <TrackClick
+            eventName={EVENTS.GAME_LAUNCH}
+            data={{ [EVENT_PROPS.GAME_NAME]: name }}
+          >
+            <Flex align="center">
+              {/* Image */}
+              <Flex.Item className="o-flex__item-fixed-size">
+                <GameThumb
+                  src={logoBackground}
+                  alt={name}
+                  mark={logo}
+                  width="64"
+                  height="64"
+                />
+              </Flex.Item>
+
+              {/* Text */}
+              <Flex.Block className="t-color-grey-dark-3 u-padding-left--sm">
+                <Text tag="div" size="sm">
+                  <DangerousHtml html={name} />
+                </Text>
+              </Flex.Block>
+            </Flex>
+          </TrackClick>
+        </Flex.Block>
+        {!game.lobby ? <RenderMoreIcon /> : <RenderPlayIcon />}
+      </Flex>
+    );
+  }
+}
---
+++ src/components/GameRowSearch/GameRowSearchContainer.js
@@ -0,0 +1,27 @@
+// @flow
+import React from "react";
+import { connect } from "react-redux";
+import { gameSelector } from "Models/schema";
+import { launchGame } from "Models/games";
+import GameRowSearch from "Components/GameRowSearch/GameRowSearch";
+
+const GameRowSearchConnected = connect(
+  (state, { id }) => ({
+    game: gameSelector(id)(state),
+    id,
+  }),
+  (dispatch, { id }) => ({
+    onLaunchGame: () => dispatch(launchGame(id)),
+  })
+)(GameRowSearch);
+
+type Props = {
+  /** The slug of the game to render */
+  id: string,
+};
+
+const GameRowSearchContainer = ({ id }: Props) => (
+  <GameRowSearchConnected id={id} />
+);
+
+export default GameRowSearchContainer;
