b491b9d8719b83ea2df2616813f78edc6f0b9f62
Michelle <michelle.dalli@casumo.com>
2019-01-22 15:54:29 +0100

    ContinuPlayingCard: updated story

--- src/components/ContinuePlayingCard/ContinuePlayingCard.stories.js
+++ src/components/ContinuePlayingCard/ContinuePlayingCard.stories.js
@@ -1,26 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import info from "Storybook/storybookInfo";
-import ContinuePlayingCard from "Components/ContinuePlayingCard/ContinuePlayingCard";
+import ContinuePlayingCard from "Components/ContinuePlayingCard";
 import MockStore from "Components/MockStore";
 import game from "Components/GameTile/__mocks__/Game.json";
-import isNotChromatic from "Storybook/isNotChromatic";
 
 const stories = storiesOf("ContinuePlayingCard", module);
-
-if (isNotChromatic) {
-  stories.add(
-    "Default (Connected)",
-    () => {
-      return (
-        <MockStore>
-          <ContinuePlayingCard id={game.slug} />
-        </MockStore>
-      );
-    },
-    info({ text: "Default Tile" })
-  );
-}
 
 stories.add(
   "Default",
@@ -31,5 +16,5 @@ stories.add(
       </MockStore>
     );
   },
-  info({ text: "Default" })
+  info({ text: "Default Continue Playing tile" })
 );
