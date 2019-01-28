da03186f74a5a83c84c337e13f627f61c71ad5a6
Michelle <michelle.dalli@casumo.com>
2019-01-28 10:07:46 +0100

    GameTileWithoutOverlayStories: disabled connected component in chromatic

--- src/components/GameTileWithoutOverlay/GameTileWithoutOverlay.stories.js
+++ src/components/GameTileWithoutOverlay/GameTileWithoutOverlay.stories.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import info from "Storybook/storybookInfo";
-import GameTileWithoutOverlay from "Components/GameTileWithoutOverlay";
+import GameTileWithoutOverlayConnected from "Components/GameTileWithoutOverlay";
+import GameTileWithoutOverlay from "Components/GameTileWithoutOverlay/GameTileWithoutOverlay";
 import MockStore from "Components/MockStore";
 import game from "Components/GameTile/__mocks__/Game.json";
+import isNotChromatic from "Storybook/isNotChromatic";
 
 const stories = storiesOf("GameTileWithoutOverlay", module);
 
+if (isNotChromatic) {
+  stories.add(
+    "Default tile without overlay (Connected)",
+    () => {
+      return (
+        <MockStore>
+          <GameTileWithoutOverlayConnected id={game.slug} />
+        </MockStore>
+      );
+    },
+    info({ text: "Default Tile" })
+  );
+}
+
 stories.add(
-  "Default",
+  "Default tile without overlay",
   () => {
     return (
       <MockStore>
-        <GameTileWithoutOverlay id={game.slug} />
+        <GameTileWithoutOverlay game={game} onLaunchGame={action(game.name)} />
       </MockStore>
     );
   },
