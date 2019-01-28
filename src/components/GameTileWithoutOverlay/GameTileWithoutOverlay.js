1058799c5e187e7b7e928396df7e789bba41bbb9
Michelle <michelle.dalli@casumo.com>
2019-01-28 14:51:57 +0100

    GameTileWithoutOverlay: added flow annotation

--- src/components/GameTileWithoutOverlay/GameTileWithoutOverlay.js
+++ src/components/GameTileWithoutOverlay/GameTileWithoutOverlay.js
@@ -1,8 +1,10 @@
+// @flow
 import React, { PureComponent } from "react";
 import Flex from "@casumo/cmp-flex";
 import GameTile from "Components/GameTile/GameTile";
+import type { Props } from "Components/GameTile/GameTile";
 
-class GameTileWithoutOverlay extends PureComponent {
+class GameTileWithoutOverlay extends PureComponent<Props> {
   render() {
     return (
       <Flex.Item className="o-flex__item-fixed-size c-top-game">
