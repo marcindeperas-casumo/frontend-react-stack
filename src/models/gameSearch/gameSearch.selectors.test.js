103ad261e678ec28bbdf223978910cb7949514ed
Cho <choishere@gmail.com>
2019-01-28 22:02:13 +0100

    gameSearch add selectors tests

--- src/models/gameSearch/gameSearch.selectors.test.js
+++ src/models/gameSearch/gameSearch.selectors.test.js
@@ -1,7 +1,11 @@
 import {
   playerGamesAllSelector,
+  isGameSearchLoading,
+  isGameSearchNoMatch,
   isGameSearchLoadedFactory,
+  gameSearchResultsSelector,
   PLAYER_ALL_GAMES_LIST_ID,
+  gameSearchEntities,
 } from "Models/gameSearch";
 import { ENTITY_KEYS } from "Models/schema";
 
@@ -29,6 +33,66 @@ describe("Models/GameSearch/Selectors", () => {
     });
   });
 
+  describe("isGameSearchLoading()", () => {
+    test("returns true if gameList is in state", () => {
+      const state = {
+        schema: {
+          [ENTITY_KEYS.GAME_LIST]: {
+            gameSearch: {
+              loading: true,
+            }
+          }
+        }
+      };
+
+      expect(isGameSearchLoading(state)).toBe(true);
+    });
+
+    test("returns false if gameList is not in state", () => {
+      const state = {
+        schema: {
+          [ENTITY_KEYS.GAME_LIST]: {
+            gameSearch: {
+              loading: false,
+            }
+          }
+        }
+      };
+
+      expect(isGameSearchLoading(state)).toBe(false);
+    });
+  });
+
+  describe("isGameSearchNoMatch()", () => {
+    test("returns true if gameList is in state", () => {
+      const state = {
+        schema: {
+          [ENTITY_KEYS.GAME_LIST]: {
+            gameSearch: {
+              noMatch: true,
+            }
+          }
+        }
+      };
+
+      expect(isGameSearchNoMatch(state)).toBe(true);
+    });
+
+    test("returns false if gameList is not in state", () => {
+      const state = {
+        schema: {
+          [ENTITY_KEYS.GAME_LIST]: {
+            gameSearch: {
+              noMatch: false,
+            }
+          }
+        }
+      };
+
+      expect(isGameSearchNoMatch(state)).toBe(false);
+    });
+  });
+
   describe("isGameSearchLoadedFactory()", () => {
     test("returns true if gameList is in state", () => {
       const games = ["foo"];
@@ -47,6 +111,36 @@ describe("Models/GameSearch/Selectors", () => {
       const state = { schema: { [ENTITY_KEYS.GAME_LIST]: {} } };
 
       expect(isGameSearchLoadedFactory(state)).toBe(false);
+    });
+  });
+
+  describe("gameSearchResultsSelector()", () => {
+    test("returns gameList", () => {
+      const state = {
+        schema: {
+          [ENTITY_KEYS.GAME_LIST]: {
+            gameSearch: {
+              games: ["foo"],
+            }
+          }
+        }
+      };
+
+      expect(gameSearchResultsSelector(state)).toEqual(["foo"]);
+    });
+
+    test("returns empty gameList is not in state", () => {
+      const state = {
+        schema: {
+          [ENTITY_KEYS.GAME_LIST]: {
+            gameSearch: {
+              games: [],
+            }
+          }
+        }
+      };
+
+      expect(gameSearchResultsSelector(state)).toEqual([]);
     });
   });
 });
