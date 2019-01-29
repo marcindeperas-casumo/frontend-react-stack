706962b1dc7a0f8db466b860fa85b477176bd3ed
Cho <choishere@gmail.com>
2019-01-29 14:23:26 +0100

    gameSearch fix no query stop saga

--- src/models/gameSearch/gameSearch.saga.search.js
+++ src/models/gameSearch/gameSearch.saga.search.js
@@ -4,6 +4,7 @@ import { normalizeData, updateEntity } from "Models/schema";
 import {
   types,
   fetchLatestPlayedSaga,
+  clearSearchSaga,
   fetchQuerySearch,
   gameSearchEntities,
 } from "Models/gameSearch";
@@ -20,6 +21,11 @@ export function* fetchQuerySaga(action) {
   );
 
   yield put(updateEntity(loadingTrueEntities));
+
+  // if there is no query, stops here
+  if (!q) {
+    return yield call(clearSearchSaga);
+  }
 
   // fetch query search
   yield put(fetchQuerySearch({ platform, country, q }));
