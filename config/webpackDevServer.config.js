d1a5654f4c73ae66bdb5f2989509a6c8115a927c
Cho <choishere@gmail.com>
2019-01-30 14:06:28 +0100

    checkout webpack dev

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const errorOverlayMiddleware = require("react-dev-utils/errorOverlayMiddleware");
 const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
 const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
