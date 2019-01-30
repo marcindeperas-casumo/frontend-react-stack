54d03751132f0f57f1aa59c059355087a10b02b5
Michał Mokijewski <hello@naf.gl>
2019-01-30 11:33:37 +0100

    fix migration component import & exports

--- src/components/MigrationComponent/index.js
+++ src/components/MigrationComponent/index.js
@@ -1,6 +1,4 @@
 // @flow
-import { MigrationComponentManager } from "./MigrationComponentManager";
-
 export type * from "./MigrationComponent"; // eslint-disable-line import/export
 export { default } from "./MigrationComponent";
-export { MigrationComponentManager };
+export { MigrationComponentManager } from "./MigrationComponentManager";
