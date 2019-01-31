87a5ec3bd488f459b334615c42f91e38cfc480eb
Michele Settepani <michele.settepani@casumo.com>
2019-01-31 15:32:59 +0100

    VirtualList: add some tests

--- src/components/VirtualList/VirtualList.test.js
+++ src/components/VirtualList/VirtualList.test.js
@@ -1,12 +1,62 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import VirtualList from "Components/VirtualList";
+import { always } from "ramda";
 
 describe("VirtualList", () => {
-  test("should do something", () => {
-    const rendered = shallow(<VirtualList msg="hi" />);
-    expect(rendered.find("div").length).toBe(1);
-    expect(rendered.text()).toBe("VirtualList says: hi");
-    expect(1).toBe(2);
+  test("should render a VirtualList given the required params", () => {
+    const rendered = mount(
+      <VirtualList
+        isRowLoaded={always(true)}
+        loadMoreRows={() => [1, 2, 3]}
+        rowHeight={10}
+        rowRenderer={() => <div />}
+        totalNumberOfRows={100}
+      />
+    );
+
+    expect(rendered.find("VirtualList").length).toBe(1);
+  });
+
+  test("should render an InfiniteLoader virtualized component", () => {
+    const rendered = mount(
+      <VirtualList
+        isRowLoaded={always(true)}
+        loadMoreRows={() => [1, 2, 3]}
+        rowHeight={10}
+        rowRenderer={() => <div />}
+        totalNumberOfRows={100}
+      />
+    );
+
+    expect(rendered.find("InfiniteLoader").length).toBe(1);
+  });
+
+  test("should render an AutoSizer virtualized component", () => {
+    const rendered = mount(
+      <VirtualList
+        isRowLoaded={always(true)}
+        loadMoreRows={() => [1, 2, 3]}
+        rowHeight={10}
+        rowRenderer={() => <div />}
+        totalNumberOfRows={100}
+      />
+    );
+
+    expect(rendered.find("AutoSizer").length).toBe(1);
+  });
+
+  test("should render a List virtualized component", () => {
+    const rendered = mount(
+      <VirtualList
+        isRowLoaded={always(true)}
+        loadMoreRows={() => [1, 2, 3]}
+        rowHeight={10}
+        rowRenderer={() => <div />}
+        totalNumberOfRows={100}
+      />
+    );
+
+    expect(rendered.find("List").length).toBe(1);
   });
 });
