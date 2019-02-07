988eda73f95099ba910616295af02befe2ff2776
Adam Pilkington <adampilks@gmail.com>
2019-02-07 16:45:11 +0100

    Adding some more interesting stories for GroupPill Casumo/Home#23322

--- src/features/sports/components/GroupPill/GroupPill.stories.js
+++ src/features/sports/components/GroupPill/GroupPill.stories.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import info from "Storybook/storybookInfo";
+import { action } from "@storybook/addon-actions";
 import GroupPill from "./GroupPill";
 
 const stories = storiesOf("Sports/GroupPill", module);
@@ -20,4 +21,34 @@ stories.add(
   "Default",
   () => <GroupPill group={groupA} />,
   info({ text: "Default" })
+);
+
+stories.add(
+  "Active",
+  () => <GroupPill group={groupA} isActive={true} />,
+  info({ text: "Active" })
+);
+
+stories.add(
+  "Group with Emoji",
+  () => <GroupPill group={groupB} />,
+  info({ text: "Group with Emoji" })
+);
+
+stories.add(
+  "With onRemove",
+  () => <GroupPill group={groupB} onRemove={action("onRemove")} />,
+  info({ text: "With onRemove" })
+);
+
+stories.add(
+  "With onClick",
+  () => (
+    <GroupPill
+      group={groupB}
+      onRemove={action("onRemove")}
+      onClick={action("onClick")}
+    />
+  ),
+  info({ text: "With onClick" })
 );
