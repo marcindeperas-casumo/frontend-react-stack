59e5e987302568de7fecde467d585ca5a8792bfb
Adam Pilkington <adampilks@gmail.com>
2019-02-12 20:51:24 +0100

    Adding state handling for both client and remote state Casumo/Home#23322

---
+++ src/features/sports/state/clientState/index.js
@@ -0,0 +1,18 @@
+/* @flow */
+import { type ClientStateConfig } from "apollo-link-state";
+
+import clientSchema from "./clientSchema.graphql";
+import resolvers from "./resolvers";
+import defaultState from "./defaultState";
+
+export const clientState: ClientStateConfig = {
+  defaults: defaultState,
+  typeDefs: clientSchema,
+  resolvers,
+};
+
+export * from "./mutations";
+
+export * from "./queries";
+
+export * from "./typedComponents";
