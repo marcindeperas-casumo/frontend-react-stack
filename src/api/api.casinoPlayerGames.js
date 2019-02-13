ad15d246df1baa195fe74f1c41352283bf8187cf
Cho <choishere@gmail.com>
2019-02-13 12:29:30 +0100

    avoid proxy for test env

--- src/api/api.casinoPlayerGames.js
+++ src/api/api.casinoPlayerGames.js
@@ -1,12 +1,15 @@
 import http from "Services/http";
 
+// TODO remove this
+const baseURL = "https://casino-player.casumotest.com/casino-games/api/v1/";
+
 export const getCasinoPlayerGames = ({ page = 1, pageSize = 100 }) => {
-  return http.get("/api/casino-games/games", {
+  return http.get(`${baseURL}/games`, {
     page,
     pageSize,
   });
 };
 
 export const getCasinoPlayerGamesCount = () => {
-  return http.get("/api/casino-games/games/count");
+  return http.get(`${baseURL}/count`);
 };
