8881ce4c87338f0fe9bc582d33a82581c08c164b
Cho <choishere@gmail.com>
2019-02-13 12:36:49 +0100

    avoid proxy for test env

--- src/api/api.casinoPlayerGames.js
+++ src/api/api.casinoPlayerGames.js
@@ -1,7 +1,10 @@
 import http from "Services/http";
-
-// TODO remove this
-const baseURL = "https://casino-player.casumotest.com/casino-games/api/v1/";
+// TODO remove this once the service is in consul
+// only to avoid routing config in test env since it is same origin
+import { isEnvProduction } from "Utils";
+const baseURL = isEnvProduction()
+  ? "https://casino-player.casumotest.com/casino-games/api/v1"
+  : "/api/casino-games";
 
 export const getCasinoPlayerGames = ({ page = 1, pageSize = 100 }) => {
   return http.get(`${baseURL}/games`, {
@@ -11,5 +14,5 @@ export const getCasinoPlayerGames = ({ page = 1, pageSize = 100 }) => {
 };
 
 export const getCasinoPlayerGamesCount = () => {
-  return http.get(`${baseURL}/count`);
+  return http.get(`${baseURL}/games/count`);
 };
