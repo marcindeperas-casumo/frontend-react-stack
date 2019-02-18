cddcafc5bb6bad444b4878ef46407f363761c96c
Cho <choishere@gmail.com>
2019-02-18 11:45:52 +0100

    base url for routing test

--- src/api/api.casinoPlayerGames.js
+++ src/api/api.casinoPlayerGames.js
@@ -1,12 +1,14 @@
 import http from "Services/http";
 
+const url = `/casino-player/casino-games/api/v1`;
+
 export const getCasinoPlayerGames = ({ page = 1, pageSize = 100 }) => {
-  return http.get("/api/casino-games/games", {
+  return http.get(`${url}/games`, {
     page,
     pageSize,
   });
 };
 
 export const getCasinoPlayerGamesCount = () => {
-  return http.get("/api/casino-games/games/count");
+  return http.get(`${url}/games/count`);
 };
