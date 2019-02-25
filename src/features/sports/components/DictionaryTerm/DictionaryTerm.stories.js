649ac4a80d20305300e587fdc342f643df24e698
Jack Westbrook <jack.westbrook@casumo.com>
2019-02-25 13:24:52 +0100

    docs(DictionaryTerm): Hide stories from Chromatic.

--- src/features/sports/components/DictionaryTerm/DictionaryTerm.stories.js
+++ src/features/sports/components/DictionaryTerm/DictionaryTerm.stories.js
@@ -11,28 +11,30 @@ import { mocks, WORKING_TERM, REPLACEMENT_TERM } from "./__mocks__/termMocks";
 
 const stories = storiesOf("Sports/DictionaryTerm/DictionaryTerm", module);
 
-stories.addDecorator(story => (
-  <MockedProvider mocks={mocks} addTypename={false}>
-    {story()}
-  </MockedProvider>
-));
+if (isNotChromatic) {
+  stories.addDecorator(story => (
+    <MockedProvider mocks={mocks} addTypename={false}>
+      {story()}
+    </MockedProvider>
+  ));
 
-stories.add(
-  "Default",
-  () => <DictionaryTerm termKey={WORKING_TERM.key} />,
-  info({ text: "Default" })
-);
+  stories.add(
+    "Default",
+    () => <DictionaryTerm termKey={WORKING_TERM.key} />,
+    info({ text: "Default" })
+  );
 
-stories.add(
-  "With replacements",
-  () => (
-    <DictionaryTerm
-      termKey={REPLACEMENT_TERM.key}
-      replacements={{
-        teamName: text("Team name", "{teamName}"),
-        goalCount: number("Goal count", "{goalCount}"),
-      }}
-    />
-  ),
-  info({ text: "Default" })
-);
+  stories.add(
+    "With replacements",
+    () => (
+      <DictionaryTerm
+        termKey={REPLACEMENT_TERM.key}
+        replacements={{
+          teamName: text("Team name", "{teamName}"),
+          goalCount: number("Goal count", "{goalCount}"),
+        }}
+      />
+    ),
+    info({ text: "Default" })
+  );
+}
