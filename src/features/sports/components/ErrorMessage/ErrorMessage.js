279f134af71b7d89cd2cc5a915c8b68ede14bd81
Charles Oliver <charles.oliver@casumo.com>
2019-02-26 14:06:39 +0100

    style ErrorMessage

--- src/features/sports/components/ErrorMessage/ErrorMessage.js
+++ src/features/sports/components/ErrorMessage/ErrorMessage.js
@@ -1,16 +1,68 @@
 // @flow
 import React from "react";
+import { equals, unless } from "ramda";
+
+import Button from "@casumo/cmp-button";
+import Flex from "@casumo/cmp-flex";
+import Text from "@casumo/cmp-text";
+
+import SadSumo from "./sad-sumo.svg";
 
 type Direction = "horizontal" | "vertical";
 
-type Props = {
+type ComponentProps = {
   direction: Direction,
-  retry: () => boolean,
+  errorMessage: string,
 };
 
-export default ({ direction, retry }: Props) => (
-  <div>
-    <h1>ERRORED AF.</h1>
-    <pre>{JSON.stringify({ direction, retry })}</pre>
-  </div>
+type RetryProps = {
+  retryMessage: string,
+  retry: () => () => void,
+};
+
+type Props = RetryProps & ComponentProps;
+
+const DEFAULT_ERROR_MESSAGE = "Loading failed";
+const DEFAULT_RETRY_MESSAGE = "Try again";
+
+const renderRetryIfHandlerProvided = ({ retry, retryMessage }: RetryProps) =>
+  unless(equals(noop), renderRetry({ retry, retryMessage }), retry);
+
+const renderRetry = ({ retry, retryMessage }: RetryProps) => () => (
+  <Flex.Item>
+    <Button variant="outline-positive" onClick={retry}>
+      {retryMessage}
+    </Button>
+  </Flex.Item>
 );
+
+const noop = () => () => undefined;
+
+export default ({
+  direction = "vertical",
+  errorMessage = DEFAULT_ERROR_MESSAGE,
+  retryMessage = DEFAULT_RETRY_MESSAGE,
+  retry = noop,
+}: Props) => {
+  const InnerFlex = direction === "horizontal" ? Flex.Block : Flex.Item;
+
+  return (
+    <Flex
+      direction={direction}
+      align="center"
+      justify={direction === "horizontal" ? "space-between" : "center"}
+      className="o-flex--1 u-padding--md"
+      spacing="lg"
+    >
+      <Flex.Item>
+        <SadSumo className="u-display--block" />
+      </Flex.Item>
+      <InnerFlex>
+        <Text className="u-font-weight-bold u-margin-vert--none">
+          {errorMessage}
+        </Text>
+      </InnerFlex>
+      {renderRetryIfHandlerProvided({ retry, retryMessage })}
+    </Flex>
+  );
+};
--- src/features/sports/components/ErrorMessage/ErrorMessage.stories.js
+++ src/features/sports/components/ErrorMessage/ErrorMessage.stories.js
@@ -3,6 +3,8 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 import { F } from "ramda";
 
+import Flex from "@casumo/cmp-flex";
+
 import ErrorMessage from "Features/sports/components/ErrorMessage";
 
 const stories = storiesOf("Sports/ErrorMessage", module);
@@ -11,12 +13,22 @@ const retry = async () => {
   await setTimeout(F, 1991);
 };
 
-stories.add("Default View (Vertical)", () => <ErrorMessage />);
+const renderError = (props = {}) => () => (
+  <Flex
+    className="t-background-grey-light-2 u-padding--2xlg"
+    style={{ minHeight: 420 }}
+  >
+    <ErrorMessage {...props} />
+  </Flex>
+);
 
-stories.add("Vertical with Retry", () => <ErrorMessage retry={retry} />);
+stories.add("Default View (Vertical)", renderError());
 
-stories.add("Horizontal", () => <ErrorMessage direction="horizontal" />);
+stories.add("Vertical with Retry", renderError({ retry }));
 
-stories.add("Horizontal with Retry", () => (
-  <ErrorMessage direction="horizontal" retry={retry} />
-));
+stories.add("Horizontal", renderError({ direction: "horizontal" }));
+
+stories.add(
+  "Horizontal with Retry",
+  renderError({ retry, direction: "horizontal" })
+);
