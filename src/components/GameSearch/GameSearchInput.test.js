627e655c08266b725f1de75c6dfd99e22418e44c
Michele Settepani <michele.settepani@casumo.com>
2019-02-26 11:00:51 +0100

    GameSearchInput: test debounced function

--- src/components/GameSearch/GameSearchInput.test.js
+++ src/components/GameSearch/GameSearchInput.test.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { shallow } from "enzyme";
+// Sinon is imported only for testing a debounced version, as there are still prroblems testing it with Jest.
+// Once Jest will fully support debounce, we should remove sinon https://github.com/facebook/jest/issues/3465
+import sinon from "sinon";
 import GameSearchInput from "./GameSearchInput";
 
 describe("GameSearchInput", () => {
+  let clock;
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+
   test("should render a SearchInput component", () => {
     const rendered = shallow(<GameSearchInput />);
 
@@ -20,4 +33,21 @@ describe("GameSearchInput", () => {
     expect(rendered.prop("placeholder")).toBe(placeholder);
     expect(rendered.prop("noResults")).toBe(noResults);
   });
+  test("should debounce fetchSearchResults", () => {
+    const initFetchQuerySearch = jest.fn();
+    const rendered = shallow(
+      <GameSearchInput initFetchQuerySearch={initFetchQuerySearch} />
+    );
+
+    expect(initFetchQuerySearch).toHaveBeenCalledTimes(0);
+
+    rendered.setState({ query: "test 1" });
+    rendered.setState({ query: "test 2" });
+    rendered.setState({ query: "test 3" });
+
+    clock.tick(500);
+
+    expect(initFetchQuerySearch).toHaveBeenCalledTimes(1);
+  });
+
 });
