bdcb424d60a63336e2fbf5d384c158d5a0722d82
Michał Mokijewski <hello@naf.gl>
2019-02-26 11:32:44 +0100

    add types to api.gamebrowser

--- src/api/api.gamebrowser.js
+++ src/api/api.gamebrowser.js
@@ -1,3 +1,4 @@
+// @flow
 import defaultHttp from "Services/http";
 
 export const URL = {
@@ -9,16 +10,30 @@ export const URL = {
   LIVE_CASINO: "/api/gamebrowser/liveCasino/tablesById",
 };
 
+type HTTPClient = typeof defaultHttp;
+
 export const getGameBrowserHandshake = (
-  { platform, country },
-  http = defaultHttp
-) => {
-  return http.get(`${URL.HANDSHAKE}/${platform}/${country}`);
-};
+  { platform, country }: { platform: string, country: string },
+  http: HTTPClient = defaultHttp
+) => http.get(`${URL.HANDSHAKE}/${platform}/${country}`);
 
 export const getGameLists = (
-  { platform, country, id, variant, page = 0, pageSize = 5 },
-  http = defaultHttp
+  {
+    platform,
+    country,
+    id,
+    variant,
+    page = 0,
+    pageSize = 5,
+  }: {
+    platform: string,
+    country: string,
+    id: string,
+    variant: string,
+    page?: number,
+    pageSize?: number,
+  },
+  http: HTTPClient = defaultHttp
 ) =>
   http.get(`${URL.GAME_LISTS}/${platform}/${country}/${id}`, {
     variant,
@@ -27,14 +42,24 @@ export const getGameLists = (
   });
 
 export const getLatestPlayedGames = (
-  { playerId, pageSize = 20 },
-  http = defaultHttp
+  { playerId, pageSize = 20 }: { playerId: string, pageSize?: number },
+  http: HTTPClient = defaultHttp
 ) =>
   http.get(`${URL.GAMES_LATEST_PLAYED}/${playerId}?numberOfGames=${pageSize}`);
 
 export const getGamesByProviderGameNames = (
-  { platform, country, variant, providerGameNames },
-  http = defaultHttp
+  {
+    platform,
+    country,
+    variant,
+    providerGameNames,
+  }: {
+    platform: "mobile" | string,
+    country: string,
+    variant: "default" | string,
+    providerGameNames: string,
+  },
+  http: HTTPClient = defaultHttp
 ) =>
   http.get(`${URL.GAMES_BY_PROVIDER}/${platform}/${country}`, {
     variant,
@@ -42,19 +67,48 @@ export const getGamesByProviderGameNames = (
   });
 
 export const getGamesBySlugs = (
-  { platform, country, variant, slugs },
-  http = defaultHttp
+  {
+    platform,
+    country,
+    variant,
+    slugs,
+  }: {
+    platform: "mobile" | string,
+    country: string,
+    variant: "default" | string,
+    slugs: string,
+  },
+  http: HTTPClient = defaultHttp
 ) =>
   http.get(`${URL.GAMES_BY_SLUGS}/${platform}/${country}`, {
     variant,
     slugs,
   });
 
-export const getLiveCasinoTable = ({ ids, currency }, http = defaultHttp) =>
+export const getLiveCasinoTable = (
+  { ids, currency }: { ids: Array<string>, currency: string },
+  http: HTTPClient = defaultHttp
+) =>
   http.get(`${URL.LIVE_CASINO}`, {
     // Even if it is an array we need to use "id"
     // as a key here, as this is how it should be transformed in the URL in the end:
     // ?id[]=123&id[]=456&id[]=789
     id: ids,
     currency,
+  });
+
+export const getAllLiveCasinoGames = (
+  {
+    country,
+    platform = "mobile",
+    variant = "default",
+  }: {
+    country: string,
+    platform?: "mobile" | string,
+    variant?: "default" | string,
+  },
+  http: HTTPClient = defaultHttp
+) =>
+  http.get(`${URL.GAME_LISTS}/${platform}/${country}/liveCasinoGamesFull`, {
+    variant,
   });
