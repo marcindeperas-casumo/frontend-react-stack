a457b538e8c4d064fab1c0c4003fd651b1ebad39
João Guedes <joao.guedes@casumo.com>
2019-03-05 12:15:33 +0000

    added skeleton for TileListHorizontal

---
+++ src/components/TileListHorizontalSkeleton/TileListHorizontalSkeleton.js
@@ -0,0 +1,54 @@
+//@flow
+import React, { PureComponent } from "react";
+import { times } from "ramda";
+import Skeleton from "@casumo/cmp-skeleton";
+
+type Props = {
+  /* Additional css classes to add to the component **/
+  className: string,
+  /* The number of tile components to show **/
+  numberOfItems: number,
+  /* radius of tile **/
+  tileRadius: number,
+  /* gutter between each tile **/
+  tileGutter: number,
+};
+
+const defaultClassNames = `
+  u-padding-top--lg
+  u-padding-top--xlg@tablet
+  u-padding-top--xlg@desktop
+  u-padding-left--md
+  u-padding-left--2xlg@tablet
+  u-padding-left--2xlg@desktop
+`;
+
+export default class TileListHorizontalSkeleton extends PureComponent<Props> {
+  static defaultProps = {
+    className: defaultClassNames,
+    numberOfItems: 5,
+    tileRadius: 80,
+    tileGutter: 10,
+  };
+
+  render() {
+    const { className, numberOfItems, tileRadius, tileGutter } = this.props;
+
+    return (
+      <Skeleton className={className} width="100%" height="240">
+        <rect x="0" y="0" rx="8" ry="8" width="150" height="15" />
+        {times(
+          n => (
+            <circle
+              key={`tile-list-horizontal-skeleton-${n}`}
+              r="80"
+              cx={(tileRadius * 2 + tileGutter) * n + tileRadius}
+              cy="120"
+            />
+          ),
+          numberOfItems
+        )}
+      </Skeleton>
+    );
+  }
+}
