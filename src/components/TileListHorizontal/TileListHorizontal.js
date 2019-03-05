b6a8c002a7d71022c1e6ff4d0a6fdaa715fd07ac
João Guedes <joao.guedes@casumo.com>
2019-03-05 12:17:22 +0000

    added presentational TileListHorizontal component, plus stories and tests

---
+++ src/components/TileListHorizontal/TileListHorizontal.js
@@ -0,0 +1,77 @@
+// @flow
+import React, { PureComponent } from "react";
+import type { Element } from "react";
+import { isEmpty, map } from "ramda";
+import ScrollableListTitle from "Components/ScrollableListTitle";
+import Scrollable from "@casumo/cmp-scrollable";
+import Tile from "./Tile";
+import { objectToHash } from "Utils";
+import TileListHorizontalSkeleton from "Components/TileListHorizontalSkeleton/TileListHorizontalSkeleton";
+
+const PADDING_PER_DEVICE = {
+  default: "md",
+  tablet: "2xlg",
+  desktop: "2xlg",
+};
+
+const DEFAULT_SPACING = "default";
+const DEFAULT_TILE = Tile;
+
+type ItemObject = {
+  url: string,
+  logo: string,
+  background: string,
+};
+
+type Props = {
+  /** fetches data needed to populate the list */
+  fetch: () => void,
+  /** title and items to be rendered  */
+  title: string,
+  items: Array<ItemObject>,
+  isLoaded: boolean,
+  /** Tile to be rendered */
+  Tile: Element<any>,
+};
+
+class TileListHorizontal extends PureComponent<Props> {
+  static defaultProps = {
+    fetch: () => {},
+    title: "",
+    items: [],
+    isLoaded: false,
+    Tile: DEFAULT_TILE,
+  };
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { title, items, isLoaded, Tile } = this.props;
+
+    if (!isLoaded) {
+      return <TileListHorizontalSkeleton />;
+    }
+
+    if (isEmpty(items)) {
+      return null;
+    }
+
+    return (
+      <div className="u-padding-top--xlg">
+        <ScrollableListTitle title={title} />
+        <Scrollable padding={PADDING_PER_DEVICE} itemSpacing={DEFAULT_SPACING}>
+          {map(
+            item => (
+              <Tile key={objectToHash(item)} {...item} />
+            ),
+            items
+          )}
+        </Scrollable>
+      </div>
+    );
+  }
+}
+
+export default TileListHorizontal;
