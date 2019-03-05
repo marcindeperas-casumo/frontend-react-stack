909c3ad926b3a2ed3195cf0f898f77b683746234
Levente Balogh <balogh.levente.hu@gmail.com>
2019-03-05 18:23:45 +0100

    fix(Jackpots): use mocked objects in the tests

--- src/components/Jackpots/Jackpots.test.js
+++ src/components/Jackpots/Jackpots.test.js
@@ -1,25 +1,24 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import Jackpots from "Components/Jackpots/Jackpots";
+import jackpotsMock from "./__mocks__/response.games.mock";
 
 describe("<Jackpots />", () => {
-  let jackpots = ["1", "2", "3", "4", "5", "6", "7"];
-
   test("renders a <ScrollableListTitle /> component", () => {
-    const rendered = shallow(<Jackpots jackpots={jackpots} />);
+    const rendered = shallow(<Jackpots jackpots={jackpotsMock} />);
 
     expect(rendered.find("ScrollableListTitle").length).toBe(1);
   });
 
   test("renders tiles for every 3 game", () => {
-    const rendered = shallow(<Jackpots jackpots={jackpots} />);
+    const rendered = shallow(<Jackpots jackpots={jackpotsMock} />);
 
-    expect(rendered.find("JackpotsColumn").length).toBe(3);
+    expect(rendered.find("JackpotsColumn").length).toBe(6);
   });
 
   test("renders all the jackpot rows", () => {
-    const rendered = mount(<Jackpots jackpots={jackpots} />);
+    const rendered = mount(<Jackpots jackpots={jackpotsMock} />);
 
-    expect(rendered.find("GameRow")).toHaveLength(7);
+    expect(rendered.find("GameRow")).toHaveLength(17);
   });
 });
