ff538ab04e597c2e5c2a1d422c39d6bc06d735b2
Cho <choishere@gmail.com>
2019-03-05 19:36:44 +0100

    rename clearSearchResultsSaga

--- src/models/gameSearch/gameSearch.saga.clear.test.js
+++ src/models/gameSearch/gameSearch.saga.clear.test.js
@@ -1,17 +1,17 @@
 import { ENTITY_KEYS, normalizeData, updateEntity } from "Models/schema";
 import { call, put } from "redux-saga/effects";
-import { clearSearchSaga } from "Models/gameSearch";
+import { clearSearchResultsSaga } from "Models/gameSearch";
 import { GAME_LIST_IDS } from "Src/constants";
 
 describe("Models/gameSearch/clearSearchResultsSaga", () => {
-  test("clearSearchSaga clears games array gameList entity with id GameSearch", () => {
+  test("clearSearchResultsSaga clears games array gameList entity with id GameSearch", () => {
     const entities = {
       [ENTITY_KEYS.GAME_LIST]: {
         id: GAME_LIST_IDS.GAME_SEARCH,
         games: [],
       },
     };
-    const generator = clearSearchSaga();
+    const generator = clearSearchResultsSaga();
 
     expect(generator.next().value).toEqual(call(normalizeData, entities));
 
