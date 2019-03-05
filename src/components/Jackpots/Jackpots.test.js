d2a53a0f5c3c9069d0242e07f648f3b7520acdd9
Levente Balogh <balogh.levente.hu@gmail.com>
2019-03-05 17:40:01 +0100

    tests(Jackpots): remove coupling between test-cases

--- src/components/Jackpots/Jackpots.test.js
+++ src/components/Jackpots/Jackpots.test.js
@@ -3,24 +3,22 @@ import { shallow, mount } from "enzyme";
 import Jackpots from "Components/Jackpots/Jackpots";
 
 describe("<Jackpots />", () => {
-  let rendered;
-  let jackpots;
-
-  beforeEach(() => {
-    jackpots = ["1", "2", "3", "4", "5", "6", "7"];
-    rendered = shallow(<Jackpots jackpots={jackpots} />);
-  });
+  let jackpots = ["1", "2", "3", "4", "5", "6", "7"];
 
   test("renders a <ScrollableListTitle /> component", () => {
+    const rendered = shallow(<Jackpots jackpots={jackpots} />);
+
     expect(rendered.find("ScrollableListTitle").length).toBe(1);
   });
 
   test("renders tiles for every 3 game", () => {
+    const rendered = shallow(<Jackpots jackpots={jackpots} />);
+
     expect(rendered.find("JackpotsColumn").length).toBe(3);
   });
 
   test("renders all the jackpot rows", () => {
-    rendered = mount(<Jackpots jackpots={jackpots} />);
+    const rendered = mount(<Jackpots jackpots={jackpots} />);
 
     expect(rendered.find("GameRow")).toHaveLength(7);
   });
