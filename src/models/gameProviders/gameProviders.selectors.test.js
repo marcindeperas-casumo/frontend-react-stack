b30f1d6e317cb76a69c190ef5515413f5e7b2b27
João Guedes <joao.guedes@casumo.com>
2019-03-05 17:00:48 +0000

    wrapped game providers selectors tests in describe function

--- src/models/gameProviders/gameProviders.selectors.test.js
+++ src/models/gameProviders/gameProviders.selectors.test.js
@@ -5,119 +5,121 @@ import {
   areGameProvidersLoaded,
 } from "./gameProviders.selectors";
 
-test("areGameProvidersLoaded", () => {
-  const state = {
-    schema: {
-      gameProvider: {
-        "casumo-services-ltd-nyx": {
-          inMaintenance: false,
+describe("Game Providers Selectors", () => {
+  test("areGameProvidersLoaded", () => {
+    const state = {
+      schema: {
+        gameProvider: {
+          "casumo-services-ltd-nyx": {
+            inMaintenance: false,
+          },
         },
       },
-    },
-  };
+    };
 
-  expect(areGameProvidersLoaded(state)).toEqual(true);
-});
+    expect(areGameProvidersLoaded(state)).toEqual(true);
+  });
 
-test("areGameProvidersLoaded", () => {
-  const state = {
-    schema: {
-      gameProvider: {},
-    },
-  };
+  test("areGameProvidersLoaded", () => {
+    const state = {
+      schema: {
+        gameProvider: {},
+      },
+    };
 
-  expect(areGameProvidersLoaded(state)).toEqual(false);
-});
+    expect(areGameProvidersLoaded(state)).toEqual(false);
+  });
 
-test("activeGameProvidersSelector", () => {
-  const state = {
-    schema: {
-      gameProvider: {
-        "casumo-services-ltd-nyx": {
-          inMaintenance: false,
-        },
-        "casumo-services-ltd-netent": {
-          inMaintenance: true,
+  test("activeGameProvidersSelector", () => {
+    const state = {
+      schema: {
+        gameProvider: {
+          "casumo-services-ltd-nyx": {
+            inMaintenance: false,
+          },
+          "casumo-services-ltd-netent": {
+            inMaintenance: true,
+          },
         },
       },
-    },
-  };
+    };
 
-  expect(activeGameProvidersSelector(state)).toEqual({
-    "casumo-services-ltd-nyx": {
+    expect(activeGameProvidersSelector(state)).toEqual({
+      "casumo-services-ltd-nyx": {
+        inMaintenance: false,
+      },
+    });
+  });
+
+  test("gameProviderBySlug", () => {
+    const state = {
+      schema: {
+        gameProvider: {
+          "casumo-services-ltd-nyx": {
+            id: 1,
+            inMaintenance: false,
+          },
+          "casumo-services-ltd-netent": {
+            id: 2,
+            inMaintenance: false,
+          },
+        },
+      },
+    };
+
+    expect(gameProviderBySlug("casumo-services-ltd-nyx")(state)).toEqual({
+      id: 1,
       inMaintenance: false,
-    },
+    });
   });
-});
 
-test("gameProviderBySlug", () => {
-  const state = {
-    schema: {
-      gameProvider: {
-        "casumo-services-ltd-nyx": {
-          id: 1,
-          inMaintenance: false,
-        },
-        "casumo-services-ltd-netent": {
-          id: 2,
-          inMaintenance: false,
+  test("gameProviderBySlug", () => {
+    const state = {
+      schema: {
+        gameProvider: {
+          "casumo-services-ltd-nyx": {
+            id: 1,
+            inMaintenance: false,
+          },
         },
       },
-    },
-  };
+    };
 
-  expect(gameProviderBySlug("casumo-services-ltd-nyx")(state)).toEqual({
-    id: 1,
-    inMaintenance: false,
+    expect(gameProviderBySlug("foo")(state)).toEqual({});
   });
-});
 
-test("gameProviderBySlug", () => {
-  const state = {
-    schema: {
-      gameProvider: {
-        "casumo-services-ltd-nyx": {
-          id: 1,
-          inMaintenance: false,
+  test("areProviderGamesLoaded", () => {
+    const state = {
+      schema: {
+        gameProvider: {
+          "casumo-services-ltd-nyx": {
+            id: 1,
+            games: [],
+            inMaintenance: false,
+          },
         },
       },
-    },
-  };
+    };
+    expect(areProviderGamesLoaded("casumo-services-ltd-nyx")(state)).toEqual(
+      false
+    );
+  });
 
-  expect(gameProviderBySlug("foo")(state)).toEqual({});
-});
-
-test("areProviderGamesLoaded", () => {
-  const state = {
-    schema: {
-      gameProvider: {
-        "casumo-services-ltd-nyx": {
-          id: 1,
-          games: [],
-          inMaintenance: false,
+  test("areProviderGamesLoaded", () => {
+    const state = {
+      schema: {
+        gameProvider: {
+          "casumo-services-ltd-nyx": {
+            id: 1,
+            games: ["foo"],
+            inMaintenance: false,
+          },
         },
       },
-    },
-  };
-  expect(areProviderGamesLoaded("casumo-services-ltd-nyx")(state)).toEqual(
-    false
-  );
-});
+    };
 
-test("areProviderGamesLoaded", () => {
-  const state = {
-    schema: {
-      gameProvider: {
-        "casumo-services-ltd-nyx": {
-          id: 1,
-          games: ["foo"],
-          inMaintenance: false,
-        },
-      },
-    },
-  };
-
-  expect(areProviderGamesLoaded("casumo-services-ltd-nyx")(state)).toEqual(
-    true
-  );
+    expect(areProviderGamesLoaded("casumo-services-ltd-nyx")(state)).toEqual(
+      true
+    );
+  });
 });
