3901abcc78c19a53f435a7e9a250cf3dd123bfb4
Cho <choishere@gmail.com>
2019-03-05 10:55:20 +0100

    fix reducer

--- src/models/playerGames/playerGames.reducer.js
+++ src/models/playerGames/playerGames.reducer.js
@@ -1,20 +1,20 @@
 import { types } from "./playerGames.constants";
 
-export const playerGames = (state, action) => {
-  if (typeof state === "undefined") {
-    return {
-      count: 0,
-    };
-  }
+const defaultState = { count: 0 };
 
-  if (action.type === types.PLAYER_GAMES_FETCH_COUNT_COMPLETE) {
-    return {
-      ...state,
-      count: action.response,
-    };
-  }
+export const playerGames = (state = defaultState, action) => {
+  switch (action.type) {
+    case types.PLAYER_GAMES_FETCH_COUNT_COMPLETE: {
+      return {
+        ...state,
+        count: action.response,
+      };
+    }
 
-  return state;
+    default: {
+      return state;
+    }
+  }
 };
 
 export default playerGames;
