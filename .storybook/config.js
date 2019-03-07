3b5859e2f56a2b68a1c805bf80d73eda1d9df72c
Jack Westbrook <jack.westbrook@casumo.com>
2019-03-07 15:18:13 +0100

    feat(Storybook): Configure to use new casumo theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { configure, addDecorator } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { withInfo } from "@storybook/addon-info";
 import { withBackgrounds } from "@storybook/addon-backgrounds";
 import "storybook-chromatic";
+import casumoTheme from "./casumoTheme";
 
 import "./stories.scss";
 import "../src/styles/index.scss";
-
-const req = require.context("../src", true, /.stories.js$/);
 
 // Prevent random errors with chromatic
 // TypeError: window.URL.createObjectURL is not a function
 // Chromatic will (eventually) solve this at there end.
 import polyfillWindowForChromatic from "Storybook/utils";
+
+const req = require.context("../src", true, /.stories.js$/);
 polyfillWindowForChromatic();
 
 function loadStories() {
@@ -36,5 +37,11 @@ addDecorator(story => (
     <div className="o-wrapper u-padding">{story()}</div>
   </>
 ));
+
+addParameters({
+  options: {
+    theme: casumoTheme,
+  },
+});
 
 configure(loadStories, module);
