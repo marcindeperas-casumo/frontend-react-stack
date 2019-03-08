90fe403e8300ed85660db7df8338f8610f30c1f1
João Guedes <joao.guedes@casumo.com>
2019-03-08 13:48:43 +0000

    using state mock on ProviderGamesList stories

--- src/components/ProviderGamesList/ProviderGamesList.stories.js
+++ src/components/ProviderGamesList/ProviderGamesList.stories.js
@@ -6,11 +6,19 @@ import ProviderGamesList from "Components/ProviderGamesList";
 import ProviderGamesListPresentational from "Components/ProviderGamesList/ProviderGamesList";
 import MockStore from "Components/MockStore";
 import isNotChromatic from "Storybook/isNotChromatic";
-
+import defaultState from "Models/__mocks__/state.mock";
 const stories = storiesOf("ProviderGamesList", module);
 
-const games = ["bloodsuckers", "easter-island"];
-
+const games = [
+  "bloodsuckers",
+  "easter-island",
+  "starburst",
+  "bakers-treat",
+  "rapunzels-tower",
+  "big-bad-wolf",
+  "book-of-ra-deluxe",
+  "diamond-mine",
+];
 const provider = {
   inMaintenance: false,
   name: "nyx",
@@ -18,58 +26,10 @@ const provider = {
   games,
 };
 
-const game = {
-  game: {
-    [games[0]]: {
-      name: "Blood Suckers",
-      slug: games[0],
-      logoBackground:
-        "https://cms.casumo.com/wp-content/uploads/2014/06/BloodSuckers_Thumb.jpg",
-      logo:
-        "https://cms.casumo.com/wp-content/uploads/2014/02/BloodSuckers_Logo.png",
-      hasPlayForFun: true,
-      inMaintenanceMode: false,
-      jackpotId: null,
-      tableId: null,
-    },
-    [games[1]]: {
-      name: "Easter Island",
-      slug: games[1],
-      logoBackground:
-        "https://cms.casumo.com/wp-content/uploads/2018/03/easter_island_thumbnail.jpg",
-      logo:
-        "https://cms.casumo.com/wp-content/uploads/2018/03/easter_island_logo.png",
-      hasPlayForFun: true,
-      inMaintenanceMode: false,
-      jackpotId: null,
-      tableId: null,
-    },
-  },
-};
-
-const state = {
-  schema: {
-    gameProvider: {
-      [provider.slug]: {
-        id: "0c900240-4904-11e6-a7a2-005056a975b1",
-        name: "nyx",
-        inMaintenance: false,
-        slug: provider.slug,
-        background:
-          "https://cms.casumo.com/wp-content/uploads/2019/02/nyx-bg.png",
-        logo: "https://cms.casumo.com/wp-content/uploads/2019/02/nyx.png",
-        url: "/en/games/provider/nyx",
-        games,
-      },
-    },
-    ...game,
-  },
-};
-
 stories.add(
   "ProviderGamesList (Presentational)",
   () => (
-    <MockStore state={{ schema: { game } }}>
+    <MockStore state={{ game: defaultState.schema.game }}>
       <ProviderGamesListPresentational isLoaded={true} provider={provider} />
     </MockStore>
   ),
@@ -80,7 +40,7 @@ if (isNotChromatic) {
   stories.add(
     "ProviderGamesList (Connected)",
     () => (
-      <MockStore state={state}>
+      <MockStore state={defaultState}>
         <ProviderGamesList provider={provider.slug} />
       </MockStore>
     ),
