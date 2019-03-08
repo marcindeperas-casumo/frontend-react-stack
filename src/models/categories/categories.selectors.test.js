68a3ba3aa90ef5678b50b0286698a9bf34752189
João Guedes <joao.guedes@casumo.com>
2019-03-08 12:05:30 +0000

    added selector and test

--- src/models/categories/categories.selectors.test.js
+++ src/models/categories/categories.selectors.test.js
@@ -1,35 +1,69 @@
-import { gameProvidersListSelector } from "./categories.selectors";
+import {
+  gameProvidersListSelector,
+  gameProvidersRoute,
+} from "./categories.selectors";
 
 describe("Category List Selectors", () => {
-  describe("gameProvidersListSelector", () => {
-    test("should transform game provider data correctly", () => {
-      const state = {
-        schema: {
-          gameProvider: {
-            "casumo-services-ltd-nyx": {
-              id: "0c900240-4904-11e6-a7a2-005056a975b1",
-              name: "nyx",
-              inMaintenance: false,
-              slug: "casumo-services-ltd-nyx",
-              background:
-                "https://cms.casumo.com/wp-content/uploads/2019/02/nyx-bg.png",
-              logo: "https://cms.casumo.com/wp-content/uploads/2019/02/nyx.png",
+  test("gameProvidersRoute", () => {
+    const state = {
+      handshake: {
+        app: {
+          "common/composition/session": { id: "p1" },
+          "common/composition/players": {
+            players: {
+              p1: {
+                id: "p1",
+                contactInfo: { primaryAddress: { country: "mt" } },
+              },
             },
           },
         },
-      };
+      },
+    };
 
-      const output = [
-        {
-          id: "game-provider-casumo-services-ltd-nyx",
-          background:
-            "https://cms.casumo.com/wp-content/uploads/2019/02/nyx-bg.png",
-          logo: "https://cms.casumo.com/wp-content/uploads/2019/02/nyx.png",
-          url: "/en/games/provider/casumo-services-ltd-nyx",
+    expect(gameProvidersRoute(state)).toBe("/mt/games/provider");
+  });
+
+  test("gameProvidersListSelector", () => {
+    const state = {
+      handshake: {
+        app: {
+          "common/composition/session": { id: "p1" },
+          "common/composition/players": {
+            players: {
+              p1: {
+                id: "p1",
+                contactInfo: { primaryAddress: { country: "mt" } },
+              },
+            },
+          },
         },
-      ];
+      },
+      schema: {
+        gameProvider: {
+          "casumo-services-ltd-nyx": {
+            id: "0c900240-4904-11e6-a7a2-005056a975b1",
+            name: "nyx",
+            inMaintenance: false,
+            slug: "casumo-services-ltd-nyx",
+            background:
+              "https://cms.casumo.com/wp-content/uploads/2019/02/nyx-bg.png",
+            logo: "https://cms.casumo.com/wp-content/uploads/2019/02/nyx.png",
+          },
+        },
+      },
+    };
 
-      expect(gameProvidersListSelector(state)).toEqual(output);
-    });
+    const output = [
+      {
+        id: "game-provider-casumo-services-ltd-nyx",
+        background:
+          "https://cms.casumo.com/wp-content/uploads/2019/02/nyx-bg.png",
+        logo: "https://cms.casumo.com/wp-content/uploads/2019/02/nyx.png",
+        url: "/mt/games/provider/casumo-services-ltd-nyx",
+      },
+    ];
+
+    expect(gameProvidersListSelector(state)).toEqual(output);
   });
 });
