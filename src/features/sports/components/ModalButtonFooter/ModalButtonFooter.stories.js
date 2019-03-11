c2b01cb950533d904e5dc5acc85327c826b38ab4
Charles Oliver <charles@cocoa.consulting>
2019-03-11 10:38:20 +0000

    remeove unused story

--- src/features/sports/components/ModalButtonFooter/ModalButtonFooter.stories.js
+++ src/features/sports/components/ModalButtonFooter/ModalButtonFooter.stories.js
@@ -7,26 +7,12 @@ import ModalButtonFooter from "./ModalButtonFooter";
 
 const stories = storiesOf("Sports/ModalButtonFooter", module);
 
-stories
-  .addParameters({ viewport: { defaultViewport: "iphone6" } })
-  .add(
-    "Phone (default)",
-    () => (
-      <ModalButtonFooter onClick={action("onClick")}>
-        Modal button footer
-      </ModalButtonFooter>
-    ),
-    info({ text: "Default" })
-  );
-
-stories
-  .addParameters({ viewport: { defaultViewport: "ipad" } })
-  .add(
-    "Tablet",
-    () => (
-      <ModalButtonFooter onClick={action("onClick")}>
-        Modal button footer
-      </ModalButtonFooter>
-    ),
-    info({ text: "Tablet" })
-  );
+stories.add(
+  "Default View",
+  () => (
+    <ModalButtonFooter onClick={action("onClick")}>
+      Modal button footer
+    </ModalButtonFooter>
+  ),
+  info({ text: "Default" })
+);
