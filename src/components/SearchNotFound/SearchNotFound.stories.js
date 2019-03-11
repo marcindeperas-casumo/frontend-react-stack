ab43659c215150fcebe9704f658d7d3215261034
Michele Settepani <michele.settepani@casumo.com>
2019-03-11 10:35:37 +0100

    SearchNotFound.stories: image as a const

--- src/components/SearchNotFound/SearchNotFound.stories.js
+++ src/components/SearchNotFound/SearchNotFound.stories.js
@@ -7,14 +7,16 @@ import SearchNotFoundContainer from "Components/SearchNotFound";
 import MockStore from "Components/MockStore";
 
 const stories = storiesOf("SearchNotFound", module);
+const noResultImage =
+  "https://cms.casumo.com/wp-content/uploads/2019/01/search_no_results.svg";
+
 const state = {
   schema: {
     cms: {
       "mobile.games-search": {
         fields: {
           no_results_title: "No results found",
-          no_results_image:
-            "https://cms.casumo.com/wp-content/uploads/2019/01/search_no_results.svg",
+          no_results_image: noResultImage,
         },
       },
     },
@@ -38,9 +40,7 @@ stories.add(
   () => (
     <div style={{ maxWidth: 360, margin: "0 auto", overflow: "hidden" }}>
       <SearchNotFound
-        image={
-          "https://cms.casumo.com/wp-content/uploads/2019/01/search_no_results.svg"
-        }
+        image={noResultImage}
         title="No results found"
         content="Find another game or continue playing <br /> your last played"
         startFetch={() => {}}
@@ -55,9 +55,7 @@ stories.add(
   () => (
     <div style={{ maxWidth: 360, margin: "0 auto", overflow: "hidden" }}>
       <SearchNotFound
-        image={
-          "https://cms.casumo.com/wp-content/uploads/2019/01/search_no_results.svg"
-        }
+        image={noResultImage}
         title="No results found"
         content="Find another game or try something popular"
         startFetch={() => {}}
