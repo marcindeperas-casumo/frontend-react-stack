e56ebed251fb4bcf5e3a7970b746d6cec48fd314
Dave Calleja <dave.calleja@gmail.com>
2019-03-11 21:14:56 +0100

    Removed unnecessary test

--- src/components/CasumoAvatar/CasumoAvatar.test.js
+++ src/components/CasumoAvatar/CasumoAvatar.test.js
@@ -10,11 +10,4 @@ describe("CasumoAvatar", () => {
 
     expect(rendered.find(`.${classModifier}`).length).toBe(1);
   });
-  test("Color class defaults to lowest level if unknown level is passed as prop", () => {
-    const belt = "erroneous value";
-    const defaultClassModifier = getClassModifier("rope");
-    const rendered = shallow(<CasumoAvatar belt={belt} />);
-
-    expect(rendered.find(`.${defaultClassModifier}`).length).toBe(1);
-  });
 });
