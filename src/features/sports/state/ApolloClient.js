365424efe5b31ce14207a02ab0e7c6a7c3c0ff5e
Adam Pilkington <adampilks@gmail.com>
2019-03-11 12:09:12 +0100

    Removing error link as we are already tracking errors server side, and network errors are tracked by rollbacl Casumo/Home#27259

--- src/features/sports/state/ApolloClient.js
+++ src/features/sports/state/ApolloClient.js
@@ -4,12 +4,10 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { setContext } from "apollo-link-context";
-import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
 import { ApolloProvider } from "react-apollo";
 import { withClientState } from "apollo-link-state";
 import { createPersistedQueryLink } from "apollo-link-persisted-queries";
-
 import { clientState } from "./clientState";
 
 type Props = {
@@ -74,21 +72,10 @@ export class SportsStateProvider extends React.Component<Props> {
       };
     });
 
-    const errorLink = onError(({ graphQLErrors, networkError }) => {
-      // TODO:(adampilks) - use proper logging
-      if (graphQLErrors) {
-        console.log("GraphQL error", graphQLErrors);
-      }
-      if (networkError) {
-        console.log("Network error", networkError);
-      }
-    });
-
     // eslint-disable-next-line fp/no-mutation
     this.sportsStateClient = new ApolloClient<InMemoryCache>({
       link: ApolloLink.from([
         persistedQueryLink,
-        errorLink,
         contextLink,
         clientStateLink,
         httpLink,
