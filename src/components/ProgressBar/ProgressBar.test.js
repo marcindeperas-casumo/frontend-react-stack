f1d3ce5d382a903a5a1a6d12dfdb705178c4febd
Dave Calleja <dave.calleja@gmail.com>
2019-03-12 07:41:52 +0100

    Fixed test

--- src/components/ProgressBar/ProgressBar.test.js
+++ src/components/ProgressBar/ProgressBar.test.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import ProgressBar from "./ProgressBar";
 
 describe("ProgressBar", () => {
   test("Sets width to percentage equal to prop", () => {
-    const rendered = shallow(<ProgressBar progress={50} />);
-    const style = rendered.find(".c-progress-bar__filler").get(0).style;
-    expect(style).to.have.property("width", "50%");
+    const progress = 50;
+    const rendered = mount(<ProgressBar progress={progress} />);
+    const style = rendered.find(".c-progress-bar__filler").get(0).props.style;
+    expect(style.width).toEqual(`${progress}%`);
   });
 });
