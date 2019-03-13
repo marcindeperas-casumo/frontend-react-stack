fd0c7ca0dc2bdd42a354a2024a8f7137cf2509b7
Charles Oliver <charles@cocoa.consulting>
2019-03-13 17:53:24 +0000

    jacking it up

--- src/features/sports/components/SportsIcon/SportsIcon.js
+++ src/features/sports/components/SportsIcon/SportsIcon.js
@@ -12,21 +12,24 @@ type Props = {
 };
 
 const SportsIcon = ({ iconSrc, activeIndicatorSrc, isActive }: Props) => (
-  <Flex.Item className="c-sports-icon">
+  <Flex.Item className="o-ratio c-sports-icon">
     {isActive && (
       <img
         alt="sport is active"
-        className="c-sports-icon__active-indicator u-position-absolute"
+        className="o-ratio__content"
         data-test="sports-icon-active-indicator"
         src={activeIndicatorSrc}
+        width={48}
       />
     )}
-    <img
-      alt="sport icon"
-      className="c-sports-icon__icon"
-      data-test="sports-icon-icon"
-      src={iconSrc}
-    />
+    <Flex justify="center" align="center" className="o-ratio__content">
+      <img
+        alt="sport icon"
+        data-test="sports-icon-icon"
+        src={iconSrc}
+        width={32}
+      />
+    </Flex>
   </Flex.Item>
 );
 
