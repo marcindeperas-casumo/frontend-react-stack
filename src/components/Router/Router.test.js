3a729b3b5fa6052ce6c0d4c478eaf33204029046
Levente Balogh <balogh.levente.hu@gmail.com>
2019-03-13 15:54:42 +0100

    fix(Router): fix test-case names

--- src/components/Router/Router.test.js
+++ src/components/Router/Router.test.js
@@ -3,7 +3,7 @@ import { shallow } from "enzyme";
 import { Router, Route } from "Components/Router";
 
 describe("Router", () => {
-  test("displays only children that should be visible", () => {
+  test("only renders the routes that are active", () => {
     const fooComponent = <Route path="foo" />;
     const barComponent = <Route path="bar" />;
     const component = shallow(
@@ -17,7 +17,7 @@ describe("Router", () => {
     expect(component.contains(barComponent)).toBe(false);
   });
 
-  test("supports an array as path", () => {
+  test("accepts multiple active paths for a single route", () => {
     const fooComponent = <Route path={["foo", "baz"]} />;
     const barComponent = <Route path="bar" />;
     const component = shallow(
@@ -31,7 +31,7 @@ describe("Router", () => {
     expect(component.contains(barComponent)).toBe(false);
   });
 
-  test("array path with one element", () => {
+  test("accepts a single path even if it is passed in as an array", () => {
     const fooComponent = <Route path={["foo"]} />;
     const barComponent = <Route path="bar" />;
     const component = shallow(
