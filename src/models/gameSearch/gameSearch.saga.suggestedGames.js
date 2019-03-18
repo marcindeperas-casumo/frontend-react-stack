0efadecdf18d4c16520a7eacab0b9d8471e79591
Michele Settepani <michele.settepani@casumo.com>
2019-03-18 10:29:47 +0100

    gameSearch.saga: fetch games lists only if not fetched yet

--- src/models/gameSearch/gameSearch.saga.suggestedGames.js
+++ src/models/gameSearch/gameSearch.saga.suggestedGames.js
@@ -5,8 +5,10 @@ import {
   fetchGamesHandshake,
   isGamesHandshakeLoaded,
   gamesHandshakeSelector,
+  types as handshakeTypes,
 } from "Models/handshake";
 import { waitForSelector } from "Utils";
+import { isFetchingStarted } from "Models/fetch";
 import { types, fetchSuggestedGamesAction } from "Models/gameSearch";
 import { GAME_LIST_IDS } from "Src/constants";
 
@@ -15,9 +17,14 @@ export function* fetchSuggestedGamesSaga(gameLookingForSuggestions) {
   const variant = "default";
   const country = yield select(countrySelector);
   const id = GAME_LIST_IDS.SUGGESTED_GAMES;
+  const areGamesFetched = yield select(
+    isFetchingStarted(handshakeTypes.FETCH_GAMES_HANDSHAKE)
+  );
 
   // We need the Games Lists for fetchSuggestedGamesAction to work
-  yield put(fetchGamesHandshake({ country }));
+  if (!areGamesFetched) {
+    yield put(fetchGamesHandshake({ country }));
+  }
 
   // Wait for the Games Lists handshake to be loaded before running the rest
   yield call(waitForSelector, isGamesHandshakeLoaded);
