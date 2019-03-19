24a95e966d180f5ada752558544704ef701af240
Michał Mokijewski <hello@naf.gl>
2019-03-19 22:22:22 +0100

    refactor rollbar integration
    Issue:
    
    We were getting issues from outside of react-stack and issues with
    incomplete informations.
    
    Previous implementation:
    
    Rollbar wasn't automatically catching uncaught errors and unhandled
    rejections. We had error listener on window that was checking if error
    originated in react-stack and those that didn't were ignored. At least
    that's the theory. Other uses of logger didn't have that check and catch
    block could catch issue that wasn't belonging to react-stack.
    
    Solution:
    
    I decided that giving rollbar power to catch all uncaught and unhandled
    thingies and adding check if error should be send to the config was best
    solution. It allows us to have the same check for all reported errors no
    matter where they are coming from.

--- src/applicationService/logger.js
+++ src/applicationService/logger.js
@@ -1,16 +1,31 @@
-import { getEnv } from "Utils";
-import { ENVS } from "Src/constants";
-import loggerNull from "Lib/logger.adapter.null";
-import loggerConsole from "Lib/logger.adapter.console";
-import loggerRollbar from "Lib/logger.adapter.rollbar";
+// @flow
+import Rollbar from "rollbar";
 
-export function getLogger(env, loggers) {
-  return loggers[env] || loggers[ENVS.DEVELOPMENT];
+function checkIgnore(isUncaught, args, payload) {
+  /**
+   * looks like all uncaught exceptions contain error object as second argument
+   * based on that we can get raw stacktrace, split it on line break and check
+   * if second line contains react-stack in path (first line contains error msg)
+   */
+  if (isUncaught && /react-stack/.test(args[1].stack.split("\n")[1])) {
+    return true;
+  }
+  return false;
 }
 
-export default getLogger(getEnv(), {
-  [ENVS.DEVELOPMENT]: loggerConsole,
-  [ENVS.PRODUCTION]: loggerRollbar,
-  [ENVS.AUTOMATED_TESTS]: loggerNull,
-  [ENVS.TEST]: loggerConsole,
+const logger = new Rollbar({
+  accessToken: "2a7d7f810245499e9250ee718205429a",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: process.env.NODE_ENV,
+    context: "react-stack",
+  },
+  autoInstrument: !__DEV__,
+  includeItemsInTelemetry: true,
+  verbose: __DEV__,
+  enabled: !__DEV__,
+  checkIgnore,
 });
+
+export default logger;
