58b46b9c03a67473ed5026fff3ad80f4957ceb98
Cho <choishere@gmail.com>
2019-03-19 17:32:37 +0100

    update search skeleton

--- src/components/SearchInput/SearchInputSkeleton.js
+++ src/components/SearchInput/SearchInputSkeleton.js
@@ -4,11 +4,9 @@ import Skeleton from "@casumo/cmp-skeleton";
 export default class SearchInputSkeleton extends PureComponent {
   render() {
     return (
-      <div>
-        <Skeleton viewBox={null} width="100%" height={72}>
-          <rect x="0" y="0" rx="0" ry="0" width="100%" height="100%" />
-        </Skeleton>
-      </div>
+      <Skeleton viewBox={null} width="100%" height={72}>
+        <rect x="0" y="0" rx="0" ry="0" width="100%" height="100%" />
+      </Skeleton>
     );
   }
 }
