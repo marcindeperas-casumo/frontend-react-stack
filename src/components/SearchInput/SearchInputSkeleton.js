4f43315de113e7d43b7b32ca07a7a733fe8cf2ee
Cho <choishere@gmail.com>
2019-03-19 17:32:03 +0100

    update search skeleton

--- src/components/SearchInput/SearchInputSkeleton.js
+++ src/components/SearchInput/SearchInputSkeleton.js
@@ -6,7 +6,7 @@ export default class SearchInputSkeleton extends PureComponent {
     return (
       <div>
         <Skeleton viewBox={null} width="100%" height={72}>
-          <rect x="0" y="0" rx="0" ry="0" width="100%" height={72} />
+          <rect x="0" y="0" rx="0" ry="0" width="100%" height="100%" />
         </Skeleton>
       </div>
     );
