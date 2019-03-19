acebb33b6d2247d5ff7b4c46b8a753aa0fe8573e
Charles Oliver <charles@cocoa.consulting>
2019-03-19 13:21:02 +0000

    add error and loading stories for SportsNav, refactor mocks

--- src/features/sports/components/SportsNav/__mocks__/userNavigationQuery.js
+++ src/features/sports/components/SportsNav/__mocks__/userNavigationQuery.js
@@ -1,16 +1,24 @@
 // @flow
+import { assocPath } from "ramda";
 import { USER_NAVIGATION_QUERY } from "../SportsNav";
 import mockData from "./userNavigationData";
 
-export default [
-  {
-    request: {
-      query: USER_NAVIGATION_QUERY,
-    },
-    result: {
-      data: {
-        sportsNavigation: mockData,
-      },
+const baseMock = {
+  request: {
+    query: USER_NAVIGATION_QUERY,
+  },
+  result: {
+    data: {
+      sportsNavigation: mockData,
     },
   },
-];
+};
+
+export default {
+  multipleSports: [baseMock],
+  singleSport: [
+    assocPath(["result", "data", "sportsNavigation"], [mockData[0]], baseMock),
+  ],
+  error: [assocPath(["error"], true, baseMock)],
+  loading: [assocPath(["result"], undefined, baseMock)],
+};
