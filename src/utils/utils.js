0dbde39c5ca97308233fd9101ef7d020d19888f6
Levente Balogh <balogh.levente.hu@gmail.com>
2019-03-21 10:56:46 +0100

    feat(Utils): add function to create reducer

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -170,3 +170,17 @@ export const injectScript = (url: string) =>
       document.head.appendChild(script);
     }
   });
+
+type Handlers = {
+  [type: string]: (state: Object, action: Object) => any,
+};
+
+export const createReducer = (initialState: Object, handlers: Handlers) => {
+  return function reducer(state: Object = initialState, action: Object) {
+    if (handlers.hasOwnProperty(action.type)) {
+      return handlers[action.type](state, action);
+    } else {
+      return state;
+    }
+  };
+};
