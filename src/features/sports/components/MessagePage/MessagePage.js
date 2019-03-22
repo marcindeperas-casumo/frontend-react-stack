0f433842949bdc830d3e52430ff21682d37a77a8
Charles Oliver <charles@cocoa.consulting>
2019-03-22 18:24:45 +0000

    update styles to match spec

--- src/features/sports/components/MessagePage/MessagePage.js
+++ src/features/sports/components/MessagePage/MessagePage.js
@@ -4,6 +4,7 @@ import type { Node } from "react";
 import Flex from "@casumo/cmp-flex";
 import Text from "@casumo/cmp-text";
 import { DictionaryTerm } from "Features/sports/components/DictionaryTerm";
+import "./MessagePage.scss";
 
 type Props = {
   headingTermKey: string,
@@ -17,16 +18,20 @@ const MessagePage = ({ image, headingTermKey, messageTermKey }: Props) => (
     spacing="md"
     justify="center"
     align="center"
-    className="t-background-grey-light-2 o-flex--1 u-padding--lg t-color-grey-dark-4"
+    className="o-flex--1 t-background-grey-light-2 t-color-grey-dark-3 t-border-r--4 u-padding--lg c-message-page"
   >
     <Flex.Item>{image}</Flex.Item>
     <Flex.Item>
-      <Text tag="h2" size="4xlg" className="u-text-align-center">
+      <Text
+        tag="h2"
+        size="3xlg"
+        className="u-text-align-center c-message-page__heading"
+      >
         <DictionaryTerm termKey={headingTermKey} />
       </Text>
     </Flex.Item>
     <Flex.Item>
-      <Text className="u-text-align-center">
+      <Text size="md" className="u-text-align-center">
         <DictionaryTerm termKey={messageTermKey} />
       </Text>
     </Flex.Item>
