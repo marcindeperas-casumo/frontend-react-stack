102d49b3e658f8045f00d4e667b3bf89857d741e
Cho <choishere@gmail.com>
2019-03-28 14:06:11 +0100

    refactor api casino player api tests

--- src/api/api.casinoPlayerGames.test.js
+++ src/api/api.casinoPlayerGames.test.js
@@ -1,66 +1,81 @@
 import {
   getGameProviders,
+  getCasinoPlayerGames,
   getCasinoPlayerGamesCount,
   URL,
 } from "./api.casinoPlayerGames";
 
 describe("API/casinoPlayerGames", () => {
+  let http;
   const sessionId = "123";
+  const page = 0;
+  const pageSize = 100;
+  const providers = [];
+  const headers = {
+    headers: {
+      "X-Request-Features": null,
+      "X-Token": sessionId,
+    },
+  };
+
+  beforeEach(() => {
+    http = {
+      get: jest.fn(() => Promise.resolve([])),
+    };
+  });
 
   describe("getCasinoPlayerGamesCount()", () => {
     test("calls http.get() with the right URL for GAMES_COUNT (without params)", () => {
-      const http = { get: jest.fn(() => Promise.resolve([])) };
-
-      localStorage.setItem("showDisabledGames", "true");
       getCasinoPlayerGamesCount({ sessionId }, http);
 
       expect(http.get).toHaveBeenCalledTimes(1);
-      expect(http.get).toHaveBeenCalledWith(
-        URL.GAMES_COUNT,
-        {},
-        {
-          headers: {
-            "X-Request-Features": "HIDDEN_GAMES",
-            "X-Token": sessionId,
-          },
-        }
-      );
+      expect(http.get).toHaveBeenCalledWith(URL.GAMES_COUNT, {}, headers);
     });
 
     test("calls http.get() with the right URL for GAMES_COUNT (with params)", () => {
-      const http = { get: jest.fn(() => Promise.resolve([])) };
-
-      localStorage.setItem("showDisabledGames", "false");
       getCasinoPlayerGamesCount({ sessionId, providers: ["foo", "bar"] }, http);
 
       expect(http.get).toHaveBeenCalledTimes(1);
       expect(http.get).toHaveBeenCalledWith(
         URL.GAMES_COUNT,
         { providerSlugs: "foo,bar" },
-        {
-          headers: {
-            "X-Request-Features": null,
-            "X-Token": sessionId,
-          },
-        }
+        headers
       );
     });
   });
 
   describe("getGameProviders()", () => {
     test("calls http.get() with the right URL", () => {
-      const http = { get: jest.fn(() => Promise.resolve([])) };
-
-      localStorage.setItem("showDisabledGames", "false");
       getGameProviders({ sessionId }, http);
 
       expect(http.get).toHaveBeenCalledTimes(1);
+      expect(http.get).toHaveBeenCalledWith(URL.GAME_PROVIDERS, {}, headers);
+    });
+  });
+
+  describe("getCasinoPlayerGames()", () => {
+    test("calls http.get() with the right URL", () => {
+      getCasinoPlayerGames({ sessionId, page, pageSize, providers }, http);
+
+      expect(http.get).toHaveBeenCalledTimes(1);
       expect(http.get).toHaveBeenCalledWith(
-        URL.GAME_PROVIDERS,
-        {},
+        URL.GAMES,
+        { page, pageSize, providerSlugs: "" },
+        headers
+      );
+    });
+
+    test("calls http.get() with the right headers if showDisabledGames is present", () => {
+      localStorage.setItem("showDisabledGames", "true");
+      getCasinoPlayerGames({ sessionId, page, pageSize, providers }, http);
+
+      expect(http.get).toHaveBeenCalledTimes(1);
+      expect(http.get).toHaveBeenCalledWith(
+        URL.GAMES,
+        { page, pageSize, providerSlugs: "" },
         {
           headers: {
-            "X-Request-Features": null,
+            "X-Request-Features": "HIDDEN_GAMES",
             "X-Token": sessionId,
           },
         }
