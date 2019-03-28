2eaa2ab1187db316ff635d386a66f679abffd88c
Cho <choishere@gmail.com>
2019-03-28 13:44:25 +0100

    localStorageMock

--- config/jest/localStorageMock.js
+++ config/jest/localStorageMock.js
@@ -1,24 +1,23 @@
+/* eslint-disable fp/no-let,fp/no-mutation,fp/no-mutating-methods */
 const localStorageMock = (function() {
-  // eslint-disable-next-line fp/no-let
   let store = {};
   return {
     getItem: function(key) {
       return store[key] || null;
     },
     setItem: function(key, value) {
-      // eslint-disable-next-line fp/no-mutation
       store[key] = value.toString();
     },
     removeItem: function(key) {
       delete store[key];
     },
     clear: function() {
-      // eslint-disable-next-line fp/no-mutation
       store = {};
     },
   };
 })();
-// eslint-disable-next-line fp/no-mutating-methods
+
 Object.defineProperty(window, "localStorage", {
   value: localStorageMock,
 });
+/* eslint-enable fp/no-let,fp/no-mutation,fp/no-mutating-methods */
