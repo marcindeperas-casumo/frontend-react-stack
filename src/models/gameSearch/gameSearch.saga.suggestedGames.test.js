a2ce491e5f7750e514eecde5e3f7c6669ae8a914
Michele Settepani <michele.settepani@casumo.com>
2019-03-29 13:03:56 +0100

    suggestedGames.test: test !response.games

--- src/models/gameSearch/gameSearch.saga.suggestedGames.test.js
+++ src/models/gameSearch/gameSearch.saga.suggestedGames.test.js
@@ -10,6 +10,7 @@ import {
   types,
   fetchSuggestedGamesAction,
   fetchSuggestedGamesSaga,
+  fetchLatestPlayedSaga,
 } from "Models/gameSearch";
 import { waitForSelector } from "Utils";
 import { GAME_LIST_IDS } from "Src/constants";
@@ -81,5 +82,65 @@ describe("Models/GameSearch/Saga", () => {
     expect(gen.next({ entities }).value).toEqual(put(updateEntity(entities)));
 
     expect(gen.next().done).toBe(true);
+  });
+
+  test("fetchSuggestedGamesSaga - empty game suggestions response", () => {
+    const game = "starburst";
+    const gen = fetchSuggestedGamesSaga(game);
+
+    const platform = "mobile";
+    const id = GAME_LIST_IDS.SUGGESTED_GAMES;
+    const areGamesFetched = false;
+    const variant = "default";
+    const handshake = { foo: "bar" };
+
+    gen.next();
+    gen.next(countrySelector);
+
+    expect(gen.next(areGamesFetched).value).toEqual(
+      put(fetchGamesHandshake({ country: countrySelector }))
+    );
+
+    expect(gen.next().value).toEqual(
+      call(waitForSelector, isGamesHandshakeLoaded)
+    );
+
+    const response = { games: [] };
+    const clearGameList = { id, games: response.games };
+    const clearEntities = {
+      [ENTITY_KEYS.GAME_LIST]: clearGameList,
+    };
+
+    expect(gen.next({ response }).value).toEqual(
+      call(normalizeData, clearEntities)
+    );
+
+    expect(gen.next({ entities: clearEntities }).value).toEqual(
+      put(updateEntity(clearEntities))
+    );
+
+    expect(gen.next().value).toEqual(select(gamesHandshakeSelector));
+
+    expect(gen.next(handshake).value).toEqual(
+      put(
+        fetchSuggestedGamesAction(
+          game,
+          handshake,
+          platform,
+          countrySelector,
+          variant
+        )
+      )
+    );
+
+    expect(gen.next().value).toEqual(
+      take(types.GAME_SEARCH_FETCH_SUGGESTED_GAMES_COMPLETE)
+    );
+
+    const gamesResponse = {};
+
+    expect(gen.next({ response: gamesResponse }).value).toEqual(
+      call(fetchLatestPlayedSaga)
+    );
   });
 });
