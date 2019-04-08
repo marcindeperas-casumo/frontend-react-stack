35aa0dd0ba807ec9399a41a7da7c4207124e93ed
Michał Mokijewski <hello@naf.gl>
2019-04-08 15:22:22 +0200

    add api requests for reel races

--- src/models/reelRaces/reelRaces.types.js
+++ src/models/reelRaces/reelRaces.types.js
@@ -26,3 +26,69 @@ export type ReelRacesTranslations = {
   title: string,
   caveat_short: ?string, // for some reason it's rather "false" than empty
 };
+
+type LeaderboardEntryRaw = {
+  playerId: string,
+  playerName: string,
+  position: number,
+  points: number,
+  remainingSpins: number,
+  mostPlayedGame: ?string,
+  boosters: {|
+    winsInARow: number,
+    triples: number,
+    wins: number,
+    bigWins: number,
+    megaWins: number,
+  |},
+};
+
+export type TournamentPrizeRaw = {
+  badgeRuleName: string, // with | acting as separator? is it always "(...)|slug=free-money-template"?
+  serializedItemUsables: [
+    // is it tuple or array?
+    {
+      usableClassName: "FreeMoneyUsableUsedEvent", // other?
+      usableJSON: string, // always "{}"?
+    },
+  ],
+  templateParameterValues: {|
+    amountGiven: string, // stringified JSON, "{\"amount\":5.0000,\"iso4217CurrencyCode\":\"EUR\"}"?
+  |},
+};
+
+type TournamentRaw = {
+  tournamentId: string,
+  status: "Scheduled" | "Started",
+  type: "Standard" | "Promoted",
+  campaignId: string,
+  contentId: "default",
+  startTime: number, // timestamp
+  endTime: number, // timestamp
+  minBetByCurrency: {
+    [string]: number,
+  },
+  prizes: Array<TournamentPrizeRaw>,
+  gameConfiguration: {
+    gamesAreInclusive: boolean,
+    gameNames: Array<string>, // Array<GameSlug>
+  },
+  leaderboard: {
+    [string]: LeaderboardEntryRaw,
+  },
+  spinLimit: number,
+  winner: ?string, // guessing, it's null everywhere
+  numberOfParticipants: number,
+  promoted: boolean, // true if type === "Promoted", duh
+};
+
+// raw response from the server
+export type TournamentsResponseRaw = {
+  campaignId: string, // "notUsed"?
+  tournaments: {
+    [string]: TournamentRaw,
+  },
+  gamesAreInclusive: boolean,
+  gameNames: Array<string>,
+  tournamentChannels: Array<string>,
+};
