68feb4793880e7c8e1e7bf0a12e53193a9701875
Michele Settepani <michele.settepani@casumo.com>
2019-04-08 15:06:53 +0200

    move up http mock

--- src/api/api.common.test.js
+++ src/api/api.common.test.js
@@ -1,11 +1,11 @@
 import { getCommonHandshake, URLS } from "./api.common";
 
 describe("API/Common", () => {
-  describe("getCommonHandshake()", () => {
-    const http = {
-      get: jest.fn(),
-    };
+  const http = {
+    get: jest.fn(),
+  };
 
+  describe("getCommonHandshake()", () => {
     test("calls http.get() with the right URL", () => {
       getCommonHandshake(http);
 
