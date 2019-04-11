09150765ebd7cda5966695c7120b670d80a10307
Michał Mokijewski <hello@naf.gl>
2019-04-11 16:20:20 +0200

    add mixpanel tracking to reel race card

--- src/components/ReelRaceCard/ReelRaceCard.js
+++ src/components/ReelRaceCard/ReelRaceCard.js
@@ -7,7 +7,10 @@ import Button from "@casumo/cmp-button";
 import { PlayIcon, TickIcon } from "@casumo/cmp-icons";
 import type { ReelRace, ReelRacesTranslations } from "Models/reelRaces";
 import { launchModal } from "Services/LaunchModalService";
-import { MODALS } from "Src/constants";
+import { MODALS, EVENT_PROPS } from "Src/constants";
+import { BUTTON_STATE } from "Models/reelRaces";
+import TrackProvider from "Components/TrackProvider";
+import TrackClick from "Components/TrackClick";
 import Timer from "Components/Timer";
 import GameThumb from "Components/GameThumb";
 import DangerousHtml from "Components/DangerousHtml";
@@ -59,16 +62,18 @@ export class ReelRaceCard extends React.Component<Props> {
     if (this.timeRemainingBeforeStart <= 0) {
       if (this.props.opted) {
         return (
-          <Button
-            variant="variant-1"
-            className="u-padding-vert--md u-padding-horiz--xlg"
-            onClick={this.props.launchGame}
-          >
-            <PlayIcon className="c-reel-race__button-icon" />
-            <Text tag="span" className="u-margin-left">
-              {t.opted_in_cta_single_game_short}
-            </Text>
-          </Button>
+          <TrackClick data={{ state: BUTTON_STATE.PLAY }}>
+            <Button
+              variant="variant-1"
+              className="u-padding-vert--md u-padding-horiz--xlg"
+              onClick={this.props.launchGame}
+            >
+              <PlayIcon className="c-reel-race__button-icon" />
+              <Text tag="span" className="u-margin-left">
+                {t.opted_in_cta_single_game_short}
+              </Text>
+            </Button>
+          </TrackClick>
         );
       }
 
@@ -77,27 +82,31 @@ export class ReelRaceCard extends React.Component<Props> {
 
     if (this.props.opted) {
       return (
-        <Button
-          variant="variant-1"
-          className="u-padding-vert--md u-padding-horiz--xlg"
-          disabled
-        >
-          <TickIcon className="c-reel-race__button-icon" />
-          <Text tag="span" className="u-margin-left">
-            {t.opted_in}
-          </Text>
-        </Button>
+        <TrackClick data={{ state: BUTTON_STATE.OPTED_IN }}>
+          <Button
+            variant="variant-1"
+            className="u-padding-vert--md u-padding-horiz--xlg"
+            disabled
+          >
+            <TickIcon className="c-reel-race__button-icon" />
+            <Text tag="span" className="u-margin-left">
+              {t.opted_in}
+            </Text>
+          </Button>
+        </TrackClick>
       );
     }
 
     return (
-      <Button
-        variant="variant-1"
-        className="u-padding-vert--md u-padding-horiz--xlg"
-        onClick={this.props.optIn}
-      >
-        <Text tag="span">{t.opt_in}</Text>
-      </Button>
+      <TrackClick data={{ state: BUTTON_STATE.OPT_IN }}>
+        <Button
+          variant="variant-1"
+          className="u-padding-vert--md u-padding-horiz--xlg"
+          onClick={this.props.optIn}
+        >
+          <Text tag="span">{t.opt_in}</Text>
+        </Button>
+      </TrackClick>
     );
   }
 
@@ -197,109 +206,124 @@ export class ReelRaceCard extends React.Component<Props> {
       return null;
     }
 
+    const trackData = {
+      [EVENT_PROPS.LOCATION]: "Reel Race",
+      splinLimit: this.props.spins,
+      timeLimit: this.duration,
+      minBet: this.props.minBet,
+      mainPrize: this.props.prize,
+      name: this.props.game.name,
+      isPromoted: this.props.promoted,
+    };
+
     return (
-      <Flex
-        className={[
-          "c-reel-race-card",
-          "o-flex__item",
-          "o-flex__item-fixed-size",
-          "o-ratio--reel-race-card",
-        ].join(" ")}
-        direction="vertical"
-      >
+      <TrackProvider data={trackData}>
         <Flex
           className={[
+            "c-reel-race-card",
             "o-flex__item",
             "o-flex__item-fixed-size",
-            "t-border-r--16",
-            "o-ratio",
             "o-ratio--reel-race-card",
-            `t-color-${this.props.color}`,
           ].join(" ")}
           direction="vertical"
-          justify="space-between"
-          spacing="none"
         >
-          <ImageLazy
-            className="o-ratio__content"
-            src={this.props.game.logoBackground}
-            alt={this.props.game.name}
-            imgixOpts={{
-              w: 330,
-              blur: 100,
-              sat: -50,
-            }}
-          />
-
           <Flex
-            className="u-padding--md o-ratio__content"
+            className={[
+              "o-flex__item",
+              "o-flex__item-fixed-size",
+              "t-border-r--16",
+              "o-ratio",
+              "o-ratio--reel-race-card",
+              `t-color-${this.props.color}`,
+            ].join(" ")}
             direction="vertical"
-            spacing="none"
             justify="space-between"
+            spacing="none"
           >
-            <Flex align="center">
-              <GameThumb
-                src={this.props.game.logoBackground}
-                alt={this.props.game.name}
-                mark={this.props.game.logo}
-              />
-              {this.props.promoted && (
-                <GrandReelRaceBadge className="c-reel-race__badge" />
-              )}
-              <Flex
-                direction="vertical"
-                spacing="sm"
-                className="u-margin-left--md"
-              >
-                <Text tag="span" className="u-font-weight-bold">
-                  {t.compete_for.replace("{{prize}}", this.props.prize)}
-                </Text>
-                <Text
-                  tag="span"
-                  size="sm"
-                  className="t-color-white u-opacity-75"
+            <ImageLazy
+              className="o-ratio__content"
+              src={this.props.game.logoBackground}
+              alt={this.props.game.name}
+              imgixOpts={{
+                w: 330,
+                blur: 100,
+                sat: -50,
+              }}
+            />
+
+            <Flex
+              className="u-padding--md o-ratio__content"
+              direction="vertical"
+              spacing="none"
+              justify="space-between"
+            >
+              <Flex align="center">
+                <GameThumb
+                  src={this.props.game.logoBackground}
+                  alt={this.props.game.name}
+                  mark={this.props.game.logo}
+                />
+                {this.props.promoted && (
+                  <GrandReelRaceBadge className="c-reel-race__badge" />
+                )}
+                <Flex
+                  direction="vertical"
+                  spacing="sm"
+                  className="u-margin-left--md"
                 >
-                  <DangerousHtml html={this.props.game.name} />
-                </Text>
+                  <Text tag="span" className="u-font-weight-bold">
+                    {t.compete_for.replace("{{prize}}", this.props.prize)}
+                  </Text>
+                  <Text
+                    tag="span"
+                    size="sm"
+                    className="t-color-white u-opacity-75"
+                  >
+                    <DangerousHtml html={this.props.game.name} />
+                  </Text>
+                </Flex>
+              </Flex>
+
+              <Flex align="center">
+                <Column top={this.props.spins} bottom={t.spins} />
+                <div className="c-reel-race__separator u-margin-horiz--md" />
+                <Column
+                  top={t.duration_template.replace(
+                    "{{duration}}",
+                    this.duration
+                  )}
+                  bottom={t.duration}
+                />
+                {this.props.minBet && (
+                  <>
+                    <div className="c-reel-race__separator u-margin-horiz--md" />
+                    <Column top={this.props.minBet} bottom={t.min_bet} />
+                  </>
+                )}
+              </Flex>
+
+              <Flex direction="horizontal" justify="space-between" align="end">
+                {this.countdown}
+                {this.button}
               </Flex>
             </Flex>
-
-            <Flex align="center">
-              <Column top={this.props.spins} bottom={t.spins} />
-              <div className="c-reel-race__separator u-margin-horiz--md" />
-              <Column
-                top={t.duration_template.replace("{{duration}}", this.duration)}
-                bottom={t.duration}
-              />
-              {this.props.minBet && (
-                <>
-                  <div className="c-reel-race__separator u-margin-horiz--md" />
-                  <Column top={this.props.minBet} bottom={t.min_bet} />
-                </>
-              )}
-            </Flex>
-
-            <Flex direction="horizontal" justify="space-between" align="end">
-              {this.countdown}
-              {this.button}
-            </Flex>
           </Flex>
+          {t.caveat_short && t.caveat_short !== "false" && (
+            <Text
+              size="xs"
+              className="c-reel-race__terms t-color-grey"
+              onClick={this.showCaveatsModal}
+            >
+              <DangerousHtml
+                html={t.caveat_short.replace(
+                  "{{{ ctaTermsAndConditions }}}",
+                  'class="t-color-black"'
+                )}
+              />
+            </Text>
+          )}
         </Flex>
-        {t.caveat_short && t.caveat_short !== "false" && (
-          <Text
-            size="xs"
-            className="c-reel-race__terms t-color-grey"
-            onClick={this.showCaveatsModal}
-          >
-            <DangerousHtml
-              html={t.caveat_short.replace(
-                "{{{ ctaTermsAndConditions }}}",
-                'class="t-color-black"'
-              )}
-            />
-          </Text>
-        )}
-      </Flex>
+      </TrackProvider>
     );
   }
 }
