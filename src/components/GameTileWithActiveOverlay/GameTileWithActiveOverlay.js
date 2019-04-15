7160eff928d4bc8b92589fd70959018af8944b19
Jack Westbrook <jack.westbrook@casumo.com>
2019-04-15 17:32:35 +0200

    refactor(GameTile): Remove now obsolete Flex dependency
    Casumo/Home#26457

--- src/components/GameTileWithActiveOverlay/GameTileWithActiveOverlay.js
+++ src/components/GameTileWithActiveOverlay/GameTileWithActiveOverlay.js
@@ -1,16 +1,11 @@
 // @flow
 import React, { PureComponent } from "react";
-import Flex from "@casumo/cmp-flex";
 import GameTile from "Components/GameTile/GameTile";
 import type { Props } from "Components/GameTile/GameTile";
 
 class GameTileWithActiveOverlay extends PureComponent<Props> {
   render() {
-    return (
-      <Flex.Item className="o-flex__item--no-shrink c-top-game">
-        <GameTile {...this.props} isOverlayAlwaysActive />
-      </Flex.Item>
-    );
+    return <GameTile {...this.props} isOverlayAlwaysActive />;
   }
 }
 
