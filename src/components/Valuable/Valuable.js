9f5e499049b4a605dcb08992dfb2704ace795c1d
Michelle <michelle.dalli@casumo.com>
2019-04-17 14:37:14 +0200

    ValuableCard: binding of data

--- src/components/Valuable/Valuable.js
+++ src/components/Valuable/Valuable.js
@@ -4,22 +4,17 @@ import Text from "@casumo/cmp-text";
 import Coin from "./coin.svg";
 import "./Valuable.scss";
 
-type Props = {
-  magnitude: string,
-};
+type Props = {};
 
 class Valuable extends PureComponent<Props> {
   render() {
-    const { magnitude } = this.props;
-
     return (
       <div className="c-valuable o-flex-align--center o-flex-justify--center u-position-relative">
         <Coin className="valuable-icon" />
-        <div className="u-position-absolute u-line-height--1 o-flex--vertical o-flex-align--center t-color-yellow-dark-3 u-font-weight-bold">
-          <Text tag="div" size="md">
-            {magnitude}
+        <div className="u-position-absolute o-flex--vertical o-flex-align--center t-color-yellow-dark-3 u-font-weight-bold">
+          <Text tag="div" size="lg">
+            €
           </Text>
-          <Text tag="div">€</Text>
         </div>
       </div>
     );
--- src/components/ValuableCard/ValuableCard.stories.js
+++ src/components/ValuableCard/ValuableCard.stories.js
@@ -1,11 +1,26 @@
 // @flow
 import React from "react";
+import { find, propEq } from "ramda";
 import { storiesOf } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs/react";
+import { select, text } from "@storybook/addon-knobs/react";
 import info from "Storybook/storybookInfo";
+import { VALUABLE_TYPES } from "Models/valuables";
+import mockData from "./__mocks__/Valuable.json";
 import ValuableCard from "./";
 
 const stories = storiesOf("ValuableCard", module);
-stories.addDecorator(withKnobs);
 
-stories.add("Default", () => <ValuableCard />, info({ text: "Default" }));
+stories.add(
+  "Default",
+  () => {
+    const valuableType =
+      select("Valuable Type", VALUABLE_TYPES, VALUABLE_TYPES.CASH) ||
+      VALUABLE_TYPES.CASH;
+    const valuableDetails =
+      find(propEq("valuableType", valuableType))(mockData) || mockData[0];
+    const { title } = valuableDetails;
+
+    return <ValuableCard title={title} valuableType={valuableType} />;
+  },
+  info({ text: "Default" })
+);
