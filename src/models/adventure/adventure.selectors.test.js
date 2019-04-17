ed43a705087dacad595c69da2df99bc3dac27ce2
Dave Calleja <dave.calleja@casumo.com>
2019-04-17 18:09:34 +0200

    Added tests

---
+++ src/models/adventure/adventure.selectors.test.js
@@ -0,0 +1,81 @@
+import {
+  adventurerSelector,
+  adventurerRawSelector,
+} from "./adventure.selectors";
+import { NUMBER_OF_POINTS_PER_LEVEL_IN_TRAVEL_MODE } from "./adventure.constants";
+import { translateTravelModeToSingleLevelProgression } from "./adventure.utils";
+
+const adventurerRaw = {
+  belt: "rope",
+  inTravelMode: false,
+  level: 12,
+  levels: [[1, 10, 100, 200], [1, 10, 100, 200]],
+  name: "casumoSmith",
+  points: 100,
+  pointsRequiredForNextSpaceCrystal: NUMBER_OF_POINTS_PER_LEVEL_IN_TRAVEL_MODE,
+  pointsVersion: 1,
+  spaceCrystals: [1],
+};
+const travellingAdventurerRaw = {
+  ...adventurerRaw,
+  inTravelMode: true,
+};
+const adventurer = {
+  belt: adventurerRaw.belt,
+  inBonusMode: adventurerRaw.inTravelMode,
+  level: adventurerRaw.level,
+  name: adventurerRaw.name,
+  points: adventurerRaw.points,
+  pointsRequiredForNextLevel: 200,
+};
+const state = {
+  schema: {
+    adventurer: adventurerRaw,
+  },
+};
+const travellingState = {
+  schema: {
+    adventurer: travellingAdventurerRaw,
+  },
+};
+
+describe("Adventure Selectors", () => {
+  describe("adventurerRawSelector()", () => {
+    test("returns adventurer object if it exists", () => {
+      expect(adventurerRawSelector(state)).toEqual(adventurerRaw);
+    });
+
+    test("returns {} if adventurer object is missing", () => {
+      expect(adventurerRawSelector({})).toEqual({});
+    });
+  });
+
+  describe("adventurerSelector()", () => {
+    test("returns processed travelling adventurer object if it exists", () => {
+      const {
+        points,
+        pointsRequiredForNextLevel,
+      } = translateTravelModeToSingleLevelProgression(
+        adventurerRaw.points,
+        NUMBER_OF_POINTS_PER_LEVEL_IN_TRAVEL_MODE,
+        adventurerRaw.spaceCrystals
+      );
+
+      const travellingAdventurer = {
+        ...adventurer,
+        inBonusMode: travellingAdventurerRaw.inTravelMode,
+        points,
+        pointsRequiredForNextLevel,
+      };
+      expect(adventurerSelector(travellingState)).toEqual(travellingAdventurer);
+    });
+
+    test("returns processed non-travelling adventurer object if it exists", () => {
+      expect(adventurerSelector(state)).toEqual(adventurer);
+    });
+
+    test("returns {} if adventurer object is missing", () => {
+      expect(adventurerSelector({})).toEqual({});
+    });
+  });
+});
