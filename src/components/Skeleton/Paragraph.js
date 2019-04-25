1c3125fc6735464844dfccab627be9838d24aff7
Michał Mokijewski <hello@naf.gl>
2019-04-26 00:11:36 +0200

    use cudl style sizes

--- src/components/Skeleton/Paragraph.js
+++ src/components/Skeleton/Paragraph.js
@@ -1,44 +1,32 @@
 // @flow
 import * as R from "ramda";
 import * as React from "react";
+import Text from "@casumo/cmp-text";
 import Skeleton from "@casumo/cmp-skeleton";
+import type { fontSizes } from "@casumo/cudl-react-prop-types";
 
 type Props = {
-  fontSize: number,
-  lineHeight: number,
-  radius: number,
-};
-
-// eslint-disable-next-line fp/no-mutation
-ParagraphSkeleton.defaultProps = {
-  radius: 2,
+  size: fontSizes,
 };
 
 export function ParagraphSkeleton(props: Props) {
   return (
-    <Skeleton viewBox={null} width="100%" height={`${4 * props.lineHeight}px`}>
-      {R.times(
-        i => (
-          <rect
-            key={i}
-            x={0}
-            y={i * props.lineHeight}
-            rx={props.radius}
-            ry={props.radius}
-            width="100%"
-            height={`${props.fontSize}px`}
-          />
-        ),
-        3
-      )}
-      <rect
-        x={0}
-        y={3 * props.lineHeight}
-        rx={props.radius}
-        ry={props.radius}
-        width="50%"
-        height={`${props.fontSize}px`}
-      />
-    </Skeleton>
+    <Text size={props.size}>
+      <Skeleton width="100%" height="5em">
+        {R.times(
+          i => (
+            <rect
+              key={i}
+              y={`${i * 1.25 + 0.125}em`}
+              rx="2"
+              ry="2"
+              width={`${i === 3 ? 50 : 100}%`}
+              height="1em"
+            />
+          ),
+          4
+        )}
+      </Skeleton>
+    </Text>
   );
 }
