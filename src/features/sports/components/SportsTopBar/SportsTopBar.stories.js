d55709dff33d4d520a3b96b572604327a42ecd2f
Charles Oliver <charles@cocoa.consulting>
2019-04-29 09:40:34 +0100

    hide top bar by default. update stories

--- src/features/sports/components/SportsTopBar/SportsTopBar.stories.js
+++ src/features/sports/components/SportsTopBar/SportsTopBar.stories.js
@@ -14,20 +14,39 @@ stories.addDecorator(story => (
   </MockedProviderWithContext>
 ));
 
-stories.add(
-  "Home Active",
-  () => <SportsTopBar currentHash={hashes.home} />,
-  info("Home Active")
+// set almost invisible background so chromatic can distinguish between `hidden` and `display: none`
+const background = "#fffffe";
+
+const render = (props, isMobile) => () => (
+  <div
+    id="c-sports-shell"
+    className={isMobile ? "c-sports-shell--mobile" : "c-sports-shell--site"}
+    style={{ background }}
+  >
+    <SportsTopBar {...props} />
+  </div>
 );
 
 stories.add(
-  "Search Active",
-  () => <SportsTopBar currentHash={hashes.home} isSearchVisible />,
-  info("Search Active")
+  "Site: Home Active",
+  render({ currentHash: hashes.home }),
+  info("Site: Home Active")
 );
 
 stories.add(
-  "My Bets Active",
-  () => <SportsTopBar currentHash={hashes.betHistory} />,
-  info("My Bets Active")
+  "Site: Search Active",
+  render({ currentHash: hashes.home, isSearchVisible: true }),
+  info("Site: Search Active")
+);
+
+stories.add(
+  "Site: My Bets Active",
+  render({ currentHash: hashes.betHistory }),
+  info("Site: My Bets Active")
+);
+
+stories.add(
+  "Mobile (hidden)",
+  render({ currentHash: hashes.home }, true),
+  info("Mobile (hidden)")
 );
