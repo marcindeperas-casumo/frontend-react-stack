afe7e18480d0be330e243a99a5331e7388bb3836
Adrian Nowicki <adrian.nowicki@casumo.com>
2019-04-30 16:27:28 +0200

    Removed promoCode. Updated Flow types.

--- flow-typed/graphql/PlayerValuablesQuery.js
+++ flow-typed/graphql/PlayerValuablesQuery.js
@@ -8,9 +8,109 @@
 // GraphQL query operation: PlayerValuablesQuery
 // ====================================================
 
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDeposit_amount_maxBonusAmount = {
+  amount: number,
+  currency: Currency,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDeposit_amount = {
+  percent: number,
+  maxBonusAmount: ?PlayerValuablesQuery_player_valuables_PlayerValuableDeposit_amount_maxBonusAmount,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDeposit_minDepositAmount = {
+  amount: number,
+  currency: Currency,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_minDepositAmount = {
+  amount: number,
+  currency: Currency,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_maxDepositAmount = {
+  amount: number,
+  currency: Currency,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_itemsToAward_usables_freeSpinsTemplate = {
+  numberOfFreeSpins: number
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_itemsToAward_usables = {
+  freeSpinsTemplate: ?PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_itemsToAward_usables_freeSpinsTemplate
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_itemsToAward = {
+  /**
+   * It turns out usables present here can now be only free spins with slightly
+   * changed structure (i.e. no promoCodeOrTemplate JSON field but template)
+   */
+  usables: ?Array<?PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_itemsToAward_usables>
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder = {
+  minDepositAmount: ?PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_minDepositAmount,
+  maxDepositAmount: ?PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_maxDepositAmount,
+  itemsToAward: Array<?PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder_itemsToAward>,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableSpins_freeSpinsTemplate_betLevels = {
+  amount: number,
+  currency: Currency,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableSpins_freeSpinsTemplate = {
+  numberOfFreeSpins: number,
+  validFrom: ?string,
+  betLevels: ?Array<?PlayerValuablesQuery_player_valuables_PlayerValuableSpins_freeSpinsTemplate_betLevels>,
+};
+
+export type PlayerValuablesQuery_player_valuables_PlayerValuableCash_cash = {
+  amount: number,
+  currency: Currency,
+};
+
 export type PlayerValuablesQuery_player_valuables = {
-  id: ?string,
-  state: ?PlayerValuableState,
+  id: string,
+  state: PlayerValuableState,
+  expirationTime: any,
+  valuableType: PlayerValuableType,
+  title: string,
+  caveat: ?string,
+  content: string,
+  amount: PlayerValuablesQuery_player_valuables_PlayerValuableDeposit_amount,
+  minDepositAmount: PlayerValuablesQuery_player_valuables_PlayerValuableDeposit_minDepositAmount,
+} | {
+  id: string,
+  state: PlayerValuableState,
+  expirationTime: any,
+  valuableType: PlayerValuableType,
+  title: string,
+  caveat: ?string,
+  content: string,
+  itemsToAwardLadder: ?Array<?PlayerValuablesQuery_player_valuables_PlayerValuableDepositLadder_itemsToAwardLadder>,
+} | {
+  id: string,
+  state: PlayerValuableState,
+  expirationTime: any,
+  valuableType: PlayerValuableType,
+  title: string,
+  caveat: ?string,
+  content: string,
+  source: ?PlayerValuableUsable_Platform,
+  promoCode: ?string,
+  gameName: string,
+  freeSpinsTemplate: ?PlayerValuablesQuery_player_valuables_PlayerValuableSpins_freeSpinsTemplate,
+} | {
+  id: string,
+  state: PlayerValuableState,
+  expirationTime: any,
+  valuableType: PlayerValuableType,
+  title: string,
+  caveat: ?string,
+  content: string,
+  cash: PlayerValuablesQuery_player_valuables_PlayerValuableCash_cash,
 };
 
 export type PlayerValuablesQuery_player = {
@@ -33,6 +133,21 @@ export type PlayerValuablesQuery = {
  * 
  */
 export type PlayerValuableState = "Consumed" | "Expired" | "Fresh" | "Used";
+
+/**
+ * 
+ */
+export type PlayerValuableType = "PlayerValuableCash" | "PlayerValuableDeposit" | "PlayerValuableDepositLadder" | "PlayerValuableSpins";
+
+/**
+ * 
+ */
+export type Currency = "CAD" | "DKK" | "EUR" | "GBP";
+
+/**
+ * 
+ */
+export type PlayerValuableUsable_Platform = "desktop" | "mobile";
 
 //==============================================================
 // END Enums and Input Objects
--- src/components/PlayerValuableListHorizontal/PlayerValuableListHorizontalContainer.js
+++ src/components/PlayerValuableListHorizontal/PlayerValuableListHorizontalContainer.js
@@ -7,6 +7,11 @@ import ScrollableList from "Components/ScrollableList";
 import { PlayerValuablesQuery as LocalQuery } from "./PlayerValuables.graphql";
 import PlayerValuableTile from "./PlayerValuableTile";
 
+// This is far from ideal and is just temporary.
+// We need to update the list of player vauables because sometimes they come through Cometd
+// channel.
+const REFRESH_INTERVAL = 15000;
+
 class PlayerValuablesTypedQuery extends Query<PlayerValuablesQuery, null> {}
 
 const findById = (list, id) => list.find(item => item.id === id);
@@ -23,7 +28,7 @@ const withValuableData = valuables => ({ id }) => {
 };
 
 const PlayerValuableListHorizontalContainer = () => (
-  <PlayerValuablesTypedQuery query={LocalQuery}>
+  <PlayerValuablesTypedQuery query={LocalQuery} pollInterval={REFRESH_INTERVAL}>
     {({ loading, data }) => {
       const { player: { valuables = [] } = {} } = data;
 
