f9ec572a0f4fb5e7823a03a2c3bb597afa8fa700
Michele Settepani <michele.settepani@casumo.com>
2019-05-07 14:18:30 +0200

    ResponsiveLayout.stories: add viewports test Casumo/Home#28438

--- src/components/ResponsiveLayout/ResponsiveLayout.stories.js
+++ src/components/ResponsiveLayout/ResponsiveLayout.stories.js
@@ -9,13 +9,35 @@ const stories = storiesOf("ResponsiveLayout", module);
 
 if (isNotChromatic) {
   stories.add(
-    "Default",
+    "Conditional rendering example",
     () => (
       <div>
         <Desktop children={<p>I'm a Desktop device 🖥 </p>} />
         <Mobile children={<p>I'm a Mobile device 📱</p>} />
       </div>
     ),
-    info({ text: "Default" })
+    info({ text: "Conditional rendering example" })
   );
 }
+
+/*
+  Desktop and mobile are currently being tested on Chromatic as it is not trivial how to test matchMedia
+  via unit-testing https://github.com/Casumo/mobile-react-stack-poc/pull/473#discussion_r281507505
+  Ideal scenario would be testing those with Jest and remove the following stories.
+*/
+
+stories.add(
+  "Desktop",
+  () => <Desktop children={<p>I'm a Desktop device 🖥 </p>} />,
+  {
+    chromatic: { viewports: [768, 1280] },
+  }
+);
+
+stories.add(
+  "Mobile",
+  () => <Mobile children={<p>I'm a Mobile device 📱</p>} />,
+  {
+    chromatic: { viewports: [768, 1280] },
+  }
+);
