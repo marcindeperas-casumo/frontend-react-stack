67723687efddbe58d839472bc67dba765663fa88
Michelle <michelle.dalli@casumo.com>
2019-05-08 10:53:30 +0200

    ValuableCard: added 4th star. WIP

--- src/components/ValuableCard/ValuableIcons.utils.js
+++ src/components/ValuableCard/ValuableIcons.utils.js
@@ -1,10 +1,10 @@
 import { VALUABLE_TYPES, VALUABLE_SPIN_TYPES } from "Models/valuables";
 import DepositIcon from "./Icons/deposit.svg";
 import SportIcon from "./Icons/sport.svg";
-import BasicSpinsIcon from "./Icons/spins-0star.svg";
-import BonusSpinsIcon from "./Icons/spins-1star.svg";
-import SuperSpinsIcon from "./Icons/spins-2star.svg";
-import MegaSpinsIcon from "./Icons/spins-3star.svg";
+import BasicSpinsIcon from "./Icons/spins-1star.svg";
+import BonusSpinsIcon from "./Icons/spins-2star.svg";
+import SuperSpinsIcon from "./Icons/spins-3star.svg";
+import MegaSpinsIcon from "./Icons/spins-4star.svg";
 
 export const VALUABLE_ICON = {
   [VALUABLE_TYPES.DEPOSIT]: DepositIcon,
