581867b7e129b1aca75959bc4a9ca582e66cb3d4
Dave Calleja <dave.calleja@casumo.com>
2019-05-13 16:44:00 +0200

    Fixed unit test

--- src/components/CasumoAvatar/CasumoAvatar.test.js
+++ src/components/CasumoAvatar/CasumoAvatar.test.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import {
-  CasumoAvatar,
-  getClassModifierByBelt,
-  getBackgroundColourClassModifier,
-} from "./CasumoAvatar";
+import { CasumoAvatar, getClassModifierByBelt } from "./CasumoAvatar";
 
 describe("CasumoAvatar", () => {
   test("Renders avatar with the correct color class applied based on belt", () => {
@@ -16,11 +12,10 @@ describe("CasumoAvatar", () => {
   });
   test("Renders avatar with the correct background colour class applied", () => {
     const backgroundColour = "violet";
-    const classModifier = getBackgroundColourClassModifier(backgroundColour);
     const rendered = shallow(
       <CasumoAvatar belt="rope" backgroundColour={backgroundColour} />
     );
 
-    expect(rendered.find(`.${classModifier}`).length).toBe(1);
+    expect(rendered.find(`.t-background-violet`).length).toBe(1);
   });
 });
