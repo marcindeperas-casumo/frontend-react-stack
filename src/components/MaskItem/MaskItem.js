4aa378716ba23a0f4eb7b3408c89447bbd6fb8f0
Michelle <michelle.dalli@casumo.com>
2019-05-14 11:03:03 +0200

    MaskItem: removed unnecessary return

--- src/components/MaskItem/MaskItem.js
+++ src/components/MaskItem/MaskItem.js
@@ -25,29 +25,25 @@ const MaskItem = ({
   shapeMask,
   /** The element to mask */
   children,
-}: Props) => {
-  return (
-    <div className={className} style={{ width, height }}>
-      <div
-        className="u-position-absolute o-mask-overlay"
-        style={{ clipPath: `url(#__mask-item-${id})` }}
-      >
-        {children}
-      </div>
-      <svg
-        viewBox={`0 0 ${width} ${height}`}
-        xmlns="http://www.w3.org/2000/svg"
-        width={width}
-        height={height}
-      >
-        <defs>
-          <clipPath id={`__mask-item-${id}`}>
-            {shapeMask && shapeMask()}
-          </clipPath>
-        </defs>
-      </svg>
+}: Props) => (
+  <div className={className} style={{ width, height }}>
+    <div
+      className="u-position-absolute o-mask-overlay"
+      style={{ clipPath: `url(#__mask-item-${id})` }}
+    >
+      {children}
     </div>
-  );
-};
+    <svg
+      viewBox={`0 0 ${width} ${height}`}
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+    >
+      <defs>
+        <clipPath id={`__mask-item-${id}`}>{shapeMask && shapeMask()}</clipPath>
+      </defs>
+    </svg>
+  </div>
+);
 
 export default MaskItem;
