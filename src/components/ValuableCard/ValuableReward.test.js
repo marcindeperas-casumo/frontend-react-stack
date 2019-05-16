5f85f2902aff33873697bb0fc65f2ce793d10abb
Michelle <michelle.dalli@casumo.com>
2019-05-16 14:30:14 +0200

    ValuableCard test PR fix

--- src/components/ValuableCard/ValuableReward.test.js
+++ src/components/ValuableCard/ValuableReward.test.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import ValuableReward from "./ValuableReward";
 
 describe("ValuableReward", () => {
   const RewardContent = () => <div>foo</div>;
 
   test("Should render component as content", () => {
-    const rendered = shallow(<ValuableReward ValuableSymbol={RewardContent} />);
+    const rendered = mount(<ValuableReward ValuableSymbol={RewardContent} />);
 
     expect(rendered.find("RewardContent")).toHaveLength(1);
+    expect(rendered.find("RewardContent").text()).toEqual("foo");
   });
 });
