8b62375b4864ad83696c8b55f62e1e9934c0d62e
João Guedes <joao.guedes@casumo.com>
2019-05-21 12:26:54 +0100

    added Notifications and NotificationsContainer

---
+++ src/components/Settings/Notifications/Notifications.js
@@ -0,0 +1,223 @@
+// @flow
+import React, { PureComponent } from "react";
+import { DirectionRightIcon } from "@casumo/cmp-icons";
+import Text from "@casumo/cmp-text";
+import Row from "Components/Settings/Row";
+import Headline from "Components/Settings/Headline";
+import Toggle from "Components/Controls/Toggle";
+import Checkbox from "Components/Controls/Checkbox";
+import Link from "Components/Settings/Link";
+
+type ToggleFn = (active: boolean) => void;
+type Props = {
+  player: PLAYER_CONTACT_SETTINGS_QUERY_player,
+  setAdventurerPublicity: ToggleFn,
+  setWithdrawalNotifications: ToggleFn,
+  setContactByPost: ToggleFn,
+  setContactByPhone: ToggleFn,
+  setNewsletterSubscription: ToggleFn,
+  setSMSNewsletterSubscription: ToggleFn,
+  labels: NotificationsLabelsQuery,
+};
+
+export const ToggleRow = ({
+  label,
+  isEnabled,
+  onChange,
+  className = "",
+}: {
+  label: string,
+  isEnabled: boolean,
+  onChange: ToggleFn,
+  className?: string,
+}) => (
+  <Row
+    className={className}
+    text={
+      <Text
+        tag="p"
+        size="sm"
+        className="t-color-grey-dark-3 u-font-weight-light"
+      >
+        {label}
+      </Text>
+    }
+    action={<Toggle checked={isEnabled} onChange={onChange} />}
+  />
+);
+
+const SubscriptionRow = ({
+  label,
+  isEnabled,
+  onChange,
+}: {
+  label: string,
+  isEnabled: boolean,
+  onChange: ToggleFn,
+}) => (
+  <Row
+    padding="md"
+    text={
+      <Text
+        tag="p"
+        size="sm"
+        className="t-color-grey-dark-3 u-font-weight-light"
+      >
+        {label}
+      </Text>
+    }
+    action={<Checkbox checked={isEnabled} onChange={onChange} />}
+  />
+);
+
+class Notifications extends PureComponent<Props> {
+  render() {
+    const {
+      setAdventurerPublicity,
+      setWithdrawalNotifications,
+      setContactByPost,
+      setContactByPhone,
+      setNewsletterSubscription,
+      setSMSNewsletterSubscription,
+      player: {
+        details: {
+          contactSettings: {
+            withdrawalNotifications,
+            adventurerPublic,
+            subscribedToNewsletters,
+            subscribedToSMSNewsletters,
+            contactByPhone,
+            contactByPost,
+          },
+        },
+        playOk: {
+          realityCheck: { canChangeInterval, intervalInMinutes },
+        },
+      },
+      labels: {
+        subscriptionsTitle,
+        subscriptionsDescription,
+        subscriptionsEmailLabel,
+        subscriptionsSMSLabel,
+        subscriptionsPhoneLabel,
+        subscriptionsPostLabel,
+        notificationsApprovedWithdrawalsEmailLabel,
+        notificationsInGameSessionUpdatesLabel,
+      },
+    } = this.props;
+
+    const ToggleWithdrawals = () => (
+      <ToggleRow
+        label={notificationsApprovedWithdrawalsEmailLabel}
+        isEnabled={withdrawalNotifications}
+        onChange={setWithdrawalNotifications}
+      />
+    );
+
+    const ToggleAdventurerPublic = () => (
+      <ToggleRow
+        label="Show my adventure and player card"
+        isEnabled={adventurerPublic}
+        onChange={setAdventurerPublicity}
+      />
+    );
+
+    const RealityCheck = () => (
+      <Link
+        target="/player/settings/reality-check"
+        enabled={canChangeInterval}
+        label={
+          <Row
+            text={
+              <Text
+                tag="p"
+                size="sm"
+                className="t-color-grey-dark-3 u-font-weight-light"
+              >
+                {notificationsInGameSessionUpdatesLabel}
+              </Text>
+            }
+            action={
+              <>
+                {intervalInMinutes ? (
+                  <Text
+                    tag="span"
+                    className="t-color-green u-font-weight-light"
+                  >
+                    {intervalInMinutes} min
+                  </Text>
+                ) : (
+                  <Text
+                    tag="span"
+                    className="t-color-grey-light-1 u-font-weight-light"
+                  >
+                    Off
+                  </Text>
+                )}
+                {canChangeInterval && (
+                  <DirectionRightIcon className="t-color-grey-light-1" />
+                )}
+              </>
+            }
+          />
+        }
+      />
+    );
+
+    const CheckEmail = () => (
+      <SubscriptionRow
+        label={subscriptionsEmailLabel}
+        isEnabled={subscribedToNewsletters}
+        onChange={setNewsletterSubscription}
+      />
+    );
+
+    const CheckSMS = () => (
+      <SubscriptionRow
+        label={subscriptionsSMSLabel}
+        isEnabled={subscribedToSMSNewsletters}
+        onChange={setSMSNewsletterSubscription}
+      />
+    );
+
+    const CheckPhone = () => (
+      <SubscriptionRow
+        label={subscriptionsPhoneLabel}
+        isEnabled={contactByPhone}
+        onChange={setContactByPhone}
+      />
+    );
+
+    const CheckPost = () => (
+      <SubscriptionRow
+        label={subscriptionsPostLabel}
+        isEnabled={contactByPost}
+        onChange={setContactByPost}
+      />
+    );
+    return (
+      <>
+        <ToggleWithdrawals />
+        <ToggleAdventurerPublic />
+        <RealityCheck />
+
+        <Row
+          text={
+            <Headline
+              title={subscriptionsTitle}
+              description={subscriptionsDescription}
+            />
+          }
+          className="t-border-bottom--none u-margin-top--md"
+        />
+
+        <CheckEmail />
+        <CheckSMS />
+        <CheckPhone />
+        <CheckPost />
+      </>
+    );
+  }
+}
+
+export default Notifications;
---
+++ src/components/Settings/Notifications/NotificationsContainer.js
@@ -0,0 +1,256 @@
+// @flow
+import React from "react";
+import { Mutation, Query } from "react-apollo";
+import { adopt } from "react-adopt";
+import { RowListSkeleton } from "Components/Settings/Row/RowListSkeleton";
+import { ErrorMessage } from "Components/ErrorMessage";
+import logger from "Services/logger";
+import { dataIdFromObject } from "Services/apolloCacheUtils";
+import {
+  SET_ADVENTURER_PUBLICITY,
+  SET_WITHDRAWAL_NOTIFICATIONS,
+  SET_CONTACT_BY_POST,
+  SET_CONTACT_BY_PHONE,
+  SET_NEWSLETTER_SUBSCRIPTION,
+  SET_SMS_NEWSLETTER_SUBSCRIPTION,
+} from "./Mutations";
+import {
+  PLAYER_CONTACT_SETTINGS_QUERY,
+  WITHDRAWAL_NOTIFICATION_FRAGMENT,
+  ADVENTURER_PUBLIC_FRAGMENT,
+  SUBSCRIBED_TO_NEWSLETTERS_FRAGMENT,
+  SUBSCRIBED_TO_SMS_NEWSLETTERS_FRAGMENT,
+  CONTACT_BY_PHONE_FRAGMENT,
+  CONTACT_BY_POST_FRAGMENT,
+} from "./PlayerContactSettingsQuery";
+import { NOTIFICATIONS_LABELS_QUERY } from "./NotificationsLabelsQuery";
+import Notifications from "./Notifications";
+
+const noop = () => {};
+
+const Composed = adopt({
+  setWithdrawalNotifications: ({ render }) => (
+    //TODO: implement error handling when we have notifications in place
+    //if onError is unset, Apollo default handler throws and tests break
+    <Mutation mutation={SET_WITHDRAWAL_NOTIFICATIONS} onError={noop}>
+      {mutation => render(mutation)}
+    </Mutation>
+  ),
+  setAdventurerPublicity: ({ render }) => (
+    <Mutation mutation={SET_ADVENTURER_PUBLICITY} onError={noop}>
+      {mutation => render(mutation)}
+    </Mutation>
+  ),
+  setContactByPost: ({ render }) => (
+    <Mutation mutation={SET_CONTACT_BY_POST} onError={noop}>
+      {mutation => render(mutation)}
+    </Mutation>
+  ),
+  setContactByPhone: ({ render }) => (
+    <Mutation mutation={SET_CONTACT_BY_PHONE} onError={noop}>
+      {mutation => render(mutation)}
+    </Mutation>
+  ),
+  setNewsletterSubscription: ({ render }) => (
+    <Mutation mutation={SET_NEWSLETTER_SUBSCRIPTION} onError={noop}>
+      {mutation => render(mutation)}
+    </Mutation>
+  ),
+  setSMSNewsletterSubscription: ({ render }) => (
+    <Mutation mutation={SET_SMS_NEWSLETTER_SUBSCRIPTION} onError={noop}>
+      {mutation => render(mutation)}
+    </Mutation>
+  ),
+  labels: ({ render }) => (
+    <Query query={NOTIFICATIONS_LABELS_QUERY}>{render}</Query>
+  ),
+});
+
+const wrap = (mutation, on, optimisticResponse, getUpdatedCacheObject) =>
+  mutation({
+    variables: { input: { on } },
+    optimisticResponse,
+    update: (cache, result) => {
+      try {
+        cache.writeFragment(getUpdatedCacheObject(result));
+      } catch (err) {
+        logger.error(
+          "Contact Settings/Notifications: Failed while writing fragment",
+          err
+        );
+      }
+    },
+  });
+
+export const withContainer = (Component: Function) => (
+  <Composed>
+    {({
+      setAdventurerPublicity,
+      setWithdrawalNotifications,
+      setContactByPost,
+      setContactByPhone,
+      setNewsletterSubscription,
+      setSMSNewsletterSubscription,
+      labels,
+    }) => {
+      return (
+        <Query query={PLAYER_CONTACT_SETTINGS_QUERY}>
+          {({ loading, data, error, refetch }) => {
+            if (loading || labels.loading) {
+              return <RowListSkeleton count={8} />;
+            }
+            if (error) {
+              return <ErrorMessage retry={() => refetch()} />;
+            }
+            if (labels.error) {
+              return <ErrorMessage retry={() => labels.refetch()} />;
+            }
+
+            const {
+              player: { id },
+            } = data;
+
+            return (
+              <Component
+                labels={labels.data}
+                player={data.player}
+                setWithdrawalNotifications={value =>
+                  wrap(
+                    setWithdrawalNotifications,
+                    value,
+                    { setWithdrawalNotifications: value },
+                    result => ({
+                      id: dataIdFromObject({ __typename: "Player", id }),
+                      fragment: WITHDRAWAL_NOTIFICATION_FRAGMENT,
+                      data: {
+                        details: {
+                          __typename: "PlayerDetails",
+                          contactSettings: {
+                            withdrawalNotifications:
+                              result.data.setWithdrawalNotifications,
+                            __typename: "PlayerContactSettings",
+                          },
+                        },
+                        __typename: "Player",
+                      },
+                    })
+                  )
+                }
+                setAdventurerPublicity={value =>
+                  wrap(
+                    setAdventurerPublicity,
+                    value,
+                    { setAdventurerPublicity: value },
+                    result => ({
+                      id: dataIdFromObject({ __typename: "Player", id }),
+                      fragment: ADVENTURER_PUBLIC_FRAGMENT,
+                      data: {
+                        details: {
+                          __typename: "PlayerDetails",
+                          contactSettings: {
+                            adventurerPublic:
+                              result.data.setAdventurerPublicity,
+                            __typename: "PlayerContactSettings",
+                          },
+                        },
+                        __typename: "Player",
+                      },
+                    })
+                  )
+                }
+                setNewsletterSubscription={value =>
+                  wrap(
+                    setNewsletterSubscription,
+                    value,
+                    { setNewsletterSubscription: value },
+                    result => ({
+                      id: dataIdFromObject({ __typename: "Player", id }),
+                      fragment: SUBSCRIBED_TO_NEWSLETTERS_FRAGMENT,
+                      data: {
+                        details: {
+                          __typename: "PlayerDetails",
+                          contactSettings: {
+                            subscribedToNewsletters:
+                              result.data.setNewsletterSubscription,
+                            __typename: "PlayerContactSettings",
+                          },
+                        },
+                        __typename: "Player",
+                      },
+                    })
+                  )
+                }
+                setSMSNewsletterSubscription={value =>
+                  wrap(
+                    setSMSNewsletterSubscription,
+                    value,
+                    { setSMSNewsletterSubscription: value },
+                    result => ({
+                      id: dataIdFromObject({ __typename: "Player", id }),
+                      fragment: SUBSCRIBED_TO_SMS_NEWSLETTERS_FRAGMENT,
+                      data: {
+                        details: {
+                          __typename: "PlayerDetails",
+                          contactSettings: {
+                            subscribedToSMSNewsletters:
+                              result.data.setSMSNewsletterSubscription,
+                            __typename: "PlayerContactSettings",
+                          },
+                        },
+                        __typename: "Player",
+                      },
+                    })
+                  )
+                }
+                setContactByPhone={value =>
+                  wrap(
+                    setContactByPhone,
+                    value,
+                    { setContactByPhone: value },
+                    result => ({
+                      id: dataIdFromObject({ __typename: "Player", id }),
+                      fragment: CONTACT_BY_PHONE_FRAGMENT,
+                      data: {
+                        details: {
+                          __typename: "PlayerDetails",
+                          contactSettings: {
+                            contactByPhone: result.data.setContactByPhone,
+                            __typename: "PlayerContactSettings",
+                          },
+                        },
+                        __typename: "Player",
+                      },
+                    })
+                  )
+                }
+                setContactByPost={value =>
+                  wrap(
+                    setContactByPost,
+                    value,
+                    { setContactByPost: value },
+                    result => ({
+                      id: dataIdFromObject({ __typename: "Player", id }),
+                      fragment: CONTACT_BY_POST_FRAGMENT,
+                      data: {
+                        details: {
+                          __typename: "PlayerDetails",
+                          contactSettings: {
+                            contactByPost: result.data.setContactByPost,
+                            __typename: "PlayerContactSettings",
+                          },
+                        },
+                        __typename: "Player",
+                      },
+                    })
+                  )
+                }
+              />
+            );
+          }}
+        </Query>
+      );
+    }}
+  </Composed>
+);
+
+export default () => withContainer(Notifications);
