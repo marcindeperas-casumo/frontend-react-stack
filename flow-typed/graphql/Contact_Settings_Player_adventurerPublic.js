587f675d8ad5f49978a746d9176ce3afb8df6e00
João Guedes <joao.guedes@casumo.com>
2019-05-21 12:25:55 +0100

    added flow types

---
+++ flow-typed/graphql/Contact_Settings_Player_adventurerPublic.js
@@ -0,0 +1,38 @@
+/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: Contact_Settings_Player_adventurerPublic
+// ====================================================
+
+export type Contact_Settings_Player_adventurerPublic_details_contactSettings = {
+  adventurerPublic: boolean
+};
+
+export type Contact_Settings_Player_adventurerPublic_details = {
+  contactSettings: Contact_Settings_Player_adventurerPublic_details_contactSettings
+};
+
+export type Contact_Settings_Player_adventurerPublic = {
+  __typename: "Player",
+  details: Contact_Settings_Player_adventurerPublic_details,
+};/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export type ContactSettingsInput = {|
+  on: boolean
+|};
+
+export type UpdateRealityCheckIntervalInput = {|
+  intervalSeconds: number
+|};
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
---
+++ flow-typed/graphql/NOTIFICATIONS_LABELS_QUERY.js
@@ -0,0 +1,36 @@
+/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: NOTIFICATIONS_LABELS_QUERY
+// ====================================================
+
+export type NOTIFICATIONS_LABELS_QUERY = {
+  subscriptionsTitle: string,
+  subscriptionsDescription: string,
+  subscriptionsEmailLabel: string,
+  subscriptionsSMSLabel: string,
+  subscriptionsPhoneLabel: string,
+  subscriptionsPostLabel: string,
+  notificationsApprovedWithdrawalsEmailLabel: string,
+  notificationsInGameSessionUpdatesLabel: string,
+};/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export type ContactSettingsInput = {|
+  on: boolean
+|};
+
+export type UpdateRealityCheckIntervalInput = {|
+  intervalSeconds: number
+|};
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
