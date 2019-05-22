2344b03d31a56ba8703303b7b2f25833a3b55868
João Guedes <joao.guedes@casumo.com>
2019-05-22 17:46:57 +0100

    added flowtypes

---
+++ flow-typed/graphql/REALITY_CHECK_LABELS_QUERY.js
@@ -0,0 +1,32 @@
+/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: REALITY_CHECK_LABELS_QUERY
+// ====================================================
+
+export type REALITY_CHECK_LABELS_QUERY = {
+  inGameSessionUpdatesLabel: string,
+  inGameSessionUpdatesFrequencyLabel: string,
+  save: string,
+  cancel: string,
+};/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export type ContactSettingsInput = {|
+  on: boolean
+|};
+
+export type UpdateRealityCheckIntervalInput = {|
+  intervalSeconds: number
+|};
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
