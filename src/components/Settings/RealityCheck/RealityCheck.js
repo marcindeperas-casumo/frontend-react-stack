139609b89dfb69045bfe3df536c3f77dd8c9ad63
João Guedes <joao.guedes@casumo.com>
2019-05-22 17:48:10 +0100

    added RealityCheck and RealityCheckContainer

---
+++ src/components/Settings/RealityCheck/RealityCheck.js
@@ -0,0 +1,78 @@
+// @flow
+import React, { PureComponent } from "react";
+import Text from "@casumo/cmp-text";
+import Button from "@casumo/cmp-button";
+import PillSelector from "Components/Controls/PillSelector/PillSelector";
+import { ToggleRow } from "Components/Settings/Notifications/Notifications";
+import { options } from "./constants";
+
+type Props = {
+  query: PLAYER_REALITY_CHECK_QUERY,
+  labels: REALITY_CHECK_LABELS_QUERY,
+  onUpdate: (interval: number) => void,
+  updateLoading: boolean,
+  onSave: () => void,
+  updateError: ?string,
+  interval: number,
+};
+
+class RealityCheck extends PureComponent<Props> {
+  render() {
+    const {
+      onUpdate,
+      onSave,
+      updateLoading,
+      interval,
+      labels: {
+        inGameSessionUpdatesLabel,
+        inGameSessionUpdatesFrequencyLabel,
+        save,
+        cancel,
+      },
+    } = this.props;
+
+    const OFF = 0;
+    const enabled = interval !== OFF;
+
+    return (
+      <>
+        <ToggleRow
+          className="t-border-bottom--none"
+          label={inGameSessionUpdatesLabel}
+          isEnabled={enabled}
+          onChange={value => onUpdate(value ? options[0].value : OFF)}
+        />
+        <div className="u-padding--md">
+          <Text
+            tag="p"
+            size="sm"
+            className="t-color-grey-dark-3 u-font-weight-light"
+          >
+            {inGameSessionUpdatesFrequencyLabel}
+          </Text>
+          <div className="u-margin-top--md">
+            <PillSelector
+              disabled={Boolean(!enabled)}
+              className="u-margin-top--md"
+              options={options}
+              value={interval}
+              onChange={value => onUpdate(value)}
+            />
+          </div>
+          <Button
+            loading={updateLoading}
+            className="u-width--1/1 u-margin-top--xlg"
+            onClick={onSave}
+          >
+            {save}
+          </Button>
+          <a onClick={() => window.history.back()}>
+            <div className="u-width--1/1 u-margin-top--lg center">{cancel}</div>
+          </a>
+        </div>
+      </>
+    );
+  }
+}
+
+export default RealityCheck;
---
+++ src/components/Settings/RealityCheck/RealityCheckContainer.js
@@ -0,0 +1,134 @@
+// @flow
+import React from "react";
+import { isNil } from "ramda";
+import {
+  Query,
+  Mutation,
+  type MutationOptions,
+  type QueryResult,
+} from "react-apollo";
+import { adopt } from "react-adopt";
+import { RowListSkeleton } from "Components/Settings/Row/RowListSkeleton";
+import { ErrorMessage } from "Components/ErrorMessage";
+import RealityCheck from "Components/Settings/RealityCheck/RealityCheck";
+import { launchModal } from "Services/LaunchModalService";
+import { MODALS } from "Src/constants";
+import { REALITY_CHECK_LABELS_QUERY } from "./RealityCheckLabelsQuery";
+import { PLAYER_REALITY_CHECK_QUERY } from "./RealityCheckQuery";
+import { UPDATE_REALITY_CHECK_INTERVAL } from "./Mutations";
+
+const Composed = adopt({
+  query: ({ render }) => (
+    <Query query={PLAYER_REALITY_CHECK_QUERY}>{render}</Query>
+  ),
+  labels: ({ render }) => (
+    <Query query={REALITY_CHECK_LABELS_QUERY}>{render}</Query>
+  ),
+});
+
+type State = {
+  intervalMinutes: ?number,
+};
+
+export const withContainer = (Component: Function) =>
+  class RealityCheckContainer extends React.Component<{}, State> {
+    state = {};
+
+    refresh = async (query: QueryResult) => {
+      const {
+        data: {
+          player: {
+            playOk: {
+              realityCheck: { intervalInMinutes },
+            },
+          },
+        },
+      } = await query.refetch();
+      this.setState({
+        intervalMinutes: intervalInMinutes,
+      });
+    };
+
+    save = async (
+      mutate: (
+        options?: MutationOptions
+      ) => Promise<PLAYER_REALITY_CHECK_QUERY>,
+      {
+        canChangeInterval,
+        isZeroIntervalAllowed,
+      }: { canChangeInterval: boolean, isZeroIntervalAllowed: boolean }
+    ) => {
+      if (!canChangeInterval) {
+        return;
+      }
+
+      if (this.state.intervalMinutes === 0 && !isZeroIntervalAllowed) {
+        return;
+      }
+      return mutate({
+        variables: {
+          input: {
+            intervalSeconds: Number(this.state.intervalMinutes) * 60,
+          },
+        },
+      });
+    };
+
+    render() {
+      return (
+        <Composed>
+          {({ query, labels }) => {
+            if (query.loading || labels.loading) {
+              return <RowListSkeleton />;
+            }
+            if (query.error) {
+              return <ErrorMessage retry={() => query.refetch()} />;
+            }
+            if (labels.error) {
+              return <ErrorMessage retry={() => labels.refetch()} />;
+            }
+
+            const {
+              data: {
+                player: {
+                  playOk: { realityCheck },
+                },
+              },
+            } = query;
+
+            const intervalMinutes = isNil(this.state.intervalMinutes)
+              ? realityCheck.intervalInMinutes
+              : this.state.intervalMinutes;
+
+            return (
+              <Mutation
+                mutation={UPDATE_REALITY_CHECK_INTERVAL}
+                onError={() => {
+                  this.refresh(query);
+                  launchModal({ modal: MODALS.ERROR });
+                }}
+                onCompleted={() => this.refresh(query)}
+              >
+                {(mutate, { loading: updateLoading, error: updateError }) => (
+                  <Component
+                    labels={labels.data}
+                    onSave={() => this.save(mutate, realityCheck)}
+                    interval={intervalMinutes}
+                    onUpdate={interval =>
+                      this.setState({
+                        intervalMinutes: interval,
+                      })
+                    }
+                    updateLoading={updateLoading}
+                    updateError={updateError}
+                  />
+                )}
+              </Mutation>
+            );
+          }}
+        </Composed>
+      );
+    }
+  };
+
+export default withContainer(RealityCheck);
