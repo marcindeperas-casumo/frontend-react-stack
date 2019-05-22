a245a28109d1129f467ee1f14efba6afebbf9ba0
João Guedes <joao.guedes@casumo.com>
2019-05-22 17:48:26 +0100

    added tests and mocks

---
+++ src/components/Settings/RealityCheck/__mocks__/Mutations.mock.js
@@ -0,0 +1,22 @@
+import { UPDATE_REALITY_CHECK_INTERVAL } from "../Mutations";
+
+export const updateRealityCheckIntervalMock = [0, 600, 900].map(
+  intervalSeconds => ({
+    request: {
+      query: UPDATE_REALITY_CHECK_INTERVAL,
+      variables: { input: { intervalSeconds } },
+    },
+    result: {
+      data: { updateRealityCheckInterval: intervalSeconds },
+    },
+  })
+);
+
+export const updateRealityCheckIntervalErrorMock = updateRealityCheckIntervalMock.map(
+  payload => ({
+    ...payload,
+    result: {
+      errors: [{ foo: "bar" }],
+    },
+  })
+);
---
+++ src/components/Settings/RealityCheck/__mocks__/Queries.mock.js
@@ -0,0 +1,129 @@
+import { REALITY_CHECK_LABELS_QUERY } from "../RealityCheckLabelsQuery";
+import { PLAYER_REALITY_CHECK_QUERY } from "../RealityCheckQuery";
+
+export const playerRealityCheckQueryMock = {
+  request: {
+    query: PLAYER_REALITY_CHECK_QUERY,
+  },
+  result: {
+    data: {
+      player: {
+        __typename: "Player",
+        id: 1,
+        playOk: {
+          __typename: "PlayerPlayOkSettings",
+          realityCheck: {
+            __typename: "PlayerRealityCheckSettings",
+            canChangeInterval: true,
+            intervalInMinutes: 15,
+            isZeroIntervalAllowed: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+export const playerRealityCheckQueryCantChangeIntervalMock = {
+  request: {
+    query: PLAYER_REALITY_CHECK_QUERY,
+  },
+  result: {
+    data: {
+      player: {
+        __typename: "Player",
+        id: 1,
+        playOk: {
+          __typename: "PlayerPlayOkSettings",
+          realityCheck: {
+            __typename: "PlayerRealityCheckSettings",
+            canChangeInterval: false,
+            intervalInMinutes: 15,
+            isZeroIntervalAllowed: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+export const playerRealityCheckQueryNoZeroIntervalMock = {
+  request: {
+    query: PLAYER_REALITY_CHECK_QUERY,
+  },
+  result: {
+    data: {
+      player: {
+        __typename: "Player",
+        id: 1,
+        playOk: {
+          __typename: "PlayerPlayOkSettings",
+          realityCheck: {
+            __typename: "PlayerRealityCheckSettings",
+            canChangeInterval: true,
+            intervalInMinutes: 15,
+            isZeroIntervalAllowed: false,
+          },
+        },
+      },
+    },
+  },
+};
+
+export const playerRealityCheckQueryErrorMock = {
+  ...playerRealityCheckQueryMock,
+  result: {
+    errors: [{ foo: "bar" }],
+  },
+};
+
+export const realityCheckLabelsQueryMock = {
+  request: {
+    query: REALITY_CHECK_LABELS_QUERY,
+  },
+  result: {
+    data: {
+      cancel: "Cancel",
+      inGameSessionUpdatesFrequencyLabel:
+        "How frequently would you like to receive them?",
+      inGameSessionUpdatesLabel: "In-game session updates",
+      save: "Save",
+    },
+  },
+};
+
+export const realityCheckLabelsQueryErrorMock = {
+  ...realityCheckLabelsQueryMock,
+  result: {
+    errors: [{ foo: "bar" }],
+  },
+};
+
+export const playerRealityCheckQueryWithInterval = intervalInMinutes => ({
+  request: {
+    query: PLAYER_REALITY_CHECK_QUERY,
+  },
+  result: {
+    data: {
+      player: {
+        __typename: "Player",
+        id: 1,
+        playOk: {
+          __typename: "PlayerPlayOkSettings",
+          realityCheck: {
+            __typename: "PlayerRealityCheckSettings",
+            canChangeInterval: true,
+            intervalInMinutes: intervalInMinutes,
+            isZeroIntervalAllowed: true,
+          },
+        },
+      },
+    },
+  },
+});
+
+export const withMockQueries = mock => [
+  ...mock,
+  playerRealityCheckQueryMock,
+  realityCheckLabelsQueryMock,
+];
