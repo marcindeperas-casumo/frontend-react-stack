98d16d5380055b92f48ddf4bc89ca8572fc7bc2d
João Guedes <joao.guedes@casumo.com>
2019-05-23 16:19:12 +0100

    added test to Checkbox

--- src/components/Controls/Checkbox/Checkbox.test.js
+++ src/components/Controls/Checkbox/Checkbox.test.js
@@ -3,10 +3,17 @@ import { shallow } from "enzyme";
 import Checkbox from "Components/Controls/Checkbox";
 
 describe("Checkbox", () => {
-  test("should trigger callback with correct value", () => {
+  test("should trigger callback with true", () => {
     const mockFn = jest.fn();
     const rendered = shallow(<Checkbox checked={false} onChange={mockFn} />);
     rendered.simulate("click");
     expect(mockFn).toBeCalledWith(true);
+  });
+
+  test("should trigger callback with false", () => {
+    const mockFn = jest.fn();
+    const rendered = shallow(<Checkbox checked={true} onChange={mockFn} />);
+    rendered.simulate("click");
+    expect(mockFn).toBeCalledWith(false);
   });
 });
