f91cead4ea43dc3e2e5bd2fe3a358537f902c0ed
João Guedes <joao.guedes@casumo.com>
2019-05-23 19:38:42 +0100

    fixed story

--- src/components/Settings/SettingsRow/SettingsRow.stories.js
+++ src/components/Settings/SettingsRow/SettingsRow.stories.js
@@ -6,4 +6,8 @@ import { SettingsRow } from "./SettingsRow";
 
 const stories = storiesOf("Settings/SettingsRow", module);
 
-stories.add("Default", () => <SettingsRow text="a label" />);
+stories.add("Default", () => (
+  <SettingsRow text="a label">
+    <Link label="Go to home" target="http://www.casumo.com" />
+  </SettingsRow>
+));
