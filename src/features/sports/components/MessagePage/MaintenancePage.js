07f13b488445d9bf6d6ee6a4a138465edf1a2501
Charles Oliver <charles@cocoa.consulting>
2019-05-24 16:00:38 +0100

    fix last bloody export

--- src/features/sports/components/MessagePage/MaintenancePage.js
+++ src/features/sports/components/MessagePage/MaintenancePage.js
@@ -3,12 +3,10 @@ import React from "react";
 import { MessagePage } from "./MessagePage";
 import MaintenanceIcon from "./icons/maintenance-icon.svg";
 
-const MaintenancePage = () => (
+export const MaintenancePage = () => (
   <MessagePage
     image={<MaintenanceIcon />}
     headingTermKey="maintenance.heading"
     messageTermKey="maintenance.message"
   />
 );
-
-export { MaintenancePage };
