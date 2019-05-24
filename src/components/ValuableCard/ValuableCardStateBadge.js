99ed0a17916770b023926a57393f28521634b81a
Michelle <michelle.dalli@casumo.com>
2019-05-24 16:41:57 +0200

    removed unnecessary span

--- src/components/ValuableCard/ValuableCardStateBadge.js
+++ src/components/ValuableCard/ValuableCardStateBadge.js
@@ -3,8 +3,6 @@ import React from "react";
 export const ValuableCardStateBadge = ({ className, text, badgeIcon }) => (
   <div className={className}>
     {badgeIcon && badgeIcon()}
-    <span>
-      <strong>{text}</strong>
-    </span>
+    <strong>{text}</strong>
   </div>
 );
