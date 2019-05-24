e6370a96dcfd5bdfa2e6ec68a71ea5ab8b198b5e
Charles Oliver <charles@cocoa.consulting>
2019-05-24 14:04:22 +0100

    extract icons to svg files

--- src/features/sports/components/MessagePage/ComingSoonPage.js
+++ src/features/sports/components/MessagePage/ComingSoonPage.js
@@ -1,20 +1,7 @@
 // @flow
 import React from "react";
 import MessagePage from "./MessagePage";
-
-// prettier-ignore
-const ComingSoonIcon = () => (
-  <svg width="64" height="48" viewBox="0 0 64 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <rect x="10.5" y="1.5" width="52" height="34" rx="2.5" stroke="#C9D6D6" stroke-width="3"/>
-  <path d="M28 46L45 46" stroke="#C9D6D6" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M31 46L32 36" stroke="#C9D6D6" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M42 46L41 36" stroke="#C9D6D6" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-  <rect x="6.5" y="23.5" width="18" height="23" rx="2.5" fill="#EEF6F6" stroke="#303838" stroke-width="3"/>
-  <rect x="1.5" y="32.5" width="10" height="14" rx="2.5" fill="#EEF6F6" stroke="#303838" stroke-width="3"/>
-  <rect x="14" y="41" width="3" height="3" rx="1.5" fill="#303838"/>
-  <rect x="5" y="41" width="3" height="3" rx="1.5" fill="#303838"/>
-  </svg>
-);
+import ComingSoonIcon from "./icons/coming-soon.svg";
 
 const ComingSoonPage = () => (
   <MessagePage
--- src/features/sports/components/MessagePage/MaintenancePage.js
+++ src/features/sports/components/MessagePage/MaintenancePage.js
@@ -1,19 +1,11 @@
 // @flow
 import React from "react";
 import MessagePage from "./MessagePage";
-
-// prettier-ignore
-const WarningIcon = () => (
-  <svg width="64" height="56" viewBox="0 0 64 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="64" height="56"><rect width="64" height="56" fill="#C4C4C4"/></mask>
-  <g mask="url(#mask0)">k<path fill-rule="evenodd" clip-rule="evenodd" d="M30.6011 19.5533C33.1184 18.7 35.8393 20.0836 36.6779 22.6451C37.0197 23.6886 36.9954 24.7676 36.6779 25.7369L33.6395 35.0123C33.36 35.8655 32.453 36.3278 31.6144 36.0434C31.1192 35.8749 30.7561 35.4776 30.6011 35.0123L27.5628 25.7369C26.7242 23.1753 28.0838 20.4067 30.6011 19.5533ZM29.0819 41.1959C29.0819 39.4877 30.4416 38.1041 32.1203 38.1041C33.7975 38.1041 35.1587 39.4877 35.1587 41.1959C35.1587 42.9042 33.7975 44.2876 32.1203 44.2876C30.4416 44.2876 29.0819 42.9042 29.0819 41.1959Z" fill="#303838"/>
-  <path d="M26.8038 5.75343C29.1132 1.75342 34.8867 1.75342 37.1961 5.75342L59.8552 45C62.1646 49 59.2778 54 54.659 54H9.34097C4.72217 54 1.83541 49 4.14481 45L26.8038 5.75343Z" stroke="#C9D6D6" stroke-width="4"/></g>
-  </svg>
-);
+import MaintenanceIcon from "./icons/maintenance-icon.svg";
 
 const MaintenancePage = () => (
   <MessagePage
-    image={<WarningIcon />}
+    image={<MaintenanceIcon />}
     headingTermKey="maintenance.heading"
     messageTermKey="maintenance.message"
   />
