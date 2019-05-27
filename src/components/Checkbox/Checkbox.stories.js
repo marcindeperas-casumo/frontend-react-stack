d9a93eb3f2abfad8288658182b2b7b4f7262c287
João Guedes <joao.guedes@casumo.com>
2019-05-27 15:50:59 +0100

    using hooks instead of stateful components

--- src/components/Checkbox/Checkbox.stories.js
+++ src/components/Checkbox/Checkbox.stories.js
@@ -1,34 +1,17 @@
 // @flow
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { storiesOf } from "@storybook/react";
 import { Checkbox } from "./Checkbox";
 
 const stories = storiesOf("Checkbox", module);
 
-type Props = {};
+stories.add("Default", () => {
+  const [checked, setChecked] = useState(false);
 
-type State = {
-  checked: boolean,
-};
-
-class CheckboxContainer extends Component<Props, State> {
-  state = {
-    checked: false,
-  };
-
-  onChange = checked => {
-    this.setState({ checked });
-  };
-
-  render() {
-    const { checked } = this.state;
-    return (
-      <>
-        <Checkbox checked={this.state.checked} onChange={this.onChange} />
-        <p>I am {checked ? "checked" : "unchecked"}</p>
-      </>
-    );
-  }
-}
-
-stories.add("Default", () => <CheckboxContainer />);
+  return (
+    <>
+      <Checkbox checked={checked} onChange={value => setChecked(value)} />
+      <p>I am {checked ? "checked" : "unchecked"}</p>
+    </>
+  );
+});
--- src/components/Toggle/Toggle.stories.js
+++ src/components/Toggle/Toggle.stories.js
@@ -1,34 +1,17 @@
 // @flow
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
 import { Toggle } from "./Toggle";
 
 const stories = storiesOf("Toggle", module);
 
-type Props = {};
+stories.add("Default", () => {
+  const [checked, setChecked] = useState(false);
 
-type State = {
-  checked: boolean,
-};
-
-class ToggleContainer extends Component<Props, State> {
-  state = {
-    checked: false,
-  };
-
-  onChange = checked => {
-    this.setState({ checked });
-  };
-
-  render() {
-    const { checked } = this.state;
-    return (
-      <>
-        <Toggle checked={this.state.checked} onChange={this.onChange} />
-        <p>I am {checked ? "checked" : "unchecked"}</p>
-      </>
-    );
-  }
-}
-
-stories.add("Default", () => <ToggleContainer />);
+  return (
+    <>
+      <Toggle checked={checked} onChange={value => setChecked(value)} />
+      <p>I am {checked ? "checked" : "unchecked"}</p>
+    </>
+  );
+});
