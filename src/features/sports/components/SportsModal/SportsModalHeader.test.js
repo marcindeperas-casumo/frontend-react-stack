6d9a0c8f533ce842ca411847193bd28da21a1e5b
Charles Oliver <charles@cocoa.consulting>
2019-05-30 14:35:39 +0100

    add failing tests for sports modal header

---
+++ src/features/sports/components/SportsModal/SportsModalHeader.test.js
@@ -0,0 +1,22 @@
+// @flow
+
+describe("SportsModalHeader", () => {
+  test("should render correctly", () => {
+    expect(true).toBe(false);
+  });
+  test("should return a header without dismiss buttons by default", () => {
+    expect(true).toBe(false);
+  });
+
+  test("should return a header without dismiss when the dismissType is 'none'", () => {
+    expect(true).toBe(false);
+  });
+
+  test("should render a back button when the dismissType is 'back'", () => {
+    expect(true).toBe(false);
+  });
+
+  test("should render a close button when the dismissType is 'close'", () => {
+    expect(true).toBe(false);
+  });
+});
