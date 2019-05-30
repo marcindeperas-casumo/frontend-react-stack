c94b10ebeec462afbbb8955ebc1bb9004de37a7c
Michelle <michelle.dalli@casumo.com>
2019-05-30 13:50:44 +0200

    updated valuable type flow

--- src/models/valuables/index.js
+++ src/models/valuables/index.js
@@ -1,1 +1,2 @@
+// @flow
 export * from "./valuables.constants";
--- src/models/valuables/valuables.constants.js
+++ src/models/valuables/valuables.constants.js
@@ -1,18 +1,25 @@
-export const VALUABLE_TYPES = {
+// @flow
+// export type ValuableType =
+//   | "cash"
+//   | "spins"
+//   | "deposit"
+//   | "sport";
+
+export const VALUABLE_TYPES = Object.freeze({
   CASH: "cash",
   SPINS: "spins",
   DEPOSIT: "deposit",
   SPORT: "sport",
-};
+});
 
-export const VALUABLE_SPIN_TYPES = {
+export const VALUABLE_SPIN_TYPES = Object.freeze({
   BASIC_SPINS: "basic_spins",
   BONUS: "bonus",
   SUPER: "super",
   MEGA: "mega",
-};
+});
 
-export const VALUABLE_STATES = {
+export const VALUABLE_STATES = Object.freeze({
   FRESH: "Fresh",
   LOCKED: "Locked",
-};
+});
