9437b564174558f5928c71c8c9e724d9c79b19d9
João Guedes <joao.guedes@casumo.com>
2019-05-30 20:12:51 +0100

    renamed event only

--- src/components/Settings/SettingsAccountDetails/SettingsAccountDetails.js
+++ src/components/Settings/SettingsAccountDetails/SettingsAccountDetails.js
@@ -6,7 +6,7 @@ import { SettingsLabelAndValue } from "Components/Settings/SettingsLabelAndValue
 import { launchModal } from "Services/LaunchModalService";
 import { MODALS, KO_EVENTS } from "Src/constants";
 import { SettingsRow } from "Components/Settings/SettingsRow/SettingsRow";
-import { PASSWORD_PLACEHOLDER_VALUE, onKOEvent } from "./utils";
+import { PASSWORD_PLACEHOLDER_VALUE, onOldStackEvent } from "./utils";
 import EditIcon from "./EditIcon.svg"; //TODO: replace on cudl v2 bump
 
 type Props = {
@@ -38,7 +38,7 @@ export class SettingsAccountDetails extends PureComponent<Props> {
 
   componentDidMount() {
     const { refetchSettings } = this.props;
-    onKOEvent(KO_EVENTS.ACCOUNT_SETTINGS.COMMAND_EXECUTED, data => {
+    onOldStackEvent(KO_EVENTS.ACCOUNT_SETTINGS.COMMAND_EXECUTED, data => {
       if (data.success) {
         refetchSettings();
       }
--- src/components/Settings/SettingsAccountDetails/utils.js
+++ src/components/Settings/SettingsAccountDetails/utils.js
@@ -2,7 +2,7 @@ import { repeat } from "ramda";
 import bridge from "Src/DurandalReactBridge";
 import { REACT_APP_EVENT_ON_CALLBACK } from "Src/constants";
 
-export const onKOEvent = (evt, callback) => {
+export const onOldStackEvent = (evt, callback) => {
   bridge.on(REACT_APP_EVENT_ON_CALLBACK, ({ event, data }) => {
     if (evt === event) {
       callback(data);
