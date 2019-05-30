e39f753622f6deb828981f65bb17775fe5dc1266
Michelle <michelle.dalli@casumo.com>
2019-05-30 15:32:33 +0200

    PR fixes

--- src/components/ValuableCard/ValuableCard.utils.test.js
+++ src/components/ValuableCard/ValuableCard.utils.test.js
@@ -1,5 +1,15 @@
-import { VALUABLE_SPIN_TYPES } from "Models/valuables";
-import { CoinValueToSpinType } from "./ValuableCard.utils";
+import {
+  VALUABLE_SPIN_TYPES,
+  VALUABLE_TYPES,
+  VALUABLE_STATES,
+} from "Models/valuables";
+import {
+  CoinValueToSpinType,
+  GetCardUrl,
+  VALUABLE_LOCKED_URL,
+  VALUABLE_SPINS_URL,
+  VALUABLE_DEPOSIT_URL,
+} from "./ValuableCard.utils";
 
 describe("ValuableCard.utils", () => {
   test("should return BASIC spinType when coinValue < 0.3", () => {
@@ -28,5 +38,29 @@ describe("ValuableCard.utils", () => {
     const expectedValue = VALUABLE_SPIN_TYPES.MEGA;
 
     expect(CoinValueToSpinType(coinValue)).toBe(expectedValue);
+  });
+
+  test("should return the lock url if valuable type is locked and type is spins", () => {
+    expect(GetCardUrl(VALUABLE_STATES.LOCKED, VALUABLE_TYPES.SPINS)).toEqual(
+      VALUABLE_LOCKED_URL
+    );
+  });
+
+  test("should return the deposit url when state is not locked and type is deposit", () => {
+    expect(GetCardUrl(VALUABLE_STATES.FRESH, VALUABLE_TYPES.DEPOSIT)).toEqual(
+      VALUABLE_DEPOSIT_URL
+    );
+  });
+
+  test("should return null when state is not locked and type is cash", () => {
+    expect(GetCardUrl(VALUABLE_STATES.FRESH, VALUABLE_TYPES.CASH)).toEqual(
+      null
+    );
+  });
+
+  test("should return spins url when state is not locked and type is spins", () => {
+    expect(GetCardUrl(VALUABLE_STATES.FRESH, VALUABLE_TYPES.SPINS)).toEqual(
+      VALUABLE_SPINS_URL
+    );
   });
 });
