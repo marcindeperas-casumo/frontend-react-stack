5047b25eb541edbf9e0578a00031152d4545f467
Michelle <michelle.dalli@casumo.com>
2019-05-31 12:52:02 +0200

    removed chromatic check

--- src/components/PlayerValuableListHorizontal/PlayerValuableListHorizontal.stories.js
+++ src/components/PlayerValuableListHorizontal/PlayerValuableListHorizontal.stories.js
@@ -3,22 +3,19 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { mockValuables } from "Components/ValuableCard/__mocks__/Valuable.mock";
 import { PlayerValuableListHorizontal } from "Components/PlayerValuableListHorizontal/PlayerValuableListHorizontal";
-import isNotChromatic from "Storybook/isNotChromatic";
 
 const stories = storiesOf("PlayerValuableListHorizontal", module);
 
-if (isNotChromatic) {
-  stories.add("PlayerValuableListHorizontal (Connected)", () => {
-    const valuables = mockValuables();
-    const consumeValuable = () => {};
+stories.add("PlayerValuableListHorizontal (Connected)", () => {
+  const valuables = mockValuables();
+  const consumeValuable = () => {};
 
-    return (
-      <PlayerValuableListHorizontal
-        loading={false}
-        title={"Player Valuables"}
-        valuables={valuables}
-        onConsumeValuable={consumeValuable}
-      />
-    );
-  });
-}
+  return (
+    <PlayerValuableListHorizontal
+      loading={false}
+      title={"Player Valuables"}
+      valuables={valuables}
+      onConsumeValuable={consumeValuable}
+    />
+  );
+});
