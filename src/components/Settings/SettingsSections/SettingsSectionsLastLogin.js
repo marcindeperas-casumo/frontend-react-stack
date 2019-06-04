bf73c4ace5222e06524ec379e5d78a0317c29b6d
João Guedes <joao.guedes@casumo.com>
2019-06-04 16:58:26 +0100

    added account activity and logout

--- src/components/Settings/SettingsSections/SettingsSectionsLastLogin.js
+++ src/components/Settings/SettingsSections/SettingsSectionsLastLogin.js
@@ -5,6 +5,7 @@ import Text from "@casumo/cmp-text";
 import Timer from "Components/Timer";
 import { ContentReplacer } from "Components/ContentReplacer";
 import { launchModal } from "Services/LaunchModalService";
+import { logout } from "Services/Logout";
 import { MODALS } from "Src/constants";
 
 type SettingsSectionsLastLoginType = {
@@ -23,6 +24,12 @@ export const SettingsSectionsLastLogin = ({
   time,
 }: SettingsSectionsLastLoginType) => {
   const dateObject = DateTime.fromMillis(time);
+
+  const openAccountActivity = () =>
+    launchModal({
+      modal: MODALS.ACCOUNT_SETTINGS.SHOW_ACCOUNT_ACTIVITY,
+    });
+
   return (
     <div className="c-bottom-bar u-text-align-center u-line-height--15">
       <div>
@@ -46,22 +53,14 @@ export const SettingsSectionsLastLogin = ({
       <Text tag="p">
         <a
           className="u-cursor-pointer u-font-weight-bold"
-          onClick={() =>
-            launchModal({
-              modal: MODALS.ACCOUNT_SETTINGS.SHOW_ACCOUNT_ACTIVITY,
-            })
-          }
+          onClick={openAccountActivity}
         >
           {accountActivityLabel}
         </a>
       </Text>
       <a
         className="u-padding--xlg u-margin-top--md t-background-white u-cursor-pointer u-display--block"
-        onClick={() =>
-          launchModal({
-            modal: "logout",
-          })
-        }
+        onClick={logout}
       >
         <Text tag="p" className="t-color-red u-font-weight-bold">
           {logoutLabel}
