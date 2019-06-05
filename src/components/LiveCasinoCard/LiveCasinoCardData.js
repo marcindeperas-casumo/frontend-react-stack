cb53b32c3ad223dc2141654d6492332bb171fe50
Ricardo Ferreira <ricardo.ferreira@casumo.com>
2019-06-05 14:47:15 +0200

    fix(LiveCasinoCardData): Add the missing key for each `Flex.Item`

--- src/components/LiveCasinoCard/LiveCasinoCardData.js
+++ src/components/LiveCasinoCard/LiveCasinoCardData.js
@@ -32,7 +32,7 @@ const renderResults = ({ results, type }) => {
           const color = getBadgeColor(type, n);
           const borderColor = getBadgeBorderColor(type, n);
           return (
-            <Flex.Item>
+            <Flex.Item key={i}>
               <Badge
                 size="sm"
                 key={i}
