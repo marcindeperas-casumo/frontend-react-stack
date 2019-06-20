01b467a1d88adfe04f2ce806eb1b2970c1bb8a25
Michele Settepani <michele.settepani@casumo.com>
2019-06-20 12:21:04 +0200

    GameListHorizontal: fix tests

--- src/components/GameListHorizontal/GameListHorizontal.test.js
+++ src/components/GameListHorizontal/GameListHorizontal.test.js
@@ -5,49 +5,56 @@ import {
   ITEM_SPACING,
   ITEM_RENDERERS,
 } from "Components/GameListHorizontal/GameListHorizontal";
+import ScrollableList from "Components/ScrollableList";
+import { GameListHorizontalSkeleton } from "Components/GameListHorizontal/GameListHorizontalSkeleton";
+import { Desktop, Mobile } from "Components/ResponsiveLayout";
 
 const getList = id => ({ id, title: "Title.", games: ["game-1", "game-2"] });
 const list = getList("id-1");
 
-describe("GameListHorizontal", () => {
+describe("GameListHorizontal Mobile", () => {
   let rendered;
 
   beforeEach(() => {
-    rendered = shallow(<GameListHorizontal list={list} isLoading={false} />);
+    rendered = shallow(
+      <GameListHorizontal list={list} isLoading={false} />
+    ).find(Mobile);
   });
 
   test("renders a ScrollableList", () => {
-    expect(rendered.find("ScrollableList")).toHaveLength(1);
+    expect(rendered.find(ScrollableList)).toHaveLength(1);
   });
 
   test("passes the list title to the ScrollableList", () => {
-    const { title } = rendered.find("ScrollableList").props();
+    const { title } = rendered.find(ScrollableList).props();
 
     expect(title).toBe(list.title);
   });
 
   test("passes the game-ids to the ScrollableList", () => {
-    const { itemIds } = rendered.find("ScrollableList").props();
+    const { itemIds } = rendered.find(ScrollableList).props();
 
     expect(itemIds).toBe(list.games);
   });
 
   test("uses the default spacing if it is not specified", () => {
-    const { spacing } = rendered.find("ScrollableList").props();
+    const { spacing } = rendered.find(ScrollableList).props();
 
     expect(spacing).toBe(ITEM_SPACING.default);
   });
 
   test("uses the default renderer if it is not specified", () => {
-    const { Component } = rendered.find("ScrollableList").props();
+    const { Component } = rendered.find(ScrollableList).props();
 
     expect(Component).toEqual(ITEM_RENDERERS.default);
   });
 
   test("speficies spacing by list id", () => {
     Object.keys(ITEM_SPACING).forEach(listId => {
-      rendered = shallow(<GameListHorizontal list={getList(listId)} />);
-      const { spacing } = rendered.props();
+      rendered = shallow(<GameListHorizontal list={getList(listId)} />).find(
+        "Mobile"
+      );
+      const { spacing } = rendered.find(ScrollableList).props();
 
       expect(spacing).toBe(ITEM_SPACING[listId]);
     });
@@ -55,7 +62,9 @@ describe("GameListHorizontal", () => {
 
   test("speficies renderer by list id", () => {
     Object.keys(ITEM_RENDERERS).forEach(listId => {
-      rendered = shallow(<GameListHorizontal list={getList(listId)} />);
+      rendered = shallow(<GameListHorizontal list={getList(listId)} />).find(
+        ScrollableList
+      );
       const { Component } = rendered.props();
 
       expect(Component).toEqual(ITEM_RENDERERS[listId]);
@@ -65,8 +74,8 @@ describe("GameListHorizontal", () => {
   test("displays a skeleton if the game-list is still loading", () => {
     rendered = shallow(<GameListHorizontal list={list} isLoading={true} />);
 
-    expect(rendered.find("GameListHorizontalSkeleton")).toHaveLength(1);
-    expect(rendered.find("ScrollableList")).toHaveLength(0);
+    expect(rendered.find(GameListHorizontalSkeleton)).toHaveLength(1);
+    expect(rendered.find(ScrollableList)).toHaveLength(0);
   });
 
   test("does not render anything if it is loaded but has no games", () => {
@@ -75,3 +84,5 @@ describe("GameListHorizontal", () => {
     expect(rendered.html()).toBeNull();
   });
 });
+
+describe("GameListHorizontal Desktop", () => {});
