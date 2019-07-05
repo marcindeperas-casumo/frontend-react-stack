1a1ce24fc7d821c90b61733841edc5f5284d8a6f
João Guedes <joao.guedes@casumo.com>
2019-07-05 16:02:04 +0100

    added query and type

---
+++ flow-typed/graphql/ValuableDetailsLabelsQuery.js
@@ -0,0 +1,34 @@
+/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: ValuableDetailsLabelsQuery
+// ====================================================
+
+export type ValuableDetailsLabelsQuery = {
+  termsAndConditionsTitle: string,
+  termsAndConditionsContent: string,
+  playNow: string,
+  playToUnlock: string,
+  depositNow: string,
+  depositToUnlock: string,
+};/* @flow */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export type ContactSettingsInput = {|
+  on: boolean
+|};
+
+export type UpdateRealityCheckIntervalInput = {|
+  intervalSeconds: number
+|};
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
