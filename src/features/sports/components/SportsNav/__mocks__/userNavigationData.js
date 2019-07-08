8c3e63e300f4e4443b120176e7f9e568d34e0b89
Charles Oliver <charles@cocoa.consulting>
2019-07-08 11:39:00 +0100

    fix svg handling for active indicator to expect xml instead of img src

--- src/features/sports/components/SportsNav/__mocks__/userNavigationData.js
+++ src/features/sports/components/SportsNav/__mocks__/userNavigationData.js
@@ -1,7 +1,6 @@
 import { range } from "ramda";
 
-const activeIndicator =
-  "https://cms.casumo.com/wp-content/uploads/2019/03/sports-active-indicator-default.svg";
+const activeIndicator = `<?xml version="1.0" encoding="UTF-8"?> <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none"> <g style="mix-blend-mode:multiply"> <rect x="20" y="19" width="22" height="22" rx="11" fill="currentColor"></rect> </g> </svg>`;
 
 export const userNavigationData = [
   {
