e8dbc315e918170bddeb252f912bb7f7e49432ea
Cho <choishere@gmail.com>
2019-07-09 10:40:32 +0200

    PromotionTeaserList use React Hooks

--- src/components/PromotionTeaserList/PromotionTeaserList.js
+++ src/components/PromotionTeaserList/PromotionTeaserList.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from "react";
+import React from "react";
 import { PromotionTeaserRow } from "Components/PromotionTeaserRow";
 
 type Props = {
@@ -8,31 +8,28 @@ type Props = {
   fetchPromotions: () => void,
 };
 
-export class PromotionTeaserList extends PureComponent<Props> {
-  componentDidMount() {
-    this.props.fetchCampaign();
-    this.props.fetchPromotions();
+export function PromotionTeaserList(props: Props) {
+  React.useEffect(() => {
+    props.fetchCampaign();
+    props.fetchPromotions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!props.promotionsSlugs.length) {
+    return null;
   }
 
-  render() {
-    const { promotionsSlugs } = this.props;
-
-    if (!promotionsSlugs.length) {
-      return null;
-    }
-
-    return (
-      <div className="u-padding-bottom--xlg">
-        <div className="u-padding-x--lg u-padding-y">
-          {promotionsSlugs.map(promotionSlug => (
-            <PromotionTeaserRow
-              slug={`promotions.${promotionSlug}`}
-              link={`promotions/${promotionSlug}`}
-              key={promotionSlug}
-            />
-          ))}
-        </div>
+  return (
+    <div className="u-padding-bottom--xlg">
+      <div className="u-padding-x--lg u-padding-y">
+        {props.promotionsSlugs.map(promotionSlug => (
+          <PromotionTeaserRow
+            slug={`promotions.${promotionSlug}`}
+            link={`promotions/${promotionSlug}`}
+            key={promotionSlug}
+          />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
