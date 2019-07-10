aa58044333f0f1b38c305c809c6d10e9af7354bc
Levente Balogh <balogh.levente.hu@gmail.com>
2019-07-10 15:09:46 +0200

    "Fix" ESLint errors.

--- src/features/sports/components/SportsNav/index.js
+++ src/features/sports/components/SportsNav/index.js
@@ -1,6 +1,8 @@
 // @flow
+/* eslint-disable import/export */
 export * from "Features/sports/components/SportsNav/SportsNav";
 export * from "Features/sports/components/SportsNav/SportsSubNav/SportsSubNav";
 export * from "Features/sports/components/SportsNav/SportsMainNav/SportsMainNav";
 export * from "Features/sports/components/SportsNav/SportsNavPagerButtons/SportsNavPagerButtons";
 export type * from "./types";
+/* eslint-enable import/export */
