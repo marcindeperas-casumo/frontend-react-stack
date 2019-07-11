275bcd3511ee2f91d54d77ced824c544f52606a5
Michele Settepani <michele.settepani@casumo.com>
2019-07-11 16:37:36 +0200

    Jackpots: remove unused import

--- src/components/Jackpots/Jackpots.test.js
+++ src/components/Jackpots/Jackpots.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import Jackpots from "Components/Jackpots/Jackpots";
 import jackpotsMock from "./__mocks__/response.games.mock";
 
