01ef96f38c41347c30c55ae89f4dfb654b8019c2
Michele Settepani <michele.settepani@casumo.com>
2019-07-24 12:00:20 +0200

    Jackpots: remove unused shallow import

--- src/components/Jackpots/Jackpots.test.js
+++ src/components/Jackpots/Jackpots.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import { setDesktopViewport, setMobileViewport } from "Utils/testUtils";
 import Jackpots from "Components/Jackpots/Jackpots";
 import jackpotsMock from "./__mocks__/response.games.mock";
