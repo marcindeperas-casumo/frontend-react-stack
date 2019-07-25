2c842794c19d7905a473408306c64bfe4350b391
Michelle <michelle.dalli@casumo.com>
2019-07-25 14:39:39 +0200

    moved dimensions as props

--- src/components/ValuableDetails/ValuableDetailsHeaderBackground.js
+++ src/components/ValuableDetails/ValuableDetailsHeaderBackground.js
@@ -3,11 +3,6 @@ import React from "react";
 import Flex from "@casumo/cmp-flex";
 import MaskImage from "Components/MaskImage";
 
-const dimensions = {
-  width: 379,
-  height: 271,
-};
-
 const ImgMask = () => (
   <path d="M378 261.753C238.58 277.769 68.4582 269.761 -1 261.753V0H376.993L378 261.753Z" />
 );
@@ -17,9 +12,17 @@ type Props = {
   id: string,
   /* Url of the image to be displayed as the background */
   imageUrl: string,
+  /* width of background */
+  width: number,
+  /* height of background */
+  height: number,
 };
 
-export const ValuableDetailsHeaderBackground = ({ id, imageUrl }: Props) => {
+export const ValuableDetailsHeaderBackground = ({
+  id,
+  imageUrl,
+  ...dimensions
+}: Props) => {
   return (
     <Flex.Item>
       <MaskImage
