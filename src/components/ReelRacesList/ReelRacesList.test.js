38b6e17120b9fbb9e5811f8158803c8cb8a5ad09
Michele Settepani <michele.settepani@casumo.com>
2019-07-25 12:19:38 +0200

    wip

--- src/components/ReelRacesList/ReelRacesList.test.js
+++ src/components/ReelRacesList/ReelRacesList.test.js
@@ -1,7 +1,10 @@
 // @flow
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import ScrollableList from "Components/ScrollableList";
+import { setDesktopViewport, setMobileViewport } from "Utils/testUtils";
+import MockStore from "Components/MockStore/index";
+import defaultState from "Models/__mocks__/state.mock";
 import { ReelRacesList } from "./ReelRacesList";
 
 const props = {
@@ -12,6 +15,7 @@ const props = {
   unsubscribeReelRacesUpdates: jest.fn(),
   areTranslationsFetched: true,
   reelRacesIds: ["1", "2", "3", "a"],
+  startTime: 1541613600000,
   t: {
     more_link: "more_link",
     spins: "spins",
@@ -29,51 +33,85 @@ const props = {
   },
 };
 
-describe("ReelRacesList", () => {
+describe("<ReelRacesList /> - Mobile", () => {
   let rendered;
 
   beforeEach(() => {
-    jest.clearAllMocks();
-    rendered = shallow(<ReelRacesList {...props} />);
+    setMobileViewport();
+    rendered = mount(
+      <MockStore state={defaultState}>
+        <ReelRacesList {...props} />
+      </MockStore>
+    );
   });
 
-  test("renders a ScrollableList", () => {
-    expect(rendered.find(ScrollableList)).toHaveLength(1);
-  });
-
-  test("passes the list title to the ScrollableList", () => {
-    const { title } = rendered.find("ScrollableList").props();
-
-    expect(title).toBe(props.t.title);
-  });
-
-  test("passes the game-ids to the ScrollableList", () => {
-    const { itemIds } = rendered.find("ScrollableList").props();
-
-    expect(itemIds).toBe(props.reelRacesIds);
-  });
-
-  test("only fetches reel races data once", () => {
-    const { fetchReelRaces } = props;
-
-    expect(fetchReelRaces).toBeCalledTimes(1);
-
-    const rendered_again = shallow(<ReelRacesList {...props} isFetched />);
-    const { title } = rendered_again.find("ScrollableList").props();
-
-    // lets make sure the second rendering actually renders something
-    expect(title).toBe(props.t.title);
-
-    expect(fetchReelRaces).toBeCalledTimes(1);
-  });
-
-  test("subscription to reel race channels on mount", () => {
-    expect(props.subscribeReelRacesUpdates).toBeCalledTimes(1);
-  });
-
-  test("unsubscription to reel race channels on unmount", () => {
-    rendered.unmount();
-
-    expect(props.unsubscribeReelRacesUpdates).toBeCalledTimes(1);
+  test("should not render ScrollableListPaginated component", () => {
+    expect(rendered.find("ScrollableListPaginated")).toHaveLength(0);
   });
 });
+
+describe("<ReelRacesList /> - Desktop", () => {
+  let rendered;
+
+  beforeEach(() => {
+    setDesktopViewport();
+    rendered = mount(
+      <MockStore state={defaultState}>
+        <ReelRacesList {...props} />
+      </MockStore>
+    );
+  });
+
+  test("should render ScrollableListPaginated component", () => {
+    expect(rendered.find("ScrollableListPaginated")).toHaveLength(1);
+  });
+});
+
+// describe("ReelRacesList", () => {
+//   let rendered;
+
+//   beforeEach(() => {
+//     jest.clearAllMocks();
+//     rendered = shallow(<ReelRacesList {...props} />);
+//   });
+
+//   test("renders a ScrollableList", () => {
+//     expect(rendered.find(ScrollableList)).toHaveLength(1);
+//   });
+
+//   test("passes the list title to the ScrollableList", () => {
+//     const { title } = rendered.find("ScrollableList").props();
+
+//     expect(title).toBe(props.t.title);
+//   });
+
+//   test("passes the game-ids to the ScrollableList", () => {
+//     const { itemIds } = rendered.find("ScrollableList").props();
+
+//     expect(itemIds).toBe(props.reelRacesIds);
+//   });
+
+//   test("only fetches reel races data once", () => {
+//     const { fetchReelRaces } = props;
+
+//     expect(fetchReelRaces).toBeCalledTimes(1);
+
+//     const rendered_again = shallow(<ReelRacesList {...props} isFetched />);
+//     const { title } = rendered_again.find("ScrollableList").props();
+
+//     // lets make sure the second rendering actually renders something
+//     expect(title).toBe(props.t.title);
+
+//     expect(fetchReelRaces).toBeCalledTimes(1);
+//   });
+
+//   test("subscription to reel race channels on mount", () => {
+//     expect(props.subscribeReelRacesUpdates).toBeCalledTimes(1);
+//   });
+
+//   test("unsubscription to reel race channels on unmount", () => {
+//     rendered.unmount();
+
+//     expect(props.unsubscribeReelRacesUpdates).toBeCalledTimes(1);
+//   });
+// });
