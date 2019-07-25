043575aa42419447e8a2c8e6c216ac36c351b45d
Michele Settepani <michele.settepani@casumo.com>
2019-07-25 15:59:55 +0200

    ReelRacesList: test responsive conditional rendering

--- src/components/ReelRacesList/ReelRacesList.test.js
+++ src/components/ReelRacesList/ReelRacesList.test.js
@@ -14,8 +14,10 @@ const props = {
   subscribeReelRacesUpdates: jest.fn(),
   unsubscribeReelRacesUpdates: jest.fn(),
   areTranslationsFetched: true,
-  reelRacesIds: ["1", "2", "3", "a"],
-  startTime: 1541613600000,
+  reelRacesIds: [
+    "edc71c70-56d6-11e9-8587-0242ac11000b",
+    "c21ee900-560d-11e9-8587-0242ac11000b",
+  ],
   t: {
     more_link: "more_link",
     spins: "spins",
@@ -37,6 +39,7 @@ describe("<ReelRacesList /> - Mobile", () => {
   let rendered;
 
   beforeEach(() => {
+    jest.clearAllMocks();
     setMobileViewport();
     rendered = mount(
       <MockStore state={defaultState}>
@@ -47,6 +50,50 @@ describe("<ReelRacesList /> - Mobile", () => {
 
   test("should not render ScrollableListPaginated component", () => {
     expect(rendered.find("ScrollableListPaginated")).toHaveLength(0);
+  });
+
+  test("renders a ScrollableList", () => {
+    expect(rendered.find(ScrollableList)).toHaveLength(1);
+  });
+
+  test("passes the list title to the ScrollableList", () => {
+    const { title } = rendered.find("ScrollableList").props();
+
+    expect(title).toBe(props.t.title);
+  });
+
+  test("passes the game-ids to the ScrollableList", () => {
+    const { itemIds } = rendered.find("ScrollableList").props();
+
+    expect(itemIds).toBe(props.reelRacesIds);
+  });
+
+  test("only fetches reel races data once", () => {
+    const { fetchReelRaces } = props;
+
+    expect(fetchReelRaces).toBeCalledTimes(1);
+
+    const rendered_again = mount(
+      <MockStore state={defaultState}>
+        <ReelRacesList {...props} isFetched />
+      </MockStore>
+    );
+    const { title } = rendered_again.find("ScrollableList").props();
+
+    // lets make sure the second rendering actually renders something
+    expect(title).toBe(props.t.title);
+
+    expect(fetchReelRaces).toBeCalledTimes(1);
+  });
+
+  test("subscription to reel race channels on mount", () => {
+    expect(props.subscribeReelRacesUpdates).toBeCalledTimes(1);
+  });
+
+  test("unsubscription to reel race channels on unmount", () => {
+    rendered.unmount();
+
+    expect(props.unsubscribeReelRacesUpdates).toBeCalledTimes(1);
   });
 });
 
@@ -65,53 +112,8 @@ describe("<ReelRacesList /> - Desktop", () => {
   test("should render ScrollableListPaginated component", () => {
     expect(rendered.find("ScrollableListPaginated")).toHaveLength(1);
   });
+
+  test("doesn't render a ScrollableList", () => {
+    expect(rendered.find(ScrollableList)).toHaveLength(0);
+  });
 });
-
-// describe("ReelRacesList", () => {
-//   let rendered;
-
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//     rendered = shallow(<ReelRacesList {...props} />);
-//   });
-
-//   test("renders a ScrollableList", () => {
-//     expect(rendered.find(ScrollableList)).toHaveLength(1);
-//   });
-
-//   test("passes the list title to the ScrollableList", () => {
-//     const { title } = rendered.find("ScrollableList").props();
-
-//     expect(title).toBe(props.t.title);
-//   });
-
-//   test("passes the game-ids to the ScrollableList", () => {
-//     const { itemIds } = rendered.find("ScrollableList").props();
-
-//     expect(itemIds).toBe(props.reelRacesIds);
-//   });
-
-//   test("only fetches reel races data once", () => {
-//     const { fetchReelRaces } = props;
-
-//     expect(fetchReelRaces).toBeCalledTimes(1);
-
-//     const rendered_again = shallow(<ReelRacesList {...props} isFetched />);
-//     const { title } = rendered_again.find("ScrollableList").props();
-
-//     // lets make sure the second rendering actually renders something
-//     expect(title).toBe(props.t.title);
-
-//     expect(fetchReelRaces).toBeCalledTimes(1);
-//   });
-
-//   test("subscription to reel race channels on mount", () => {
-//     expect(props.subscribeReelRacesUpdates).toBeCalledTimes(1);
-//   });
-
-//   test("unsubscription to reel race channels on unmount", () => {
-//     rendered.unmount();
-
-//     expect(props.unsubscribeReelRacesUpdates).toBeCalledTimes(1);
-//   });
-// });
