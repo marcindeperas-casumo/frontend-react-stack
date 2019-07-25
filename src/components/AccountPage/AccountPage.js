bacf2a3f4f2f63e7af61c49325e5e8c33c34f3f6
Dave Calleja <dave.calleja@casumo.com>
2019-07-25 10:55:16 +0200

    Added note about temp account page

--- src/components/AccountPage/AccountPage.js
+++ src/components/AccountPage/AccountPage.js
@@ -1,4 +1,7 @@
 // @flow
+// This is a temporary page to be able to view the valuables while on production
+// Route is only available to those who know about it
+
 import React from "react";
 import AdventureCard from "Components/AdventureCard";
 import {
