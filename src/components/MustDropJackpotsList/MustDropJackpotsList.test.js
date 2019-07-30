762f9c41b78077a0bbbb36e8ddefecc0ebdc9218
Michele Settepani <michele.settepani@casumo.com>
2019-07-30 10:04:06 +0200

    MustDropJackpotsList: test real components with find

--- src/components/MustDropJackpotsList/MustDropJackpotsList.test.js
+++ src/components/MustDropJackpotsList/MustDropJackpotsList.test.js
@@ -3,8 +3,11 @@ import { mount } from "enzyme";
 import * as R from "ramda";
 import MustDropJackpotsList from "Components/MustDropJackpotsList/MustDropJackpotsList";
 import { setDesktopViewport, setMobileViewport } from "Utils/testUtils";
+import { ScrollableListTitleRow } from "Components/ScrollableListTitleRow";
+import JackpotsListTile from "Components/JackpotsListTile";
 import MockStore from "Components/MockStore/index";
 import defaultState from "Models/__mocks__/state.mock";
+import { ScrollableListPaginated } from "Components/ScrollableListPaginated";
 
 const ids = R.map(R.toString, R.range(0, 8));
 
@@ -17,17 +20,17 @@ describe("<MustDropJackpotsList /> - Mobile", () => {
   );
 
   test("should not render ScrollableListPaginated component", () => {
-    expect(rendered.find("ScrollableListPaginated")).toHaveLength(0);
+    expect(rendered.find(ScrollableListPaginated)).toHaveLength(0);
   });
 
   test("passes down jackpot-ids to the tiles", () => {
-    const firstTile = rendered.find("JackpotsListTile").first();
+    const firstTile = rendered.find(JackpotsListTile).first();
 
     expect(firstTile.props().ids).toEqual(["0", "1", "2"]);
   });
 
-  test("Should render a ScrollableListTitle component", () => {
-    expect(rendered.find("ScrollableListTitle")).toHaveLength(1);
+  test("Should render a ScrollableListTitleRow component", () => {
+    expect(rendered.find(ScrollableListTitleRow)).toHaveLength(1);
   });
 
   test("Should render a MustDropJackpotsWidgetContainer component", () => {
@@ -35,7 +38,7 @@ describe("<MustDropJackpotsList /> - Mobile", () => {
   });
 
   test("renders tiles for every 3 game", () => {
-    expect(rendered.find("JackpotsListTile").length).toBe(3);
+    expect(rendered.find(JackpotsListTile).length).toBe(3);
   });
 });
 
@@ -48,11 +51,11 @@ describe("<MustDropJackpotsList /> - Desktop", () => {
   );
 
   test("Should render a ScrollableListPaginated component", () => {
-    expect(rendered.find("ScrollableListPaginated")).toHaveLength(1);
+    expect(rendered.find(ScrollableListPaginated)).toHaveLength(1);
   });
 
-  test("Should render a ScrollableListTitle component", () => {
-    expect(rendered.find("ScrollableListTitle")).toHaveLength(1);
+  test("Should render a ScrollableListTitleRow component", () => {
+    expect(rendered.find(ScrollableListTitleRow)).toHaveLength(1);
   });
 
   test("Should render a link to must-drop-jackpots pages with the right URL and text", () => {
