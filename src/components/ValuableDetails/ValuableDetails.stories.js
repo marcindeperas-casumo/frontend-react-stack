68d0c384671c365e0acecc9a6d6b7a9c235c136b
Michelle <michelle.dalli@casumo.com>
2019-07-30 15:00:51 +0200

    placed valuableCard

--- src/components/ValuableDetails/ValuableDetails.stories.js
+++ src/components/ValuableDetails/ValuableDetails.stories.js
@@ -1,6 +1,8 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import mockValuable from "Components/ValuableCard/__mocks__/Valuable.json";
+import { ValuableCard } from "Components/ValuableCard";
 import translations from "./__mocks__/Translations.json";
 import mock from "./__mocks__/Valuables.json";
 import { ValuableDetails } from "./ValuableDetails";
@@ -8,16 +10,6 @@ import { ValuableDetails } from "./ValuableDetails";
 const stories = storiesOf("ValuableDetails/ValuableDetails", module);
 
 stories.add("Default", () => {
-  const ValuableCard = () => (
-    <div
-      style={{
-        height: "218px",
-        width: "160px",
-        background: "white",
-        "box-shadow": "0px 10px 8px 0px rgba(201, 214, 214, 0.4)",
-      }}
-    ></div>
-  );
   const valuableMock = mock[0];
 
   return (
@@ -26,7 +18,7 @@ stories.add("Default", () => {
       translations={translations}
       width={379}
       height={271}
-      valuableRenderer={<ValuableCard />}
+      valuableRenderer={<ValuableCard {...mockValuable[0]} />}
     />
   );
 });
