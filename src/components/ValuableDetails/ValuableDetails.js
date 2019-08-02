5e1e4d05a652fef2e4b874268615195236ce28e9
Michelle <michelle.dalli@casumo.com>
2019-08-02 16:08:58 +0200

    ValuableDetails: expiration badge logic

--- src/components/ValuableDetails/ValuableDetails.js
+++ src/components/ValuableDetails/ValuableDetails.js
@@ -5,12 +5,35 @@ import Badge from "@casumo/cmp-badge";
 import Text from "@casumo/cmp-text";
 import classNames from "classnames";
 import MaskImage from "Components/MaskImage";
+import { interpolate } from "Utils";
 
-type Translations = {
+export const expirationBadgeClasses = {
+  red: "t-background-red",
+  grey: "t-background-grey-light-1",
+};
+
+type badgeInfoType = {
+  key: string,
+  value: number,
+};
+
+type DurationTranslations = {
+  hours: {
+    plural: string,
+    singular: string,
+  },
+  days: {
+    plural: string,
+    singular: string,
+  },
+};
+
+type Translations = DurationTranslations & {
   playNowLabel: string,
   playToUnlockLabel: string,
   depositToUnlockLabel: string,
   termsAndConditionLabel: string,
+  expirationTimeLabel: string,
 };
 
 type Props = {
@@ -34,79 +57,127 @@ const HeaderImgMask = () => (
   <path d="M378 261.753C238.58 277.769 68.4582 269.761 -1 261.753V0H376.993L378 261.753Z" />
 );
 
-export const ValuableDetails = ({
-  id,
-  backgroundImageUrl,
-  details,
-  caveat,
-  termsContent,
-  expirationTimeInHours,
-  translations,
-  children,
-}: Props) => {
-  const { termsAndConditionLabel } = translations;
-
-  return (
-    <>
-      <div className="o-ratio o-ratio--valuable-details">
-        <div className="o-ratio__content c-valuable-details__header">
-          <MaskImage
-            id={`${id}-detail`}
-            imageUrl={backgroundImageUrl}
-            width={375}
-            height={334}
-          >
-            <HeaderImgMask />
-          </MaskImage>
-        </div>
-        <Flex
-          className="o-ratio__content"
-          justify="end"
-          align="center"
-          direction="vertical"
-        >
-          <div data-test-id="valuable-renderer-wrapper">{children}</div>
-        </Flex>
-      </div>
-      <div className="u-margin-top--2xlg">
-        <Flex direction="vertical" align="center" className="u-padding-x--md">
-          <Flex.Item>
-            <Text tag="p" size="md">
-              {details}
-            </Text>
-          </Flex.Item>
-          <Flex.Item className="u-margin-top--lg">
-            <Badge
-              tag="p"
-              size="sm"
-              className={classNames(
-                "u-text-transform-uppercase u-font-weight-bold",
-                expirationTimeInHours > 24 && "t-background-grey-light-1"
-              )}
-            >
-              Expires In {expirationTimeInHours} HOURS
-            </Badge>
-          </Flex.Item>
-          <Flex.Item className="u-margin-top--lg">
-            <Text tag="p" className="t-color-grey" size="sm">
-              {caveat}
-            </Text>
-          </Flex.Item>
-          <Flex.Item className="u-width--1/3 u-margin-y--xlg">
-            <hr className="t-color-grey-light-2 t-border-width--md t-border-r--pill" />
-          </Flex.Item>
-          <Flex.Item>
-            <Text tag="strong" className="t-color-grey" size="sm">
-              {termsAndConditionLabel}
-            </Text>
-          </Flex.Item>
-          <Flex.Item>
-            <Text tag="p" className="t-color-grey u-text-align-left" size="sm">
-              {termsContent}
-            </Text>
-          </Flex.Item>
-        </Flex>
-      </div>
-    </>
-  );
+// TODO to move this to utils
+const convertHoursToDays = hours => {
+  return Math.floor(hours / 24);
 };
+
+// TODO: to move this to somewhere more localised
+// TODO: add other formats
+const getDurationTranslation = (
+  expiration: badgeInfoType,
+  translations: DurationTranslations
+): $Keys<DurationTranslations> => {
+  const { key, value } = expiration;
+  const { singular, plural } = translations[key];
+
+  return value > 1 ? plural : singular;
+};
+
+export class ValuableDetails extends React.PureComponent<Props> {
+  get isLessThan24Hours() {
+    const { expirationTimeInHours } = this.props;
+
+    return expirationTimeInHours <= 24;
+  }
+
+  get expirationBadgeInfo(): badgeInfoType {
+    const { expirationTimeInHours } = this.props;
+
+    return this.isLessThan24Hours
+      ? { key: "hours", value: expirationTimeInHours }
+      : { key: "days", value: convertHoursToDays(expirationTimeInHours) };
+  }
+
+  render() {
+    const {
+      id,
+      backgroundImageUrl,
+      details,
+      caveat,
+      termsContent,
+      expirationTimeInHours,
+      translations,
+      children,
+    } = this.props;
+    const { termsAndConditionLabel, expirationTimeLabel } = translations;
+
+    const expirationInfo = this.expirationBadgeInfo;
+    const translatedDuration = getDurationTranslation(
+      expirationInfo,
+      translations
+    );
+    const expirationValueText = interpolate(translatedDuration, {
+      value: expirationInfo.value,
+    });
+
+    return (
+      <>
+        <div className="o-ratio o-ratio--valuable-details">
+          <div className="o-ratio__content c-valuable-details__header">
+            <MaskImage
+              id={`${id}-detail`}
+              imageUrl={backgroundImageUrl}
+              width={375}
+              height={334}
+            >
+              <HeaderImgMask />
+            </MaskImage>
+          </div>
+          <Flex
+            className="o-ratio__content"
+            justify="end"
+            align="center"
+            direction="vertical"
+          >
+            <div data-test-id="valuable-renderer-wrapper">{children}</div>
+          </Flex>
+        </div>
+        <div className="u-margin-top--2xlg">
+          <Flex direction="vertical" align="center" className="u-padding-x--md">
+            <Flex.Item>
+              <Text tag="p" size="md">
+                {details}
+              </Text>
+            </Flex.Item>
+            <Flex.Item className="u-margin-top--lg">
+              <Badge
+                tag="p"
+                size="sm"
+                data-test="valuable-expiration-badge"
+                className={classNames(
+                  "u-text-transform-uppercase u-font-weight-bold",
+                  expirationTimeInHours > 24 && expirationBadgeClasses.grey
+                )}
+              >
+                {`${expirationTimeLabel} ${expirationValueText}`}
+              </Badge>
+            </Flex.Item>
+            <Flex.Item className="u-margin-top--lg">
+              <Text tag="p" className="t-color-grey" size="sm">
+                {caveat}
+              </Text>
+            </Flex.Item>
+            <Flex.Item className="u-width--1/3 u-margin-y--xlg">
+              <hr className="t-color-grey-light-2 t-border-width--md t-border-r--pill" />
+            </Flex.Item>
+            <Flex.Item>
+              <Text tag="strong" className="t-color-grey" size="sm">
+                {termsAndConditionLabel}
+              </Text>
+            </Flex.Item>
+            <Flex.Item>
+              <Text
+                tag="p"
+                className="t-color-grey u-text-align-left"
+                size="sm"
+              >
+                {termsContent}
+              </Text>
+            </Flex.Item>
+          </Flex>
+        </div>
+      </>
+    );
+  }
+}
--- src/components/ValuableDetails/ValuableDetails.test.js
+++ src/components/ValuableDetails/ValuableDetails.test.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { ValuableDetails } from "./ValuableDetails";
+import { ValuableDetails, expirationBadgeClasses } from "./ValuableDetails";
 import mockValuables from "./__mocks__/Valuables.json";
-import translations from "./__mocks__/Translations.json";
-import { ValuableDetailsBody } from "./ValuableDetailsBody";
+import mockTranslations from "./__mocks__/Translations.json";
 
 describe("ValuableDetails", () => {
   let rendered;
+  const mockValuable = mockValuables[0];
   const Foo = () => <div>baz</div>;
 
   beforeEach(() => {
-    const mockValuable = mockValuables[0];
-
     rendered = shallow(
-      <ValuableDetails {...mockValuable} translations={translations}>
+      <ValuableDetails {...mockValuable} translations={mockTranslations}>
         <Foo />
       </ValuableDetails>
     );
@@ -25,7 +23,57 @@ describe("ValuableDetails", () => {
     expect(foo).toHaveLength(1);
   });
 
-  test("should render a valuabledetails body", () => {
-    expect(rendered.find(ValuableDetailsBody)).toHaveLength(1);
+  test("should render red expiration badge if expirationInHours is >= 24", () => {
+    expect(
+      rendered
+        .find("[data-test='valuable-expiration-badge']")
+        .hasClass(expirationBadgeClasses.grey)
+    ).toBe(true);
+  });
+
+  test("should display the expiration in hours if expiration is <= 24 hours", () => {
+    const expirationHours = 5;
+    const expectedExpirationText = `${mockTranslations.expirationTimeLabel} ${expirationHours} Hours`;
+    rendered = shallow(
+      <ValuableDetails
+        {...mockValuable}
+        expirationTimeInHours={expirationHours}
+        translations={mockTranslations}
+      >
+        <Foo />
+      </ValuableDetails>
+    );
+
+    expect(
+      rendered
+        .find("[data-test='valuable-expiration-badge']")
+        .dive()
+        .find("span")
+        .text()
+    ).toEqual(expectedExpirationText);
+  });
+
+  test("should display the expiration in days if expiration is >= 24 hours", () => {
+    const expirationHours = 300;
+    const days = Math.floor(expirationHours / 24);
+    const expectedExpirationText = `${mockTranslations.expirationTimeLabel} ${days} Days`;
+
+    rendered = shallow(
+      <ValuableDetails
+        {...mockValuable}
+        expirationTimeInHours={expirationHours}
+        translations={mockTranslations}
+      >
+        <Foo />
+      </ValuableDetails>
+    );
+
+    expect(
+      rendered
+        .find("[data-test='valuable-expiration-badge']")
+        .dive()
+        .find("span")
+        .text()
+    ).toEqual(expectedExpirationText);
   });
 });
