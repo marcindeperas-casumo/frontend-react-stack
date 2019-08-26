6ce59bbf6341902f5dc9f8802d91418a08fb554a
Michelle <michelle.dalli@casumo.com>
2019-08-26 13:07:41 +0200

    removed unused vars

--- src/components/ValuableDetails/ValuableDetailsContainer.js
+++ src/components/ValuableDetails/ValuableDetailsContainer.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Query } from "react-apollo";
-import { launchGame } from "Models/games";
-import { noop } from "Utils";
 // $FlowIgnore - Flow doesn't understand the queries imported by name.
 import { ValuableDetailsQuery } from "./ValuableDetails.graphql"; // to convert to js
 import { ValuableDetails } from "./ValuableDetails";
