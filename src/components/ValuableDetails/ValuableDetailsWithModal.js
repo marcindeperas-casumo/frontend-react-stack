344a933ca67612869a02fa73312ed2d4d94dbc13
Michelle <michelle.dalli@casumo.com>
2019-09-04 15:21:23 +0200

    ValuablesDetails: responsive

--- src/components/ValuableDetails/ValuableDetailsWithModal.js
+++ src/components/ValuableDetails/ValuableDetailsWithModal.js
@@ -1,7 +1,10 @@
 //@flow
 import React, { type Node } from "react";
+import classNames from "classnames";
 import { AbstractModal } from "Components/AbstractModal";
 import { ValuableDetailsContainer } from "Components/ValuableDetails";
+import { Desktop, Mobile } from "Components/ResponsiveLayout";
+import { ORIENTATION_VALUES } from "Components/ResponsiveLayout/ResponsiveLayout.types";
 import "./ValuableDetails.scss";
 
 type Props = {
@@ -19,14 +22,28 @@ export const ValuableDetailsWithModal = ({
   children,
   ...props
 }: Props) => {
-  return (
+  const ValuableModal = ({ className }) => (
     <AbstractModal
       isOpen={isOpen}
       hideModal={onClose}
-      className="c-valuable-details-modal c-valuable-details-modal--mobile-landscape"
+      className={classNames("c-valuable-details-modal", className)}
       closeTimeoutMS={100}
     >
       <ValuableDetailsContainer {...props}>{children}</ValuableDetailsContainer>
     </AbstractModal>
+  );
+
+  return (
+    <>
+      <Desktop>
+        <ValuableModal className="c-valuable-details-modal--desktop" />
+      </Desktop>
+      <Mobile orientation={ORIENTATION_VALUES.LANDSCAPE}>
+        <ValuableModal className="c-valuable-details-modal--mobile-landscape" />
+      </Mobile>
+      <Mobile orientation={ORIENTATION_VALUES.PORTRAIT}>
+        <ValuableModal className="c-valuable-details-modal--mobile-portrait" />
+      </Mobile>
+    </>
   );
 };
