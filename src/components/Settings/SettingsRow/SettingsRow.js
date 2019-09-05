1b7fdb9a2eb2c7f57842ddb34353e2270d167b30
Jack Westbrook <jack.westbrook@casumo.com>
2019-09-05 12:08:24 +0200

    refactor(SettingsRow): classNames to match reposition designs

--- src/components/Settings/SettingsRow/SettingsRow.js
+++ src/components/Settings/SettingsRow/SettingsRow.js
@@ -28,13 +28,13 @@ export class SettingsRow extends PureComponent<Props> {
           "u-padding-x--md",
           "t-border-bottom",
           "t-border--current-color",
-          "t-color-grey-light-2",
+          "t-color-chrome-light-1",
           "t-background-white",
           className
         )}
       >
         <Flex.Item>{text}</Flex.Item>
-        {children ? <Flex.Item>{children}</Flex.Item> : ""}
+        {children && <Flex.Item>{children}</Flex.Item>}
       </Flex>
     );
   }
