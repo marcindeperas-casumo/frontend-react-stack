960cc7e4f11dfd729717548d29b0b890266d0392
Dave Calleja <dave.calleja@casumo.com>
2019-09-06 08:32:34 +0200

    Added

---
+++ src/components/PlayerValuableList/PlayerValuableListVertical.js
@@ -0,0 +1,130 @@
+/* @flow */
+import React, { useEffect, useState } from "react";
+import { equals } from "ramda";
+import List from "@casumo/cmp-list";
+import Flex from "@casumo/cmp-flex";
+import { VALUABLE_TYPES, type ValuableType } from "Models/valuables";
+import logger from "Services/logger";
+import { GameRowSkeleton } from "Components/GameRowSkeleton";
+import { ValuableCard } from "Components/ValuableCard";
+import ScrollableListTitle from "Components/ScrollableListTitle";
+import { ValuableDetailsWithModal } from "Components/ValuableDetails";
+import { ValuableRow } from "Components/ValuableRow";
+import { launchGame } from "Models/games";
+import { subscribeToItemCreatedEvent } from "./utils";
+import { type PlayerValuableListProps } from "./PlayerValuableList.types";
+import "./PlayerValuableListHorizontal.scss";
+
+export function PlayerValuableListVertical(props: PlayerValuableListProps) {
+  const {
+    error,
+    loading,
+    valuables,
+    translations,
+    refetch,
+    onConsumeValuable,
+  } = props;
+  const { listTitleLabel, hoursLabel } = translations;
+  const [selectedValuable, setSelectedValuable] = useState(null);
+
+  const showModal = valuable => {
+    setSelectedValuable(valuable);
+  };
+
+  const closeModal = () => {
+    setSelectedValuable(null);
+  };
+
+  const consumeValuable = ({
+    id,
+    valuableType,
+    gameSlug,
+  }: {
+    id: string,
+    valuableType: ValuableType,
+    gameSlug: ?string,
+  }) => {
+    onConsumeValuable(id).then(() => {
+      if (equals(valuableType, VALUABLE_TYPES.SPINS)) {
+        launchGame(gameSlug);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const handler = subscribeToItemCreatedEvent(({ success }) => {
+      if (success) {
+        refetch();
+      }
+    });
+
+    return function cleanup() {
+      handler.unsubscribe();
+    };
+  });
+
+  if (error) {
+    logger.error(`
+      PlayerValuableListHorizontal failed:
+      ${error}
+    `);
+    return null;
+  }
+
+  if (loading) {
+    return <GameRowSkeleton />;
+  }
+
+  return (
+    <div className="u-padding-top--lg c-player-valuables-list u-padding-bottom--lg t-background-white">
+      {listTitleLabel && (
+        <Flex justify="space-between">
+          <Flex.Item>
+            <ScrollableListTitle paddingLeft title={listTitleLabel} />
+          </Flex.Item>
+        </Flex>
+      )}
+      <List
+        items={valuables}
+        className="t-background-white"
+        data-test="vertical-valuables-list"
+        render={valuable => (
+          <div
+            key={`valuable-row-${valuable.id}`}
+            id={`valuable-row-${valuable.id}`}
+          >
+            <ValuableRow
+              translatedHoursUnit={hoursLabel}
+              {...valuable}
+              onCardClick={() => showModal(valuable)}
+            />
+          </div>
+        )}
+      />
+
+      {selectedValuable && (
+        <ValuableDetailsWithModal
+          isOpen={Boolean(selectedValuable)}
+          onClose={closeModal}
+          onConsumeValuable={consumeValuable}
+          valuableDetails={selectedValuable}
+        >
+          <div className="c-valuable-list__valuable-card">
+            <ValuableCard
+              translatedHoursUnit={hoursLabel}
+              {...selectedValuable}
+              caveat={null}
+            />
+          </div>
+        </ValuableDetailsWithModal>
+      )}
+    </div>
+  );
+}
+
+// eslint-disable-next-line fp/no-mutation
+PlayerValuableListVertical.defaultProps = {
+  loading: false,
+  refetch: () => {},
+  valuable: [],
+};
---
+++ src/components/PlayerValuableList/PlayerValuableListVertical.test.js
@@ -0,0 +1,104 @@
+//@flow
+import React from "react";
+import { shallow, mount } from "enzyme";
+import List from "@casumo/cmp-list";
+import mockedValuables from "Components/ValuableCard/__mocks__/Valuable.json";
+import bridge from "Src/DurandalReactBridge";
+import { REACT_APP_EVENT_ON_CALLBACK, KO_EVENTS } from "Src/constants";
+import { ValuableRow } from "Components/ValuableRow";
+import { PlayerValuableListVertical } from "./PlayerValuableListVertical";
+import translationsMock from "./__mocks__/translations.mock.json";
+
+describe("PlayerValuableListVertical", () => {
+  const consumeValuable = jest.fn();
+  const refetchMock = jest.fn();
+  let rendered;
+
+  beforeEach(() => {
+    rendered = shallow(
+      <PlayerValuableListVertical
+        valuables={mockedValuables}
+        loading={false}
+        onConsumeValuable={consumeValuable}
+        translations={translationsMock}
+        refetch={refetchMock}
+      />
+    );
+  });
+
+  test("should render skeleton while loading", () => {
+    rendered = shallow(
+      <PlayerValuableListVertical
+        valuables={mockedValuables}
+        loading={true}
+        onConsumeValuable={consumeValuable}
+        translations={translationsMock}
+      />
+    );
+
+    expect(rendered.find("GameRowSkeleton").exists()).toBe(true);
+  });
+
+  test("should render the correct number of items", () => {
+    rendered = mount(
+      <PlayerValuableListVertical
+        valuables={mockedValuables}
+        loading={false}
+        onConsumeValuable={consumeValuable}
+        translations={translationsMock}
+        refetch={refetchMock}
+      />
+    );
+    expect(rendered.find("GameRowSkeleton").exists()).toBe(false);
+    expect(rendered.find(List).find(ValuableRow)).toHaveLength(
+      mockedValuables.length
+    );
+  });
+
+  test("should render the list title", () => {
+    expect(rendered.find("ScrollableListTitle").prop("title")).toEqual(
+      translationsMock.listTitleLabel
+    );
+  });
+
+  test("should refetch when VALUABLES/ITEM_CREATED event is received", () => {
+    const mock = jest.fn();
+    rendered = mount(
+      <PlayerValuableListVertical
+        valuables={mockedValuables}
+        loading={false}
+        onConsumeValuable={consumeValuable}
+        translations={translationsMock}
+        refetch={mock}
+      />
+    );
+    bridge.emit(REACT_APP_EVENT_ON_CALLBACK, {
+      event: KO_EVENTS.VALUABLES.ITEM_CREATED,
+      data: {
+        success: true,
+      },
+    });
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+
+  test("should not refetch when component is unmounted", () => {
+    const mock = jest.fn();
+    rendered = mount(
+      <PlayerValuableListVertical
+        valuables={mockedValuables}
+        loading={false}
+        onConsumeValuable={consumeValuable}
+        translations={translationsMock}
+        refetch={mock}
+      />
+    );
+    rendered.unmount();
+    bridge.emit(REACT_APP_EVENT_ON_CALLBACK, {
+      event: KO_EVENTS.VALUABLES.ITEM_CREATED,
+      data: {
+        success: true,
+      },
+    });
+    expect(mock).toHaveBeenCalledTimes(0);
+  });
+});
