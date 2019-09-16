9446b5a36501f7130c5a9d883ea30c1fc1df1cb9
Jack Westbrook <jack.westbrook@casumo.com>
2019-09-16 14:26:22 +0200

    refactor(promotioncardlist): remove custom styling in favour of ScrollableList

--- src/components/PromotionCardList/PromotionCardList.js
+++ src/components/PromotionCardList/PromotionCardList.js
@@ -1,9 +1,6 @@
 // @flow
 import React, { PureComponent } from "react";
-import classNames from "classnames";
-import Scrollable from "@casumo/cmp-scrollable";
-import { createModifierClasses } from "@casumo/cudl-react-utils";
-import { ScrollableListTitleRow } from "Components/ScrollableListTitleRow";
+import ScrollableList from "Components/ScrollableList";
 import PromotionCardContainer from "Components/PromotionCard";
 import { ScrollableListPaginated } from "Components/ScrollableListPaginated";
 import { Desktop, Mobile } from "Components/ResponsiveLayout";
@@ -14,15 +11,7 @@ type Props = {
   fetchCampaign: () => void,
   fetchPromotions: () => void,
   title?: string,
-  titleColor?: string,
-  backgroundColor?: string,
   seeMore: string,
-};
-
-const marginPerDevice = {
-  default: "md",
-  tablet: "3xlg",
-  desktop: "3xlg",
 };
 
 class PromotionCardList extends PureComponent<Props> {
@@ -32,22 +21,10 @@ class PromotionCardList extends PureComponent<Props> {
   }
 
   render() {
-    const {
-      title = "",
-      titleColor,
-      backgroundColor,
-      promotionsSlugs,
-      seeMore,
-    } = this.props;
+    const { title = "", promotionsSlugs, seeMore } = this.props;
     const hasNoPromotionSlugs = !promotionsSlugs || !promotionsSlugs.length;
     const seeMoreUrl = "/promotions";
     const itemClassName = "c-promotion-card";
-    const parentClassName = classNames(
-      backgroundColor && `t-background-${backgroundColor}`,
-      titleColor && `t-color-${titleColor}`,
-      "u-padding-bottom--lg",
-      "c-promotion-card-list"
-    );
     const promotionCardContainerRenderer = ({ id }) => (
       <PromotionCardContainer
         slug={`promotions.${id}`}
@@ -61,49 +38,33 @@ class PromotionCardList extends PureComponent<Props> {
     }
 
     return (
-      <div className="u-margin-x--3xlg@desktop u-padding-top--xlg">
+      <div className="u-margin-x--3xlg@desktop">
         <div className="o-wrapper">
           <Mobile>
-            <ScrollableListTitleRow
-              seeMore={{ text: seeMore, url: seeMoreUrl }}
+            <ScrollableList
+              itemClassName={itemClassName}
               title={title}
-              paddingLeft
+              seeMoreText={seeMore}
+              seeMoreUrl={seeMoreUrl}
+              itemIds={promotionsSlugs}
+              Component={promotionCardContainerRenderer}
             />
-            <div
-              className={classNames(
-                createModifierClasses("u-margin-x", marginPerDevice)
-              )}
-            >
-              <div className={parentClassName}>
-                <Scrollable
-                  itemClassName={itemClassName}
-                  itemSpacing="md"
-                  className="u-padding-top--lg"
-                >
-                  {promotionsSlugs.map(id =>
-                    promotionCardContainerRenderer({ id })
-                  )}
-                </Scrollable>
-              </div>
-            </div>
           </Mobile>
           <Desktop>
-            <ScrollableListTitleRow
-              seeMore={{ text: seeMore, url: seeMoreUrl }}
-              title={title}
+            <ScrollableListPaginated
+              list={{
+                title,
+                itemIds: promotionsSlugs,
+              }}
+              Component={promotionCardContainerRenderer}
+              className={itemClassName}
+              itemControlClass="c-scrollable-list-paginated__button"
+              tileHeight={318}
+              seeMore={{
+                text: seeMore,
+                url: seeMoreUrl,
+              }}
             />
-            <div className={parentClassName}>
-              <ScrollableListPaginated
-                list={{
-                  itemIds: promotionsSlugs,
-                }}
-                Component={promotionCardContainerRenderer}
-                className={itemClassName}
-                itemControlClass="c-scrollable-list-paginated__button"
-                tileHeight={308}
-                itemSpacing="md"
-              />
-            </div>
           </Desktop>
         </div>
       </div>
