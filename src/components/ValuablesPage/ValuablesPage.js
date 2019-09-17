2ebe6c23af57bd2703401f13954ce190d0f162c0
Dave Calleja <dave.calleja@casumo.com>
2019-09-17 08:12:00 +0200

    Removed unnecessary comments

--- src/components/ValuablesPage/ValuablesPage.js
+++ src/components/ValuablesPage/ValuablesPage.js
@@ -1,6 +1,4 @@
 // @flow
-// This is a temporary page to be able to view the valuables while on production
-// Route is only available to those who know about it
 
 import React from "react";
 import {
