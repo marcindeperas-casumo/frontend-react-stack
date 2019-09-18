15bb3b2d773a3f05ed051e384bf36289b3d4ad41
Michelle <michelle.dalli@casumo.com>
2019-09-18 15:14:31 +0200

    removed unnecesary check

--- src/components/AbstractModal/CloseButton.js
+++ src/components/AbstractModal/CloseButton.js
@@ -3,7 +3,6 @@ import React from "react";
 import classNames from "classnames";
 import { CrossIcon } from "@casumo/cmp-icons";
 import Flex from "@casumo/cmp-flex";
-import { noop } from "Utils";
 import "./CloseButton.scss";
 
 type ButtonProps = {
@@ -18,8 +17,7 @@ export const CloseButton = ({ className, onClick }: ButtonProps) => (
     onClick={onClick}
     className={classNames(
       "c-close-button t-background-grey-light-2 t-border-r--circle",
-      className,
-      onClick === noop && "u-visibility--hidden"
+      className
     )}
   >
     <CrossIcon />
