11d2da690107f1f1dddef65ed836d4574b20d220
Michelle <michelle.dalli@casumo.com>
2019-09-30 22:20:00 +0200

    Updated stories

--- src/components/ValuableCard/__mocks__/Valuable.mock.js
+++ src/components/ValuableCard/__mocks__/Valuable.mock.js
@@ -6,3 +6,16 @@ import { DateTime } from "luxon";
 export const mockValuable = valuableType => {
     return find(propEq("valuableType", valuableType))(mockData) || mockData[0];
 };
+
+export const mockExpiryDate = expiresWith24Hours => {
+  const hours = expiresWith24Hours ? 4 : 30;
+
+  return addHoursToNow(hours) / 1000;
+}
+
+
+const addHoursToNow = (hours: number) => {
+  const result = new Date(Date.now());
+
+  return result.setHours(result.getHours() + hours);
+};
