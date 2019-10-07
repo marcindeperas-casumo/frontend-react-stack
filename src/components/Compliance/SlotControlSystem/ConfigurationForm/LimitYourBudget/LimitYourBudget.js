e847c597cf1b8f46dc5874ed506d2ac67a67efd9
Adrian Nowicki <adrian.nowicki@casumo.com>
2019-10-07 16:53:04 +0200

    Split ErrorMessage out of LimitYourBudget. Extracted utility functions into Utils.

--- src/components/Compliance/SlotControlSystem/ConfigurationForm/LimitYourBudget/LimitYourBudget.js
+++ src/components/Compliance/SlotControlSystem/ConfigurationForm/LimitYourBudget/LimitYourBudget.js
@@ -7,6 +7,8 @@ import Text from "@casumo/cmp-text";
 import { ArrowRightIcon } from "@casumo/cmp-icons";
 import { TextInput } from "Components/Compliance/TextInput";
 import { interpolate, formatCurrency, getSymbolForCurrency } from "Utils";
+import { isBudgetInvalid } from "../Utils";
+import { ErrorMessage } from "./ErrorMessage";
 import "./LimitYourBudget.scss";
 
 const { useCallback, useState } = React;
@@ -48,7 +50,7 @@ export function LimitYourBudget(props: Props) {
       className={classNames(
         "u-padding--md u-height--1/1 t-background-white c-scs__limit-your-budget",
         budget &&
-          isBudgetWrong(budgetBalance) &&
+          isBudgetInvalid(budgetBalance) &&
           "c-scs__limit-your-budget--error"
       )}
     >
@@ -63,7 +65,7 @@ export function LimitYourBudget(props: Props) {
           className="c-scs__limit-your-budget__field"
         />
         <Button
-          disabled={isBudgetWrong(budgetBalance)}
+          disabled={isBudgetInvalid(budgetBalance)}
           variant="primary"
           size="sm"
           className="u-width--1/4 u-margin-left--md u-padding--md t-box-shadow"
@@ -88,65 +90,4 @@ export function LimitYourBudget(props: Props) {
       </Button>
     </Flex>
   );
-}
-
-type ErrorMessageProps = {
-  t: {
-    error_budget_too_low: string,
-    error_budget_too_high: string,
-  },
-  balance: number,
-  budget: number,
-  locale: string,
-  currency: string,
-};
-
-function ErrorMessage(props: ErrorMessageProps) {
-  const { budget, balance, currency, locale } = props;
-  const budgetBalance = { budget, balance };
-  const formattedBalance = formatCurrency({
-    value: balance,
-    currency,
-    locale,
-  });
-  const formattedMinBudget = formatCurrency({
-    value: 0.1,
-    currency,
-    locale,
-  });
-
-  if (!isBudgetWrong(props)) {
-    return null;
-  }
-
-  return (
-    <Text className="t-color-red">
-      {isBudgetTooLow(budgetBalance) &&
-        interpolate(props.t.error_budget_too_low, {
-          budget: formattedMinBudget,
-        })}
-      {isBudgetTooHigh(budgetBalance) &&
-        interpolate(props.t.error_budget_too_high, {
-          balance: formattedBalance,
-        })}
-    </Text>
-  );
-}
-
-function isBudgetTooLow({ budget }: { budget: number }) {
-  return budget < 0.1;
-}
-
-function isBudgetTooHigh({
-  budget,
-  balance,
-}: {
-  budget: number,
-  balance: number,
-}) {
-  return budget > balance;
-}
-
-function isBudgetWrong(props: { budget: number, balance: number }) {
-  return isNaN(props.budget) || isBudgetTooLow(props) || isBudgetTooHigh(props);
 }
