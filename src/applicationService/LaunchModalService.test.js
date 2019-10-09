8ab21fae18a2ed2564b7d377f12bb9e34efefb92
Dave Calleja <dave.calleja@casumo.com>
2019-10-09 08:10:26 +0200

    Added navigationService to trigger navigation on ko stack and added launchErrorModal service to trigger launching of error modals on ko stack

--- src/applicationService/LaunchModalService.test.js
+++ src/applicationService/LaunchModalService.test.js
@@ -1,19 +1,41 @@
 // @flow
-import { KO_APP_EVENT_LAUNCH_MODAL } from "Src/constants";
+import {
+  KO_APP_EVENT_LAUNCH_MODAL,
+  KO_APP_EVENT_LAUNCH_ERROR_MODAL,
+} from "Src/constants";
 import bridge from "../DurandalReactBridge";
-import { launchModal } from "./LaunchModalService";
+import { launchModal, launchErrorModal } from "./LaunchModalService";
 
 jest.mock("../DurandalReactBridge");
 
-test("calls emit on the bridge with the modal and other props", () => {
-  const modal = "foo";
-  const otherProp = "bar";
+describe("LaunchModalService", () => {
+  beforeEach(() => {
+    bridge.emit.mockReset();
+  });
 
-  launchModal({ modal, otherProp });
+  test("calls emit on the bridge with the modal and other props", () => {
+    const modal = "foo";
+    const otherProp = "bar";
 
-  expect(bridge.emit).toHaveBeenCalledTimes(1);
-  expect(bridge.emit).toHaveBeenCalledWith(KO_APP_EVENT_LAUNCH_MODAL, {
-    modal,
-    otherProp,
+    launchModal({ modal, otherProp });
+
+    expect(bridge.emit).toHaveBeenCalledTimes(1);
+    expect(bridge.emit).toHaveBeenCalledWith(KO_APP_EVENT_LAUNCH_MODAL, {
+      modal,
+      otherProp,
+    });
+  });
+
+  test("calls emit on the bridge with the rejectReasonId and status (when provided", () => {
+    const rejectReasonId = "foo";
+    const status = 123;
+
+    launchErrorModal({ rejectReasonId, status });
+
+    expect(bridge.emit).toHaveBeenCalledTimes(1);
+    expect(bridge.emit).toHaveBeenCalledWith(KO_APP_EVENT_LAUNCH_ERROR_MODAL, {
+      rejectReasonId,
+      status,
+    });
   });
 });
