e8af8570ccb29d252409502e8f1d498bb69813d2
Jack Westbrook <jack.westbrook@casumo.com>
2019-10-23 15:28:45 +0200

    docs(GameTileHeart): remove decoration and provide active story state

--- src/components/GameTileHeart/GameTileHeart.stories.js
+++ src/components/GameTileHeart/GameTileHeart.stories.js
@@ -1,6 +1,5 @@
 // @flow
 import React from "react";
-import Flex from "@casumo/cmp-flex";
 import { storiesOf } from "@storybook/react";
 import { boolean, withKnobs } from "@storybook/addon-knobs/react";
 import { GameTileHeart } from "./GameTileHeart";
@@ -8,15 +7,20 @@ import { GameTileHeart } from "./GameTileHeart";
 const stories = storiesOf("GameTileHeart", module);
 stories.addDecorator(withKnobs);
 
-stories.add("Default", () => {
-  return (
-    <Flex align="center" justify="center" style={{ height: 300, width: 300 }}>
-      <div className="t-color-red" style={{ width: 50, height: 50 }}>
-        <GameTileHeart
-          isActive={boolean("is active", false)}
-          onClick={() => {}}
-        />
-      </div>
-    </Flex>
-  );
-});
+stories.add("Default", () => (
+  <div className="t-color-red" style={{ width: 50, height: 50 }}>
+    <GameTileHeart isActive={boolean("is active", false)} onClick={() => {}} />
+  </div>
+));
+
+stories.add(
+  "Active",
+  () => (
+    <div className="t-color-red" style={{ width: 50, height: 50 }}>
+      <GameTileHeart isActive={true} onClick={() => {}} />
+    </div>
+  ),
+  {
+    chromatic: { delay: 375 },
+  }
+);
