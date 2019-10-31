213611e301869fa02cd596c8fbfc282fa1f19a3c
Jack Westbrook <jack.westbrook@casumo.com>
2019-10-31 10:41:49 +0100

    revert(cmsApi): don't force myList

--- src/api/api.cms.js
+++ src/api/api.cms.js
@@ -11,15 +11,5 @@ export const getPageBySlug = async ({ slug, hash, lang }: getPageBySlugArg) => {
   const queryParams = { hash };
   const queryString = stringify(queryParams, { skipNulls: true });
   const url = `/api/cmsquery/v2/root/${lang}/${slug}?${queryString}`;
-  return await http.get(url).then(result => {
-    if (result.slug.indexOf("top-lists") !== -1) {
-      const myList = {
-        acf_fc_layout: "GAMES_LIST",
-        id: "myList",
-      };
-      // eslint-disable-next-line fp/no-mutating-methods
-      result.fields.content_builder.splice(1, 0, myList);
-    }
-    return result;
-  });
+  return await http.get(url);
 };
