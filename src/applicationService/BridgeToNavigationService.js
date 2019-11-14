a19c674b6557fed83cb9b996726a6cc579ee3292
Dave Calleja <dave.calleja@casumo.com>
2019-11-14 14:55:31 +0100

    Refactored to simpler logic for BridgeToNavigationService based on PR feedback

--- src/applicationService/BridgeToNavigationService.js
+++ src/applicationService/BridgeToNavigationService.js
@@ -4,20 +4,8 @@ import bridge from "../DurandalReactBridge";
 
 export const BridgeToNavigationService = () => {
   bridge.on(REACT_APP_EVENT_ROUTE_CHANGE, ({ config, params }) => {
-    const pattern = config.routePattern;
-    // eslint-disable-next-line fp/no-let
-    let route = config.route
-      .split("/")
-      .filter(segment => segment)
-      .join("/");
+    const { pathname, search } = window.location;
 
-    const parameterMatches = [...route.match(pattern)].slice(1);
-
-    params.forEach((param, index) => {
-      // eslint-disable-next-line fp/no-mutation
-      route = route.replace(parameterMatches[index], param);
-    });
-
-    navigate(`/${route}`, { replace: true });
+    navigate(`${pathname}${search}`, { replace: true });
   });
 };
