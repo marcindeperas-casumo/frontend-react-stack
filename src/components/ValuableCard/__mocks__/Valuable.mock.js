9a6d7f7783870d0c18e3d9659d44f99f89272ab7
Michelle <michelle.dalli@casumo.com>
2019-11-26 22:10:56 +0100

    Update mock

--- src/components/ValuableCard/__mocks__/Valuable.mock.js
+++ src/components/ValuableCard/__mocks__/Valuable.mock.js
@@ -1,32 +1,30 @@
-// @flow
 import mockData from "./Valuable";
-import { VALUABLE_TYPES, type ValuableType } from "Models/valuables";
+import { VALUABLE_TYPES, type ValuableType, type ValuableListProps } from "Models/valuables";
 import { find, propEq } from "ramda";
 import { DateTime } from "luxon";
 
-export const mockValuable = (valuableType: ValuableType) => {
+export const mockValuable = (valuableType) => {
     const mockedValuable = find(propEq("valuableType", valuableType))(mockData) || mockData[0];
     return {
         ...mockedValuable,
         expiryDate: mockExpiryDate(false),
     }
 };
-
-// TODO: set return type instead of any
-export const mockValuables = (valuableType?: ValuableType): any[] => {
+// ValuableDetails_PlayerValuable
+export const mockValuables = (valuableType = null) => {
   return valuableType ?
     mockData.filter(valuable => valuable.valuableType === valuableType) :
     mockData;
 }
 
-export const mockExpiryDate = (expiresWith24Hours: boolean) => {
+export const mockExpiryDate = (expiresWith24Hours) => {
   const hours = expiresWith24Hours ? 4 : 30;
 
   return addHoursToNow(hours) / 1000;
 }
 
 
-const addHoursToNow = (hours: number) => {
+const addHoursToNow = (hours) => {
   const result = new Date(Date.now());
 
   return result.setHours(result.getHours() + hours);
