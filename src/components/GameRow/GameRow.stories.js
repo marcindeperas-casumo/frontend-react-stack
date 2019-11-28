3c559c676594565fd55fefff3892a2ad63401922
joseaplwork <joseaplwork@gmail.com>
2019-11-28 16:01:52 +0100

    Change jackpot mocks folder

--- src/components/GameRow/GameRow.stories.js
+++ src/components/GameRow/GameRow.stories.js
@@ -3,12 +3,13 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import game from "Components/GameTile/__mocks__/Game.json";
-import jackpot from "Components/GameTile/__mocks__/Jackpot.js";
+import jackpots from "Components/Jackpots/__mocks__/response.jackpots.mock.js";
 import liveCasinoGame from "Components/LiveCasinoCard/__mocks__/Roulette.json";
 import { GameRow } from "Components/GameRow/GameRow";
 
 const stories = storiesOf("GameRow", module);
 const gonzosQuest = "gonzos-quest";
+const jackpot = jackpots[0];
 
 stories.add("Default", () => (
   <GameRow game={game} onLaunchGame={action(gonzosQuest)} />
--- src/components/GameTile/__mocks__/Jackpot.js
+++
@@ -1,8 +0,0 @@
-export default {
-  id: "netent-megajackpot1",
-  gameProvider: "NETENT",
-  value: {
-    currency: "EUR",
-    amount: 0
-  }
-}
