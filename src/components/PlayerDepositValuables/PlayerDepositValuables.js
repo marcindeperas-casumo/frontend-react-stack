0b98a635f6b91c43fb2ea7976876ff37ac8ef790
Michelle <michelle.dalli@casumo.com>
2019-11-28 17:36:57 +0100

    WIP

--- src/components/PlayerDepositValuables/PlayerDepositValuables.js
+++ src/components/PlayerDepositValuables/PlayerDepositValuables.js
@@ -4,6 +4,8 @@ import { ValuablesVerticalList } from "Components/ValuablesVerticalList";
 import { usePlayerValuableList } from "Components/PlayerValuableList/usePlayerValuableList";
 import { GameRowSkeleton } from "Components/GameRowSkeleton";
 import { VALUABLE_TYPES } from "Models/valuables";
+import { ValuableRowShell } from "Components/ValuableRow/ValuableRowShell";
+import "./PlayerDepositValuables.scss";
 
 export const PlayerDepositValuables = () => {
   const { loading, valuables, translations } = usePlayerValuableList(
@@ -15,11 +17,19 @@ export const PlayerDepositValuables = () => {
   }
 
   return (
-    <ValuablesVerticalList
-      className="u-padding-x--md"
-      valuables={valuables}
-      translations={translations}
-      loading={loading}
-    />
+    <div className="list-wrap">
+      <div className="u-overflow-y--auto c-tst">
+        <div className="u-padding-x--md">
+          <ValuablesVerticalList
+            valuables={valuables}
+            translations={translations}
+            loading={loading}
+          />
+        </div>
+      </div>
+      <div className="u-padding-x--md">
+        <ValuableRowShell text="Don't use bonus" />
+      </div>
+    </div>
   );
 };
---
+++ src/components/ValuableRow/ValuableRowShell.js
@@ -0,0 +1,22 @@
+// @flow
+import React from "react";
+import Flex from "@casumo/cmp-flex";
+import Text from "@casumo/cmp-text";
+import { ValuablesCardDefaultIcon } from "Components/ValuableThumbnail/icons";
+
+type Props = {
+  text: string,
+};
+
+export const ValuableRowShell = ({ text }: Props) => (
+  <Flex data-test="valuable-row">
+    <Flex.Item className="c-valuable-row-thumbnail o-flex__item--no-shrink">
+      <ValuablesCardDefaultIcon />
+    </Flex.Item>
+    <Flex align="center" justify="center" className="u-margin-left--md">
+      <Text className="u-font-weight-bold" size="sm" tag="span">
+        {text}
+      </Text>
+    </Flex>
+  </Flex>
+);
