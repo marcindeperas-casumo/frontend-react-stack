3983c323a7407295b2aa88c2d369e09917e08393
Michał Ignaszewski <michal.ignaszewski@casumo.com>
2019-12-05 17:48:43 +0100

    Propper links

--- src/components/SideBar/SideBar.utils.js
+++ src/components/SideBar/SideBar.utils.js
@@ -6,30 +6,33 @@ export const generateMenu = (fields: Object) => {
   return [
     {
       name: fields["settings_link_text"] || `Your settings`,
-      link: `/somewhere/settings`, //TODO: link needed
+      link: `/player/settings`,
     },
     {
       name: fields["play_okay_settings_link_text"] || `Play Okay Settings`,
-      link: `/somewhere/settingsok`, //TODO: link needed
+      link: `/player/play-ok-settings`,
     },
     {
       name: fields["contact_us_link_text"] || `Email us`,
       link: `mailto:hey@casumo.com`,
     },
-    { name: fields["help_text"], link: `/somewhere/chat/` },
+    {
+      name: fields["help_text"],
+      action: () => console.warn("Logout action..."),
+    },
     {
       name: fields["play_okay_link_text"] || `Play Okay`,
-      link: `/somewhere/pok`,
-    }, //TODO: LINK NEEDED
+      link: `/play-ok`,
+    },
     { name: fields["blog_menu_text"] || `Blog`, link: fields["blog_menu_url"] },
-    { name: fields["faq_link_text"] || `FAQ`, link: `/somewhere/faq` }, //TODO: LINK NEEDED
+    { name: fields["faq_link_text"] || `FAQ`, link: `/faq` },
     {
       name: fields["about_us_link_text"] || `About Casumo`,
-      link: `/somewhere/about`,
-    }, //TODO: link needed
+      link: `/about-casumo`,
+    },
     {
       name: fields["log_out_link_text"] || `Log out`,
-      link: `/somewhere/logout`,
-    }, //TODO: log out action
+      action: () => console.warn("Logout action..."),
+    },
   ];
 };
