34005402947fab7e34648d84ca8adf67c3d8f5e4
Dave Calleja <dave.calleja@casumo.com>
2019-12-06 09:20:55 +0100

    Removing useLanguage

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -2,6 +2,5 @@
 export { useTranslations } from "./useTranslations";
 export { useDelayedCleanup } from "./useDelayedCleanup";
 export { useLocale } from "./useLocale";
-export { useLanguage } from "./useLanguage";
 export { useWalletAmount } from "./useWalletAmount";
 export { useGameLaunchData } from "./useGameLaunchData";
--- src/utils/hooks/useLanguage.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import { useSelector, shallowEqual } from "react-redux";
-import { languageSelector } from "Models/handshake";
-
-export function useLanguage() {
-  return useSelector(languageSelector, shallowEqual);
-}
