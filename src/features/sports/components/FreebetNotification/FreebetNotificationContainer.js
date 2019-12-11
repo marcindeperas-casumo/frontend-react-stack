e8f79cf7dbf499c7a4e8d6baba8aef3e90e46272
Levente Balogh <balogh.levente.hu@gmail.com>
2019-12-11 11:23:01 +0100

    FreebetNotification: only show it for locked free-bets

--- src/features/sports/components/FreebetNotification/FreebetNotificationContainer.js
+++ src/features/sports/components/FreebetNotification/FreebetNotificationContainer.js
@@ -3,6 +3,7 @@ import * as R from "ramda";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import * as storage from "Services/storage";
+import { VALUABLE_STATES } from "Models/valuables";
 import { FreebetNotification } from "./FreebetNotification";
 
 // We are persisting the users action of hiding the notification to localStorage.
@@ -32,6 +33,13 @@ export const FreebetNotificationContainer = () => {
   const [valuable = {}] = R.pathOr([], ["player", "valuables"], data);
   const isHiddenByDefault = storage.get(IS_HIDDEN_STORAGE_KEY, false);
   const onClose = () => storage.set(IS_HIDDEN_STORAGE_KEY, true);
+
+  // Only display it for locked free bets.
+  // This is something that we would like to change to support both, but as we are short on time we would like go in small steps.
+  // We have put it here in order to not pollute the generic the FreebetNotification component unnecessarily.
+  if (valuable.valuableState !== VALUABLE_STATES.LOCKED) {
+    return null;
+  }
 
   return loading ? null : (
     <FreebetNotification
