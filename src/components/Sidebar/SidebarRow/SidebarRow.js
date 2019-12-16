dc56decb4b6758cfd2edc4ada632db55400243f9
Cho <choishere@gmail.com>
2019-12-16 17:08:54 +0100

    sidebarrow function

--- src/components/Sidebar/SidebarRow/SidebarRow.js
+++ src/components/Sidebar/SidebarRow/SidebarRow.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from "react";
+import React from "react";
 import Flex from "@casumo/cmp-flex";
 import Text from "@casumo/cmp-text";
 import classNames from "classnames";
@@ -17,74 +17,68 @@ type Props = {
   action?: Function,
 };
 
-export class SidebarRow extends PureComponent<Props> {
-  render() {
-    const {
-      text,
-      label,
-      Icon,
-      isSelected,
-      isWhiteRow,
-      openNewTab,
-      link,
-      action,
-    } = this.props;
+export const SidebarRow = (props: Props) => {
+  const {
+    text,
+    label,
+    Icon,
+    isSelected,
+    isWhiteRow,
+    openNewTab,
+    link,
+    action,
+  } = props;
 
-    const stylesRow = classNames(
-      "u-font-weight-bold",
-      "u-margin--none",
-      "u-padding--none",
-      "u-position-relative",
-      "u-overflow-hidden",
-      "t-color-white",
-      {
-        "c-sidebar__nav-item--active t-background-turquoise t-color-white": isSelected,
-        "c-sidebar__nav-item--white t-background-white t-color-grey-dark-1": isWhiteRow,
-        "c-sidebar__nav-item t-background-plum": !isWhiteRow,
-      }
-    );
+  const rowClasses = classNames(
+    "u-font-weight-bold",
+    "u-margin--none",
+    "u-padding--none",
+    "u-position-relative",
+    "u-overflow-hidden",
+    "t-color-white",
+    {
+      "c-sidebar__nav-item--active t-background-turquoise t-color-white": isSelected,
+      "c-sidebar__nav-item--white t-background-white t-color-grey-dark-1": isWhiteRow,
+      "c-sidebar__nav-item t-background-plum": !isWhiteRow,
+    }
+  );
 
-    const stylesLink = classNames(
-      isWhiteRow ? "t-color-grey-dark-1" : "t-color-white"
-    );
-
-    return (
-      <li className={stylesRow} data-test-id="sidebar-li">
-        <a
-          data-test-id="sidebar-link"
-          className={stylesLink}
-          onClick={action}
-          href={link || "#"}
-          target={openNewTab ? "_blank" : "_self"}
+  return (
+    <li className={rowClasses} data-test-id="sidebar-li">
+      <a
+        data-test-id="sidebar-link"
+        className={isWhiteRow ? "t-color-grey-dark-1" : "t-color-white"}
+        onClick={action}
+        href={link || "#"}
+        target={openNewTab ? "_blank" : "_self"}
+      >
+        <Flex
+          align="center"
+          justify="center"
+          direction="vertical"
+          className="u-height--full"
         >
-          <Flex
-            align="center"
-            justify="center"
-            direction="vertical"
-            className="u-height--full"
-          >
-            {Icon && (
-              <Icon
-                className="u-height--3xlg u-width--3xlg u-margin-bottom--sm"
-                data-test-id="sidebar-icon"
-              />
-            )}
-            <Text tag="span" data-test-id="sidebar-text">
-              {text}
+          {Icon && (
+            <Icon
+              className="u-height--3xlg u-width--3xlg u-margin-bottom--sm"
+              data-test-id="sidebar-icon"
+            />
+          )}
+          <Text tag="span" data-test-id="sidebar-text">
+            {text}
+          </Text>
+          {label && (
+            <Text
+              tag="span"
+              size="sm"
+              className="t-color-turquoise"
+              data-test-id="sidebar-text-small"
+            >
+              {label}
             </Text>
-            {label && (
-              <Text
-                tag="span"
-                size="sm"
-                className="t-color-turquoise"
-                data-test-id="sidebar-text-small"
-              >
-                {label}
-              </Text>
-            )}
-          </Flex>
-        </a>
-      </li>
-    );
-  }
-}
+          )}
+        </Flex>
+      </a>
+    </li>
+  );
+};
