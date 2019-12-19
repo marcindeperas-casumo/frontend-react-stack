28c6f0b36a58ca7737ff91b5b29db9731ab274eb
Michele Settepani <michele.settepani@casumo.com>
2019-12-19 14:52:03 +0100

    index: enable react devtools on prod via localStorage

--- src/index.js
+++ src/index.js
@@ -48,12 +48,19 @@ renderApp(App);
 
 initNumberOfVisits();
 
+// In order to be able to properly debug in production, we would need to be able to use the REACT DEVTOOLS
+// This isDebugMode flag needs to be set manually on the client via localStorage
+const isDebugMode = storage.get("isDebugMode");
+const isReactDevtoolsAvailable =
+  typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object";
+const isProdAndNotDebugMode = !__DEV__ && !isDebugMode;
+
 // Call this to disable react DevTools integration, meaning that this will
 // prevent the react DevTools extension to scan the elements and show anything
 // react related in the extension tab.
 // We need it to prevent people to look into our React tree with the extension
 // in production.
-if (!__DEV__ && typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {
+if (isProdAndNotDebugMode && isReactDevtoolsAvailable) {
   Object.entries(window.__REACT_DEVTOOLS_GLOBAL_HOOK__).forEach(
     ([key, value]) => {
       // eslint-disable-next-line fp/no-mutation
