a920d9ef7386ece3b7db4b8bd162e65a891b1060
Cho <choishere@gmail.com>
2019-12-20 10:28:41 +0100

    rm comment

--- src/models/app/app.actions.js
+++ src/models/app/app.actions.js
@@ -9,7 +9,6 @@ export const appStarted = () => ({
 export const logout = () => ({
   type: fetchTypes.FETCH,
   name: types.APP_LOGOUT,
-  // postFetch: types.CURATED_FETCH_GAME_COMPLETE,
   asyncCall: commandLogout,
   asyncCallData: {},
 });
