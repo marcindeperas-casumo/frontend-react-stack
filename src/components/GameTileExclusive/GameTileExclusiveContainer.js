f25f724e74d07855391819269dce19eef60fcb07
Jack Westbrook <jack.westbrook@casumo.com>
2020-01-21 13:46:28 +0100

    refactor(gameTileExclusive): rename props, use apollo types

--- src/components/GameTileExclusive/GameTileExclusiveContainer.js
+++ src/components/GameTileExclusive/GameTileExclusiveContainer.js
@@ -1,9 +1,16 @@
 // @flow
 import React from "react";
 import GameTileExclusive from "Components/GameTileExclusive/GameTileExclusive";
+import * as A from "Types/apollo";
 
-// __FIX__: change "id" to "game" here + add Flow typing
-export const GameTileExclusiveContainer = ({ id }) => {
+type Props = {
+  item: A.GameTile_Game,
+};
+
+// __FIX__: this should take a game but because of
+// https://github.com/Casumo/frontend-react-stack/blob/master/src/components/ScrollableList/ScrollableList.js#L42
+// it's set as something else.
+export const GameTileExclusiveContainer = ({ item }: Props) => {
   // __FIX__: update all properties to come from the Apollo store
-  return <GameTileExclusive game={id} onLaunchGame={() => {}} />;
+  return <GameTileExclusive game={item} onLaunchGame={() => {}} />;
 };
