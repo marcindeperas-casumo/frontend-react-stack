8337f1e091e0b7c4d428e3be75bb651bd64612d6
Jack Westbrook <jack.westbrook@casumo.com>
2020-01-21 13:45:02 +0100

    refactor(scrollableList): rename prop, use apollo types, expect id on keyGetter

--- src/components/ScrollableList/ScrollableList.js
+++ src/components/ScrollableList/ScrollableList.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { PureComponent } from "react";
 import Scrollable from "@casumo/cmp-scrollable";
-import { isEmpty, prop, path } from "ramda";
+import { isEmpty } from "ramda";
 import type {
   spacerSizes,
   responsiveSpacerSizes,
@@ -20,7 +20,7 @@ type Props = {
   title: string,
   /** url to "see more" page, if null will not render "see more" button */
   seeMoreUrl?: string,
-  itemIds: Array<string>,
+  items: Array<any>,
   Component: Function,
   spacing: spacerSizes | responsiveSpacerSizes,
   /** "see more" link translation */
@@ -30,28 +30,21 @@ type Props = {
 
 export default class ScrollableList extends PureComponent<Props> {
   static defaultProps = {
-    itemIds: [],
+    items: [],
     spacing: DEFAULT_SPACING,
     Component: GameTile,
   };
 
-  // __FIX__ - this needs solving elegantly.
-  keyGetter = (i: number) => {
-    if (typeof this.props.itemIds[i] === "object") {
-      return this.props.itemIds[i].id;
-    }
-    return prop(i, this.props.itemIds);
-  };
+  keyGetter = (i: number) => this.props.items[i].id;
 
   itemRenderer = (i: number) => {
-    const { Component, itemIds } = this.props;
-    // __FIX__: change "id" to "item" here
-    return <Component id={itemIds[i]} />;
+    const { Component, items } = this.props;
+    return <Component item={items[i]} />;
   };
 
   render() {
     const {
-      itemIds,
+      items,
       seeMoreText,
       seeMoreUrl,
       spacing,
@@ -59,7 +52,7 @@ export default class ScrollableList extends PureComponent<Props> {
       itemClassName,
     } = this.props;
 
-    if (isEmpty(itemIds)) {
+    if (isEmpty(items)) {
       return null;
     }
 
@@ -71,7 +64,7 @@ export default class ScrollableList extends PureComponent<Props> {
           title={title}
         />
         <Scrollable
-          numberOfItems={itemIds.length}
+          numberOfItems={items.length}
           keyGetter={this.keyGetter}
           itemRenderer={this.itemRenderer}
           itemClassName={itemClassName}
