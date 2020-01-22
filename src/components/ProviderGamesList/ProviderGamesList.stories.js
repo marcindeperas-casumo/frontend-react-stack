a1bd35a192c0cd13e948f353eceb220c4b264b13
Levente Balogh <balogh.levente.hu@gmail.com>
2020-01-22 14:27:58 +0100

    refactor(ProviderGamesList): remove unnecessary <MockStore> from stories

--- src/components/ProviderGamesList/ProviderGamesList.stories.js
+++ src/components/ProviderGamesList/ProviderGamesList.stories.js
@@ -1,7 +1,6 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import MockStore from "Components/MockStore";
 import { ProviderGamesList } from "./ProviderGamesList";
 import { games } from "./__mocks__";
 import "./ProviderGamesList.stories.scss";
@@ -9,23 +8,19 @@ import "./ProviderGamesList.stories.scss";
 const stories = storiesOf("ProviderGamesList", module);
 
 stories.add("Default", () => (
-  <MockStore>
-    <ProviderGamesList
-      loading={false}
-      games={games}
-      gamesCount={games.length}
-      onLoadMore={() => Promise.resolve(true)}
-    />
-  </MockStore>
+  <ProviderGamesList
+    loading={false}
+    games={games}
+    gamesCount={games.length}
+    onLoadMore={() => Promise.resolve(true)}
+  />
 ));
 
 stories.add("Loading State", () => (
-  <MockStore>
-    <ProviderGamesList
-      loading={true}
-      games={[]}
-      gamesCount={0}
-      onLoadMore={() => Promise.resolve(true)}
-    />
-  </MockStore>
+  <ProviderGamesList
+    loading={true}
+    games={[]}
+    gamesCount={0}
+    onLoadMore={() => Promise.resolve(true)}
+  />
 ));
