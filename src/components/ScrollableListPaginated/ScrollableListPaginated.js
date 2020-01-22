42ee3d3e49e2b0bf14545b35c3d381d41969ced7
Jack Westbrook <jack.westbrook@casumo.com>
2020-01-22 15:33:59 +0100

    refactor(scrollableListPaginated): set cellrenderer at call site. reduce component api

--- src/components/ScrollableListPaginated/ScrollableListPaginated.js
+++ src/components/ScrollableListPaginated/ScrollableListPaginated.js
@@ -3,22 +3,11 @@ import * as React from "react";
 import Flex from "@casumo/cmp-flex";
 import { DirectionRightIcon } from "@casumo/cmp-icons";
 import type { CellRendererParams } from "react-virtualized";
-import classNames from "classnames";
-import { createModifierClasses } from "@casumo/cudl-react-utils";
-import type {
-  spacerSizes,
-  responsiveSpacerSizes,
-} from "@casumo/cudl-react-prop-types";
 import ScrollablePaginated from "Components/ScrollablePaginated";
 import { ScrollableListTitleRow } from "Components/ScrollableListTitleRow";
 import type { ClickHandlerType } from "Components/ScrollablePaginated";
 
 import "./ScrollableListPaginated.scss";
-
-type ListObject = {
-  title?: string,
-  itemIds: Array<Object>,
-};
 
 export type SeeMoreProps = {
   /** The text to render on the seeMore button. */
@@ -32,18 +21,16 @@ export type SeeMoreProps = {
 type Props = {
   /** The item height. */
   tileHeight: number,
-  /** The style to apply to the list items. */
-  className: string,
   /** The style to apply to the list control buttons. */
   itemControlClass: string,
   /** The text and url to render on the seeMore button. */
   seeMore?: SeeMoreProps,
   /** The list of items to be rendered. */
-  list: ListObject,
-  /** Apply margins to the scrollable items */
-  itemSpacing?: spacerSizes | responsiveSpacerSizes,
+  list: Array<Object>,
+  /** The list title */
+  listTitle: string,
   /** The item renderer. */
-  itemRenderer: (i: number) => React.Node,
+  itemRenderer: CellRendererParams => any,
 };
 
 export class ScrollableListPaginated extends React.PureComponent<Props> {
@@ -88,39 +75,18 @@ export class ScrollableListPaginated extends React.PureComponent<Props> {
     );
   };
 
-  cellRenderer = ({ columnIndex, style }: CellRendererParams) => {
-    const { list, className, Component, itemSpacing = "default" } = this.props;
-    const { itemIds } = list;
-    const itemId = itemIds[columnIndex];
-    const isNotFirstElement = columnIndex > 0;
-    const elementClassNames = classNames(
-      "u-height--full",
-      isNotFirstElement && createModifierClasses("u-margin-left", itemSpacing)
-    );
-
-    return (
-      <div style={style}>
-        <div className={elementClassNames}>
-          <div className={className}>
-            {/* __FIX__ change "id" to "item" here */}
-            <Component key={itemId} id={itemId} />
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   render() {
-    const { list, tileHeight, seeMore } = this.props;
-    const { title, itemIds } = list;
+    const { list, listTitle, tileHeight, seeMore, itemRenderer } = this.props;
 
     return (
       <div className="u-padding-top--xlg" data-test="scrollable-list-paginated">
-        {title && <ScrollableListTitleRow title={title} seeMore={seeMore} />}
+        {listTitle && (
+          <ScrollableListTitleRow title={listTitle} seeMore={seeMore} />
+        )}
         <ScrollablePaginated
           className="c-scrollable-list-paginated"
-          columnCount={itemIds.length}
-          cellRenderer={this.cellRenderer}
+          columnCount={list.length}
+          cellRenderer={itemRenderer}
           buttonRenderer={this.buttonRenderer}
           height={tileHeight}
           overlappingItemCount={0}
