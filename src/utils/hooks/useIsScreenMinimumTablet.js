8a734acbf9f960e05394397c94cc8eaaa3a48176
Cho <choishere@gmail.com>
2020-01-23 09:55:22 +0100

    sidebar hooks add return type

--- src/utils/hooks/useIsScreenMinimumTablet.js
+++ src/utils/hooks/useIsScreenMinimumTablet.js
@@ -2,10 +2,10 @@
 import useMedia from "react-use/lib/useMedia";
 import breakpoints from "Src/styles/_settings.breakpoints.scss";
 
-export function useIsScreenMinimumTablet() {
+export function useIsScreenMinimumTablet(): boolean {
   const minWidth = parseInt(breakpoints.tablet) - 1;
+  // skips phablets landscape
+  const media = `screen and (min-width: ${minWidth}px) and (min-height: 500px)`;
 
-  return useMedia(
-    `screen and (min-width: ${minWidth}px) and (min-height: 500px)` // skips phablets landscape
-  );
+  return useMedia(media);
 }
--- src/utils/hooks/useIsSidebarOpen.js
+++ src/utils/hooks/useIsSidebarOpen.js
@@ -2,6 +2,6 @@
 import { useSelector, shallowEqual } from "react-redux";
 import { isSidebarOpen } from "Models/sidebar";
 
-export function useIsSidebarOpen() {
+export function useIsSidebarOpen(): boolean {
   return useSelector(isSidebarOpen, shallowEqual);
 }
