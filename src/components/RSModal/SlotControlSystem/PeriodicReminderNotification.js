1d5a1a48adf91b76f28bf2fe2a2420d9690174eb
Michał Mokijewski <hello@naf.gl>
2020-01-23 12:45:55 +0100

    Remove details not required by regulations
    It removes session start & end time as well as chosen session length.
    It'll make modal less crowded and won't require scrolling to make user
    see accept button on mobile.

--- src/components/RSModal/SlotControlSystem/PeriodicReminderNotification.js
+++ src/components/RSModal/SlotControlSystem/PeriodicReminderNotification.js
@@ -1,6 +1,5 @@
 // @flow
 import * as React from "react";
-import { DateTime, Duration } from "luxon";
 import Flex from "@casumo/cmp-flex";
 import Button from "@casumo/cmp-button";
 import type { ModalContentComponent } from "Components/RSModal";
@@ -10,7 +9,7 @@ import Timer from "Components/Timer";
 import { Row } from "Components/Compliance/SlotControlSystem/SessionDetails/Row";
 import { ModalSkin } from "./ModalSkin";
 
-type ContentType = {
+type ContentType = {|
   time_played: string,
   session_loss_limit: string,
   remaining_session_loss_limit: string,
@@ -18,10 +17,7 @@ type ContentType = {
   money_won: string,
   modal_title: string,
   continue_playing_button: string,
-  session_start: string,
-  session_end: string,
-  session_time_limit: string,
-};
+|};
 
 export function PeriodicReminderNotification({
   t,
@@ -41,24 +37,6 @@ export function PeriodicReminderNotification({
       t={{ modal_title: t.modal_title }}
     >
       <Flex direction="vertical">
-        <Row
-          label={t.session_time_limit}
-          value={Duration.fromObject({
-            seconds: activeGameSession.durationInSecs,
-          }).toFormat("hh:mm:ss")}
-        />
-        <Row
-          label={t.session_start}
-          value={DateTime.fromMillis(activeGameSession.startedTime).toFormat(
-            "HH:mm:ss"
-          )}
-        />
-        <Row
-          label={t.session_end}
-          value={DateTime.fromMillis(activeGameSession.expiringTime).toFormat(
-            "HH:mm:ss"
-          )}
-        />
         <Row
           label={t.time_played}
           value={
