2d344a3539d00ab7f7c96e27910788bac53aa84b
Jack Westbrook <jack.westbrook@casumo.com>
2020-01-24 15:03:36 +0100

    fix(mustDropJackpotsWidgetContainer): add pollInterval, fix query imports

--- src/components/MustDropJackpotsWidget/MustDropJackpotsWidgetContainer.js
+++ src/components/MustDropJackpotsWidget/MustDropJackpotsWidgetContainer.js
@@ -4,11 +4,14 @@ import { useQuery } from "@apollo/react-hooks";
 import * as A from "Types/apollo";
 import { MustDropJackpotsWidget } from "Components/MustDropJackpotsWidget/MustDropJackpotsWidget";
 import { MustDropJackpotsWidgetSkeleton } from "Components/MustDropJackpotsWidget/MustDropJackpotsWidgetSkeleton";
-import { MustDropJackpotsQuery } from "./MustDropJackpotsWidget.graphql";
+import { MustDropJackpotsQuery } from "./MustDropJackpotsWidgetContainer.graphql";
+
+const pollInterval = 30000;
 
 const MustDropJackpotsWidgetContainer = () => {
   const { data, loading } = useQuery<A.MustDropJackpotsQuery, null>(
-    MustDropJackpotsQuery
+    MustDropJackpotsQuery,
+    { pollInterval }
   );
   if (loading) {
     return <MustDropJackpotsWidgetSkeleton />;
@@ -16,6 +19,8 @@ const MustDropJackpotsWidgetContainer = () => {
   if (data && data.mustDropJackpots) {
     return <MustDropJackpotsWidget jackpots={data.mustDropJackpots} />;
   }
+
+  return null;
 };
 
 export default MustDropJackpotsWidgetContainer;
