e461d3b38fef94a96de1473c65da78233d8054cf
Jack Westbrook <jack.westbrook@casumo.com>
2020-01-24 15:17:08 +0100

    refactor(mustDropJackpotsListContainer): prefer optional chaining

--- src/components/MustDropJackpotsList/MustDropJackpotsListContainer.js
+++ src/components/MustDropJackpotsList/MustDropJackpotsListContainer.js
@@ -2,7 +2,6 @@
 import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
-import { path } from "ramda";
 import { EVENT_PROPS } from "Src/constants";
 import TrackProvider from "Components/TrackProvider";
 import MustDropJackpotsList from "./MustDropJackpotsList";
@@ -28,21 +27,24 @@ const QUERY = gql`
 
 const MustDropJackpotsListContainer = () => {
   const { data, loading } = useQuery(QUERY);
-  const getName = path(["gamesList", "name"]);
-  const getGames = path(["gamesList", "games"]);
+  if (loading) {
+    return null;
+  }
 
-  return loading ? null : (
-    <TrackProvider
-      data={{ [EVENT_PROPS.LOCATION]: "Must Drop Jackpots - Top Lists" }}
-    >
-      <MustDropJackpotsList
-        jackpots={getGames(data)}
-        name={getName(data)}
-        //__FIX__ make sure to add in CMS field.
-        seeMore="..."
-      />
-    </TrackProvider>
-  );
+  if (data) {
+    return (
+      <TrackProvider
+        data={{ [EVENT_PROPS.LOCATION]: "Must Drop Jackpots - Top Lists" }}
+      >
+        <MustDropJackpotsList
+          jackpots={data?.gamesList?.games}
+          name={data?.gamesList?.name}
+          //__FIX__ make sure to add in CMS field.
+          seeMore="..."
+        />
+      </TrackProvider>
+    );
+  }
 };
 
 export default MustDropJackpotsListContainer;
