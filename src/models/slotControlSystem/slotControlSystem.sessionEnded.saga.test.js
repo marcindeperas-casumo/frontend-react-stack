7fbc31888c20a694461117989311ecb6d91081af
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-01-29 13:58:17 +0100

    Fixed saga test.

--- src/models/slotControlSystem/slotControlSystem.sessionEnded.saga.test.js
+++ src/models/slotControlSystem/slotControlSystem.sessionEnded.saga.test.js
@@ -14,6 +14,7 @@ describe("Models/slotControlSystem/sessionEndedSaga", () => {
 
     expect(dispatchedAction.response).toEqual({
       activeSession: null,
+      activeExclusion: null,
       lastEndedSession: endedSessionMock,
     });
   });
