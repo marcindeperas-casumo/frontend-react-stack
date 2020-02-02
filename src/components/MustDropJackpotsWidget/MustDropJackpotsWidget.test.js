e8150e440d1196cddaa6b498adb4f0a10a6f5504
Levente Balogh <balogh.levente.hu@gmail.com>
2020-02-02 11:23:32 +0100

    tests(MustDropJackpotsWidget): fix tests

--- src/components/MustDropJackpotsWidget/MustDropJackpotsWidget.test.js
+++ src/components/MustDropJackpotsWidget/MustDropJackpotsWidget.test.js
@@ -5,41 +5,10 @@ import jackpots from "./__mocks__/jackpots.json";
 
 describe("MustDropJackpotsWidget", () => {
   test("should render an a tag with a MustDropJackpot for each jackpot", () => {
-    const fetchJackpots = jest.fn();
-    const fetchCmsContent = jest.fn();
-    const subscribe = jest.fn();
-    const rendered = shallow(
-      <MustDropJackpotsWidget
-        isFetched={true}
-        fetchJackpots={fetchJackpots}
-        fetchCmsContent={fetchCmsContent}
-        subscribeToUpdates={subscribe}
-        jackpots={jackpots}
-      />
-    );
+    const rendered = shallow(<MustDropJackpotsWidget jackpots={jackpots} />);
     expect(rendered.find("a").prop("href")).toBe(
       "/en/games/must-drop-jackpots"
     );
     expect(rendered.find("MustDropJackpot").length).toBe(jackpots.length);
-    expect(subscribe).toBeCalledTimes(1);
-  });
-
-  test("should call fetch and fetchContent on component did mount", () => {
-    const fetchJackpots = jest.fn();
-    const fetchCmsContent = jest.fn();
-    const subscribe = jest.fn();
-    const rendered = shallow(
-      <MustDropJackpotsWidget
-        isFetched={false}
-        fetchJackpots={fetchJackpots}
-        fetchCmsContent={fetchCmsContent}
-        subscribeToUpdates={subscribe}
-        jackpots={jackpots}
-      />
-    );
-    expect(rendered.find("MustDropJackpotsWidgetSkeleton").length).toBe(1);
-    expect(fetchJackpots).toBeCalledTimes(1);
-    expect(fetchCmsContent).toBeCalledTimes(1);
-    expect(subscribe).toBeCalledTimes(1);
   });
 });
