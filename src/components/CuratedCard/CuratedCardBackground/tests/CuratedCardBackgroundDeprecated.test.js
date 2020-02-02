b7e1eba9a1ecbff98f1f22802444491370c915ab
Levente Balogh <balogh.levente.hu@gmail.com>
2020-02-02 16:38:33 +0100

    tests(CuratedCard): fix tests

--- src/components/CuratedCard/CuratedCardBackground/tests/CuratedCardBackgroundDeprecated.test.js
+++ src/components/CuratedCard/CuratedCardBackground/tests/CuratedCardBackgroundDeprecated.test.js
@@ -1,28 +1,22 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { omit } from "ramda";
 import curatedData from "Models/curated/__mocks__/curated.json";
 import { CuratedCardBackgroundDeprecated } from "../CuratedCardBackgroundDeprecated";
 
-const deprecatedData = {
-  ...omit("image", curatedData),
-  small_image: curatedData.image,
-  medium_image: curatedData.image,
-  large_image: curatedData.image,
-};
-
 describe("CuratedCardBackgroundDeprecated", () => {
   test("should trigger onClick", () => {
-    const onLaunchGame = jest.fn();
+    const onClick = jest.fn();
     const component = shallow(
       <CuratedCardBackgroundDeprecated
-        onLaunchGame={onLaunchGame}
-        {...deprecatedData}
+        onClick={onClick}
+        small_image={curatedData.image}
+        medium_image={curatedData.image}
+        large_image={curatedData.image}
       />
     );
 
     component.find("a").simulate("click");
 
-    expect(onLaunchGame).toHaveBeenCalled();
+    expect(onClick).toHaveBeenCalled();
   });
 });
