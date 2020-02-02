65a35ea339e4457efba389ee523c5e02dda902ac
Levente Balogh <balogh.levente.hu@gmail.com>
2020-02-02 09:49:19 +0100

    tests(JackpotsListTile): fix tests

--- src/components/JackpotsListTile/JackpotsListTile.test.js
+++ src/components/JackpotsListTile/JackpotsListTile.test.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { GameRow } from "Components/GameRow";
+import { GameRow } from "Components/GameRow/GameRow";
 import { JackpotsListTile } from "./JackpotsListTile";
 
 describe("<JackpotsListTile />", () => {
   test("renders a <GameRow /> for each game", () => {
-    const games = ["one", "two", "three"];
+    const games = [
+      { id: 1, slug: "one" },
+      { id: 2, slug: "two" },
+      { id: 3, slug: "three" },
+    ];
     const rendered = shallow(<JackpotsListTile games={games} />);
 
     expect(rendered.find(GameRow)).toHaveLength(games.length);
