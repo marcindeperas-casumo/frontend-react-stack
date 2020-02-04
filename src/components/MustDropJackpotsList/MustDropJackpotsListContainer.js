ad820a1527a1f76ebb4ba2cafdc4d24e040373cb
Levente Balogh <balogh.levente.hu@gmail.com>
2020-02-04 17:35:34 +0100

    fix(MustDropJackpots): fetch see-more link

--- src/components/MustDropJackpotsList/MustDropJackpotsListContainer.js
+++ src/components/MustDropJackpotsList/MustDropJackpotsListContainer.js
@@ -6,12 +6,11 @@ import { EVENT_PROPS } from "Src/constants";
 import TrackProvider from "Components/TrackProvider";
 import MustDropJackpotsList from "./MustDropJackpotsList";
 
-//__FIX__ this query is missing the following information:
-// slug: `built-pages.top-lists-${marketSelector(state)}`,
-// field: "more_link",
-
 const QUERY = gql`
   query gamesListQuery {
+    seeMore: getText(
+      id: "root:built-pages.top-lists-translations:fields.more_link"
+    )
     gamesList(listId: "mustDropJackpotGames") {
       name
       games {
@@ -39,8 +38,7 @@ const MustDropJackpotsListContainer = () => {
         <MustDropJackpotsList
           jackpots={data?.gamesList?.games}
           name={data?.gamesList?.name}
-          //__FIX__ make sure to add in CMS field.
-          seeMore="..."
+          seeMore={data?.seeMore}
         />
       </TrackProvider>
     );
