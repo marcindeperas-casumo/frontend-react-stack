2f1ef0a128216a658cfcfa8cb52753b2af7e36fa
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-02-05 15:42:03 +0100

    Added tests for logout saga.

---
+++ src/models/app/app.manualLogout.saga.test.js
@@ -0,0 +1,56 @@
+// @flow
+import { cloneableGenerator } from "redux-saga/utils";
+import { REACT_APP_MODAL } from "Src/constants";
+import { showModal, isModalHiddenSelector } from "Models/modal";
+import { waitForSelector } from "Utils";
+import { appManualLogoutSaga, logout } from "Models/app";
+
+describe("appManualLogoutSaga()", () => {
+  const clonedGenerator = cloneableGenerator(appManualLogoutSaga)();
+  const delayTest = () => {
+    const effect = clonedGenerator.next();
+
+    expect(effect.value).toBeInstanceOf(Promise);
+  };
+
+  test("it triggers showing Slot System Modal first", () => {
+    const expectedAction = showModal(
+      REACT_APP_MODAL.ID.SLOT_CONTROL_SYSTEM_BEFORE_LOGGING_OUT,
+      {
+        mustAccept: true,
+      }
+    );
+    const action = clonedGenerator.next().value.PUT.action;
+
+    expect(action).toEqual(expectedAction);
+  });
+
+  test("next it triggers delay", delayTest);
+
+  test("next it waits till the modal, if any, has been closed", () => {
+    const effect = clonedGenerator.next();
+
+    expect(effect.value.CALL.fn).toEqual(waitForSelector);
+    expect(effect.value.CALL.args).toEqual([isModalHiddenSelector]);
+  });
+
+  test("next it dispatches logout store action", () => {
+    const effect = clonedGenerator.next();
+
+    expect(effect.value.PUT.action).toEqual(logout());
+  });
+
+  test("next it triggers delay", delayTest);
+
+  test("next it calls a function to navigate browser", () => {
+    const effect = clonedGenerator.next();
+
+    expect(effect.value.CALL.fn).toBeInstanceOf(Function);
+  });
+
+  test("next it's done", () => {
+    const effect = clonedGenerator.next();
+
+    expect(effect.done).toEqual(true);
+  });
+});
