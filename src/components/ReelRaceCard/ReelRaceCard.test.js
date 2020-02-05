7dcae5f9728598722656994d95438fd45292e43f
Michele <michele.settepani@casumo.com>
2020-02-05 17:37:22 +0100

    ReelRaceCard: fix tests

--- src/components/ReelRaceCard/ReelRaceCard.test.js
+++ src/components/ReelRaceCard/ReelRaceCard.test.js
@@ -4,82 +4,72 @@ import { shallow } from "enzyme";
 import { ReelRaceCard } from "./ReelRaceCard";
 
 const props = {
-  tournamentId: "1",
-  color: "yellow-light-1",
-  spins: 666,
-  minBet: "€0.50",
-  prize: "€666",
-  gameSlug: "gonzos-quest",
+  id: "edc71c70-56d6-11e9-8587-0242ac11000b",
+  startTime: 1580882400000,
+  optedIn: false,
+  endTime: 1580883600000,
+  spinLimit: 140,
+  minBet: null,
+  promoted: false,
+  formattedPrize: "€20",
+  remainingSpins: 99,
   game: {
-    name: "Gonzo&#8217;s Quest",
-    slug: "gonzos-quest",
-    logoBackground:
-      "https://cms.casumo.com/wp-content/uploads/2014/06/GonzosQuest_Thumb.jpg",
+    id: "fa9aa550-6be1-11e4-a1d6-005056a03af2",
+    name: "Jack and the Beanstalk",
     logo:
-      "https://cms.casumo.com/wp-content/uploads/2014/02/GonzosQuest_Logo.png",
-    jackpot: null,
-    lobby: null,
+      "https://cms.casumo.com/wp-content/uploads/2014/02/JackOfTheBeanstalk_Logo.png",
+    backgroundImage:
+      "https://cms.casumo.com/wp-content/uploads/2014/06/JackOfTheBeanstalk_Thumb.jpg",
+    slug: "jack-the-beanstalk",
   },
-  t: {
+  translations: {
+    optedInCtaSingleGameShort: "Play",
+    optIn: "Opt in",
+    optedIn: "Opted in",
+    endingIn: "Ending in",
+    startingIn: "Starting in:",
+    competeFor: "Compete for {{prize}}",
     spins: "Spins",
     duration: "Duration",
-    duration_template: "{{{duration}}} min",
-    min_bet: "Min Bet",
-    starting_in: "Starting in",
-    ending_in: "Ending in",
-    opt_in: "Opt In",
-    opted_in: "Opted In",
-    opted_in_cta_single_game_short: "Play",
-    compete_for: "Compete for {{prize}}",
-    title: "Reel Races",
-    caveat_short: "false",
+    durationTemplate: "{{{duration}}} min",
+    minBet: "Min Bet",
+    caveatShort: "false",
   },
-  launchGame: () => {},
-  optIn: () => {},
 };
+
 const minute = 60 * 1000;
 
 const launchGame = jest.fn();
 
-describe("ReelRaceCard", () => {
+describe.only("ReelRaceCard", () => {
   describe("Empty game object", () => {
-    const propsNoGame = {
-      tournamentId: "1",
-      color: "yellow-light-1",
-      spins: 666,
-      minBet: "€0.50",
-      prize: "€666",
-      gameSlug: "gonzos-quest",
-      // $FlowFixMe
+    const reelRace = {
+      id: "edc71c70-56d6-11e9-8587-0242ac11000b",
+      startTime: 1580882400000,
+      optedIn: false,
+      endTime: 1580883600000,
+      spinLimit: 140,
+      minBet: null,
+      promoted: false,
+      formattedPrize: "€20",
+      remainingSpins: 99,
       game: {},
-      t: {
+      translations: {
+        optedInCtaSingleGameShort: "Play",
+        optIn: "Opt in",
+        optedIn: "Opted in",
+        endingIn: "Ending in",
+        startingIn: "Starting in:",
+        competeFor: "Compete for {{prize}}",
         spins: "Spins",
         duration: "Duration",
-        duration_template: "{{{duration}}} min",
-        min_bet: "Min Bet",
-        starting_in: "Starting in",
-        ending_in: "Ending in",
-        opt_in: "Opt In",
-        opted_in: "Opted In",
-        opted_in_cta_single_game_short: "Play",
-        compete_for: "Compete for {{prize}}",
-        title: "Reel Races",
-        caveat_short: "false",
+        durationTemplate: "{{{duration}}} min",
+        minBet: "Min Bet",
+        caveatShort: "false",
       },
-      launchGame: () => {},
-      optIn: () => {},
     };
 
-    const now = Date.now();
-    const rendered = shallow(
-      <ReelRaceCard
-        {...propsNoGame}
-        promoted={false}
-        opted={false}
-        startTime={now + 30 * minute}
-        endTime={now + 60 * minute}
-      />
-    );
+    const rendered = shallow(<ReelRaceCard reelRace={reelRace} />);
 
     test("should not show any card if game object is empty", () => {
       expect(rendered.isEmptyRender()).toEqual(true);
@@ -88,54 +78,57 @@ describe("ReelRaceCard", () => {
 
   describe("Scheduled", () => {
     const now = Date.now();
-    const rendered = shallow(
-      <ReelRaceCard
-        {...props}
-        promoted={false}
-        opted={false}
-        startTime={now + 30 * minute}
-        endTime={now + 60 * minute}
-      />
-    );
+    const reelRace = {
+      ...props,
+      promoted: false,
+      opted: false,
+      startTime: now + 30 * minute,
+      endTime: now + 60 * minute,
+    };
+
+    const rendered = shallow(<ReelRaceCard reelRace={reelRace} />);
 
     test('should show "Starting In" text', () => {
-      expect(rendered.contains(props.t.starting_in)).toBe(true);
+      expect(rendered.contains(props.translations.startingIn)).toBe(true);
     });
     test('should show "Opt In" button', () => {
       expect(
         rendered
           .find("OptInButton")
           .dive()
-          .contains(props.t.opt_in)
+          .contains(props.translations.optIn)
       ).toBe(true);
     });
 
     test('should show "Opted In" button if user opted for race', () => {
-      rendered.setProps({ opted: true });
+      rendered.setProps({
+        reelRace: {
+          ...reelRace,
+          optedIn: true,
+        },
+      });
       expect(
         rendered
           .find("OptInButton")
           .dive()
-          .contains(props.t.opted_in)
+          .contains(props.translations.optedIn)
       ).toBe(true);
     });
   });
 
   describe("Ongoing", () => {
     const now = Date.now();
-    const rendered = shallow(
-      <ReelRaceCard
-        {...props}
-        promoted={false}
-        opted
-        startTime={now}
-        endTime={now + 30 * minute}
-        launchGame={launchGame}
-      />
-    );
+    const reelRace = {
+      ...props,
+      optedIn: true,
+      startTime: now,
+      endTime: now + 30 * minute,
+      launchGame: launchGame,
+    };
+    const rendered = shallow(<ReelRaceCard reelRace={reelRace} />);
 
     test('should show "Ending In" text', () => {
-      expect(rendered.contains(props.t.ending_in)).toBe(true);
+      expect(rendered.contains(props.translations.endingIn)).toBe(true);
     });
 
     test('should show "Play" button', () => {
@@ -143,19 +136,8 @@ describe("ReelRaceCard", () => {
         rendered
           .find("Button")
           .children()
-          .contains(props.t.opted_in_cta_single_game_short)
+          .contains(props.translations.optedInCtaSingleGameShort)
       ).toBe(true);
-    });
-
-    test("should launch game if click on game info", () => {
-      expect(launchGame).toHaveBeenCalledTimes(0);
-
-      rendered
-        .find("Flex.u-cursor-pointer")
-        .first()
-        .simulate("click");
-
-      expect(launchGame).toHaveBeenCalledTimes(1);
     });
 
     test("shouldn't contain promoted badge", () => {
@@ -165,15 +147,14 @@ describe("ReelRaceCard", () => {
 
   test("should show promoted badge", () => {
     const now = Date.now();
-    const rendered = shallow(
-      <ReelRaceCard
-        {...props}
-        promoted
-        opted
-        startTime={now}
-        endTime={now + 30 * minute}
-      />
-    );
+    const reelRace = {
+      ...props,
+      optedIn: true,
+      promoted: true,
+      startTime: now,
+      endTime: now + 30 * minute,
+    };
+    const rendered = shallow(<ReelRaceCard reelRace={reelRace} />);
 
     expect(rendered.find(".c-reel-race__badge")).toHaveLength(1);
   });
