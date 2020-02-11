1fff1bfcc6092295b2bd0ffc2aa5d0630bc5afb0
Michele <michele.settepani@casumo.com>
2020-02-11 15:34:34 +0100

    ReelRaceCardContainer: use correct type from apollo

--- src/components/ReelRaceCard/ReelRaceCardContainer.js
+++ src/components/ReelRaceCard/ReelRaceCardContainer.js
@@ -1,12 +1,17 @@
 // @flow
 import React from "react";
 import { useMutation } from "@apollo/react-hooks";
+import * as A from "Types/apollo";
 import { ReelRaceCard } from "./ReelRaceCard";
 import { OptInForReelRace } from "./ReelRaceCard.graphql";
 
+type Props = {
+  item: A.ReelRaceCard_ReelRace,
+};
+
 // __FIX__: this should take a game but because of
 // https://github.com/Casumo/frontend-react-stack/blob/master/src/components/ScrollableList/ScrollableList.js#L42
-export const ReelRaceCardContainer = ({ item }) => {
+export const ReelRaceCardContainer = ({ item }: Props) => {
   const { id } = item;
   const [optInForReelRace] = useMutation(OptInForReelRace, {
     variables: {
