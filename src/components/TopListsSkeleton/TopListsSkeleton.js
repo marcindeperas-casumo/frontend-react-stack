072776dee7065bb8b914cc811f34f14c222c9e2f
Levente Balogh <balogh.levente.hu@gmail.com>
2020-02-12 12:46:16 +0100

    refactor(TopListsSkeleton): simplify looping

--- src/components/TopListsSkeleton/TopListsSkeleton.js
+++ src/components/TopListsSkeleton/TopListsSkeleton.js
@@ -1,11 +1,15 @@
 /* @flow */
 import React from "react";
+import { times } from "ramda";
 import { GameListHorizontalSkeleton } from "Components/GameListHorizontal/GameListHorizontalSkeleton";
 
 export const TopListsSkeleton = () => (
   <div>
-    {Array.from(Array(4).keys()).map(i => (
-      <GameListHorizontalSkeleton key={i} />
-    ))}
+    {times(
+      i => (
+        <GameListHorizontalSkeleton key={i} />
+      ),
+      4
+    )}
   </div>
 );
