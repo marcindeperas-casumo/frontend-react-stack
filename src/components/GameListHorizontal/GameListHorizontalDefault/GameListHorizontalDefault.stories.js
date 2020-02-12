7da2c60534a5c177926fe16663d9ec8ecc2ef9e5
Jack Westbrook <jack.westbrook@casumo.com>
2020-02-12 15:05:46 +0100

    chore(gameLists): move mocks out to individual files

--- src/components/GameListHorizontal/GameListHorizontalDefault/GameListHorizontalDefault.stories.js
+++ src/components/GameListHorizontal/GameListHorizontalDefault/GameListHorizontalDefault.stories.js
@@ -4,6 +4,7 @@ import { storiesOf } from "@storybook/react";
 import { MockedProvider } from "@apollo/react-testing";
 import { GameListHorizontalDefault } from "./GameListHorizontalDefault";
 import { GameListQuery } from "./GameListHorizontalDefault.graphql";
+import { gamesListMock } from "./__mock__";
 
 const stories = storiesOf("GameListHorizontalDefault", module);
 const mocks = [
@@ -16,89 +17,7 @@ const mocks = [
     },
     result: {
       data: {
-        gamesList: {
-          id: "latestPlayedGames",
-          name: "Continue playing",
-          games: [
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2018/05/pirates_charm_thumbnail.jpg",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2018/05/pirates_charm_logo.png",
-              name: "Pirate’s Charm",
-              slug: "pirates-charm",
-              id: "d134f7e0-529e-11e8-a764-0242ac110002",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2019/07/Sticky_bandits_wild_Thumbnail_BG.png",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2019/07/Sticky_bandits_wild_Thumbnail_Logo.png",
-              name: "Sticky Bandits Wild Return",
-              slug: "sticky-bandits-wild-return",
-              id: "2db3b650-b1da-11e9-9665-0242ac110002",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2016/06/PlayNGo-FireJoker-Thumb.jpg",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2016/06/PlayNGo-FireJoker-Logo.png",
-              name: "Fire Joker",
-              slug: "fire-joker",
-              id: "fa0cc9f0-8cb9-11e7-b1fd-005056a03af2",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2018/03/easter_island_thumbnail.jpg",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2018/03/easter_island_logo.png",
-              name: "Easter Island",
-              slug: "easter-island",
-              id: "4b452d50-2db7-11e8-9e3c-0242ac110003",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2018/10/dragons_fire_thumbnail.jpg",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2018/10/dragons_fire_logo.png",
-              name: "Dragon’s Fire",
-              slug: "dragon-fire",
-              id: "2f93c820-d387-11e8-b1e4-0242ac110002",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2017/09/rapunzels_tower_thumbnail.jpg",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2017/09/rapunzels_tower_logo.png",
-              name: "Rapunzel’s Tower",
-              slug: "rapunzels-tower",
-              id: "8f1bceb0-d378-11e7-b562-005056a03af2",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2015/08/bigbadwolfbgthumb.jpg",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2015/08/bigbadwolf_logo.png",
-              name: "Big Bad Wolf",
-              slug: "big-bad-wolf",
-              id: "9738a211-311a-11e5-9734-005056a03af2",
-              isInMyList: false,
-            },
-          ],
-        },
+        gamesList: gamesListMock,
       },
     },
   },
--- src/components/GameListHorizontal/GameListHorizontalExclusive/GameListHorizontalExclusive.stories.js
+++ src/components/GameListHorizontal/GameListHorizontalExclusive/GameListHorizontalExclusive.stories.js
@@ -5,6 +5,7 @@ import { MockedProvider } from "@apollo/react-testing";
 import MockStore from "Components/MockStore";
 import { GameListHorizontalExclusive } from "./GameListHorizontalExclusive";
 import { GameListExclusiveQuery } from "./GameListHorizontalExclusive.graphql";
+import { gamesListMock } from "./__mock__";
 
 const stories = storiesOf("GameListHorizontalExclusive", module);
 const mocks = [
@@ -17,45 +18,7 @@ const mocks = [
     },
     result: {
       data: {
-        gamesList: {
-          id: "exclusiveGames",
-          name: "Only At Casumo",
-          games: [
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2020/01/wild_cauldron_Thumbnail_BG.png",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2020/01/wild_cauldron_Thumbnail_Logo.png",
-              name: "Wild Cauldron",
-              slug: "wild-cauldron",
-              id: "37cf2833-cfeb-4e18-90fd-6a43002f1d68",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2020/01/beat_the_beast_kraken_s_lair_Thumbnail_BG.png",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2020/01/beat_the_beast_kraken_s_lair_Thumbnail_Logo.png",
-              name: "Beat the Beast: Kraken’s Lair",
-              slug: "beat-the-beast-krakens-lair",
-              id: "badd838d-4c9b-405b-93ce-d9d9a202d7a2",
-              isInMyList: false,
-            },
-            {
-              isInMaintenance: false,
-              backgroundImage:
-                "https://cms.casumo.com/wp-content/uploads/2019/11/Diamond_Mine_Thumbnail_BG.png",
-              logo:
-                "https://cms.casumo.com/wp-content/uploads/2019/11/Diamond_Mine_Thumbnail_Logo.png",
-              name: "Diamond Mine Megaways: Extra Gold",
-              slug: "diamond-mine-extra-gold",
-              id: "b9802f00-0c3f-11ea-8451-0242ac110002",
-              isInMyList: false,
-            },
-          ],
-        },
+        gamesList: gamesListMock,
       },
     },
   },
