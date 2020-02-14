54a192feab11a792d59fd168076a367d8fc6bf8d
Michele <michele.settepani@casumo.com>
2020-02-14 10:07:35 +0100

    GameTile: remove container

--- src/components/GameTile/GameTile.stories.js
+++ src/components/GameTile/GameTile.stories.js
@@ -4,25 +4,11 @@ import { storiesOf } from "@storybook/react";
 import { boolean, withKnobs } from "@storybook/addon-knobs/react";
 import { action } from "@storybook/addon-actions";
 import MockStore from "Components/MockStore";
-import { GameTile as GameTileConnected } from "Components/GameTile";
-import { GameTile } from "Components/GameTile/GameTile";
-import isNotChromatic from "Storybook/isNotChromatic";
+import { GameTile } from "Components/GameTile";
 import game from "./__mocks__/Game.json";
 
 const stories = storiesOf("GameTile", module);
 stories.addDecorator(withKnobs);
-
-if (isNotChromatic) {
-  stories.add("Default (Connected)", () => {
-    return (
-      <div className="c-top-game">
-        <MockStore>
-          <GameTileConnected id="starburst" />
-        </MockStore>
-      </div>
-    );
-  });
-}
 
 stories.add("Default", () => {
   const inMaintenanceMode = boolean(
--- src/components/GameTile/index.js
+++ src/components/GameTile/index.js
@@ -1,3 +1,2 @@
 // @flow
-export type * from "./GameTileContainer";
-export { GameTileContainer as GameTile } from "./GameTileContainer";
+export { GameTile } from "./GameTile";
