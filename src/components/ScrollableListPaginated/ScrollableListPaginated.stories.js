6fc0b5a2246ec46a918df71911d52dde3331db40
Levente Balogh <balogh.levente.hu@gmail.com>
2020-02-19 11:35:51 +0100

    fix(ScrollablePaginated): fixing stories round 1.

--- src/components/ScrollableListPaginated/ScrollableListPaginated.stories.js
+++ src/components/ScrollableListPaginated/ScrollableListPaginated.stories.js
@@ -1,9 +1,10 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { ScrollableListPaginated } from "Components/ScrollableListPaginated";
+import { MockedProvider } from "@apollo/react-testing";
 import { GameTile } from "Components/GameTile/GameTile";
 import { gamesListMock } from "Components/GameListHorizontal/GameListHorizontalDefault/__mock__";
+import { ScrollableListPaginated } from "./ScrollableListPaginated";
 
 const stories = storiesOf("ScrollableListPaginated", module);
 
@@ -13,18 +14,22 @@ const ScrollableListPaginatedStory = () => {
       <GameTile game={gamesListMock.games[columnIndex]} />
     </div>
   );
+
   return (
-    <ScrollableListPaginated
-      title={gamesListMock.name}
-      itemCount={gamesListMock.games.length}
-      tileHeight={204}
-      itemControlClass="c-scrollable-list-paginated__button"
-      seeMore={{
-        url: "/aw-gidi",
-        text: "Aw gidi",
-      }}
-      itemRenderer={itemRenderer}
-    />
+    <MockedProvider>
+      <ScrollableListPaginated
+        title={gamesListMock.name}
+        itemCount={gamesListMock.games.length}
+        tileHeight={204}
+        tileWidth={204}
+        itemControlClass="c-scrollable-list-paginated__button"
+        seeMore={{
+          url: "/aw-gidi",
+          text: "Aw gidi",
+        }}
+        itemRenderer={itemRenderer}
+      />
+    </MockedProvider>
   );
 };
 
