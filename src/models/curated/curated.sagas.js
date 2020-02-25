6cf050b387a95372f5751fdcfb233e38e7578876
Cho <choishere@gmail.com>
2020-02-25 15:20:01 +0100

    fix(curated) confused cms page with selected game id

--- src/models/curated/curated.sagas.js
+++ src/models/curated/curated.sagas.js
@@ -1,18 +1,12 @@
 import { select, put, take, call } from "redux-saga/effects";
-import {
-  types,
-  curatedSelector,
-  fetchCuratedGame,
-  prefixCuratedSlug,
-} from "Models/curated";
+import { pathOr } from "ramda";
+import { types, curatedSelector, fetchCuratedGame } from "Models/curated";
 import { normalizeData, updateEntity } from "Models/schema";
 import { countrySelector } from "Models/handshake";
 
 export function* fetchCuratedGameSaga(action) {
   const platform = "mobile";
-  const { type } = action;
-  const gameId = type.split(".")[1];
-  const slug = prefixCuratedSlug(gameId);
+  const slug = action.type.split("-")[1];
   const curated = yield select(curatedSelector(slug));
   const country = yield select(countrySelector);
 
@@ -22,12 +16,13 @@ export function* fetchCuratedGameSaga(action) {
     return;
   }
 
-  const slugs = [gameId];
+  const slugs = pathOr([], ["response", "fields", "game"], action);
 
   yield put(fetchCuratedGame({ platform, country, slugs }));
 
   // pause execution until request is completed, normalize and update the store
   const { response } = yield take(types.CURATED_FETCH_GAME_COMPLETE);
   const { entities } = yield call(normalizeData, response);
+
   yield put(updateEntity(entities));
 }
