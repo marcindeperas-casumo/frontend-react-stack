c8018a3e81fae3fb40ed00fb79547b11312b69bf
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-02-27 14:15:36 +0100

    Reduced flickering of configuration form in before launching a game and also after logout modal has been shown. Added logoutStarted player property along with selector, reducer, action.

--- src/models/player/index.js
+++ src/models/player/index.js
@@ -1,4 +1,6 @@
 // @flow
+export * from "./player.constants";
+export * from "./player.actions";
 export * from "./player.selectors";
 export * from "./player.types";
 export { default as reducer } from "./player.reducer";
