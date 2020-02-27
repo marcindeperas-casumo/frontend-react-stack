cff3c2b3ac5c1f23e5ee8c68f7d238b5b2c975e1
Cho <choishere@gmail.com>
2020-02-27 19:53:45 +0100

    feat(sports los) fix flow errors

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: "graphql-server-games",
-      url: "http://localhost:4000/",
+      url: "http://graphql-server-games.at.casumotest.local:8080",
       // optional disable SSL validation check
       skipSSLValidation: true,
     },
--- src/features/sports/components/SportsLOS/SportsLOSContainer.js
+++ src/features/sports/components/SportsLOS/SportsLOSContainer.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { useState } from "react";
 import { useMutation } from "@apollo/react-hooks";
-import type { ExecutionResult } from "@apollo/react-hooks";
 import * as A from "Types/apollo";
 import { LAUNCH_KAMBI_LOS_MUTATION } from "Models/apollo/mutations";
 import KambiClient from "Features/sports/components/KambiClient/KambiClient";
@@ -10,50 +9,39 @@ import { SportsNav } from "Features/sports/components/SportsNav";
 import SportsShellSkeleton from "Features/sports/components/SportsShell/SportsShellSkeleton";
 import { useLanguage } from "Utils/hooks";
 
-export type LaunchKambiType = ExecutionResult<A.LaunchKambiLoS>;
+export type PlayForFun = { playForFun: boolean };
 
 export const SportsLOSContainer = () => {
   const [bootstrapUrl, setBootstrapUrl] = useState(null);
-  const [launchKambi] = useMutation<A.LaunchKambiLoS, { playForFun: boolean }>(
+  const [launchKambi, { data }] = useMutation<A.LaunchKambiLoS, PlayForFun>(
     LAUNCH_KAMBI_LOS_MUTATION
   );
   const language = useLanguage();
 
   React.useEffect(() => {
-    launchKambi({ variables: { playForFun: true } }).then(({ data }) => {
-      if (
-        !bootstrapUrl &&
-        data &&
-        data.launchKambi &&
-        data.launchKambi.clientBootstrapUrl
-      ) {
+    if (!bootstrapUrl) {
+      launchKambi({ variables: { playForFun: true } });
+      if (data && data.launchKambi) {
         setBootstrapUrl(data.launchKambi.clientBootstrapUrl);
       }
-    });
-  }, [bootstrapUrl, launchKambi]);
+    }
+  }, [bootstrapUrl, data, launchKambi]);
 
   if (!language || !bootstrapUrl) {
     return <SportsShellSkeleton />;
   }
 
   return (
-    <>
+    <div className="t-background-chrome-light-2">
       <SportsHashWatcher>
-        {({ currentHash }) => (
-          <div className="t-background-chrome-light-2">
-            <SportsNav currentHash={currentHash} />
-          </div>
-        )}
+        {({ currentHash }) => <SportsNav currentHash={currentHash} />}
       </SportsHashWatcher>
       <KambiClient
         currency={"GBP"}
         market={"GB"}
         locale={`${language}_GB`}
         bootstrapUrl={bootstrapUrl}
-        ticket={""}
-        // $FlowFixMe
-        sessionKeepAlive={() => {}}
       />
-    </>
+    </div>
   );
 };
