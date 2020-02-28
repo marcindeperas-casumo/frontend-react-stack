4c697e19c3af6c42cfa9454349283270b8c8757b
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-02-28 10:17:03 +0100

    Instead of commenting out tests, skip() them.

--- src/components/Settings/SettingsSections/SettingsSectionsContainer.test.js
+++ src/components/Settings/SettingsSections/SettingsSectionsContainer.test.js
@@ -1,87 +1,96 @@
-describe("SettingsSections", () => {
-  // TODO PCC-469
+import React from "react";
+import { mount } from "enzyme";
+import { MockedProvider } from "@apollo/react-testing";
+import { waitAndUpdateWrapper } from "Utils";
+import { withContainer } from "./SettingsSectionsContainer";
+import {
+  playerSectionsQueryMock,
+  playerSectionsLabelsQueryMock,
+  playerSectionsQueryErrorMock,
+  playerSectionsLabelsQueryErrorMock,
+} from "./__mocks__/Queries.mock";
 
-  // beforeEach(() => {
-  //   Component = props => <div />;
-  //   SettingsSectionsContainer = () => withContainer(Component);
-  // });
+let Component, SettingsSectionsContainer;
+// TODO PCC-469 REMOVE REACT-ADOPT AND REWRITE COMPONENTS/TESTS
+describe.skip("SettingsSections", () => {
+  beforeEach(() => {
+    Component = props => <div />;
+    SettingsSectionsContainer = () => withContainer(Component);
+  });
 
   describe("Player Settings", () => {
-    test("TESTS COMMENTED OUT - PCC-469 - REMOVE REACT-ADOPT AND REWRITE COMPONENTS/TESTS", () => {});
-    //   test("should render loader", async () => {
-    //     const rendered = mount(
-    //       <MockedProvider
-    //         mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryMock]}
-    //       >
-    //         <SettingsSectionsContainer />
-    //       </MockedProvider>
-    //     );
+    test("should render loader", () => {
+      const rendered = mount(
+        <MockedProvider
+          mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryMock]}
+        >
+          <SettingsSectionsContainer />
+        </MockedProvider>
+      );
 
-    //     expect(rendered.find("SettingsRowListSkeleton")).toHaveLength(1);
+      expect(rendered.find("SettingsRowListSkeleton")).toHaveLength(1);
+    });
 
-    //     await waitAndUpdateWrapper(rendered);
-    //   });
+    test("should show error", async () => {
+      const rendered = mount(
+        <MockedProvider
+          mocks={[playerSectionsQueryErrorMock, playerSectionsLabelsQueryMock]}
+        >
+          <SettingsSectionsContainer />
+        </MockedProvider>
+      );
 
-    //   test("should show error", async () => {
-    //     const rendered = mount(
-    //       <MockedProvider
-    //         mocks={[playerSectionsQueryErrorMock, playerSectionsLabelsQueryMock]}
-    //       >
-    //         <SettingsSectionsContainer />
-    //       </MockedProvider>
-    //     );
+      await waitAndUpdateWrapper(rendered);
 
-    //     await waitAndUpdateWrapper(rendered);
+      expect(rendered.find("ErrorMessage")).toHaveLength(1);
+    });
 
-    //     expect(rendered.find("ErrorMessage")).toHaveLength(1);
-    //   });
+    test("should pass correct player to child", async () => {
+      const rendered = mount(
+        <MockedProvider
+          mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryMock]}
+        >
+          <SettingsSectionsContainer />
+        </MockedProvider>
+      );
 
-    //   test("should pass correct player to child", async () => {
-    //     const rendered = mount(
-    //       <MockedProvider
-    //         mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryMock]}
-    //       >
-    //         <SettingsSectionsContainer />
-    //       </MockedProvider>
-    //     );
+      await waitAndUpdateWrapper(rendered);
 
-    //     await waitAndUpdateWrapper(rendered);
+      expect(
+        rendered.find("Component").prop("playerLoginHistory")
+      ).toStrictEqual(playerSectionsQueryMock.result.data);
+    });
+  });
 
-    //     expect(
-    //       rendered.find("Component").prop("playerLoginHistory")
-    //     ).toStrictEqual(playerSectionsQueryMock.result.data);
-    //   });
-    // });
+  describe("Labels", () => {
+    test("should show error", async () => {
+      const rendered = mount(
+        <MockedProvider
+          mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryErrorMock]}
+        >
+          <SettingsSectionsContainer />
+        </MockedProvider>
+      );
 
-    // describe("Labels", () => {
-    //   test("should show error", async () => {
-    //     const rendered = mount(
-    //       <MockedProvider
-    //         mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryErrorMock]}
-    //       >
-    //         <SettingsSectionsContainer />
-    //       </MockedProvider>
-    //     );
+      await waitAndUpdateWrapper(rendered);
 
-    //     await waitAndUpdateWrapper(rendered);
+      expect(rendered.find("ErrorMessage")).toHaveLength(1);
+    });
 
-    //     expect(rendered.find("ErrorMessage")).toHaveLength(1);
-    //   });
+    test("should pass correct player to child", async () => {
+      const rendered = mount(
+        <MockedProvider
+          mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryMock]}
+        >
+          <SettingsSectionsContainer />
+        </MockedProvider>
+      );
 
-    //   test("should pass correct player to child", async () => {
-    //     const rendered = mount(
-    //       <MockedProvider
-    //         mocks={[playerSectionsQueryMock, playerSectionsLabelsQueryMock]}
-    //       >
-    //         <SettingsSectionsContainer />
-    //       </MockedProvider>
-    //     );
+      await waitAndUpdateWrapper(rendered);
 
-    //     await waitAndUpdateWrapper(rendered);
-
-    //     expect(
-    //       JSON.parse(JSON.stringify(rendered.find("Component").prop("labels")))
-    //     ).toStrictEqual(playerSectionsLabelsQueryMock.result.data);
-    //   });
+      expect(
+        JSON.parse(JSON.stringify(rendered.find("Component").prop("labels")))
+      ).toStrictEqual(playerSectionsLabelsQueryMock.result.data);
+    });
   });
 });
