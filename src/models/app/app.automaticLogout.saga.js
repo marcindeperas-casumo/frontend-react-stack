d4d931395933334cb86915223a7df826526255c7
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-03-02 10:59:50 +0100

    Added setting logoutStarted flag in automatic logout flow.

--- src/models/app/app.automaticLogout.saga.js
+++ src/models/app/app.automaticLogout.saga.js
@@ -2,10 +2,13 @@
 import { type Saga, delay } from "redux-saga";
 import { put, call } from "redux-saga/effects";
 import { showModal, isModalHiddenSelector } from "Models/modal";
+import { setPlayerLogoutStarted } from "Models/player";
 import { REACT_APP_MODAL } from "Src/constants";
 import { waitForSelector, navigateToRootWithReload } from "Utils";
 
 export function* appAutomaticLogoutSaga(): Saga {
+  yield put(setPlayerLogoutStarted());
+
   yield put(
     showModal(REACT_APP_MODAL.ID.SLOT_CONTROL_SYSTEM_BEFORE_LOGGING_OUT, {
       mustAccept: true,
