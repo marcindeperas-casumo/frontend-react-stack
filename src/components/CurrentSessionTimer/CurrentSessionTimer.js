e0691ae8521be0d9392d86ff5f2e5952ad7389b9
Dave Calleja <dave.calleja@casumo.com>
2020-03-06 12:44:15 +0100

    Used Time component directly instead of wrapping

--- src/components/CurrentSessionTimer/CurrentSessionTimer.js
+++ src/components/CurrentSessionTimer/CurrentSessionTimer.js
@@ -1,19 +1,5 @@
 // @flow
-import React from "react";
+
 import Timer from "Components/Timer";
 
-type TimeState = {
-  days: string,
-  hours: string,
-  minutes: string,
-  seconds: string,
-  hasEnded: boolean,
-};
-type Props = {
-  render: (state: TimeState) => string,
-  startTime: number,
-};
-
-export const CurrentSessionTimer = (props: Props) => {
-  return <Timer render={props.render} startTime={props.startTime} />;
-};
+export const CurrentSessionTimer = Timer;
