4ad7a069acb175863502c459664b677c96b93631
Levente Balogh <balogh.levente.hu@gmail.com>
2020-03-12 13:50:23 +0100

    Wip

--- src/components/ComponentBuilder/ComponentBuilder.stories.js
+++ src/components/ComponentBuilder/ComponentBuilder.stories.js
@@ -1,21 +1,42 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { MockedProvider } from "@apollo/react-testing";
 import MockStore from "Components/MockStore";
+import { GAMES_LIST_HORIZONTAL_ITEMS_LIMIT } from "Src/constants";
+import { GameListQuery } from "Components/GameListHorizontal/GameListHorizontalDefault/GameListHorizontalDefault.graphql";
+import { gamesListMock } from "Components/GameListHorizontal/GameListHorizontalDefault/__mock__";
 import { ComponentBuilderRenderer } from "./ComponentBuilderRenderer";
 
 const componentDefinitions = [
-  { acf_fc_layout: "GAMES_LIST", id: "popularGames" },
   {
     acf_fc_layout: "HTML_CONTENT",
     html: "<br /><br /><div>This is some custom HTML code.</div>",
   },
-  { acf_fc_layout: "GAMES_LIST", id: "exclusiveGames" },
+  { acf_fc_layout: "GAMES_LIST", id: "latestPlayedGames" },
   {
     acf_fc_layout: "HTML_CONTENT",
     html: "<br /><br /><div>HTML code again.</div><br /><br />",
   },
 ];
+
+const mocks = [
+  {
+    request: {
+      query: GameListQuery,
+      variables: {
+        id: "latestPlayedGames",
+        numberOfGames: GAMES_LIST_HORIZONTAL_ITEMS_LIMIT,
+      },
+    },
+    result: {
+      data: {
+        gamesList: gamesListMock,
+      },
+    },
+  },
+];
+
 const state = {
   schema: {
     cms: {
@@ -32,9 +53,9 @@ const state = {
 const stories = storiesOf("ComponentBuilder", module);
 
 const ComponentBuilderRendererStory = () => (
-  <MockStore state={state}>
+  <MockedProvider mocks={mocks}>
     <ComponentBuilderRenderer componentDefinitions={componentDefinitions} />
-  </MockStore>
+  </MockedProvider>
 );
 
 stories.add("ComponentBuilderRenderer", ComponentBuilderRendererStory, {
