391fcb2c45ea94b0ecdf9f0b30b794b1cc3624d6
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-03-17 12:33:43 +0100

    Removed GameRowSessionDetails as it's not needed anymore.

--- src/components/GameRow/index.js
+++ src/components/GameRow/index.js
@@ -1,6 +1,3 @@
 // @flow
 export type * from "./GameRowContainer";
 export { GameRowContainer as GameRow } from "./GameRowContainer";
-export {
-  GameRowSessionDetailsContainer as GameRowSessionDetails,
-} from "./GameRowSessionDetails/GameRowSessionDetailsContainer";
