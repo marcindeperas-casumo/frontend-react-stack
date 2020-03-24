394b0431056c4ed5271ab49aa1aadec3cd13747d
Michele <michele.settepani@casumo.com>
2020-03-24 15:07:12 +0100

    refactor(LiveCasinoCard): add type from apollo

--- src/components/LiveCasinoCard/LiveCasinoCard.js
+++ src/components/LiveCasinoCard/LiveCasinoCard.js
@@ -18,7 +18,7 @@ export type Props = {
   game: A.GameListLiveCasinoQuery_gamesList_games,
   onLaunchGame: Function,
   playNowText: string,
-  blackjackText: Object,
+  blackjackText: A.LiveCasinoCardCmsQuery,
 };
 
 export default class LiveCasinoCard extends PureComponent<Props> {
