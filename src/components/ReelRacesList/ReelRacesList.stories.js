2ba50359dd6b46858b94b289f8a1d61e942ee2c7
Jack Westbrook <jack.westbrook@casumo.com>
2020-03-27 14:10:59 +0100

    test(reelraces): use mockedprovider and mock file for stories and tests

--- src/components/ReelRacesList/ReelRacesList.stories.js
+++ src/components/ReelRacesList/ReelRacesList.stories.js
@@ -1,97 +1,17 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import MockStore from "Components/MockStore";
-import isNotChromatic from "Storybook/isNotChromatic";
+import { MockedProvider } from "@apollo/react-testing";
 import { ReelRacesList } from "./ReelRacesList";
-import state from "./__mocks__/reelRacesStore";
+import { reelRacesListQueryMock } from "./__mocks__/reelRacesStore";
 const stories = storiesOf("ReelRacesList", module);
 
-const props = {
-  isFetched: false,
-  areTranslationsFetched: true,
-  title: "Reel Race",
-  seeMore: "See more",
-  reelRaces: [
-    {
-      id: "edc71c70-56d6-11e9-8587-0242ac11000b",
-      startTime: 1580882400000,
-      optedIn: false,
-      endTime: 1580883600000,
-      spinLimit: 140,
-      minBet: null,
-      promoted: false,
-      formattedPrize: "€20",
-      remainingSpins: 99,
-      game: {
-        id: "fa9aa550-6be1-11e4-a1d6-005056a03af2",
-        name: "Jack and the Beanstalk",
-        logo:
-          "https://cms.casumo.com/wp-content/uploads/2014/02/JackOfTheBeanstalk_Logo.png",
-        backgroundImage:
-          "https://cms.casumo.com/wp-content/uploads/2014/06/JackOfTheBeanstalk_Thumb.jpg",
-        slug: "jack-the-beanstalk",
-      },
-      status: "Scheduled",
-      translations: {
-        optedInCtaSingleGameShort: "Play",
-        optIn: "Opt in",
-        optedIn: "Opted in",
-        endingIn: "Ending in",
-        startingIn: "Starting in:",
-        competeFor: "Compete for {{prize}}",
-        spins: "Spins",
-        duration: "Duration",
-        durationTemplate: "{{{duration}}} min",
-        minBet: "Min Bet",
-        caveatShort: "false",
-        today: "Today",
-        tomorrow: "Tomorrow",
-      },
-    },
-    {
-      id: "c21ee900-560d-11e9-8587-0242ac11000b",
-      startTime: 1580882400000,
-      optedIn: false,
-      endTime: 1580883600000,
-      spinLimit: 140,
-      minBet: null,
-      promoted: false,
-      formattedPrize: "€20",
-      remainingSpins: 99,
-      game: {
-        id: "789f90f0-4181-11e8-9251-0242ac110002",
-        name: "Wild Heist at Peacock Manor",
-        logo:
-          "https://cms.casumo.com/wp-content/uploads/2018/04/wild_heist_logo.png",
-        backgroundImage:
-          "https://cms.casumo.com/wp-content/uploads/2018/04/wild_heist_thumbnail.jpg",
-        slug: "wild-heist-at-peacock-manor",
-      },
-      status: "Scheduled",
-      translations: {
-        optedInCtaSingleGameShort: "Play",
-        optIn: "Opt in",
-        optedIn: "Opted in",
-        endingIn: "Ending in",
-        startingIn: "Starting in:",
-        competeFor: "Compete for {{prize}}",
-        spins: "Spins",
-        duration: "Duration",
-        durationTemplate: "{{{duration}}} min",
-        minBet: "Min Bet",
-        caveatShort: "false",
-        today: "Today",
-        tomorrow: "Tomorrow",
-      },
-    },
-  ],
-};
-
-if (isNotChromatic) {
-  stories.add("ReelRacesList", () => (
-    <MockStore state={state}>
-      <ReelRacesList {...props} />
-    </MockStore>
-  ));
-}
+stories.add("ReelRacesList", () => (
+  <MockedProvider mocks={[reelRacesListQueryMock]}>
+    <ReelRacesList
+      reelRaces={reelRacesListQueryMock.result.data.reelRaces}
+      title={reelRacesListQueryMock.result.data.title}
+      seeMore={reelRacesListQueryMock.result.data.seeMore}
+    />
+  </MockedProvider>
+));
--- src/components/ReelRacesList/__mocks__/reelRacesStore.js
+++ src/components/ReelRacesList/__mocks__/reelRacesStore.js
@@ -1,327 +1,128 @@
-const state = {
-  schema: {
-    cms: {
-      "reel-races.reel-race-templates": {
-        id: "45726",
-        slug: "reel-races.reel-race-templates",
-        title: "Reel Race Templates",
-        content: "",
-        attachments: [
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterbigwin@2x-2.png",
-            title: "img-boosterbigwin@2x (2)",
-          },
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-boostermegawin@2x-2.png",
-            title: "img-boostermegawin@2x (2)",
-          },
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterwinstreak@2x-2.png",
-            title: "img-boosterwinstreak@2x (2)",
-          },
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterwinstreak@2x-1.png",
-            title: "img-boosterwinstreak@2x (1)",
-          },
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-boostermegawin@2x-1.png",
-            title: "img-boostermegawin@2x (1)",
-          },
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterbigwin@2x-1.png",
-            title: "img-boosterbigwin@2x (1)",
-          },
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-defaultrace@2x.png",
-            title: "img-defaultrace@2x",
-          },
-          {
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/facebook.png",
-            title: "facebook",
-          },
-        ],
-        custom_fields: {},
-        fields: {
-          starting_in: "Starting in:",
-          spin_count: "Available spins",
-          min_bet: "Min Bet",
-          duration: "Duration",
-          opt_in_cta: "Join Now",
-          opt_in_cta_single_game: "Join Now",
-          opted_in_cta: "Pick a game",
-          opted_in_cta_single_game: "Play {{{name}}}",
-          all_prizes_cta: "View prizes",
-          title: "Reel Race",
-          title_single_game: "{{{name}}} <br /> Reel Race",
-          points_template: "{{{points}}}pts",
-          prize_win_tagline: "Win {{{name}}}",
-          "": false,
-          booster_images: [
-            {
-              uid: "winsInARow",
-              image: {
-                id: 46876,
-                alt: "",
-                title: "img-boosterwinstreak@2x (2)",
-                caption: "",
-                description: "",
-                mime_type: "image/png",
-                url:
-                  "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterwinstreak@2x-2.png",
-                width: 136,
-                height: 136,
-                sizes: {
-                  thumbnail:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterwinstreak@2x-2.png",
-                  "thumbnail-width": 136,
-                  "thumbnail-height": 136,
-                  medium:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterwinstreak@2x-2.png",
-                  "medium-width": 136,
-                  "medium-height": 136,
-                  large:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterwinstreak@2x-2.png",
-                  "large-width": 136,
-                  "large-height": 136,
-                  "post-thumbnail":
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterwinstreak@2x-2.png",
-                  "post-thumbnail-width": 136,
-                  "post-thumbnail-height": 136,
-                },
-              },
-              points: "5",
-            },
-            {
-              uid: "bigWins",
-              image: {
-                id: 46874,
-                alt: "",
-                title: "img-boosterbigwin@2x (2)",
-                caption: "",
-                description: "",
-                mime_type: "image/png",
-                url:
-                  "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterbigwin@2x-2.png",
-                width: 136,
-                height: 136,
-                sizes: {
-                  thumbnail:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterbigwin@2x-2.png",
-                  "thumbnail-width": 136,
-                  "thumbnail-height": 136,
-                  medium:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterbigwin@2x-2.png",
-                  "medium-width": 136,
-                  "medium-height": 136,
-                  large:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterbigwin@2x-2.png",
-                  "large-width": 136,
-                  "large-height": 136,
-                  "post-thumbnail":
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boosterbigwin@2x-2.png",
-                  "post-thumbnail-width": 136,
-                  "post-thumbnail-height": 136,
-                },
-              },
-              points: "35",
-            },
-            {
-              uid: "megaWins",
-              image: {
-                id: 46875,
-                alt: "",
-                title: "img-boostermegawin@2x (2)",
-                caption: "",
-                description: "",
-                mime_type: "image/png",
-                url:
-                  "https://cms.casumo.com/wp-content/uploads/2016/04/img-boostermegawin@2x-2.png",
-                width: 136,
-                height: 136,
-                sizes: {
-                  thumbnail:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boostermegawin@2x-2.png",
-                  "thumbnail-width": 136,
-                  "thumbnail-height": 136,
-                  medium:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boostermegawin@2x-2.png",
-                  "medium-width": 136,
-                  "medium-height": 136,
-                  large:
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boostermegawin@2x-2.png",
-                  "large-width": 136,
-                  "large-height": 136,
-                  "post-thumbnail":
-                    "https://cms.casumo.com/wp-content/uploads/2016/04/img-boostermegawin@2x-2.png",
-                  "post-thumbnail-width": 136,
-                  "post-thumbnail-height": 136,
-                },
-              },
-              points: "100",
-            },
-          ],
-          prize_won_tagline: "Won {{{name}}}",
-          spins: "Spins",
-          leaderboard_cta: "See Leaderboard",
-          winner_of_reel_race: "Winner of {{{title}}}",
-          game_played: "Game Played",
-          facebook_href: "https://www.casumo.com",
-          facebook_title: "Win {{{firstPrize}}} in {{{title}}}",
-          facebook_description:
-            "In the Casumo Reel Races you compete against other players in fast paced, action packed casino tournaments crammed with amazing prizes. Join now on casumo.com!",
-          facebook_picture: {
-            id: 46552,
-            alt: "",
-            title: "facebook",
-            caption: "",
-            description: "",
-            mime_type: "image/png",
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/facebook.png",
-            width: 1200,
-            height: 630,
-            sizes: {
-              thumbnail:
-                "https://cms.casumo.com/wp-content/uploads/2016/04/facebook-150x150.png",
-              "thumbnail-width": 150,
-              "thumbnail-height": 150,
-              medium:
-                "https://cms.casumo.com/wp-content/uploads/2016/04/facebook-300x158.png",
-              "medium-width": 300,
-              "medium-height": 158,
-              large:
-                "https://cms.casumo.com/wp-content/uploads/2016/04/facebook-1024x538.png",
-              "large-width": 660,
-              "large-height": 347,
-              "post-thumbnail":
-                "https://cms.casumo.com/wp-content/uploads/2016/04/facebook-825x510.png",
-              "post-thumbnail-width": 825,
-              "post-thumbnail-height": 510,
-            },
-          },
-          in_progress: "In Progress",
-          default_tournament_background_image: {
-            id: 46504,
-            alt: "",
-            title: "img-defaultrace@2x",
-            caption: "",
-            description: "",
-            mime_type: "image/png",
-            url:
-              "https://cms.casumo.com/wp-content/uploads/2016/04/img-defaultrace@2x.png",
-            width: 720,
-            height: 656,
-            sizes: {
-              thumbnail:
-                "https://cms.casumo.com/wp-content/uploads/2016/04/img-defaultrace@2x-150x150.png",
-              "thumbnail-width": 150,
-              "thumbnail-height": 150,
-              medium:
-                "https://cms.casumo.com/wp-content/uploads/2016/04/img-defaultrace@2x-300x273.png",
-              "medium-width": 300,
-              "medium-height": 273,
-              large:
-                "https://cms.casumo.com/wp-content/uploads/2016/04/img-defaultrace@2x.png",
-              "large-width": 660,
-              "large-height": 601,
-              "post-thumbnail":
-                "https://cms.casumo.com/wp-content/uploads/2016/04/img-defaultrace@2x-720x510.png",
-              "post-thumbnail-width": 720,
-              "post-thumbnail-height": 510,
-            },
-          },
-          default_tournament_background_colour: "purple",
-          duration_template: "{{{duration}}} min",
-          share: "Share",
-          opt_in_cta_you_are_in: "You are in",
-          tournament_time_on_the_hour: "{{startTime | moment:'HH:mm'}}",
-          tournament_time: "{{startTime | moment:'HH:mm'}}",
-          critical_for_compliance: false,
-          caveat_normal:
-            'Separate opt-in for every race. Points system determines the winner. Min bet, max spins and time restrictions vary between races. Games, country & currency restrictions. <a href="#" {{{ ctaTermsAndConditions }}} class="decorated" style="color: currentColor"><strong>T&C’s apply.</strong></a>',
-          caveat_long: "false",
-          caveat_short: "false",
-          opted_in_cta_single_game_short: "Play",
-          compete_for: "Compete for {{prize}}",
-          ending_in: "Ending in",
-          opted_in: "Opted in",
-          opt_in: "Opt in",
-        },
-        children: [],
-        childSlugs: ["new-promoted", "promoted", "standard"],
-      },
+// @flow
+import { ReelRaceListQuery } from "../ReelRacesListContainer.graphql";
+
+export const reelRaces = [
+  {
+    id: "e84d5f50-6de3-11ea-a450-0242ac110006",
+    game: {
+      id: "fab03150-4873-11e8-b2c4-0242ac110002",
+      name: "Legacy of Egypt",
+      logo:
+        "https://cms.casumo.com/wp-content/uploads/2018/04/legacy_of_egypt_logo.png",
+      backgroundImage:
+        "https://cms.casumo.com/wp-content/uploads/2018/04/legacy_of_egypt_thumbnail.jpg",
+      slug: "legacy-of-egypt",
     },
-    reelRaces: {
-      "153f7060-5c57-11e9-8587-0242ac11000b": {
-        endTime: 1555613400000,
-        promoted: true,
-        startTime: 1555610400000,
-        tournamentId: "153f7060-5c57-11e9-8587-0242ac11000b",
-        opted: false,
-        prize: "€1,000",
-        gameSlug: "jammin-jars",
-        spins: 500,
-        minBet: null,
-        color: "yellow-light-1",
-      },
-      "1538e0b0-5c57-11e9-8587-0242ac11000b": {
-        endTime: 1555609800000,
-        promoted: false,
-        startTime: 1555606800000,
-        tournamentId: "1538e0b0-5c57-11e9-8587-0242ac11000b",
-        opted: false,
-        prize: "€100",
-        gameSlug: "hanzos-dojo",
-        spins: 325,
-        minBet: null,
-        color: "yellow-light-1",
-      },
-      "3f2867a0-5d20-11e9-8587-0242ac11000b": {
-        endTime: 1555636800000,
-        promoted: false,
-        startTime: 1555635600000,
-        tournamentId: "3f2867a0-5d20-11e9-8587-0242ac11000b",
-        opted: false,
-        prize: "€20",
-        gameSlug: "golden-ticket",
-        spins: 130,
-        minBet: null,
-        color: "yellow-light-1",
-      },
-      "154ba560-5c57-11e9-8587-0242ac11000b": {
-        endTime: 1555620600000,
-        promoted: false,
-        startTime: 1555617600000,
-        tournamentId: "154ba560-5c57-11e9-8587-0242ac11000b",
-        opted: false,
-        prize: "€500",
-        gameSlug: "eastern-emeralds",
-        spins: 550,
-        minBet: null,
-        color: "yellow-light-1",
-      },
-      "3f456580-5d20-11e9-8587-0242ac11000b": {
-        endTime: 1555654800000,
-        promoted: false,
-        startTime: 1555653600000,
-        tournamentId: "3f456580-5d20-11e9-8587-0242ac11000b",
-        opted: false,
-        prize: "€20",
-        gameSlug: "crystal-queen",
-        spins: 150,
-        minBet: null,
-        color: "yellow-light-1",
-      },
+    startTime: 1585335600000,
+    optedIn: false,
+    endTime: 1585338600000,
+    status: "Scheduled",
+    spinLimit: 500,
+    promoted: true,
+    formattedPrize: "£500",
+    remainingSpins: 0,
+    translations: {
+      optedInCtaSingleGameShort: "Play",
+      optIn: "Opt in",
+      optedIn: "Opted in",
+      endingIn: "Ending in",
+      startingIn: "Starting in:",
+      competeFor: "Compete for {{prize}}",
+      spins: "Spins",
+      duration: "Duration",
+      durationTemplate: "{{{duration}}} min",
+      caveatShort:
+        'Opt in. Spins/Games restrictions. <a href="#" {{{ ctaTermsAndConditions }}}><strong>T&C’s apply.</strong></a>',
+      today: "Today",
+      tomorrow: "Tomorrow",
+    },
+  },
+  {
+    id: "e7083520-6de3-11ea-a450-0242ac110006",
+    game: {
+      id: "6940f040-36b7-11ea-9bcb-0242ac110003",
+      name: "Piggy Riches Megaways",
+      logo:
+        "https://cms.casumo.com/wp-content/uploads/2020/01/piggy_riches_Thumbnail_Logo.png",
+      backgroundImage:
+        "https://cms.casumo.com/wp-content/uploads/2020/01/piggy_riches_Thumbnail_BG.png",
+      slug: "piggy-riches-megaways",
+    },
+    startTime: 1585312200000,
+    optedIn: false,
+    endTime: 1585313700000,
+    status: "Started",
+    spinLimit: 190,
+    promoted: false,
+    formattedPrize: "£20",
+    remainingSpins: 0,
+    translations: {
+      optedInCtaSingleGameShort: "Play",
+      optIn: "Opt in",
+      optedIn: "Opted in",
+      endingIn: "Ending in",
+      startingIn: "Starting in:",
+      competeFor: "Compete for {{prize}}",
+      spins: "Spins",
+      duration: "Duration",
+      durationTemplate: "{{{duration}}} min",
+      caveatShort:
+        'Opt in. Spins/Games restrictions. <a href="#" {{{ ctaTermsAndConditions }}}><strong>T&C’s apply.</strong></a>',
+      today: "Today",
+      tomorrow: "Tomorrow",
+    },
+  },
+  {
+    id: "e72d4950-6de3-11ea-a450-0242ac110006",
+    game: {
+      id: "3e054070-b89c-11e7-b304-005056a03af2",
+      name: "Reactoonz",
+      logo:
+        "https://cms.casumo.com/wp-content/uploads/2017/10/reactoonz_logo.png",
+      backgroundImage:
+        "https://cms.casumo.com/wp-content/uploads/2017/10/reactoonz_thumbnail.jpg",
+      slug: "reactoonz",
+    },
+    startTime: 1585314000000,
+    optedIn: false,
+    endTime: 1585315500000,
+    status: "Scheduled",
+    spinLimit: 190,
+    promoted: false,
+    formattedPrize: "£20",
+    remainingSpins: 0,
+    translations: {
+      optedInCtaSingleGameShort: "Play",
+      optIn: "Opt in",
+      optedIn: "Opted in",
+      endingIn: "Ending in",
+      startingIn: "Starting in:",
+      competeFor: "Compete for {{prize}}",
+      spins: "Spins",
+      duration: "Duration",
+      durationTemplate: "{{{duration}}} min",
+      caveatShort:
+        'Opt in. Spins/Games restrictions. <a href="#" {{{ ctaTermsAndConditions }}}><strong>T&C’s apply.</strong></a>',
+      today: "Today",
+      tomorrow: "Tomorrow",
+    },
+  },
+];
+
+export const reelRacesTranslations = {
+  title: "Reel Race",
+  seeMore: "See More",
+};
+
+export const reelRacesListQueryMock = {
+  request: {
+    query: ReelRaceListQuery,
+    variables: { limit: 3 },
+  },
+  result: {
+    data: {
+      ...reelRacesTranslations,
+      reelRaces,
     },
   },
 };
-
-export default state;
