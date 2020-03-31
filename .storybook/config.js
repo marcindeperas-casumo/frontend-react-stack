aa9176930b3330a5abe8d9b028d403dacf28ae62
lukKowalski <lukasz.kowalski@casumo.com>
2020-03-31 16:52:44 +0200

    possibility to disable global decorator from story level

--- .storybook/config.js
+++ .storybook/config.js
@@ -16,14 +16,22 @@ function loadStories() {
 addDecorator(withInfo);
 addDecorator(withKnobs);
 
-addDecorator(Story => (
+const globalDecorator = Component => (
   <>
     <div id="portal-host-element" />
     <div className="o-wrapper u-padding">
-      <Story />
+      <Component />
     </div>
   </>
-));
+);
+
+addDecorator((Component, story) => {
+  if (story.parameters.noGlobalDecorator) {
+    return <Component />;
+  }
+
+  return globalDecorator(Component);
+});
 
 addParameters({
   backgrounds: [
