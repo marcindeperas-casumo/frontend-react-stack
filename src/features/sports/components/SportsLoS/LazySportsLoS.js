3eca911724fccc59e0fd81bd0599b3d732353846
Cho <choishere@gmail.com>
2020-04-01 15:35:18 +0200

    feat(PRS-364): sports los remove portal, use react suspense

--- src/features/sports/components/SportsLoS/LazySportsLoS.js
+++ src/features/sports/components/SportsLoS/LazySportsLoS.js
@@ -1,18 +1,20 @@
 import React from "react";
-import LazyPortal from "Components/LazyPortal";
 import SportsShellSkeleton from "Features/sports/components/SportsShell/SportsShellSkeleton";
+
+const LazyComponent = React.lazy(() =>
+  import("Features/sports/components/SportsLoS").then(module => ({
+    default: module.SportsLoSContainer,
+  }))
+);
 
 export const LazySportsLoS = React.memo(
   props => (
-    <LazyPortal
-      hostElementId="sports-los-portal"
-      loader={() => import("Features/sports/components/SportsLoS")}
-      fallback={<SportsShellSkeleton />}
-      namedExport="SportsLoSContainer"
-      props={{ urlPrefix: props.urlPrefix }}
-    />
+    <React.Suspense fallback={<SportsShellSkeleton />}>
+      <LazyComponent urlPrefix={props.urlPrefix} />
+    </React.Suspense>
   ),
-  // Reach router will pass down the window.location which will cause a rerender every time the
-  // Kambi client changes page as it uses hashes.  This Memo ensures it will only rerender when the URI changes
+  // Reach router will pass down the window.location which will cause
+  // a rerender every time the Kambi client changes page as it uses hashes.
+  // This Memo ensures it will only rerender when the URI changes
   (prevProps, nextProps) => prevProps.uri === nextProps.uri
 );
