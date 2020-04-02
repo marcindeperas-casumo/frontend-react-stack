62fc602f8d9087ded4626da250645f0cffbe9009
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-04-02 14:55:26 +0200

    Removed unused pieces of api.transactionsBetsHistory.

--- src/models/transactionsBetsHistory/transactionsBetsHistory.types.js
+++ src/models/transactionsBetsHistory/transactionsBetsHistory.types.js
@@ -1,5 +1,4 @@
 // @flow
-import { DateTime } from "luxon";
 import { CURRENCIES } from "Src/constants";
 
 type RealBonusAmountType = {
@@ -22,50 +21,10 @@ export type AnnualOverviewType = {
   realMoneyGiven: number,
 };
 
-export type AnnualOverviewTypeWrapper = {
-  data?: AnnualOverviewType,
-  meta: {
-    isFetching?: boolean,
-  },
-};
-
-export type WalletTransactionsProps = {
-  walletId: string,
-  startTime: DateTime,
-  endTime: DateTime,
-  perPage?: number,
-};
-
 export type FetchAnnualOverviewProps = {
   year: number,
   meta?: {
     resolve?: () => void,
     reject?: () => void,
   },
-};
-
-export type Action = {
-  name: string,
-  type: string,
-};
-
-export type AmountWithCodeResponseRaw = {
-  amount: number,
-  iso4217CurrencyCode: string,
-};
-
-export type TransactionResponseRaw = {
-  balanceBefore: AmountWithCodeResponseRaw,
-  balanceAfter: AmountWithCodeResponseRaw,
-  delta: AmountWithCodeResponseRaw,
-  fee: ?AmountWithCodeResponseRaw,
-  id: string,
-  paymentMethodId: ?string,
-  reason: ?string,
-  sequenceNumber: number,
-  state: ?string,
-  timestamp: number,
-  walletId: string,
-  walletUpdateSource: string,
-  withdrawalLocked: boolean,
 };
