398b8f4877315952865cb16a5e6605baec3b2161
Jack Westbrook <jack.westbrook@casumo.com>
2020-04-02 14:56:41 +0200

    test(gamedetails): introduce story, tests and mock

--- src/components/GameDetails/GameDetails.stories.js
+++ src/components/GameDetails/GameDetails.stories.js
@@ -1,8 +1,20 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { boolean } from "@storybook/addon-knobs/react";
 import { GameDetails } from "./GameDetails";
+import { gameDetails } from "./__mocks__/gameDetailsMock";
 
 const stories = storiesOf("GameDetails", module);
 
-stories.add("Default", () => <GameDetails msg="howdy! 🤠" />);
+stories.add("Default", () => {
+  const data = {
+    ...gameDetails,
+    game: {
+      ...gameDetails.game,
+      isInMaintenance: boolean("Is in maintenance", false),
+    },
+  };
+
+  return <GameDetails data={data} />;
+});
