48580ef57ded41758eee509c5ea119c2aecd9248
Michele <michele.settepani@casumo.com>
2020-04-09 13:00:55 +0200

    fix(query.valuableDetails): remove ValuableDetailsQuery

--- src/components/ValuableDetails/__mocks__/query.valuableDetails.mock.js
+++ src/components/ValuableDetails/__mocks__/query.valuableDetails.mock.js
@@ -1,9 +1,7 @@
-import { ValuableDetailsQuery } from "../ValuableDetails.graphql";
 import translations from "Models/valuables/__mocks__/valuableDetailsTranslations.mock";
 
 export default {
   request: {
-    query: ValuableDetailsQuery,
     variables: {},
   },
   result: {
