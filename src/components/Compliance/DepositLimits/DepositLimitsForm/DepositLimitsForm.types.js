1bc36db2451d0e70b2e30860a791636ce1078a57
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-04-24 15:37:53 +0200

    Adjusted styles on DepositLimitsForm to match Figma. Also refactored DepositLimitsForm to exclude validate to decrease its complexity. Extracted some local types into separate file.

---
+++ src/components/Compliance/DepositLimits/DepositLimitsForm/DepositLimitsForm.types.js
@@ -0,0 +1,59 @@
+// @flow
+import type {
+  AllLimits,
+  AllLimitsOnlyValues,
+  LimitLock,
+  DepositKinds,
+  ResponsibleGamblingTest,
+  DepositLimitsAdjustment,
+} from "Models/playOkay/depositLimits";
+
+export type FormProps = {
+  currency: string,
+  locale: string,
+  t: {
+    daily_short: string,
+    daily: string,
+    weekly_short: string,
+    weekly: string,
+    monthly_short: string,
+    monthly: string,
+    remove_selected: string,
+    input_validation: {
+      lock: string,
+      lowest_limit: string,
+      highest_limit: string,
+      cant_be_higher: string,
+      cant_be_lower: string,
+      has_to_be_lower_than_pending_adjustment: string,
+      has_to_be_lower_while_locked: string,
+      has_to_be_lower_after_responsible_gambling_test_failed: string,
+    },
+  },
+  responsibleGamblingTestRequired?: boolean,
+  responsibleGamblingTest: ResponsibleGamblingTest,
+  limits: AllLimits,
+  limitChanges: AllLimitsOnlyValues,
+  initiallyVisible: DepositKinds,
+  applyLimitsChanges: AllLimitsOnlyValues => void,
+  lock: ?LimitLock,
+  pendingLimitChanges: ?DepositLimitsAdjustment,
+  fetchTranslations: () => void,
+};
+
+export type Limits = {
+  daily: ?number,
+  weekly: ?number,
+  monthly: ?number,
+};
+
+export type LimitInput = {
+  value: ?number,
+  onChange: (event: SyntheticInputEvent<HTMLInputElement>) => void,
+};
+
+export type LimitInputs = {
+  daily: LimitInput,
+  weekly: LimitInput,
+  monthly: LimitInput,
+};
