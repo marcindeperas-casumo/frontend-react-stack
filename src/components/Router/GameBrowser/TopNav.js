c6231c6de784f7391b896bd8a1cfa602efd4a393
Michał Mokijewski <hello@naf.gl>
2020-05-05 09:31:11 +0200

    extract GameBrowser part from router

---
+++ src/components/Router/GameBrowser/TopNav.js
@@ -0,0 +1,106 @@
+// @flow
+import * as React from "react";
+import classNames from "classnames";
+import { Link, useMatch } from "@reach/router";
+import Flex from "@casumo/cmp-flex";
+import Text from "@casumo/cmp-text";
+import { HeartIcon, SearchIcon } from "@casumo/cmp-icons";
+
+const NavLinkDesktop = ({
+  Icon,
+  text,
+  to,
+}: {
+  Icon: React.StatelessFunctionalComponent<any>,
+  text: string,
+  to: string,
+}) => {
+  const match = useMatch(to);
+  const active = Boolean(match);
+
+  return (
+    <Flex.Item>
+      <Link to={to}>
+        <Flex
+          direction="vertical"
+          align="center"
+          justify="center"
+          className={classNames("u-padding--sm", {
+            "t-color-chrome-dark-3": active,
+            "t-color-chrome-dark-2": !active,
+          })}
+        >
+          <Icon size="md" />
+          <Text className="u-font-weight-bold">{text}</Text>
+        </Flex>
+        <Flex
+          style={{ height: 3 }}
+          className={classNames("t-border-r", {
+            "t-background-plum": active,
+          })}
+        />
+      </Link>
+    </Flex.Item>
+  );
+};
+export const TopNavDesktop = () => (
+  <Flex className="o-wrapper u-padding-y--xlg" align="center" justify="center">
+    <Flex spacing="lg" className="u-padding-y">
+      <NavLinkDesktop Icon={HeartIcon} text="Top lists" to="top" />
+      <NavLinkDesktop Icon={SearchIcon} text="Search" to="search" />
+    </Flex>
+  </Flex>
+);
+
+const NavLinkMobile = ({
+  Icon,
+  text,
+  to,
+}: {
+  Icon: React.StatelessFunctionalComponent<any>,
+  text: string,
+  to: string,
+}) => {
+  const match = useMatch(to);
+  const active = Boolean(match);
+
+  return (
+    <Flex.Item>
+      <Link
+        to={to}
+        style={{
+          /* kill styles from knockout side */
+          borderBottom: "none",
+          padding: 0,
+        }}
+      >
+        <Flex
+          direction="vertical"
+          align="center"
+          justify="center"
+          className={classNames("u-padding", {
+            "t-color-chrome-dark-3": active,
+            "t-color-chrome-dark-2": !active,
+          })}
+        >
+          <Icon className="u-padding-y" />
+          <Text size="xs" className="u-font-weight-bold">
+            {text}
+          </Text>
+        </Flex>
+        <Flex
+          style={{ height: 3 }}
+          className={classNames("t-border-r", {
+            "t-background-plum": active,
+          })}
+        />
+      </Link>
+    </Flex.Item>
+  );
+};
+export const TopNavMobile = () => (
+  <Flex spacing="sm">
+    <NavLinkMobile Icon={HeartIcon} text="Top lists" to="top" />
+    <NavLinkMobile Icon={SearchIcon} text="Search" to="search" />
+  </Flex>
+);
