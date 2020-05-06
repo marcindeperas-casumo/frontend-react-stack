7c1c6e6e354fd3cb7e3a3670aacaba2a2ed17679
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-05-06 14:39:39 +0200

    Simplified story.

--- src/components/Compliance/DepositLimits/DepositLimitsTopNavigation/DepositLimitsTopNavigation.stories.js
+++ src/components/Compliance/DepositLimits/DepositLimitsTopNavigation/DepositLimitsTopNavigation.stories.js
@@ -1,23 +1,16 @@
 // @flow
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import { decorate } from "@storybook/addon-actions";
+import { action } from "@storybook/addon-actions";
 import { DepositLimitsTopNavigation } from "./DepositLimitsTopNavigation";
 
-const stories = storiesOf(
-  "Compliance/DepositLimits/DepositLimitsTopNavigation",
-  module
-);
 const t = {
   yourStuffLabel: "Your Stuff",
   detailsLabel: "Details & Settings",
   playOkayLabel: "Play Okay Settings",
 };
-const firstArg = decorate([args => args.slice(0, 1)]);
 
-stories.add("Default", () => (
-  <DepositLimitsTopNavigation
-    t={t}
-    action={firstArg.action("Navigated away")}
-  />
-));
+storiesOf("DepositLimits/DepositLimitsTopNavigation", module).add(
+  "Default",
+  () => <DepositLimitsTopNavigation t={t} action={action("Navigated away")} />
+);
