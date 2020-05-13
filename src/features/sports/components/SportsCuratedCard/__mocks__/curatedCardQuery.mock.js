5b5c186b18582f28df785e890b39fd1da43fc068
Cho <choishere@gmail.com>
2020-05-13 16:36:00 +0200

    PRS-387 sports curated fix mocks

--- src/features/sports/components/SportsCuratedCard/__mocks__/curatedCardQuery.mock.js
+++ src/features/sports/components/SportsCuratedCard/__mocks__/curatedCardQuery.mock.js
@@ -4,15 +4,15 @@ export const curatedCardQueryMock = {
   request: {
     query: CuratedCardQuery,
     variables: {
-      slug: "welcome-offer-sports",
+      slug: "curated-sports",
     },
   },
   result: {
     data: {
       curatedCard: {
-        id: "welcome-offer-sports",
-        slug: "welcome-offer-sports",
-        type: "welcome offer",
+        id: "curated-sports",
+        slug: "curated-sports",
+        type: "sports",
         image: null,
         smallImage:
           "https://cms.casumo.com/wp-content/uploads/2019/09/Mobile-2@4x.png",
@@ -20,12 +20,11 @@ export const curatedCardQueryMock = {
           "https://cms.casumo.com/wp-content/uploads/2019/09/Tablet-1@4x.png",
         largeImage:
           "https://cms.casumo.com/wp-content/uploads/2019/09/Tablet-1@4x.png",
-        header: "Get your</br>Sports Welcome Bonus",
-        subtitle: "WELCOME BONUS",
+        header: "Get your</br>Sports Free Bets",
+        subtitle: "yay",
         promotionSlug: null,
-        promotionLegalText:
-          "Min bet £10 at odds 1.6 or higher, receive £10 Free bet. Qualifying and Free bet must be placed within 35 days of registration. Free bet available after qualifying bet settled. Free bet non-withdrawable. T&Cs apply",
-        launchGameText: "Get your Welcome Bonus",
+        promotionLegalText: "",
+        launchGameText: "",
         game: null,
       },
     },
