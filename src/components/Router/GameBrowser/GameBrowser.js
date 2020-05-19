de506da40f036218f9eda98e5aa32052e1be4cc1
Michał Mokijewski <hello@naf.gl>
2020-05-19 14:20:57 +0200

    add GameBrowserCategories component

--- src/components/Router/GameBrowser/GameBrowser.js
+++ src/components/Router/GameBrowser/GameBrowser.js
@@ -3,8 +3,13 @@ import * as React from "react";
 import { Router, Redirect } from "@reach/router";
 import { WaitForHostElement } from "Components/WaitForHostElement";
 import Portal from "Components/Portal";
-import { TabletAndDesktop, Mobile } from "Components/ResponsiveLayout";
+import {
+  TabletAndDesktop,
+  Mobile,
+  isMobile,
+} from "Components/ResponsiveLayout";
 import { TopNavDesktop, TopNavMobile } from "./TopNav";
+import { GameBrowserSets } from "./GameBrowserSets";
 
 const ComponentBuilder = React.lazy(() =>
   import("Components/ComponentBuilder").then(module => ({
@@ -16,12 +21,14 @@ const GameSearch = React.lazy(() =>
     default: module.GameSearch,
   }))
 );
-const MustDropJackpotList = React.lazy(() =>
-  import("Components/MustDropJackpotList")
-);
 const ProviderGamesList = React.lazy(() =>
   import("Components/ProviderGamesList").then(module => ({
     default: module.ProviderGamesList,
+  }))
+);
+const GameListPage = React.lazy(() =>
+  import("Components/GameListPage").then(module => ({
+    default: module.GameListPage,
   }))
 );
 const LiveCasinoDetailPage = React.lazy(() =>
@@ -29,6 +36,7 @@ const LiveCasinoDetailPage = React.lazy(() =>
     default: module.LiveCasinoDetailPage,
   }))
 );
+const LiveCasinoPage = isMobile() ? LiveCasinoDetailPage : GameListPage;
 
 const TopList = () => (
   <ComponentBuilder path="top" slug="built-pages.top-lists-{{market}}" />
@@ -36,38 +44,39 @@ const TopList = () => (
 
 const hostElementId = "react-host-games-lists";
 const mobileNav = "react-host-top-nav";
-export const GameBrowser = () => {
-  return (
-    <>
-      <Mobile>
-        <WaitForHostElement hostElementId={mobileNav}>
-          <Portal hostElementId={mobileNav}>
-            <TopNavMobile />
-          </Portal>
-        </WaitForHostElement>
-      </Mobile>
-      <WaitForHostElement hostElementId={hostElementId}>
-        <Portal hostElementId={hostElementId}>
-          <TabletAndDesktop>
-            <TopNavDesktop />
-          </TabletAndDesktop>
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <Router className="u-padding-bottom--2xlg">
-              <Redirect from="/" to="top" noThrow />
-              <TopList path="top" />
-              <GameSearch path="search" />
-              <MustDropJackpotList path="must-drop-jackpots" />
-              {/* $FlowIgnore:  missing 'provider' prop will come from ':provider' part in path */}
-              <ProviderGamesList path="provider/:provider" />
-              <LiveCasinoDetailPage path="live-casino" />
-            </Router>
-          </React.Suspense>
+export const GameBrowser = () => (
+  <>
+    <Mobile>
+      <WaitForHostElement hostElementId={mobileNav}>
+        <Portal hostElementId={mobileNav}>
+          <TopNavMobile />
         </Portal>
       </WaitForHostElement>
-    </>
-  );
-};
-// eslint-disable-next-line fp/no-mutation
-GameBrowser.defaultProps = {
-  path: "games/*",
-};
+    </Mobile>
+    <WaitForHostElement hostElementId={hostElementId}>
+      <Portal hostElementId={hostElementId}>
+        <TabletAndDesktop>
+          <TopNavDesktop />
+          <GameBrowserSets />
+        </TabletAndDesktop>
+        <React.Suspense fallback={null}>
+          <Router className="u-padding-bottom--2xlg">
+            <Redirect from="/" to="top" noThrow />
+            <TopList path="top" />
+            <GameSearch path="search" />
+            <LiveCasinoPage path="live-casino" listId="liveCasino" />
+            {/* $FlowIgnore:  missing 'provider' prop will come from ':provider' part in path */}
+            <ProviderGamesList path="provider/:provider" />
+            <GameListPage path="slots" listId="slotGames" />
+            <GameListPage path="table" listId="tableGames" />
+            <GameListPage path="jackpots" listId="jackpotGames" />
+            <GameListPage
+              path="must-drop-jackpots"
+              listId="mustDropJackpotGames"
+            />
+          </Router>
+        </React.Suspense>
+      </Portal>
+    </WaitForHostElement>
+  </>
+);
