d381a70a83d24cef1a006262bde55ea6be10833d
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-06-01 14:23:54 +0200

    Resigned from using renderHook and switched to HookWrapper + mount.

--- src/components/Compliance/Sweden/TimeLimits/TimeLimitsForm/useTimeLimitsFormState.test.js
+++ src/components/Compliance/Sweden/TimeLimits/TimeLimitsForm/useTimeLimitsFormState.test.js
@@ -1,14 +1,19 @@
 // @flow
 import * as React from "react";
-import { renderHook, act } from "@testing-library/react-hooks";
+import { mount } from "enzyme";
+import { act } from "react-dom/test-utils";
+// import { renderHook, act } from "@testing-library/react-hooks";
 import MockStore from "Components/MockStore";
 import { type PlayOkayReduxStore } from "Models/playOkay";
+import { HookWrapper, expectHook } from "Utils/HookWrapper";
 import {
   useTimeLimitsFormState,
   limitErrorMessage,
 } from "./useTimeLimitsFormState";
 
 jest.useFakeTimers();
+
+const findHookProp = (wrapper: any) => wrapper.find("div").prop("hook");
 
 const prepareState = (daily, weekly, monthly) => {
   const playOkay: PlayOkayReduxStore = {
@@ -26,9 +31,12 @@ const prepareState = (daily, weekly, monthly) => {
   };
 };
 
-const prepareWrapper = state => ({ children }) => (
-  <MockStore state={state}>{children}</MockStore>
-);
+const prepareWrapper = state =>
+  mount(
+    <MockStore state={state}>
+      <HookWrapper hook={useTimeLimitsFormState} args={[]} />
+    </MockStore>
+  );
 
 describe("Components/Compliance/Sweden/TimeLimits/useTimeLimitsFormState()", () => {
   test("it returns values initially set from Redux store", () => {
@@ -36,141 +44,215 @@ describe("Components/Compliance/Sweden/TimeLimits/useTimeLimitsFormState()", () 
     const weekly = 10;
     const monthly = 15;
     const wrapper = prepareWrapper(prepareState(daily, weekly, monthly));
-    const { result } = renderHook(() => useTimeLimitsFormState(), { wrapper });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), { wrapper });
 
-    expect(result.current.hrsPerDay).toEqual(daily);
-    expect(result.current.hrsPerWeek).toEqual(weekly);
-    expect(result.current.hrsPerMonth).toEqual(monthly);
+    expectHook(wrapper).toMatchObject({
+      hrsPerDay: daily,
+      hrsPerWeek: weekly,
+      hrsPerMonth: monthly,
+    });
+    // expect(result.current.hrsPerDay).toEqual(daily);
+    // expect(result.current.hrsPerWeek).toEqual(weekly);
+    // expect(result.current.hrsPerMonth).toEqual(monthly);
   });
 
   test("it updates minHrsPerWeek and minHrsPerMonth when hrsPerDay changes", () => {
     const wrapper = prepareWrapper(prepareState(5, 10, 15));
-    const { result } = renderHook(() => useTimeLimitsFormState(), {
-      wrapper,
-    });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), {
+    //   wrapper,
+    // });
 
     act(() => {
-      result.current.setHrsPerDay(10);
+      findHookProp(wrapper).setHrsPerDay(10);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.minHrsPerWeek).toEqual(10);
-    expect(result.current.minHrsPerMonth).toEqual(10);
+    // expect(result.current.minHrsPerWeek).toEqual(10);
+    // expect(result.current.minHrsPerMonth).toEqual(10);
+    expectHook(wrapper).toMatchObject({
+      minHrsPerWeek: 10,
+      minHrsPerMonth: 10,
+    });
   });
 
   test("it updates maxHrsPerDay and minHrsPerMonth when hrsPerWeek changes", () => {
     const wrapper = prepareWrapper(prepareState(null, null, null));
-    const { result } = renderHook(() => useTimeLimitsFormState(), {
-      wrapper,
-    });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), {
+    //   wrapper,
+    // });
 
     act(() => {
-      result.current.setHrsPerWeek(19);
+      // result.current.setHrsPerWeek(19);
+      findHookProp(wrapper).setHrsPerWeek(19);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.maxHrsPerDay).toEqual(19);
-    expect(result.current.minHrsPerMonth).toEqual(19);
+    expectHook(wrapper).toMatchObject({
+      maxHrsPerDay: 19,
+      minHrsPerMonth: 19,
+    });
+
+    // expect(result.current.maxHrsPerDay).toEqual(19);
+    // expect(result.current.minHrsPerMonth).toEqual(19);
   });
 
   test("it updates maxHrsPerWeek when hrsPerMonth changes", () => {
     const wrapper = prepareWrapper(prepareState(1, 1, 1));
-    const { result } = renderHook(() => useTimeLimitsFormState(), {
-      wrapper,
-    });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), {
+    //   wrapper,
+    // });
 
     act(() => {
-      result.current.setHrsPerMonth(17);
+      // result.current.setHrsPerMonth(17);
+      findHookProp(wrapper).setHrsPerMonth(17);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.maxHrsPerWeek).toEqual(17);
+    // expect(result.current.maxHrsPerWeek).toEqual(17);
+    expectHook(wrapper).toMatchObject({
+      maxHrsPerWeek: 17,
+    });
   });
 
   test("it updates maxHrsPerWeek when hrsPerMonth changes but only to the max allowed value", () => {
     const wrapper = prepareWrapper(prepareState(1, 1, 1));
-    const { result } = renderHook(() => useTimeLimitsFormState(), {
-      wrapper,
-    });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), {
+    //   wrapper,
+    // });
 
     act(() => {
-      result.current.setHrsPerMonth(201);
+      // result.current.setHrsPerMonth(201);
+      findHookProp(wrapper).setHrsPerMonth(201);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.maxHrsPerWeek).toEqual(167);
+    // expect(result.current.maxHrsPerWeek).toEqual(167);
+    expectHook(wrapper).toMatchObject({
+      maxHrsPerWeek: 167,
+    });
   });
 
   test("it updates dailyLimitErrorMessage when hrsPerDay changes to value outside of range", () => {
     const weekly = 10;
     const wrapper = prepareWrapper(prepareState(3, weekly, 30));
-    const { result } = renderHook(() => useTimeLimitsFormState(), {
-      wrapper,
-    });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), {
+    //   wrapper,
+    // });
 
     act(() => {
-      result.current.setHrsPerDay(201);
+      // result.current.setHrsPerDay(201);
+      findHookProp(wrapper).setHrsPerDay(201);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.dailyLimitErrorMessage).toEqual(
-      limitErrorMessage(1, weekly, 201)
-    );
+    // expect(result.current.dailyLimitErrorMessage).toEqual(
+    //   limitErrorMessage(1, weekly, 201)
+    // );
+    expectHook(wrapper).toMatchObject({
+      dailyLimitErrorMessage: limitErrorMessage(1, weekly, 201),
+    });
 
+    // eslint-disable-next-line sonarjs/no-identical-functions
     act(() => {
-      result.current.setHrsPerDay(10);
+      // result.current.setHrsPerDay(10);
+      findHookProp(wrapper).setHrsPerDay(10);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.dailyLimitErrorMessage).toEqual("");
+    // expect(result.current.dailyLimitErrorMessage).toEqual("");
+    expectHook(wrapper).toMatchObject({
+      dailyLimitErrorMessage: "",
+    });
   });
 
   test("it updates weeklyLimitErrorMessage when hrsPerWeek changes to value outside of range", () => {
     const monthly = 30;
     const wrapper = prepareWrapper(prepareState(3, 10, monthly));
-    const { result } = renderHook(() => useTimeLimitsFormState(), {
-      wrapper,
-    });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), {
+    //   wrapper,
+    // });
     const newValue = 170;
 
     act(() => {
-      result.current.setHrsPerWeek(newValue);
+      // result.current.setHrsPerWeek(newValue);
+      findHookProp(wrapper).setHrsPerWeek(newValue);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.weeklyLimitErrorMessage).toEqual(
-      limitErrorMessage(1, monthly, newValue)
-    );
+    // expect(result.current.weeklyLimitErrorMessage).toEqual(
+    //   limitErrorMessage(1, monthly, newValue)
+    // );
+    expectHook(wrapper).toMatchObject({
+      weeklyLimitErrorMessage: limitErrorMessage(1, monthly, newValue),
+    });
 
     act(() => {
-      result.current.setHrsPerWeek(100);
+      // result.current.setHrsPerWeek(100);
+      findHookProp(wrapper).setHrsPerWeek(100);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.weeklyLimitErrorMessage).toEqual(
-      limitErrorMessage(1, monthly, newValue)
-    );
+    // expect(result.current.weeklyLimitErrorMessage).toEqual(
+    //   limitErrorMessage(1, monthly, newValue)
+    // );
+    expectHook(wrapper).toMatchObject({
+      weeklyLimitErrorMessage: limitErrorMessage(1, monthly, newValue),
+    });
 
     act(() => {
-      result.current.setHrsPerWeek(29);
+      // result.current.setHrsPerWeek(29);
+      findHookProp(wrapper).setHrsPerWeek(29);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.weeklyLimitErrorMessage).toEqual("");
+    // expect(result.current.weeklyLimitErrorMessage).toEqual("");
+    expectHook(wrapper).toMatchObject({
+      weeklyLimitErrorMessage: "",
+    });
   });
 
   test("it updates monthlyLimitErrorMessage when hrsPerMonth changes to value outside of range", () => {
     const weekly = 10;
     const monthly = 30;
     const wrapper = prepareWrapper(prepareState(3, weekly, monthly));
-    const { result } = renderHook(() => useTimeLimitsFormState(), {
-      wrapper,
-    });
+    // const { result } = renderHook(() => useTimeLimitsFormState(), {
+    //   wrapper,
+    // });
     const newValue = 7;
 
     act(() => {
-      result.current.setHrsPerMonth(newValue);
+      // result.current.setHrsPerMonth(newValue);
+      findHookProp(wrapper).setHrsPerMonth(newValue);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.monthlyLimitErrorMessage).toEqual(
-      limitErrorMessage(weekly, monthly, newValue)
-    );
+    // expect(result.current.monthlyLimitErrorMessage).toEqual(
+    //   limitErrorMessage(weekly, monthly, newValue)
+    // );
+    expectHook(wrapper).toMatchObject({
+      monthlyLimitErrorMessage: limitErrorMessage(weekly, monthly, newValue),
+    });
 
     act(() => {
-      result.current.setHrsPerMonth(100);
+      // result.current.setHrsPerMonth(100);
+      findHookProp(wrapper).setHrsPerMonth(100);
+      jest.runAllTimers();
+      wrapper.update();
     });
 
-    expect(result.current.monthlyLimitErrorMessage).toEqual("");
+    // expect(result.current.monthlyLimitErrorMessage).toEqual("");
+    expectHook(wrapper).toMatchObject({
+      monthlyLimitErrorMessage: "",
+    });
   });
 });
