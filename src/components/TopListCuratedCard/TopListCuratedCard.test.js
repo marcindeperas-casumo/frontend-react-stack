feca4235b7e5b9f4ce87f60c437523cff3fbf753
Mariusz Klinger <mariusz.klinger@casumo.com>
2020-06-09 15:57:16 +0200

    PCT-214 - mobile curated component content based on welcome-offer-id - cashback / welcome bonus

--- src/components/TopListCuratedCard/TopListCuratedCard.test.js
+++ src/components/TopListCuratedCard/TopListCuratedCard.test.js
@@ -2,13 +2,18 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 import { CuratedCard } from "Components/CuratedCard";
-import { TopListCuratedCard, WELCOME_OFFER_SLUG } from "./TopListCuratedCard";
+import {
+  TopListCuratedCard,
+  CURATED_COMPONENT_GENERAL_SLUG,
+} from "./TopListCuratedCard";
 
 describe("TopListCuratedCard", () => {
   test("renders the injected card IF the user has already deposited", () => {
     const slug = "foo";
     const rendered = shallow(
       <TopListCuratedCard
+        market={""}
+        welcomeOfferId={"welcome-bonus-offer"}
         card={slug}
         hasDeposited={true}
         enforceOriginalSlug={false}
@@ -23,6 +28,8 @@ describe("TopListCuratedCard", () => {
     const slug = "foo";
     const rendered = shallow(
       <TopListCuratedCard
+        market={""}
+        welcomeOfferId={"welcome-bonus-offer"}
         card={slug}
         hasDeposited={false}
         enforceOriginalSlug={false}
@@ -30,13 +37,17 @@ describe("TopListCuratedCard", () => {
     );
 
     expect(rendered.find(CuratedCard)).toHaveLength(1);
-    expect(rendered.find(CuratedCard).props().slug).toBe(WELCOME_OFFER_SLUG);
+    expect(rendered.find(CuratedCard).props().slug).toBe(
+      CURATED_COMPONENT_GENERAL_SLUG
+    );
   });
 
   test("if the 'card' prop is an array it takes the first item", () => {
     const slug = ["foo", "bar"];
     const rendered = shallow(
       <TopListCuratedCard
+        market={""}
+        welcomeOfferId={"welcome-bonus-offer"}
         card={slug}
         hasDeposited={true}
         enforceOriginalSlug={false}
@@ -51,6 +62,8 @@ describe("TopListCuratedCard", () => {
     const slug = "foo";
     const rendered = shallow(
       <TopListCuratedCard
+        market={""}
+        welcomeOfferId={"welcome-bonus-offer"}
         card={slug}
         hasDeposited={false}
         enforceOriginalSlug={true}
