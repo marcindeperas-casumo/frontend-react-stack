d76b70aaffa7b00db52de3786c93ff1d7112a5dc
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-06-10 10:19:03 +0200

    Added logic in TimeLimitsFormOutro to display different textual content.

--- src/components/Compliance/Sweden/TimeLimits/TimeLimitsFormOutro/TimeLimitsFormOutro.js
+++ src/components/Compliance/Sweden/TimeLimits/TimeLimitsFormOutro/TimeLimitsFormOutro.js
@@ -1,9 +1,12 @@
 // @flow
 import * as React from "react";
+import { DateTime } from "luxon";
 import Flex from "@casumo/cmp-flex";
 import Text from "@casumo/cmp-text";
 import Button from "@casumo/cmp-button";
 import { TickIcon } from "@casumo/cmp-icons";
+import { type LoginTimeLimit } from "Models/playOkay";
+import { interpolate } from "Utils";
 
 type Props = {
   t: {
@@ -11,11 +14,25 @@ type Props = {
     time_limits_form_outro_copy_decreasing: string,
     time_limits_form_outro_copy_increasing: string,
     time_limits_form_outro_cta: string,
+    period_daily: string,
+    period_weekly: string,
+    period_monthly: string,
   },
+  initial?: boolean,
   onClickCta: () => void,
+  dailyLimit: LoginTimeLimit,
+  weeklyLimit: LoginTimeLimit,
+  monthlyLimit: LoginTimeLimit,
 };
 
-export function TimeLimitsFormOutro({ t, onClickCta }: Props) {
+export function TimeLimitsFormOutro({
+  t,
+  initial,
+  onClickCta,
+  dailyLimit,
+  weeklyLimit,
+  monthlyLimit,
+}: Props) {
   return (
     <Flex
       direction="vertical"
@@ -26,10 +43,14 @@ export function TimeLimitsFormOutro({ t, onClickCta }: Props) {
       <Flex.Item className="u-text-align-center">
         <TickIcon size="lg" className="t-color-green" />
       </Flex.Item>
-      <Flex.Item>
-        <Text className="u-text-align-center u-margin-bottom--5xlg">
-          {t.time_limits_form_outro_copy_initial}
-        </Text>
+      <Flex.Item className="u-margin-bottom--5xlg">
+        <Copy
+          t={t}
+          initial={initial}
+          dailyLimit={dailyLimit}
+          weeklyLimit={weeklyLimit}
+          monthlyLimit={monthlyLimit}
+        />
       </Flex.Item>
       <Flex.Item>
         <Button
@@ -42,5 +63,80 @@ export function TimeLimitsFormOutro({ t, onClickCta }: Props) {
         </Button>
       </Flex.Item>
     </Flex>
+  );
+}
+
+type CopyProps = {
+  initial?: boolean,
+  dailyLimit: LoginTimeLimit,
+  weeklyLimit: LoginTimeLimit,
+  monthlyLimit: LoginTimeLimit,
+  t: {
+    period_daily: string,
+    period_weekly: string,
+    period_monthly: string,
+    time_limits_form_outro_copy_initial: string,
+    time_limits_form_outro_copy_decreasing: string,
+    time_limits_form_outro_copy_increasing: string,
+  },
+};
+
+function Copy({
+  t,
+  initial,
+  dailyLimit,
+  weeklyLimit,
+  monthlyLimit,
+}: CopyProps) {
+  if (initial) {
+    return (
+      <Text className="u-text-align-center">
+        {t.time_limits_form_outro_copy_initial}
+      </Text>
+    );
+  }
+
+  return (
+    <>
+      <LimitCopy t={t} limit={dailyLimit} />
+      <LimitCopy t={t} limit={weeklyLimit} />
+      <LimitCopy t={t} limit={monthlyLimit} />
+    </>
+  );
+}
+
+type LimitCopyProps = {
+  limit: LoginTimeLimit,
+  t: {
+    period_daily: string,
+    period_weekly: string,
+    period_monthly: string,
+    time_limits_form_outro_copy_decreasing: string,
+    time_limits_form_outro_copy_increasing: string,
+  },
+};
+
+function LimitCopy({ limit, t }: LimitCopyProps) {
+  const replacements = {
+    period: t[`period_${limit.period.toLowerCase()}`],
+  };
+
+  if (limit.comingLimit) {
+    return (
+      <Text className="u-text-align-center">
+        {interpolate(t.time_limits_form_outro_copy_increasing, {
+          ...replacements,
+          date: DateTime.fromMillis(limit.comingLimit.activationTime).toFormat(
+            "DD"
+          ),
+        })}
+      </Text>
+    );
+  }
+
+  return (
+    <Text className="u-text-align-center">
+      {interpolate(t.time_limits_form_outro_copy_decreasing, replacements)}
+    </Text>
   );
 }
