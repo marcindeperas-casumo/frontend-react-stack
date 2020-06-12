a6a575fce191cbbc92f530fe8c618e0ea8ffb958
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-06-12 15:38:00 +0200

    Added check for SGA.

--- src/models/playOkay/realityCheck/realityCheck.periodicNotification.saga.js
+++ src/models/playOkay/realityCheck/realityCheck.periodicNotification.saga.js
@@ -1,9 +1,19 @@
 // @flow
 import { type Saga } from "redux-saga";
-import { put } from "redux-saga/effects";
+import { put, select, call } from "redux-saga/effects";
 import { showModal } from "Models/modal";
-import { REACT_APP_MODAL } from "Src/constants";
+import { jurisdictionSelector } from "Models/handshake";
+import { REACT_APP_MODAL, JURISDICTIONS } from "Src/constants";
+import { waitForSelector } from "Utils";
 
 export function* periodicNotificationSaga(): Saga {
+  yield call(waitForSelector, jurisdictionSelector);
+
+  const jurisdiction = yield select(jurisdictionSelector);
+
+  if (jurisdiction !== JURISDICTIONS.SGA) {
+    return;
+  }
+
   yield put(showModal(REACT_APP_MODAL.ID.REALITY_CHECK));
 }
