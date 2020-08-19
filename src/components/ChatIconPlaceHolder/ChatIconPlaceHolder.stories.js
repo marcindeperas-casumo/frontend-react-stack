9a7d3001819338185eb97876a265356fda9f070f
IVB <ian.baldacchino@casumo.com>
2020-08-19 09:04:36 +0200

    Update: onclick storybook action added

--- src/components/ChatIconPlaceHolder/ChatIconPlaceHolder.stories.js
+++ src/components/ChatIconPlaceHolder/ChatIconPlaceHolder.stories.js
@@ -1,15 +1,14 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { ChatIconPlaceHolder } from "./ChatIconPlaceHolder";
 
 const stories = storiesOf("ChatIconPlaceHolder", module);
 
 stories.add("Default", () => {
-  const fakeClickEvent = () => null;
-
   return (
     <>
-      <ChatIconPlaceHolder onClickCallBack={fakeClickEvent} />
+      <ChatIconPlaceHolder onClick={action("clicked")} />
     </>
   );
 });
