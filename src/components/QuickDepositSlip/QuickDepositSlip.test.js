a078097556e8cbcf27594ac2099446ed5fffdb3e
Christian Ciantar <christian.ciantar@mobile.local>
2020-09-01 16:59:00 +0200

    quick-deposit-slip: fix tests

--- src/components/QuickDepositSlip/QuickDepositSlip.test.js
+++ src/components/QuickDepositSlip/QuickDepositSlip.test.js
@@ -1,6 +1,7 @@
 // @flow
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
 import { QuickDepositSlip } from "./QuickDepositSlip";
 import { translations as t } from "./__mocks__/cms";
 
@@ -12,41 +13,53 @@ const props = {
   onDeposit: () => {},
 };
 
+const DATA_TEST_ID = "deposit-amount-selector";
+
 describe("<QuickDepositSlip />", () => {
   describe("validation", () => {
     let rendered;
+    let depositAmountSelector;
+    let input;
     const setState = jest.fn();
     const useStateSpy = jest.spyOn(React, "useState");
     useStateSpy.mockImplementation(init => [init, setState]);
 
     beforeEach(() => {
-      // $FlowFixMe
-      rendered = mount(shallow(<QuickDepositSlip {...props} />).get(0));
+      rendered = mount(<QuickDepositSlip {...props} />);
+      depositAmountSelector = rendered.find(`[data-test-id='${DATA_TEST_ID}']`);
+      input = depositAmountSelector.find("input").at(0);
     });
 
     afterEach(() => {
       jest.clearAllMocks();
     });
 
-    test("should show error for minimum amount", () => {
-      const depositAmountSelector = rendered.find(
-        "[data-test-id='deposit-amount-selector']"
-      );
-
-      const inputSelector = depositAmountSelector.find("input").at(0);
-      inputSelector.instance().value = 5;
-      inputSelector.simulate("change");
-      expect(setState).toHaveBeenCalledWith(5);
+    test("should show validation error messages for minimum amount", () => {
+      act(() => {
+        input.instance().value = 5;
+        input.simulate("change");
+        rendered.update();
+      });
 
       const helperTextSelector = depositAmountSelector.find("span").last();
 
       expect(helperTextSelector.text()).toBe(
-        `Minimum deposit amount is ${props.currencySymbol}${props.minAmount}`
+        `Minimum deposit amount is ${props.minAmount}`
       );
     });
 
-    test("should show error for maximum amount", () => {});
+    test("should show error for maximum amount", () => {
+      act(() => {
+        input.instance().value = 101;
+        input.simulate("change");
+        rendered.update();
+      });
 
-    test("should not allow submit when one or more errors occured", () => {});
+      const helperTextSelector = depositAmountSelector.find("span").last();
+
+      expect(helperTextSelector.text()).toBe(
+        `Maximum deposit amount is ${props.maxAmount}`
+      );
+    });
   });
 });
