fb67556756db8c189ebc53381016768faf4670c7
Mariusz <mariusz.klinger@casumo.com>
2020-09-03 12:25:54 +0200

    added new testers for ios embedded games feature

--- src/constants.js
+++ src/constants.js
@@ -439,5 +439,10 @@ export const EMBEDDED_GAMES = {
     "ms@50script.com",
     "sebastian.steek@casumo.com",
     "boguslaw.parol+30@casumo.com",
+    "andre.formosa@casumo.com",
+    "daniel.gauci@gmail.com",
+    "peter.noer+TEST00070@casumo.com",
+    "mark.busuttil@casumo.com",
+    "alessandro.pontes@casumo.com",
   ],
 };
