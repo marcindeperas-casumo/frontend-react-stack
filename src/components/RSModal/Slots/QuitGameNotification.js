9eb274752e9c90e60d8c0c6bbcd02e2e6ed32cd3
IVB <ian.baldacchino@casumo.com>
2020-09-07 13:06:42 +0200

    Update: Reverted translations to be retrieved by RSModal as it breaks storybook file to have translations retrieved by component

--- src/components/RSModal/Slots/QuitGameNotification.js
+++ src/components/RSModal/Slots/QuitGameNotification.js
@@ -2,8 +2,7 @@
 import * as React from "react";
 import Text from "@casumo/cmp-text";
 import Modal from "@casumo/cmp-modal";
-import { useCrossCodebaseNavigation, useTranslations } from "Utils/hooks";
-import { CMS_SLUGS } from "Models/playing/playing.constants";
+import { useCrossCodebaseNavigation } from "Utils/hooks";
 import { ROUTE_IDS } from "Src/constants";
 
 export type CmsContent = {
@@ -11,28 +10,29 @@ export type CmsContent = {
   quit_game_modal_text: string,
   quit_game_cta_text: string,
 };
-
 type QuitGameNotificationProps = {
   acceptModal?: () => void,
   config: {
     onCloseCallback?: () => void,
   },
+  t: any | CmsContent,
 };
 
 export const QuitGameNotification = ({
   acceptModal = () => null,
   config: { onCloseCallback },
+  t,
+  t: { quit_game_modal_title, quit_game_modal_text, quit_game_cta_text },
 }: QuitGameNotificationProps) => {
   const { navigateToKO } = useCrossCodebaseNavigation();
   const redirectToCashierPage = () => navigateToKO(ROUTE_IDS.CASH_DEPOSIT);
-  const t = useTranslations(CMS_SLUGS.IFRAME_SOLUTION);
   if (!t) {
     return null;
   }
   const modalProps = {
-    bigTitle: t.quit_game_modal_title,
+    bigTitle: quit_game_modal_title,
     primaryButton: {
-      text: t.quit_game_cta_text,
+      text: quit_game_cta_text,
       action: redirectToCashierPage,
     },
     closeIcon: {
@@ -47,7 +47,7 @@ export const QuitGameNotification = ({
 
   return (
     <Modal {...modalProps}>
-      <Text tag="span">{t.quit_game_modal_text}</Text>
+      <Text tag="span">{quit_game_modal_text}</Text>
     </Modal>
   );
 };
--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -8,7 +8,9 @@ export { useMarket } from "./useMarket";
 export { useUrlPrefix } from "./useUrlPrefix";
 export { useLanguage } from "./useLanguage";
 export { useWalletAmount } from "./useWalletAmount";
-export { usePlayerSessionInvalidRedirect } from "./usePlayerSessionInvalidRedirect";
+export {
+  usePlayerSessionInvalidRedirect,
+} from "./usePlayerSessionInvalidRedirect";
 export { useGameLaunchData } from "./useGameLaunchData";
 export { useCrossCodebaseNavigation } from "./useCrossCodebaseNavigation";
 export { useJurisdiction } from "./useJurisdiction";
