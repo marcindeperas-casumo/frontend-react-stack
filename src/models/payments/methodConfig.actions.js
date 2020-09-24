2d0a950df2a4ee16889ef8fdd35b9d7592d1fa60
lukKowalski <lukasz.kowalski@casumo.com>
2020-09-24 13:31:57 +0200

    extracted specific type into its own var, using it instead of type expression

--- src/models/payments/methodConfig.actions.js
+++ src/models/payments/methodConfig.actions.js
@@ -1,16 +1,16 @@
 //@flow
 import { actionTypes } from "./methodConfig.constants";
-import { LOCAL_PAYMENT_TYPES } from "./piq.constants";
+import type { localPaymentTypesValues } from "./piq.constants";
 
 export const preparePaymentMethodConfig = (
-  methodType: $Values<typeof LOCAL_PAYMENT_TYPES>
+  methodType: localPaymentTypesValues
 ) => ({
   type: actionTypes.PREPARE_METHOD_CONFIG,
   methodType,
 });
 
 export const setPaymentMethodConfig = (
-  methodType: $Values<typeof LOCAL_PAYMENT_TYPES>,
+  methodType: localPaymentTypesValues,
   config: any
 ) => ({
   type: actionTypes.SET_METHOD_CONFIG,
