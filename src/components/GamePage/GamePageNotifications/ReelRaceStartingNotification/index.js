c27d7934c597f491916c9613b97b2e49b1d01d9b
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-09-25 09:56:58 +0200

    refactor(reelracestartingnotification): renamed exported comp

--- src/components/GamePage/GamePageNotifications/ReelRaceStartingNotification/index.js
+++ src/components/GamePage/GamePageNotifications/ReelRaceStartingNotification/index.js
@@ -1,4 +1,4 @@
 // @flow
 export {
-  ReelRaceStartingNotificationContainer,
+  ReelRaceStartingNotificationContainer as ReelRaceStartingNotification,
 } from "./ReelRaceStartingNotificationContainer";
