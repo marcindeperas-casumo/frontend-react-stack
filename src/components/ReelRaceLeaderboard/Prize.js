36e220b8440a4b1a30e19775632e2ee1cede42bc
Piotr Wyrobek <piotr.wyrobek@casumo.com>
2020-10-01 16:02:32 +0200

    feat(FC-144): Fix typo

--- src/components/ReelRaceLeaderboard/Prize.js
+++ src/components/ReelRaceLeaderboard/Prize.js
@@ -29,7 +29,7 @@ export const Prize = ({ prize, highlighted, className }: Props) => (
     >
       <Flex.Item>
         <TournamentIcon
-          className={cx(`c${baseClassName}__icon`, {
+          className={cx(`${baseClassName}__icon`, {
             "t-color-grey-50": !highlighted,
             "t-color-white": highlighted,
           })}
