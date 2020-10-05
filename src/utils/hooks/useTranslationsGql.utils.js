90f6b24c2fd9c1bf53109c8597ac63cde0956acb
Christian Ciantar <christian.ciantar@mobile.local>
2020-10-05 14:34:02 +0200

    update query for cms field resolver change

--- src/utils/hooks/useTranslationsGql.utils.js
+++ src/utils/hooks/useTranslationsGql.utils.js
@@ -2,7 +2,19 @@ import * as R from "ramda";
 
 export const generateQueries = R.pipe(
   R.mapObjIndexed(
-    (value, key) => `${key}: getCMSField(id: "${value}") { id, text }`
+    (value, key) => `${key}: getCMSField(id: "${value}") { 
+      ... on CmsText {
+        id
+        text
+      }
+    	... on CmsFieldList {
+        id
+        fields {
+          id
+          text
+        }
+    	}
+    }`
   ),
   R.values
 );
