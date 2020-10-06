a80b662fe672e18d76433f93e409c9fcd9073d75
Michał Mokijewski <hello@naf.gl>
2020-10-06 12:22:12 +0200

    Use jackpots widget inside blue ribbon section

--- src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonChristmas.js
+++ src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonChristmas.js
@@ -2,12 +2,20 @@
 import * as React from "react";
 import * as R from "ramda";
 import { useQuery } from "@apollo/react-hooks";
+import Flex from "@casumo/cmp-flex";
 import Scrollable from "@casumo/cmp-scrollable";
 import { ScrollableListTitleRow } from "Components/ScrollableListTitleRow";
 import * as A from "Types/apollo";
-import { JackpotsListTile } from "Components/JackpotsListTile";
+import { GameRow, GameRowText } from "Components/GameRow";
 import { GameListQuery } from "Components/GameListHorizontal/GameListHorizontalDefault/GameListHorizontalDefault.graphql";
-import { useFetch } from "Utils/hooks";
+import { useFetch, useTranslations } from "Utils/hooks";
+import {
+  urls,
+  jackpotWidgetContentPage,
+  type JackpotWidgetContentPage,
+} from "./blueRibbonConsts";
+import { usePotStateChangeEvent } from "./useBlueRibbonSDK";
+import { BlueRibbonJackpotsWidget } from "./BlueRibbonJackpotsWidget";
 
 const PADDING_PER_DEVICE = {
   default: "md",
@@ -20,9 +28,9 @@ type Props = {
 };
 
 export function BlueRibbonChristmas({ id }: Props) {
-  const { response } = useFetch(
-    "/casino-player/blueribbon-jackpot-integration/api/v1/handshake"
-  );
+  const { response } = useFetch(urls.handshake);
+  const t = useTranslations<JackpotWidgetContentPage>(jackpotWidgetContentPage);
+  const pots = usePotStateChangeEvent();
   const { data } = useQuery<A.GameListQuery, A.GameListQueryVariables>(
     GameListQuery,
     {
@@ -33,20 +41,50 @@ export function BlueRibbonChristmas({ id }: Props) {
   const columns = R.splitEvery(3, R.pathOr([], ["gamesList", "games"], data));
   const available = R.propOr(false, "available", response);
 
-  if (!available) {
+  if (!available || !t || !response) {
     return null;
   }
 
   return (
-    <div className="o-wrapper">
-      <div className="u-padding-top--xlg">
-        <ScrollableListTitleRow paddingLeft title="Blue Ribbon Campaign" />
-        <Scrollable
-          numberOfItems={columns.length}
-          itemRenderer={i => <JackpotsListTile games={columns[i]} />}
-          padding={PADDING_PER_DEVICE}
-        />
-      </div>
-    </div>
+    <Flex direction="vertical" className="o-wrapper u-padding-top--xlg">
+      <ScrollableListTitleRow paddingLeft title={t.section_title} />
+      <Scrollable
+        numberOfItems={columns.length + 1}
+        itemRenderer={i => {
+          if (i === 0) {
+            return (
+              <BlueRibbonJackpotsWidget
+                key="br-widget"
+                t={t}
+                jackpots={response.jackpots[0].pots
+                  .map(
+                    ({ communityWinRatio, mainWinRatio, potId, potName }) => ({
+                      value: pots[potId]?.progressive,
+                      label: potName,
+                      status: pots[potId]?.potStatus,
+                      potId,
+                      communityWinRatio,
+                      mainWinRatio,
+                    })
+                  )
+                  .filter(x => x.value)}
+              />
+            );
+          }
+
+          return columns[i - 1].map(game => (
+            <div key={game.id} className="u-padding-bottom">
+              <GameRow
+                big
+                game={game}
+                className="t-background-white u-padding--md t-border-r--md t-elevation--10"
+                renderText={() => <GameRowText name={game.name} />}
+              />
+            </div>
+          ));
+        }}
+        padding={PADDING_PER_DEVICE}
+      />
+    </Flex>
   );
 }
