bb03433f23664ce1ab2a99c81a5cdd0f45d55b3f
Michał Mokijewski <hello@naf.gl>
2020-10-06 12:21:17 +0200

    Add blue ribbon jackpots widget

---
+++ src/components/PromotionalGameLists/BlueRibbonChristmas/__mocks__/jackpots.js
@@ -0,0 +1,26 @@
+// @flow
+
+export const jackpots = [
+  {
+    communityWinRatio: 0,
+    label: "MINI",
+    mainWinRatio: 1,
+    potId: "EmfHE1601029307968",
+    status: "WARM",
+    value: 10000,
+  }, {
+    communityWinRatio: 0,
+    label: "MAJOR",
+    mainWinRatio: 1,
+    potId: "mdWQO1601029307972",
+    status: "CHILLY",
+    value: 100000,
+  }, {
+    communityWinRatio: 0.5,
+    label: "MEGA",
+    mainWinRatio: 0.5,
+    potId: "tikxy1601029307975",
+    status: "CHILLY",
+    value: 1000000,
+  }
+];
