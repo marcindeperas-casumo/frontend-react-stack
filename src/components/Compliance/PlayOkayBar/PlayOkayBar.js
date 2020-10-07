b2fbc277503aadbf3e30241bb55e2e526146b682
Dave Calleja <dave.calleja@casumo.com>
2020-10-07 10:06:15 +0200

    Styling fix of PlayOkayBar

--- src/components/Compliance/PlayOkayBar/PlayOkayBar.js
+++ src/components/Compliance/PlayOkayBar/PlayOkayBar.js
@@ -27,7 +27,7 @@ export const PlayOkayBar = ({ jurisdiction, pauseGame, resumeGame }: Props) => {
     <PlayOkarBarComponent
       pauseGame={pauseGame}
       resumeGame={resumeGame}
-      classNames="u-height--full c-playokay-bar t-background-grey-90 t-color-white u-padding-x u-position-relative"
+      classNames="c-playokay-bar t-background-grey-90 t-color-white u-padding-x u-position-relative"
     />
   );
 };
