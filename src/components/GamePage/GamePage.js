069b8b784bf2f2f7d785332ed58a3199e26b4888
Dave Calleja <dave.calleja@casumo.com>
2020-10-13 18:36:49 +0200

    Re-adding vertical stretcher but disabling swipe requirement

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -5,6 +5,7 @@ import Flex from "@casumo/cmp-flex";
 import { FullscreenView } from "Components/FullscreenView";
 import { GameLauncher } from "Components/GameLauncher";
 import { InfoBar } from "Components/Compliance/SlotControlSystem/InfoBar";
+import { VerticalStretcher } from "Components/VerticalStretcher";
 import type { GameProviderModel } from "GameProviders";
 import { PlayOkayBar } from "Components/Compliance/PlayOkayBar";
 import { useInGameBonusOrRealBalanceCheck } from "Utils/hooks";
@@ -32,41 +33,47 @@ export const GamePage = ({
 
   return (
     <FullscreenView className="u-height--full u-width--screen t-background-grey-90">
-      <Flex
-        className="u-width--full u-height--full t-background-grey-90 t-color-white"
-        direction="vertical"
-        spacing="none"
-      >
-        <Flex.Item>
-          <PlayOkayBar pauseGame={pauseGame} resumeGame={resumeGame} />
-        </Flex.Item>
-        <Flex direction="horizontal" spacing="none" className="u-height--full">
+      <VerticalStretcher gameProviderModel={gameProviderModel}>
+        <Flex
+          className="u-width--full u-height--full t-background-grey-90 t-color-white"
+          direction="vertical"
+          spacing="none"
+        >
           <Flex.Item>
-            {/* sidebar for pinned items */}
-            {sidebar}
+            <PlayOkayBar pauseGame={pauseGame} resumeGame={resumeGame} />
           </Flex.Item>
-          <Flex.Block className="u-position-relative o-flex c-game-page__flexible-game-container">
-            <div
-              className={classNames(
-                "u-inset-0 u-position-absolute",
-                gameProviderModel.gameWrapperClasses || []
-              )}
-            >
-              <GameLauncher
-                gameProviderModel={gameProviderModel}
-                className="c-game-page__game-launcher"
-              />
-            </div>
-            <GamePageNotifications />
-          </Flex.Block>
+          <Flex
+            direction="horizontal"
+            spacing="none"
+            className="u-height--full"
+          >
+            <Flex.Item>
+              {/* sidebar for pinned items */}
+              {sidebar}
+            </Flex.Item>
+            <Flex.Block className="u-position-relative o-flex c-game-page__flexible-game-container">
+              <div
+                className={classNames(
+                  "u-inset-0 u-position-absolute",
+                  gameProviderModel.gameWrapperClasses || []
+                )}
+              >
+                <GameLauncher
+                  gameProviderModel={gameProviderModel}
+                  className="c-game-page__game-launcher"
+                />
+              </div>
+              <GamePageNotifications />
+            </Flex.Block>
+          </Flex>
+          {shouldShowSlotControlSystem && (
+            <Flex.Item>
+              <InfoBar />
+            </Flex.Item>
+          )}
         </Flex>
-        {shouldShowSlotControlSystem && (
-          <Flex.Item>
-            <InfoBar />
-          </Flex.Item>
-        )}
-      </Flex>
-      <QuickDepositSlipController />
+        <QuickDepositSlipController />
+      </VerticalStretcher>
     </FullscreenView>
   );
 };
