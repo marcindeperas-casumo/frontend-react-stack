0f2f0c2f3bc87091ba43e3a5cc9d13a1d8fccee4
Michał Ignaszewski <michal.ignaszewski@casumo.com>
2020-10-29 11:50:13 +0100

    removing fav competition selection

--- src/features/sports/components/FavouriteSportsAndCompetitionsSelectorModal/FavouriteSportsSelector/FavouriteSportsSelectorListItem.test.js
+++ src/features/sports/components/FavouriteSportsAndCompetitionsSelectorModal/FavouriteSportsSelector/FavouriteSportsSelectorListItem.test.js
@@ -5,7 +5,6 @@ import { withFavouritesResult } from "../StageFavouritesContext/__mocks__/favour
 import FavouriteSportsSelectorListItem from "./FavouriteSportsSelectorListItem";
 
 const footballGroup = withFavouritesResult.groups[0];
-const tennisGroup = withFavouritesResult.groups[1];
 
 describe("<FavouriteSportsSelectorListItem />", () => {
   test("renders FavouriteListItem for this group", () => {
@@ -30,48 +29,6 @@ describe("<FavouriteSportsSelectorListItem />", () => {
     );
   });
 
-  test("should render a CompetitionPillsList if the group has selectable competitions and is selected", () => {
-    const renderedNotFavourite = shallow(
-      <FavouriteSportsSelectorListItem
-        group={footballGroup}
-        onToggleFavouriteSport={() => {}}
-        isFavourite={false}
-        showCompetitionIntro={false}
-        onAddCompetition={() => {}}
-        onRemoveFavouriteCompetition={() => {}}
-        isOnboarding={false}
-      />
-    );
-
-    const renderedFavourite = shallow(
-      <FavouriteSportsSelectorListItem
-        group={footballGroup}
-        onToggleFavouriteSport={() => {}}
-        isFavourite={true}
-        showCompetitionIntro={false}
-        onAddCompetition={() => {}}
-        onRemoveFavouriteCompetition={() => {}}
-        isOnboarding={false}
-      />
-    );
-
-    const renderedNoSubgroups = shallow(
-      <FavouriteSportsSelectorListItem
-        group={tennisGroup}
-        onToggleFavouriteSport={() => {}}
-        isFavourite={true}
-        showCompetitionIntro={false}
-        onAddCompetition={() => {}}
-        onRemoveFavouriteCompetition={() => {}}
-        isOnboarding={false}
-      />
-    );
-
-    expect(renderedNotFavourite.find("CompetitionPillsList")).toHaveLength(0);
-    expect(renderedFavourite.find("CompetitionPillsList")).toHaveLength(1);
-    expect(renderedNoSubgroups.find("CompetitionPillsList")).toHaveLength(0);
-  });
-
   test("should render a CompetitionsIntro and also hide add button in CompetitionPillsList if showCompetitionIntro is true", () => {
     const renderedNoIntro = shallow(
       <FavouriteSportsSelectorListItem
@@ -84,31 +41,10 @@ describe("<FavouriteSportsSelectorListItem />", () => {
         isOnboarding={false}
       />
     );
-    const renderedWithIntro = shallow(
-      <FavouriteSportsSelectorListItem
-        group={footballGroup}
-        onToggleFavouriteSport={() => {}}
-        isFavourite={true}
-        showCompetitionIntro={true}
-        onAddCompetition={() => {}}
-        onRemoveFavouriteCompetition={() => {}}
-        isOnboarding={false}
-      />
-    );
 
     expect(
       renderedNoIntro.find("FavouriteSportsSelectorCompetitionsIntro")
     ).toHaveLength(0);
-    expect(
-      renderedNoIntro.find("CompetitionPillsList").props().onAdd
-    ).toBeTruthy();
-
-    expect(
-      renderedWithIntro.find("FavouriteSportsSelectorCompetitionsIntro")
-    ).toHaveLength(1);
-    expect(
-      renderedWithIntro.find("CompetitionPillsList").props().onAdd
-    ).toBeFalsy();
   });
 
   test("should call onToggleFavouriteSport when the ListItem is clicked", () => {
@@ -128,79 +64,5 @@ describe("<FavouriteSportsSelectorListItem />", () => {
     rendered.find("FavouriteListItem").simulate("click");
 
     expect(onToggleFavouriteSport).toHaveBeenCalledWith(footballGroup.id);
-  });
-
-  test("should call onAddCompetition when add button on CompetitionPillsList/FavouriteSportsSelectorCompetitionsIntro is clicked", () => {
-    const onAddCompetition = jest.fn();
-    const rendered = shallow(
-      <FavouriteSportsSelectorListItem
-        group={footballGroup}
-        onToggleFavouriteSport={() => {}}
-        isFavourite={true}
-        showCompetitionIntro={false}
-        onAddCompetition={onAddCompetition}
-        onRemoveFavouriteCompetition={() => {}}
-        isOnboarding={false}
-      />
-    );
-    const renderedWithIntro = shallow(
-      <FavouriteSportsSelectorListItem
-        group={footballGroup}
-        onToggleFavouriteSport={() => {}}
-        isFavourite={true}
-        showCompetitionIntro={true}
-        onAddCompetition={onAddCompetition}
-        onRemoveFavouriteCompetition={() => {}}
-        isOnboarding={false}
-      />
-    );
-
-    rendered
-      .find("CompetitionPillsList")
-      .props()
-      .onAdd();
-
-    renderedWithIntro
-      .find("FavouriteSportsSelectorCompetitionsIntro")
-      .props()
-      .onAdd();
-
-    expect(onAddCompetition).toHaveBeenNthCalledWith(
-      1,
-      footballGroup.id,
-      footballGroup.name,
-      false
-    );
-    expect(onAddCompetition).toHaveBeenNthCalledWith(
-      2,
-      footballGroup.id,
-      footballGroup.name,
-      false
-    );
-  });
-
-  test("should call onRemoveFavouriteCompetition call when user requests to remove a favourited competition in the CompetitionPillsList", () => {
-    const onRemoveFavouriteCompetition = jest.fn();
-    const rendered = shallow(
-      <FavouriteSportsSelectorListItem
-        group={footballGroup}
-        onToggleFavouriteSport={() => {}}
-        isFavourite={true}
-        showCompetitionIntro={false}
-        onAddCompetition={() => {}}
-        onRemoveFavouriteCompetition={onRemoveFavouriteCompetition}
-        isOnboarding={false}
-      />
-    );
-
-    rendered
-      .find("CompetitionPillsList")
-      .props()
-      .onRemove(footballGroup.favouriteCompetitions[0]);
-
-    expect(onRemoveFavouriteCompetition).toHaveBeenCalledWith(
-      footballGroup.id,
-      footballGroup.favouriteCompetitions[0]
-    );
   });
 });
--- src/features/sports/components/SportsShell/SportsShellSkeleton.js
+++ src/features/sports/components/SportsShell/SportsShellSkeleton.js
@@ -1,12 +1,13 @@
 /* @flow */
 import * as React from "react";
+import { SportsNavigationSkeleton } from "@casumo/sports-navigation";
 import SportsTopBarSkeleton from "Features/sports/components/SportsTopBar/SportsTopBarSkeleton";
 import KambiClientSkeleton from "Features/sports/components/KambiClient/KambiClientSkeleton";
-/* TODO: import sports-navigation skeleton from shared component */
 
 const SportsShellSkeleton = () => (
   <>
     <SportsTopBarSkeleton />
+    <SportsNavigationSkeleton />
     <KambiClientSkeleton />
   </>
 );
