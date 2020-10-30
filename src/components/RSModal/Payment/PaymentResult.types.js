4f01519864ac8a65404d5dd0bd9a7e7edc5f99f0
Christian Ciantar <christian.ciantar@casumo.com>
2020-10-30 09:38:36 +0100

    remove error translations from param

--- src/components/RSModal/Payment/PaymentResult.types.js
+++ src/components/RSModal/Payment/PaymentResult.types.js
@@ -8,9 +8,7 @@ export type PaymentResultStatus = $Values<typeof PAYMENT_RESULT_STATUS>;
 
 export type CmsContent = {
   payment_result_title_success: string,
-  payment_result_title_fail: string,
   payment_result_content_success: string,
-  payment_result_content_fail: string,
 };
 
 export type PaymentResultProps = {
--- src/components/RSModal/Payment/__mocks__/paymentResult.mocks.js
+++ src/components/RSModal/Payment/__mocks__/paymentResult.mocks.js
@@ -4,8 +4,6 @@ import type { CmsContent } from "../PaymentResult.types";
 const translations: CmsContent = {
     payment_result_title_success: "Your {{amount}} deposit was successful",
     payment_result_content_success: "You can find your receipt and details in 'History'.",
-    payment_result_title_fail: "Deposit not successful",
-    payment_result_content_fail: "We had a problem while processing your payment, please try again.",
 }
 
 export default translations;
