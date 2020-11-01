2d6a7f0d78e8e2b5651316714c49c5625b4cc4c6
Christian Ciantar <christian.ciantar@casumo.com>
2020-11-01 14:56:17 +0100

    fix flow errors

--- src/components/RSModal/Payments/PaymentResult.types.js
+++ src/components/RSModal/Payments/PaymentResult.types.js
@@ -7,6 +7,6 @@ export const PAYMENT_RESULT_STATUS = Object.freeze({
 export type PaymentResultStatus = $Values<typeof PAYMENT_RESULT_STATUS>;
 
 export type CmsContent = {
-  payment_result_success_title: string,
-  payment_result_success_message: string,
+  payment_result_success_title: ?string,
+  payment_result_success_message: ?string,
 };
