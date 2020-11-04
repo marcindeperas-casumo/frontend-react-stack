c1e3b36718567f923e0f4c3bad37e65c9330117f
Marcin Strzeszkowski <marcin.strzeszkowski@casumo.com>
2020-11-05 00:32:59 +0100

    TPS-610 Added interaction timeout for panic button

--- src/components/Compliance/PlayOkayBar/GGLBar.js
+++ src/components/Compliance/PlayOkayBar/GGLBar.js
@@ -1,16 +1,9 @@
 // @flow
-
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
 import Flex from "@casumo/cmp-flex";
-import { ButtonSecondary } from "@casumo/cmp-button";
 import { TimeLockedIcon } from "@casumo/cmp-icons";
-import { showModal } from "Models/modal";
-import http from "Lib/http";
-import { playerIdSelector } from "Models/handshake";
-import { useTranslations } from "Utils/hooks";
-import { REACT_APP_MODAL } from "Src/constants";
 import { CurrentSessionTimer } from "Components/CurrentSessionTimer";
+import { PanicButton } from "./PanicButton";
 import { type PauseResumeProps } from "./PlayOkayBarContainer";
 import "./PlayOkayBar.scss";
 
@@ -19,47 +12,10 @@ type Props = PauseResumeProps & {
 };
 
 export const GGLBar = ({ pauseGame, resumeGame, className = "" }: Props) => {
-  const content = useTranslations("ggl-panic-button");
-  const playerId = useSelector(playerIdSelector);
-  const dispatch = useDispatch();
-
-  const onClick = () => {
-    http
-      .post("/api/common/command/player/selfExclude", {
-        playerId: playerId,
-        duration: 1,
-        marketingClosure: true,
-        reason: "panic-button",
-      })
-      .then(() => {
-        dispatch(
-          showModal(REACT_APP_MODAL.ID.GGL_POST_PANIC_BUTTON, {
-            content,
-          })
-        );
-      });
-  };
-
-  useEffect(() => {
-    if (content && !window.localStorage.preGamePanicButtonOverlayShown) {
-      dispatch(
-        showModal(REACT_APP_MODAL.ID.GGL_PRE_PANIC_BUTTON, {
-          content,
-        })
-      );
-    }
-  }, [content, dispatch]);
-
   return (
     <Flex justify="end" align="center" className={className}>
       <Flex.Block className="u-text-align-center">
-        <ButtonSecondary
-          className="t-background-grey-70 t-color-white u-text-transform-uppercase c-playokay-bar__button"
-          onClick={onClick}
-          size="xs"
-        >
-          {content?.button_label}
-        </ButtonSecondary>
+        <PanicButton />
       </Flex.Block>
       <Flex.Item className="c-playokay-bar__clock u-font-xs t-color-grey-5 u-display--flex">
         <TimeLockedIcon size="sm" className="u-margin-right" />
---
+++ src/components/Compliance/PlayOkayBar/PanicButton.js
@@ -0,0 +1,62 @@
+// @flow
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ButtonSecondary } from "@casumo/cmp-button";
+import { showModal } from "Models/modal";
+import http from "Lib/http";
+import { playerIdSelector } from "Models/handshake";
+import { useTranslations } from "Utils/hooks";
+import { REACT_APP_MODAL } from "Src/constants";
+
+const INTERACTION_TIME_SECONDS = 3;
+
+export const PanicButton = () => {
+  const content = useTranslations("ggl-panic-button");
+  const playerId = useSelector(playerIdSelector);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (content && !window.localStorage.preGamePanicButtonOverlayShown) {
+      dispatch(
+        showModal(REACT_APP_MODAL.ID.GGL_PRE_PANIC_BUTTON, {
+          content,
+        })
+      );
+    }
+  }, [content]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onTransitionEnd = ({ elapsedTime }) => {
+    // Not really required but not harmful either. Extra sanity check if someone
+    // was to change the default transition-duration to non-zero value 🙂
+    if (elapsedTime !== INTERACTION_TIME_SECONDS) {
+      return;
+    }
+
+    http
+      .post("/api/common/command/player/selfExclude", {
+        playerId: playerId,
+        duration: 1,
+        marketingClosure: true,
+        reason: "panic-button",
+      })
+      .then(() => {
+        dispatch(
+          showModal(REACT_APP_MODAL.ID.GGL_POST_PANIC_BUTTON, {
+            content,
+          })
+        );
+      });
+  };
+
+  return (
+    <ButtonSecondary
+      className="t-background-grey-70 t-color-white u-text-transform-uppercase
+        o-position--relative u-overflow--hidden c-playokay-bar__panic-button"
+      size="xs"
+      style={{ "--panic-btn-duration": `${INTERACTION_TIME_SECONDS}s` }}
+      onTransitionEnd={onTransitionEnd}
+    >
+      {content?.button_label}
+    </ButtonSecondary>
+  );
+};
