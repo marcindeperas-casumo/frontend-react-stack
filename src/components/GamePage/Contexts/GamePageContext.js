c526b6ea1d3c35adc51786fd04142d38d9aa7095
Dave Calleja <dave.calleja@casumo.com>
2020-11-04 20:16:05 +0100

    Removing unused types

--- src/components/GamePage/Contexts/GamePageContext.js
+++ src/components/GamePage/Contexts/GamePageContext.js
@@ -4,42 +4,12 @@ import { SumoIconContextProvider } from "Components/SumoIcon/SumoIconContext";
 import { GameModelContextProvider } from "./GameModelContext";
 import { PinnedWidgetsContextProvider } from "./PinnedWidgetsContext";
 
-type GameModelContextType = {
-  pauseGame: () => Promise<void>,
-  resumeGame: () => void,
-  gameProviderModel: any,
-  slug: string,
-  playForFun: boolean,
-  error: boolean,
-};
-
-type PinnedWidgetsContextType = {
-  pinnedWidgets: Array<string>,
-  togglePin: (drawer: string) => void,
-};
-
 type GamePageContextProps = {
   children: React.Node,
   slug: string,
   playForFun: boolean,
   remoteGameLaunchData: ?Object,
 };
-
-export const GameModelContext = React.createContext<GameModelContextType>({
-  pauseGame: () => Promise.resolve(),
-  resumeGame: () => {},
-  gameProviderModel: null,
-  slug: "",
-  playForFun: false,
-  error: false,
-});
-
-export const PinnedWidgetsContext = React.createContext<PinnedWidgetsContextType>(
-  {
-    pinnedWidgets: [],
-    togglePin: () => {},
-  }
-);
 
 export const GamePageContextProvider = ({
   children,
