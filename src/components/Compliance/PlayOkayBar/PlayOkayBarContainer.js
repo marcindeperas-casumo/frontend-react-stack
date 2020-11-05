281da67a9174b8777fb19a238f8671fc6f318bef
Dave Calleja <dave.calleja@casumo.com>
2020-11-05 07:24:54 +0100

    Re-adding PauseResumeProps to keep status quo

--- src/components/Compliance/PlayOkayBar/PlayOkayBarContainer.js
+++ src/components/Compliance/PlayOkayBar/PlayOkayBarContainer.js
@@ -4,6 +4,11 @@ import React from "react";
 import { useJurisdiction } from "Utils/hooks";
 import { PlayOkayBar } from "./PlayOkayBar";
 
+export type PauseResumeProps = {
+  pauseGame: () => Promise<void>,
+  resumeGame: () => void,
+};
+
 export const PlayOkayBarContainer = () => {
   const { jurisdiction } = useJurisdiction();
 
