dccc1392bfe61e63a39fb1457caf00eb30cd2ce6
IVB <ian.baldacchino@casumo.com>
2020-11-10 14:19:45 +0100

    Fix: Commented out 100vh css property and interval inside verticalStretcher to test playtech ui bug

--- src/components/VerticalStretcher/VerticalStretcher.js
+++ src/components/VerticalStretcher/VerticalStretcher.js
@@ -1,8 +1,7 @@
 //@flow
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import type { Element } from "react";
-import debounce from "lodash.debounce";
 import Flex from "@casumo/cmp-flex";
 import { isMobile } from "Components/ResponsiveLayout";
 import { isNativeByUserAgent } from "GameProviders";
@@ -30,80 +29,77 @@ export const VerticalStretcher = ({
   swipeUpPanelEnabled = false,
   gameProviderModel,
   fullScreenElement = document.body,
-}: // eslint-disable-next-line sonarjs/cognitive-complexity
-Props) => {
+}: Props) => {
   const heightContainer = useRef(null);
-  const [showSwipePanel, setShowSwipePanel] = useState(true);
-  const [controllScroll, setControllScroll] = useState(true);
-  const [alreadyTriggeredOnce, setAlreadyTriggeredOnce] = useState(false);
+  const [showSwipePanel] = useState(true);
 
   const isNative = isNativeByUserAgent();
 
-  useEffect(() => {
-    const debouncedScrollToTop = debounce(() => {
-      if (controllScroll) {
-        window.scrollTo(0, 0);
-      }
-    }, 50);
+  // useEffect(() => {
+  //   // const debouncedScrollToTop = debounce(() => {
+  //   //   if (controllScroll) {
+  //   //     window.scrollTo(0, 0);
+  //   //   }
+  //   // }, 50);
 
-    const interval = setInterval(() => {
-      if (heightContainer.current && document.body) {
-        /**
-         * So far this is the only way i've found which solves the problem
-         * of browser toolbars overlaying game content when they appear.
-         */
-        /* eslint-disable-next-line fp/no-mutation */
-        heightContainer.current.style.height = `${window.innerHeight}px`;
+  //   // const interval = setInterval(() => {
+  //   //   if (heightContainer.current && document.body) {
+  //   //     /**
+  //   //      * So far this is the only way i've found which solves the problem
+  //   //      * of browser toolbars overlaying game content when they appear.
+  //   //      */
+  //   //     /* eslint-disable-next-line fp/no-mutation */
+  //   //     heightContainer.current.style.height = `${window.innerHeight}px`;
 
-        /**
-         * Fix for evolution games, they set our body.height to calc(100px + 100vh)
-         * to emulate their own "swipe to play" feature which we don't want :)
-         */
-        /* eslint-disable-next-line fp/no-mutation */
-        document.body.style.height = "100vh";
-        /**
-         * This is just called here to trigger resize event which causes
-         * game container to match size of it's parent after changing
-         * top-lvl parent dimensions
-         */
-        window.dispatchEvent(new Event("resize"));
+  //   //     /**
+  //   //      * Fix for evolution games, they set our body.height to calc(100px + 100vh)
+  //   //      * to emulate their own "swipe to play" feature which we don't want :)
+  //   //      */
+  //   //     /* eslint-disable-next-line fp/no-mutation */
+  //   //     document.body.style.height = "100vh";
+  //   //     /**
+  //   //      * This is just called here to trigger resize event which causes
+  //   //      * game container to match size of it's parent after changing
+  //   //      * top-lvl parent dimensions
+  //   //      */
+  //   //     window.dispatchEvent(new Event("resize"));
 
-        /**
-         * swipePanel allows to force player to go fullscreen to play the game
-         * when toolbars are being shown and they are eating part of the screen
-         */
-        const deviceNotInFullScreenMode =
-          window.innerHeight < document.body?.clientHeight;
+  //   //     /**
+  //   //      * swipePanel allows to force player to go fullscreen to play the game
+  //   //      * when toolbars are being shown and they are eating part of the screen
+  //   //      */
+  //   //     const deviceNotInFullScreenMode =
+  //   //       window.innerHeight < document.body?.clientHeight;
 
-        if (deviceNotInFullScreenMode) {
-          if (!alreadyTriggeredOnce) {
-            setShowSwipePanel(true);
-            setControllScroll(false);
-          }
-        } else {
-          if (showSwipePanel) {
-            setAlreadyTriggeredOnce(true);
-          }
-          setShowSwipePanel(false);
-          setControllScroll(true);
-        }
-      }
-    }, 100);
+  //   //     if (deviceNotInFullScreenMode) {
+  //   //       if (!alreadyTriggeredOnce) {
+  //   //         setShowSwipePanel(true);
+  //   //         setControllScroll(false);
+  //   //       }
+  //   //     } else {
+  //   //       if (showSwipePanel) {
+  //   //         setAlreadyTriggeredOnce(true);
+  //   //       }
+  //   //       setShowSwipePanel(false);
+  //   //       setControllScroll(true);
+  //   //     }
+  //   //   }
+  //   // }, 100);
 
-    /**
-     * This prevents the situation when game content (resized to window.innerHeight)
-     * is smaller than available viewport, (they are different when browsers toolbars are on)
-     * and player is able to scroll a game up a bit by dragging vertically top or bottom bar (playOkay, spain)
-     * and then game stays like that and you can't do anything about it because usually game canvas prevents default
-     * scroll behavior, thus you can't scroll down anymore, because now you only see the game content
-     */
-    window.addEventListener("scroll", debouncedScrollToTop);
+  //   /**
+  //    * This prevents the situation when game content (resized to window.innerHeight)
+  //    * is smaller than available viewport, (they are different when browsers toolbars are on)
+  //    * and player is able to scroll a game up a bit by dragging vertically top or bottom bar (playOkay, spain)
+  //    * and then game stays like that and you can't do anything about it because usually game canvas prevents default
+  //    * scroll behavior, thus you can't scroll down anymore, because now you only see the game content
+  //    */
+  //   // window.addEventListener("scroll", debouncedScrollToTop);
 
-    return () => {
-      window.removeEventListener("scroll", debouncedScrollToTop);
-      clearInterval(interval);
-    };
-  });
+  //   // return () => {
+  //   //   window.removeEventListener("scroll", debouncedScrollToTop);
+  //   //   clearInterval(interval);
+  //   // };
+  // });
 
   const shouldShowSwipePanel =
     gameProviderModel.swipeUpToPlayPanelPossible &&
