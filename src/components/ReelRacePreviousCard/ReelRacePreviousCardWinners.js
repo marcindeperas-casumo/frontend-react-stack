af309343a5cc226a11da39c5b71573aced012124
Cho <choishere@gmail.com>
2020-11-11 11:05:13 +0100

    fix(reelRacesPage): hide show leader board if none

--- src/components/ReelRacePreviousCard/ReelRacePreviousCardWinners.js
+++ src/components/ReelRacePreviousCard/ReelRacePreviousCardWinners.js
@@ -90,11 +90,13 @@ export function ReelRacePreviousCardWinners({
     <Flex direction="vertical" className="u-padding-x--md">
       {winners.map((p, i) => renderRow(p, i, winners.length, true))}
       {full && rest.map((p, i) => renderRow(p, i, rest.length))}
-      <Flex.Block className="u-text-align-center u-margin-bottom--lg">
-        <ButtonPrimary size="md" onClick={toggleFull}>
-          {full ? t?.hide_leaderboard_button : t?.show_leaderboard_button}
-        </ButtonPrimary>
-      </Flex.Block>
+      {winners.length && rest.length && (
+        <Flex.Block className="u-text-align-center u-margin-bottom--lg">
+          <ButtonPrimary size="md" onClick={toggleFull}>
+            {full ? t?.hide_leaderboard_button : t?.show_leaderboard_button}
+          </ButtonPrimary>
+        </Flex.Block>
+      )}
     </Flex>
   );
 }
