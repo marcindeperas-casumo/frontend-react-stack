d90fa380d54f0a3a342a7da63dea7e72b55eaa4a
Adrian Nowicki <adrian.nowicki@casumo.com>
2020-11-23 16:08:46 +0100

    improvement(profileicon): it now displays level progression and actual player's belt colour
    Also applied opacity=0.75 to top bar, added AdventurerContext and refactored two components to use
    it. Added AdventurerContext to GamePage stack.

--- src/components/GamePageHeader/GamePageHeader.js
+++ src/components/GamePageHeader/GamePageHeader.js
@@ -23,12 +23,12 @@ export const GamePageHeader = () => {
 
   return (
     <Flex align="center" spacing="none" className="u-padding--md@desktop">
-      <Flex.Item className="c-game-page-header__sumoticon-container">
+      <Flex.Item className="c-game-page-header__sumoticon-container t-background-grey-90 t-opacity-background--75">
         <ProfileIconWithDrawer />
       </Flex.Item>
       <Flex.Block>
         <Flex
-          className="u-position-relative u-zindex--content-overlay t-background-grey-90 t-border-r@desktop u-padding-y@desktop u-padding-left--lg@desktop"
+          className="u-position-relative u-zindex--content-overlay t-background-grey-90 t-opacity-background--75 t-border-r@desktop u-padding-y@desktop u-padding-left--lg@desktop"
           align="center"
           spacing="none"
         >
