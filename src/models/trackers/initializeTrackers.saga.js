6c2e117530e6c8e415b0ab570c09c46f1c279e64
Christian Ciantar <christian.ciantar@casumo.com>
2020-12-14 16:54:59 +0100

    further cleanup

--- src/models/trackers/initializeTrackers.saga.js
+++ src/models/trackers/initializeTrackers.saga.js
@@ -1,21 +1,15 @@
 // @flow
 import { call, select } from "redux-saga/effects";
-import {
-  commonContextSelector,
-  playerIdSelector,
-  // playerReferrerInfoSelector,
-} from "Models/handshake";
+import { commonContextSelector, playerIdSelector } from "Models/handshake";
 import { testSubjectIDSelector } from "Models/ABTesting";
 import { init, type TSharedEventConfig } from "Components/GoogleTagManager";
 import { waitForSelector, getAppSubType } from "Utils";
 
 export function* initializeTrackers(action: any): any {
-  // yield call(waitForSelector, playerReferrerInfoSelector);
   yield call(waitForSelector, testSubjectIDSelector);
   yield call(waitForSelector, playerIdSelector);
   yield call(waitForSelector, commonContextSelector);
 
-  // const referrerInfo = yield select(playerReferrerInfoSelector);
   const testSubjectId = yield select(testSubjectIDSelector);
   const userId = yield select(playerIdSelector);
   const { targetDevice: appType, countryCodeByIp } = yield select(
@@ -27,8 +21,6 @@ export function* initializeTrackers(action: any): any {
   const eventConfig: TSharedEventConfig = {
     userId,
     testSubjectId,
-    // affiliateId: referrerInfo.metadata.affId() || "",
-    // btag: referrerInfo.metadata.bannerId() || "",
     userStatus: userId ? "Logged-In" : "Logged-Out",
   };
 
