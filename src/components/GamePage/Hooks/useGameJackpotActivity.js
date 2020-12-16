fdbebdbaddb975edc0f17d2f96ce351405dfe8ec
IVB <ian.baldacchino@casumo.com>
2020-12-16 11:45:59 +0100

    Update: TCAS-463 ommitted async await inside useGameJackpotActivity although useEffect still doesn't suffice hence useMemo is still required as to not have balance updates leak to the ui

--- src/components/GamePage/Hooks/useGameJackpotActivity.js
+++ src/components/GamePage/Hooks/useGameJackpotActivity.js
@@ -3,28 +3,24 @@ import { WALLET_BALANCE_DELAY_DURATION } from "Models/playing/playing.constants"
 import { useGameWalletActivity } from "../Hooks/useGameWalletActivity";
 
 export const useGameJackpotActivity = () => {
-  const [gameJackpotBusy, setGameJackpotBusy] = useState(false);
   const awardedBonus = useGameWalletActivity();
+  const [gameJackpotBusy, setGameJackpotBusy] = useState(awardedBonus);
   const jackpotDelayTimeoutRef = useRef();
 
   useMemo(() => {
     if (awardedBonus) {
       setGameJackpotBusy(true);
-      async function delayed() {
-        // Timeout required in cases like blueribbon where the wallet balance update event is received before the notificationAdded event
-        await new Promise(resolve => {
-          if (jackpotDelayTimeoutRef.current) {
-            clearTimeout(jackpotDelayTimeoutRef.current);
-          }
-          // eslint-disable-next-line fp/no-mutation
-          jackpotDelayTimeoutRef.current = setTimeout(() => {
-            setGameJackpotBusy(false);
-            resolve();
-          }, WALLET_BALANCE_DELAY_DURATION);
-        });
-      }
-      delayed();
+      // Timeout required in cases like blueribbon where the wallet balance update event is received before the notificationAdded event
+      clearTimeout(jackpotDelayTimeoutRef.current);
+      // eslint-disable-next-line fp/no-mutation
+      jackpotDelayTimeoutRef.current = setTimeout(() => {
+        setGameJackpotBusy(false);
+      }, WALLET_BALANCE_DELAY_DURATION);
     }
+
+    return () => {
+      clearTimeout(jackpotDelayTimeoutRef.current);
+    };
   }, [awardedBonus]);
 
   return gameJackpotBusy;
