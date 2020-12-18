b5dae01ca0d1b56d9d07a198270705e54497e165
lukKowalski <lukasz.kowalski@casumo.com>
2020-12-18 11:04:38 +0100

    removed one unnecessary story

--- src/components/BlueRibbon/BlueRibbon.stories.js
+++ src/components/BlueRibbon/BlueRibbon.stories.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { BlueRibbonAnimation } from "./BlueRibbon";
-import { CoinContainer } from "./Coins";
 import { CounterScreen } from "./CounterScreen";
 
 const stories = storiesOf("BlueRibbonWinAnimation", module).addParameters({
@@ -21,32 +20,6 @@ const t = {
   jackpot_win_title: "You won {{type}} jackpot!",
   continue_button: "Continue",
 };
-
-const coin = {
-  id: "Frame1",
-  order: 1,
-  left: 0,
-  top: 0,
-  group: "big",
-  width: 92,
-  height: 88,
-};
-
-const CoinWrapper = () => {
-  return (
-    <div className="u-width--full u-height--full">
-      <div className="c-coins-container">
-        <CoinContainer
-          definition={coin}
-          isVisible={true}
-          isHighlighted={true}
-        />
-      </div>
-    </div>
-  );
-};
-
-stories.add("Single coin", () => <CoinWrapper />);
 
 stories.add("Counter screen", () => {
   return (
