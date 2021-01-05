071f6f2e77b4ab8adbd83d95161d9ab2c7082e49
Michal Mokijewski <hello@naf.gl>
2021-01-05 14:22:49 +0100

    Align modals with designs
    - change background to black
    - add rounded corners

--- src/components/RSModal/RSModalBase.js
+++ src/components/RSModal/RSModalBase.js
@@ -1,7 +1,9 @@
 // @flow
 import * as React from "react";
 import ReactModal from "react-modal";
+import classNames from "classnames";
 import { useJurisdiction } from "Utils/hooks";
+import { isMobile } from "Components/ResponsiveLayout";
 import "./rsmodals.scss";
 
 const CLOSING_ANIMATION_LENGTH_MS = 150;
@@ -23,7 +25,12 @@ export function ModalBase({
     <ReactModal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      className="t-background-white o-flex--vertical t-border-r c-rsmodal"
+      className={classNames(
+        "t-background-white o-flex--vertical c-rsmodal u-overflow--hidden",
+        {
+          "t-border-r--md": !isMobile(),
+        }
+      )}
       overlayClassName={`c-rsmodal__overlay c-rsmodal__overlay--${jurisdiction}`}
       closeTimeoutMS={CLOSING_ANIMATION_LENGTH_MS}
       shouldCloseOnOverlayClick={!mustAccept}
