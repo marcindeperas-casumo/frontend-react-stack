7a3063ee315baeb83e488c6317808eaf4cd26fe9
IVB <ian.baldacchino@casumo.com>
2021-01-05 10:40:29 +0100

    Update: TCAS-779 fixes tests and lint issues

--- src/components/GameTile/GameTileContainer.js
+++ src/components/GameTile/GameTileContainer.js
@@ -4,14 +4,17 @@ import { useTranslations } from "Utils/hooks";
 import { CMS_SLUGS as CMS_SLUG } from "../GameListPage/GameList.constants";
 import { GameTile } from "./GameTile";
 
+// eslint-disable-next-line flowtype/no-types-missing-file-annotation
 export type Props = {
-  className?: string,
+  className?: ?string,
   game: A.GameTile_Game,
-  imgixOpts?: Object,
-  ratio?: string,
+  imgixOpts?: ?Object,
+  ratio?: ?string,
 };
 
 export const GameTileContainer = props => {
-  const t = useTranslations("new-game-browser.top-nav");
+  const t =
+    useTranslations(`${CMS_SLUG.LOBBY}.${CMS_SLUG.GAMES_LIST}`) |
+    { play_button_text_game_tile: "Play" };
   return <GameTile t={t} {...props} />;
 };
