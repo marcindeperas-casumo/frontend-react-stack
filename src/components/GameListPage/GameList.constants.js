94d53167c5bc9d7a3ba75925cd21aa48d519db8b
IVB <ian.baldacchino@casumo.com>
2021-01-06 13:37:28 +0100

    Update: TCAS-779 fixes for translations not showing on game tile play button

--- src/components/GameListPage/GameList.constants.js
+++ src/components/GameListPage/GameList.constants.js
@@ -1,5 +1,5 @@
 export const CMS_SLUGS = {
   LOBBY: "new-game-browser",
-  GAMES_LIST: "top-nav", // Todo to replace with "games-list" already created on WP just returns null,
+  GAMES_LIST: "top-nav",
   PLAY_BUTTON_TEXT_GAME_TILE: "play_button_text_game_tile",
 };
--- src/components/GameTile/GameTileContainer.js
+++ src/components/GameTile/GameTileContainer.js
@@ -1,7 +1,7 @@
 // @Flow
 import React from "react";
 import { useTranslations } from "Utils/hooks";
-import { CMS_SLUGS as CMS_SLUG } from "../GameListPage/GameList.constants";
+import { CMS_SLUGS } from "../GameListPage/GameList.constants";
 import { GameTile } from "./GameTile";
 
 // eslint-disable-next-line flowtype/no-types-missing-file-annotation
@@ -13,8 +13,6 @@ export type Props = {
 };
 
 export const GameTileContainer = props => {
-  const t =
-    useTranslations(`${CMS_SLUG.LOBBY}.${CMS_SLUG.GAMES_LIST}`) |
-    { play_button_text_game_tile: "Play" };
+  const t = useTranslations(`${CMS_SLUGS.LOBBY}.${CMS_SLUGS.GAMES_LIST}`);
   return <GameTile t={t} {...props} />;
 };
