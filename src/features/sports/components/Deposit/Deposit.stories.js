8aa6b83e86eaea216b5b38aa5c5ff9685b34cefb
Michał Ignaszewski <michal.ignaszewski@casumo.com>
2021-02-03 11:39:28 +0100

    TSPO-730, TSPO-612 story fix

--- src/features/sports/components/Deposit/Deposit.stories.js
+++ src/features/sports/components/Deposit/Deposit.stories.js
@@ -1,20 +1,25 @@
 // @flow
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { viewports } from "Storybook/viewports";
 import defaultState from "Models/__mocks__/state.mock";
 import MockStore from "Components/MockStore";
 import { Deposit } from "./Deposit";
 
 const stories = storiesOf("Sports/Deposit", module);
 
-stories.add("Deposit default", () => (
-  <MockStore state={defaultState}>
-    <Deposit
-      hasDeposited={true}
-      balance={123456}
-      bonus={666}
-      locale="en-IN"
-      currency="INR"
-    />
-  </MockStore>
-));
+stories.add(
+  "Deposit default",
+  () => (
+    <MockStore state={defaultState}>
+      <Deposit
+        hasDeposited={true}
+        balance={123456}
+        bonus={666}
+        locale="en-IN"
+        currency="INR"
+      />
+    </MockStore>
+  ),
+  viewports.mobile
+);
