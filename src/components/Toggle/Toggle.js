184a0238f349fd4894fdf3fb810db6b3253de42c
Mariusz <mariusz.klinger@casumo.com>
2021-02-05 14:30:53 +0100

    1st

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,44 +1,53 @@
 // @flow
-import React, { PureComponent } from "react";
-import Flex from "@casumo/cmp-flex";
+import React from "react";
+// import div from "@casumo/cmp-flex";
+import Text from "@casumo/cmp-text";
 import { Checkbox } from "Components/Checkbox/Checkbox";
 import "./Toggle.scss";
 
 type Props = {
+  labelOn?: string,
+  labelOff?: string,
   checked?: boolean,
   onChange: (active: boolean) => void,
 };
 
-const Unchecked = () => (
-  <Flex
-    justify="start"
-    className="c-toggle u-cursor-pointer t-border-r--pill u-overflow--hidden t-background-grey-5 t-color-white"
-  >
-    <svg width="28" viewBox="0 0 28 28" className="c-toggle-circle--inactive">
-      <circle cx="14" cy="14" r="14" fill="currentColor" />
-    </svg>
-  </Flex>
+type UncheckedProps = {
+  label?: string,
+};
+
+type CheckedProps = {
+  label?: string,
+};
+
+const Unchecked = ({ label }: UncheckedProps) => (
+  <div className="c-toggle u-cursor-pointer t-border-r--pill u-overflow--hidden t-background-grey-5 t-color-white">
+    <div className="c-toggle-circle--inactive">
+      <svg width="28" viewBox="0 0 28 28">
+        <circle cx="14" cy="14" r="12" fill="currentColor" />
+      </svg>
+    </div>
+    <Text className="c-toggle__label">{label}</Text>
+  </div>
 );
 
-const Checked = () => (
-  <Flex
-    justify="start"
-    className="c-toggle u-cursor-pointer t-border-r--pill u-overflow--hidden t-background-purple-60 t-color-white"
-  >
-    <svg width="28" viewBox="0 0 28 28" className="c-toggle-circle--active">
-      <circle cx="14" cy="14" r="14" fill="currentColor" />
-    </svg>
-  </Flex>
+const Checked = ({ label }: CheckedProps) => (
+  <div className="c-toggle u-cursor-pointer t-border-r--pill u-overflow--hidden t-background-purple-60 t-color-white">
+    <div className="c-toggle-circle--active">
+      <svg width="28" viewBox="0 0 28 28">
+        <circle cx="14" cy="14" r="12" fill="currentColor" />
+      </svg>
+    </div>
+    <Text className="c-toggle__label">{label}</Text>
+  </div>
 );
 
-export class Toggle extends PureComponent<Props> {
-  render() {
-    return (
-      <Checkbox
-        {...this.props}
-        renderChecked={Checked}
-        renderUnchecked={Unchecked}
-      />
-    );
-  }
+export function Toggle(props: Props) {
+  return (
+    <Checkbox
+      {...props}
+      renderChecked={() => <Checked label={props.labelOn} />}
+      renderUnchecked={() => <Unchecked label={props.labelOff} />}
+    />
+  );
 }
--- src/components/Toggle/Toggle.stories.js
+++ src/components/Toggle/Toggle.stories.js
@@ -10,8 +10,25 @@ stories.add("Default", () => {
 
   return (
     <>
+      <h2>Basic usage:</h2>
       <Toggle checked={checked} onChange={value => setChecked(value)} />
       <p>I am {checked ? "checked" : "unchecked"}</p>
+
+      <h2>Short labels: </h2>
+      <Toggle
+        labelOn="ON"
+        labelOff="OFF"
+        checked={checked}
+        onChange={value => setChecked(value)}
+      />
+
+      <h2>Long label: </h2>
+      <Toggle
+        labelOn="That toggle is really ON!"
+        labelOff="That one is for sure OFF now!"
+        checked={checked}
+        onChange={value => setChecked(value)}
+      />
     </>
   );
 });
