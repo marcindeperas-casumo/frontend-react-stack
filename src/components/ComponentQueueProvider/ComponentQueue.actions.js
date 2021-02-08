e86b34408d5298143f4b6ffbb809baab0a8c3885
Christian Ciantar <christian.ciantar@casumo.com>
2021-02-08 17:02:04 +0100

    remove unused typings

--- src/components/ComponentQueueProvider/ComponentQueue.actions.js
+++ src/components/ComponentQueueProvider/ComponentQueue.actions.js
@@ -6,30 +6,3 @@ export const ACTION_TYPES = {
   UNSHIFT: "[QUEUE] UNSHIFT",
   CLEAR: "[QUEUE] CLEAR",
 };
-
-// TODO: Define payload typings
-type TPushAction = {
-  type: ACTION_TYPES.PUSH,
-  payload: {},
-};
-// TODO: Define payload typings
-type TPopAction = {
-  type: ACTION_TYPES.POP,
-  payload: {},
-};
-// TODO: Define payload typings
-type TShiftAction = {
-  type: ACTION_TYPES.SHIFT,
-  payload: {},
-};
-// TODO: Define payload typings
-type TUnshiftAction = {
-  type: ACTION_TYPES.UNSHIFT,
-  payload: {},
-};
-
-export type TQueueAction =
-  | TPushAction
-  | TPopAction
-  | TShiftAction
-  | TUnshiftAction;
