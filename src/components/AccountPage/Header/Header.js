f5bfbd019287952674beaac32c7b9a5d771df770
Adrian Nowicki <adrian.nowicki@casumo.com>
2021-02-09 16:26:28 +0100

    improvement(accountpage/header): removed gridArea prop for now

--- src/components/AccountPage/Header/Header.js
+++ src/components/AccountPage/Header/Header.js
@@ -7,17 +7,15 @@ import { getMediaQuery, desktopBreakpoint } from "Components/ResponsiveLayout";
 
 type Props = {
   children: React.Node,
-  gridArea: string,
 };
 
-export function Header({ children, gridArea }: Props) {
+export function Header({ children }: Props) {
   const isDesktop = useMedia(getMediaQuery(desktopBreakpoint));
 
   return (
     <Text
       tag="h3"
       size={isDesktop ? "md" : "default"}
-      style={{ gridArea }}
       className={cx(
         "u-font-weight-bold",
         "u-padding-top--lg u-padding-top--xlg@desktop u-padding-bottom--md"
