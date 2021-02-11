dc167d06eb0bf56f7470ee5b0269d802e1f49ee6
Michal Mokijewski <hello@naf.gl>
2021-02-11 13:31:52 +0100

    Refactor leaderboards to use new hook for data

--- src/components/ReelRacesDrawerWidget/ReelRacesDrawerWidget.test.js
+++ src/components/ReelRacesDrawerWidget/ReelRacesDrawerWidget.test.js
@@ -1,7 +1,21 @@
 // @flow
 import React from "react";
 import { mount } from "enzyme";
+import MockStore from "Components/MockStore";
 import { ReelRacesDrawerWidget } from "./ReelRacesDrawerWidget";
+
+const getCurrentRace = (value = null) => ({
+  position: value || 1,
+  remainingSpins: value || 99,
+  points: value || 42,
+  startTime: Date.now() - 10000,
+  endTime: Date.now() + 3000,
+  boosters: {
+    winsInARow: 0,
+    bigWins: 0,
+    megaWins: 0,
+  },
+});
 
 const props = {
   spinsLeft: "329",
@@ -16,10 +30,31 @@ const props = {
     reel_races_drawer_spins: "spins",
     reel_races_drawer_full_leaderboard: "full leaderbaord",
   },
+  currentRace: getCurrentRace(),
 };
+const playerId = "a1";
 
 describe("ReelRaceDrawer", () => {
-  const rendered = mount(<ReelRacesDrawerWidget {...props} />);
+  const rendered = mount(
+    <MockStore
+      state={{
+        handshake: {
+          app: {
+            "common/composition/session": {
+              id: playerId,
+            },
+          },
+        },
+        reelRaces: {
+          leaderboard: {
+            [playerId]: getCurrentRace(),
+          },
+        },
+      }}
+    >
+      <ReelRacesDrawerWidget {...props} />
+    </MockStore>
+  );
 
   test("should contain two checkered flags", () => {
     expect(rendered.find(".c-checkered-flag").length).toBe(2);
