e6c5d594c21eb2f20c2888a6f1e3828de9ce6716
Christian Ciantar <christian.ciantar@casumo.com>
2021-02-15 18:24:19 +0100

    feat(navbar): add component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,65 @@
+// @flow
+import * as React from "react";
+import cx from "classnames";
+import { Link } from "@reach/router";
+import Flex from "@casumo/cmp-flex";
+import Text from "@casumo/cmp-text";
+import { isMobile } from "Components/ResponsiveLayout";
+
+type NavbarItemProps = {
+  Icon: React.StatelessFunctionalComponent<any>,
+  text: ?string,
+  to: string,
+  active?: boolean,
+};
+
+type Props = {
+  items: Array<NavbarItemProps>,
+  sticky?: boolean,
+};
+
+const NavItem = ({ Icon, text, to, active }: NavbarItemProps) => {
+  const direction = isMobile() ? "vertical" : "horizontal";
+  return (
+    <Flex.Block>
+      <Link to={to}>
+        <Flex
+          direction={direction}
+          align="center"
+          justify="center"
+          className={cx("u-padding--sm", {
+            "t-color-purple-60": active,
+            "t-color-grey-70": !active,
+          })}
+        >
+          <Icon
+            className={cx({
+              "u-padding-right": !isMobile(),
+              "u-padding-bottom--sm": isMobile(),
+            })}
+            size="md"
+          />
+          <Text className="u-font-weight-bold u-margin--none">{text}</Text>
+        </Flex>
+      </Link>
+    </Flex.Block>
+  );
+};
+
+export const Navbar = ({ items, sticky = false }: Props) => {
+  return sticky ? (
+    <Flex className={cx({ "o-position-sticky": true })}>
+      <Flex spacing="lg">
+        {items.map(x => (
+          <NavItem key={x.to} {...x} />
+        ))}
+      </Flex>
+    </Flex>
+  ) : (
+    <Flex spacing="lg">
+      {items.map(x => (
+        <NavItem key={x.to} {...x} />
+      ))}
+    </Flex>
+  );
+};
---
+++ src/components/Navbar/Navbar.stories.js
@@ -0,0 +1,32 @@
+// @flow
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import {
+  CasinoTabSelectedIcon,
+  CasumoProfileIcon,
+  FootballIcon,
+} from "@casumo/cmp-icons";
+import { Navbar } from "Components/Navbar";
+const stories = storiesOf("Navbar", module);
+const items = [
+  {
+    Icon: CasinoTabSelectedIcon,
+    text: "Casino",
+    to: "/",
+    active: true,
+  },
+  {
+    Icon: FootballIcon,
+    text: "Sports",
+    to: "/",
+    active: false,
+  },
+  {
+    Icon: CasumoProfileIcon,
+    text: "Samuel L. Jackson",
+    to: "/",
+    active: false,
+  },
+];
+
+stories.add("Default", () => <Navbar items={items} sticky />);
