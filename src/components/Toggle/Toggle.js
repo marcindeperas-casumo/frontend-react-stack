5cc60df670c8656f1a055f224a6e1bb8018c8b58
Mariusz <mariusz.klinger@casumo.com>
2021-02-16 10:10:29 +0100

    addressed PR comments

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -21,7 +21,7 @@ type InnerToggleProps = {
 const ToggleInner = ({ labelOn, labelOff, checked }: InnerToggleProps) => (
   <div
     className={cx(
-      "c-toggle t-border--md u-font-weight-bold u-cursor-pointer t-border-r--pill u-overflow--hidden",
+      "c-toggle u-position-relative t-border--md u-font-weight-bold u-cursor-pointer t-border-r--pill u-overflow--hidden",
       {
         "t-background-purple-60 t-color-white t-border-white": checked,
         "t-background-white t-color-grey-5 t-border-grey-5": !checked,
@@ -29,8 +29,8 @@ const ToggleInner = ({ labelOn, labelOff, checked }: InnerToggleProps) => (
     )}
   >
     <div
-      className={cx({
-        "c-toggle__circle--active": checked,
+      className={cx("o-position--absolute o-inset-top--none", {
+        "c-toggle__circle--active ": checked,
         "c-toggle__circle--inactive t-color-grey-9": !checked,
         "t-color-grey-5": !checked,
       })}
@@ -40,9 +40,12 @@ const ToggleInner = ({ labelOn, labelOff, checked }: InnerToggleProps) => (
       </svg>
     </div>
     <Text
-      className={cx("c-toggle__label", {
-        "t-color-grey-90": !checked,
-      })}
+      className={cx(
+        "c-toggle__label u-text-nowrap u-overflow--hidden u-text-overflow--ellipsis",
+        {
+          "t-color-grey-90": !checked,
+        }
+      )}
       size="xs"
     >
       {checked ? labelOn : labelOff}
