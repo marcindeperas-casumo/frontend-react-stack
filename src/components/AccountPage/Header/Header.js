ec0cb756d03d55b42007fb7c9415d8b270b211cf
Adrian Nowicki <adrian.nowicki@casumo.com>
2021-02-17 15:51:10 +0100

    improvement(accountpage/header): enabled responsive font size classes and applied them to Header

--- src/components/AccountPage/Header/Header.js
+++ src/components/AccountPage/Header/Header.js
@@ -1,23 +1,18 @@
 // @flow
 import * as React from "react";
-import { useMedia } from "react-use";
 import cx from "classnames";
 import Text from "@casumo/cmp-text";
-import { getMediaQuery, desktopBreakpoint } from "Components/ResponsiveLayout";
 
 type Props = {
   children: React.Node,
 };
 
 export function Header({ children }: Props) {
-  const isDesktop = useMedia(getMediaQuery(desktopBreakpoint));
-
   return (
     <Text
       tag="h3"
-      size={isDesktop ? "md" : "default"}
       className={cx(
-        "u-font-weight-bold",
+        "u-font-weight-bold u-font-md@desktop",
         "u-padding-top--lg u-padding-top--xlg@desktop u-padding-bottom--md"
       )}
     >
