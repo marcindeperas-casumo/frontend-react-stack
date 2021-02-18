fc6de5c3ab7b98723156e3d6511dedd97f7fe32d
Mariusz <mariusz.klinger@casumo.com>
2021-02-18 09:46:51 +0100

    added stolen Panel

---
+++ src/components/Panel/Panel.js
@@ -0,0 +1,32 @@
+// @flow
+import * as React from "react";
+import cx from "classnames";
+
+export type TProps = {
+  children?: React.Node,
+  className?: string,
+  roundedTop?: boolean,
+  roundedBottom?: boolean,
+};
+
+export function Panel({
+  className,
+  children,
+  roundedTop,
+  roundedBottom,
+}: TProps) {
+  return (
+    <div
+      className={cx(
+        "t-background-white u-padding-x--md u-padding-y--lg",
+        {
+          "t-border-r-top-left--md t-border-r-top-right--md": roundedTop,
+          "t-border-r-bottom-left--md t-border-r-bottom-right--md": roundedBottom,
+        },
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ src/components/Panel/index.js
@@ -0,0 +1,3 @@
+// @flow
+export { Panel } from "./Panel";
+export type { TProps as TPanelProps } from "./Panel";
