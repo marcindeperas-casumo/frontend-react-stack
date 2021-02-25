2bf0e52b9953a1d1db801f7d6adf6e5a2b8af5c6
Christian Ciantar <christian.ciantar@casumo.com>
2021-02-25 16:46:24 +0100

    add correct type

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -5,13 +5,14 @@ import { Link } from "@reach/router";
 import Text from "@casumo/cmp-text";
 import Flex from "@casumo/cmp-flex";
 import { ChevronDownIcon } from "@casumo/cmp-icons";
+import type { iconSizes } from "@casumo/cudl-react-prop-types";
 import { useDropdown } from "./Dropdown.hooks";
 
 import "./Dropdown.scss";
 
 export type TDropdownItemContentProps = {
-  Icon?: React.StatelessFunctionalComponent<any>,
-  DescriptionIcon?: React.StatelessFunctionalComponent<any>,
+  Icon?: React.StatelessFunctionalComponent<{ size: iconSizes }>,
+  DescriptionIcon?: React.StatelessFunctionalComponent<{ size: iconSizes }>,
   label: string,
   description?: string,
 };
@@ -27,7 +28,7 @@ export type TDropdownProps = {
   className?: string,
   children: React.Node,
   triggerLabel?: string,
-  TriggerIcon?: React.StatelessFunctionalComponent<any>,
+  TriggerIcon?: React.StatelessFunctionalComponent<{ size: iconSizes }>,
   triggerClassName?: string,
   showImmediately?: boolean,
   withRoundedCorners?: boolean,
