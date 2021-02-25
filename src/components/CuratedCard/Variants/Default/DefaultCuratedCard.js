cbfb88559ccc69f940ea5a7a57434d1e49476cf8
lukKowalski <lukasz.kowalski@casumo.com>
2021-02-25 15:22:34 +0100

    added default curatedCard mapping, refactoring dir structure for curated cards

---
+++ src/components/CuratedCard/Variants/Default/DefaultCuratedCard.js
@@ -0,0 +1,28 @@
+// @flow
+import * as React from "react";
+import cx from "classnames";
+import { CuratedCardContainer as CuratedCard } from "Components/CuratedCard/CuratedCardContainer";
+import { xPaddingClasses } from "Components/GameListHorizontal/constants";
+
+type Props = {
+  card: string | Array<string>,
+};
+
+// We cannot name the property to "slug" easily here, we have to keep it as "card" as it getting
+// the properties from the CMS and there it is used as "card" in a lot of places.
+export const DefaultCuratedCard = ({ card }: Props) => {
+  const normalizedSlug = Array.isArray(card) ? card[0] : card;
+
+  return (
+    <div
+      className={cx(
+        "u-margin-top--md u-margin-top--none@desktop",
+        xPaddingClasses
+      )}
+    >
+      <div className="o-wrapper u-overflow--hidden t-border-r--md">
+        <CuratedCard slug={normalizedSlug} />
+      </div>
+    </div>
+  );
+};
