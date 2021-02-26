58b4ba3b597220ace9fcf2c84a8d1d2cdc91b52b
chogarcia <choishere@gmail.com>
2021-02-26 14:06:19 +0100

    feat(RRSchedule): unwanted changes

--- src/pages/CasinoPage/CasinoPage.js
+++ src/pages/CasinoPage/CasinoPage.js
@@ -1,7 +1,7 @@
 // @flow
 import React from "react";
 import { Router, Link } from "@reach/router";
-import { ContentWrapper } from "Components/ContentWrapper";
+import { SubNavLayout } from "Layouts/SubNavLayout/index";
 import {
   CasinoSearchPage,
   CasinoListsPage,
@@ -12,22 +12,32 @@ import {
 } from "./subPages";
 
 export const CasinoPage = () => {
+  const subNavLinks = [
+    {
+      to: "/casino/games",
+      text: "Games",
+    },
+    {
+      to: "/casino/reel-races",
+      text: "Races",
+    },
+  ];
+
   return (
     <div>
-      <ContentWrapper>
-        <Link to="search">Search</Link>
-        <Link to="lists">Lists</Link>
-        <Link to="must-drop-jackpots">Must drop jackpots</Link>
-        <Link to="live-casino">Live Casino</Link>
-        <Router>
-          <ReelRacesPage path="/reel-races" />
-          <CasinoSearchPage path="/search" />
-          <CasinoListsPage path="/lists" />
-          <MustDropJackpotsPage path="/must-drop-jackpots" />
-          <LiveCasinoPage path="/live-casino" />
-          <GameProviderGamesPage path="/provider/:provider" />
-        </Router>
-      </ContentWrapper>
+      <SubNavLayout links={subNavLinks} />
+      <Link to="search">Search</Link>
+      <Link to="lists">Lists</Link>
+      <Link to="must-drop-jackpots">Must drop jackpots</Link>
+      <Link to="live-casino">Live Casino</Link>
+      <Router>
+        <ReelRacesPage path="/reel-races" />
+        <CasinoSearchPage path="/search" />
+        <CasinoListsPage path="/lists" />
+        <MustDropJackpotsPage path="/must-drop-jackpots" />
+        <LiveCasinoPage path="/live-casino" />
+        <GameProviderGamesPage path="/provider/:provider" />
+      </Router>
     </div>
   );
 };
