31549cc81c3b8365a10fd9f75b6ad9cc09c45cdb
lukKowalski <lukasz.kowalski@casumo.com>
2021-02-26 08:13:35 +0100

    fixed type names

--- src/components/CuratedCard/Variants/Default/DefaultCuratedCard.js
+++ src/components/CuratedCard/Variants/Default/DefaultCuratedCard.js
@@ -4,13 +4,13 @@ import cx from "classnames";
 import { CuratedCardContainer as CuratedCard } from "Components/CuratedCard/CuratedCardContainer";
 import { xPaddingClasses } from "Components/GameListHorizontal/constants";
 
-type Props = {
+type TProps = {
   card: string | Array<string>,
 };
 
 // We cannot name the property to "slug" easily here, we have to keep it as "card" as it getting
 // the properties from the CMS and there it is used as "card" in a lot of places.
-export const DefaultCuratedCard = ({ card }: Props) => {
+export const DefaultCuratedCard = ({ card }: TProps) => {
   const normalizedSlug = Array.isArray(card) ? card[0] : card;
 
   return (
