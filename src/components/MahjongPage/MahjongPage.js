2178fd8de2e6f895161d9ba8e6b6d0051ce44a49
lukKowalski <lukasz.kowalski@casumo.com>
2021-02-26 08:10:49 +0100

    removed unnecesary lazy loading

--- src/components/MahjongPage/MahjongPage.js
+++ src/components/MahjongPage/MahjongPage.js
@@ -3,12 +3,6 @@ import { WaitForHostElement } from "Components/WaitForHostElement";
 import Portal from "Components/Portal";
 import { ComponentBuilder } from "Components/ComponentBuilder";
 
-// const ComponentBuilder = React.lazy(() =>
-//   import("Components/ComponentBuilder").then(module => ({
-//     default: module.ComponentBuilder,
-//   }))
-// );
-
 export const MahjongPage = () => (
   <WaitForHostElement hostElementId="react-host-mahjong">
     <Portal hostElementId="react-host-mahjong">
