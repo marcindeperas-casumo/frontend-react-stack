30c79c57bb3cf2d62849682154bf1d206fe4bcf3
lukKowalski <lukasz.kowalski@casumo.com>
2021-03-03 15:33:33 +0100

    making code nicer :)

--- src/components/GameLinkButton/GameLinkButton.js
+++ src/components/GameLinkButton/GameLinkButton.js
@@ -14,22 +14,20 @@ type TProps = {
   game: string,
 };
 
-export const GameLinkButton = (props: TProps) => {
-  return (
-    <div
-      className={classNames(
-        "u-text-align-center",
-        topMarginClasses,
-        xPaddingClasses
-      )}
+export const GameLinkButton = (props: TProps) => (
+  <div
+    className={classNames(
+      "u-text-align-center",
+      topMarginClasses,
+      xPaddingClasses
+    )}
+  >
+    <ButtonPrimary
+      className="u-width--full@mobile u-width--full@phablet u-width--1/2@tablet u-width--1/3"
+      onClick={() => launchGame({ slug: props.game })}
     >
-      <ButtonPrimary
-        className="u-width--full@mobile u-width--full@phablet u-width--1/2@tablet u-width--1/3"
-        onClick={() => launchGame({ slug: props.game })}
-      >
-        <span className="u-margin-right">{props.text}</span>
-        <PlayIcon />
-      </ButtonPrimary>
-    </div>
-  );
-};
+      <span className="u-margin-right">{props.text}</span>
+      <PlayIcon />
+    </ButtonPrimary>
+  </div>
+);
