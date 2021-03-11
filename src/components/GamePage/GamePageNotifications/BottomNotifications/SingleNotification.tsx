5d929d036a1d6ac35d8143449885c0cef9820233
lukKowalski <lukasz.kowalski@casumo.com>
2021-03-11 13:11:17 +0100

    replaced classes with cudl ones

--- src/components/GamePage/GamePageNotifications/BottomNotifications/SingleNotification.tsx
+++ src/components/GamePage/GamePageNotifications/BottomNotifications/SingleNotification.tsx
@@ -28,7 +28,7 @@ export const SingleNotification = ({
   return (
     <div
       className={classNames(
-        "t-color-grey-20 u-font-xs u-text-align-center u-position-absolute c-game-page-bottom-notification u-width--full u-padding t-background-grey-80",
+        "t-color-grey-20 u-font-xs u-text-align-center o-position-absolute c-game-page-bottom-notification u-width--full u-padding t-background-grey-80",
         show && "o-inset-bottom--none"
       )}
     >
