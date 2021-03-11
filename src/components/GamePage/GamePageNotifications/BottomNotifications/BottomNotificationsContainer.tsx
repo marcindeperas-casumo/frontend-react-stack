8ef55f20122ce551af256882e2dc99577c7c27da
lukKowalski <lukasz.kowalski@casumo.com>
2021-03-11 12:42:53 +0100

    added return types for useTranslations, connecting message key with value to display

--- src/components/GamePage/GamePageNotifications/BottomNotifications/BottomNotificationsContainer.tsx
+++ src/components/GamePage/GamePageNotifications/BottomNotifications/BottomNotificationsContainer.tsx
@@ -3,15 +3,23 @@ import { useGameModelContext } from "Components/GamePage/Contexts";
 import { useTranslations } from "Utils/hooks";
 import { BottomNotifications } from "./BottomNotifications";
 
+type NotificationsPage = {
+  meta_properties: Array<{ key: string; value: string }>;
+};
+
 export const BottomNotificationsGamePageContainer = () => {
   const { gameProviderModel } = useGameModelContext();
   const gameNotifications = gameProviderModel.onLaunchNotifications || [];
 
-  const t = useTranslations("iframe-solution.game-page-notifications");
-
-  const translatedNotifications = gameNotifications.map(key => key); //find in translations
+  const t = useTranslations<NotificationsPage>(
+    "iframe-solution.game-page-notifications"
+  );
 
   if (gameNotifications.length && t) {
+    const translatedNotifications = gameNotifications.map(
+      key => t.meta_properties.find(item => item.key === key).value
+    );
+
     return <BottomNotifications notifications={translatedNotifications} />;
   }
 
