611d5a175418e3da2f39d48d93d5242f668a227f
Adrian Nowicki <adrian.nowicki@casumo.com>
2021-03-12 10:28:11 +0100

    fix(dgoj depositlimits): form now does not warn if user wants to create new limits

--- src/components/Compliance/DepositLimits/DepositLimitsConfirmations/__mocks__/cms.ts
+++ src/components/Compliance/DepositLimits/DepositLimitsConfirmations/__mocks__/cms.ts
@@ -12,7 +12,7 @@ export default {
   rg_fail_title: "Your answers have indicated some issues.",
   rg_fail_content:
     "For your own safety, keep to your current limits for now. You can try make another adjustment in 7 days time.",
-  rg_required_title: "To activate your increased limits, you must complete a ten-question test.",
+  rg_required_title: "You have to answer 10 quick questions before you can increase or remove the limits.",
   rg_required_content: "This might take a few days. We'll inform you once it's all done.",
   button_back_to_limits: "Back to limits",
   button_answer_questions: "Answer questions",
