b7f664b0743fe77ba779d33020694862ee4bb107
Adrian Nowicki <adrian.nowicki@casumo.com>
2021-03-15 13:29:13 +0100

    feat(dgoj adjustment approved/rejected): added cometd event handler to update UI

---
+++ src/models/playOkay/depositLimits/depositLimits.adjustmentNotificationAdded.saga.ts
@@ -0,0 +1,39 @@
+import { delay } from "redux-saga";
+import { put, PutEffect } from "redux-saga/effects";
+import * as actions from "./depositLimits.actions";
+
+type TStoreAction =
+  | typeof actions.getAllLimits
+  | typeof actions.limitPreadjust
+  | typeof actions.getRemainingLimits
+  | ReturnType<typeof actions.getLimitsHistory>;
+type TAction = {
+  data: {
+    notificationAdded: {
+      type:
+        | "DGOJ_DEPOSIT_LIMIT_ADJUSTMENT_APPROVED"
+        | "DGOJ_DEPOSIT_LIMIT_ADJUSTMENT_REJECTED"
+        | string;
+    };
+  };
+};
+
+export function* adjustmentNotificationAddedSaga(
+  action: TAction
+): Generator<PutEffect<TStoreAction> | Promise<boolean>> {
+  if (
+    ![
+      "DGOJ_DEPOSIT_LIMIT_ADJUSTMENT_APPROVED",
+      "DGOJ_DEPOSIT_LIMIT_ADJUSTMENT_REJECTED",
+    ].includes(action.data.notificationAdded.type)
+  ) {
+    return;
+  }
+
+  yield delay(1000);
+
+  yield put(actions.getAllLimits);
+  yield put(actions.limitPreadjust);
+  yield put(actions.getRemainingLimits);
+  yield put(actions.getLimitsHistory());
+}
