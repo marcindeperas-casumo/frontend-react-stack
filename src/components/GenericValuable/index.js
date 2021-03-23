563c8fb7c7d16a3734cbf42f20d3c2ba460262a9
Mariusz <mariusz.klinger@casumo.com>
2021-03-23 14:44:15 +0100

    removed redundant code

--- src/components/GenericValuable/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export { ValuableDetailsContainer } from "./ValuableDetailsContainer";
-export { ValuableDetailsContainer as default } from "./ValuableDetailsContainer";
-export { ValuableDetailsWithModal } from "./ValuableDetailsWithModal";
