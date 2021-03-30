483a8ab36015085a09f728446b07df1f3da4ef0d
IanVB <ian.baldacchino@casumo.com>
2021-03-30 14:39:00 +0200

    Update: TCAS-1050 wrapping PromotionCardListContainer with PromotionCardListPrefixContainer due to a temporary cms change from slug to slug_2

--- src/components/ComponentBuilder/ComponentBuilderContainer.tsx
+++ src/components/ComponentBuilder/ComponentBuilderContainer.tsx
@@ -2,7 +2,6 @@ import { useQuery, gql } from "@apollo/client";
 import React from "react";
 import { propOr } from "ramda";
 import logger from "Services/logger";
-import { prefixCampaignPromotion } from "./ComponentBuilder.utils";
 import { ComponentBuilderRenderer } from "./ComponentBuilderRenderer";
 
 type Props = {
@@ -21,24 +20,17 @@ export const ComponentBuilderContainer = ({ slug }: Props) => {
   const id = `root:${slug}:fields.${cmsField}`;
   const variables = { id };
   const { data, loading } = useQuery(QUERY, { variables });
-  const componentDefinitionJSONUnformatted = propOr(
-    null,
-    "componentDefinitionJSON",
-    data
-  );
+  const componentDefinitionJSON = propOr(null, "componentDefinitionJSON", data);
 
-  if (loading && !componentDefinitionJSONUnformatted) {
+  if (loading && !componentDefinitionJSON) {
     return null;
   }
-  // Below is a prefixing fix for PROMOTION_CARDS_HORIZONTAL since https://cms.casumo.com/wp-admin/post.php?post=208661&action=edit slug_2 was created to not break existing functionality for mahjong
-  const componentDefinitionJSON = prefixCampaignPromotion(
-    // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '<T, V>(val: T) => V' is not assi... Remove this comment to see the full error message
-    componentDefinitionJSONUnformatted
-  );
+
   try {
     return (
       <ComponentBuilderRenderer
-        componentDefinitions={componentDefinitionJSON}
+        // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '<T, V>(val: T) => V' is not assi... Remove this comment to see the full error message
+        componentDefinitions={JSON.parse(componentDefinitionJSON)}
       />
     );
   } catch (e) {
