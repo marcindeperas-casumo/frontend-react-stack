d5f3c507628ce49527f9f02810136ce1fdbcac5d
chogarcia <choishere@gmail.com>
2021-04-14 11:43:39 +0200

    feat(sports): upgrade nav

---
+++ src/features/sports/components/Virtuals/VirtualsPromoPage.tsx
@@ -0,0 +1,6 @@
+import { ComponentBuilder } from "Components/ComponentBuilder";
+import React from "react";
+
+export const VirtualsPromoPage: React.FC<{}> = () => {
+  return <ComponentBuilder slug="promotions.virtual-sports" />;
+};
