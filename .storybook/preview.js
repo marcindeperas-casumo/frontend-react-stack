79dc349a2f629c42cfda65bc02b689a0abc7cc50
Dave Calleja <dave.calleja@casumo.com>
2021-04-19 09:21:58 +0200

    Added global decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "../src/styles/index.scss";
 import "./stories.scss";
 
@@ -10,3 +11,22 @@ export const parameters = {
     },
   },
 };
+
+const globalDecorator = Story => (
+  <>
+    <div id="portal-host-element" />
+    <div className="o-wrapper u-padding">
+      <Story />
+    </div>
+  </>
+);
+
+export const decorators = [
+  (Story, anotherProp, other) => {
+    if (Story.parameters?.noGlobalDecorator) {
+      return <Story />;
+    }
+
+    return globalDecorator(Story);
+  },
+];
