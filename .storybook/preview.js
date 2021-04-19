c32c8d120296c4bc699dfa6b1298fc03ee3cbed0
Dave Calleja <dave.calleja@casumo.com>
2021-04-19 09:49:25 +0200

    Added casumo theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,30 @@
 import React from "react";
+import casumoTheme, { color } from "./casumoTheme";
 import "../src/styles/index.scss";
 import "./stories.scss";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    values: [
+      { name: "light", value: "#ffffff", default: true },
+      { name: "colorful", value: "#0085c1" },
+      { name: "dark", value: "#444f4f" },
+    ],
+  },
+  options: {
+    theme: casumoTheme,
+  },
+  info: {
+    disable: navigator.userAgent.match(/Chromatic/),
+    styles: {
+      button: {
+        base: {
+          background: color.secondary,
+        },
+      },
+    },
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
