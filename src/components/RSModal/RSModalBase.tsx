320aedf93d82e076132a8be10f790161f974d4f2
Michał Ignaszewski <michal.ignaszewski@casumo.com>
2021-04-21 17:36:36 +0200

    adding TandC to sports

--- src/components/RSModal/RSModalBase.tsx
+++ src/components/RSModal/RSModalBase.tsx
@@ -28,7 +28,7 @@ export function ModalBase({
       // @ts-expect-error ts-migrate(2769) FIXME: No overload matches this call.
       onRequestClose={onRequestClose}
       className={classNames(
-        "t-background-white o-flex--vertical c-rsmodal u-overflow--hidden",
+        "t-background-white o-flex--vertical c-rsmodal",
         {
           "t-border-r--md": !isMobile(),
           "c-rsmodal__wide": isWide,
