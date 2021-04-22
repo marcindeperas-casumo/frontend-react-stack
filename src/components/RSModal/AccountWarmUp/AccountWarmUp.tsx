96db06ff59b0d6ad892198adf828db6b360f9e8d
Christian Ciantar <christian.ciantar@casumo.com>
2021-04-22 13:26:54 +0200

    refctor button config + styles

--- src/components/RSModal/AccountWarmUp/AccountWarmUp.tsx
+++ src/components/RSModal/AccountWarmUp/AccountWarmUp.tsx
@@ -3,7 +3,6 @@ import cx from "classnames";
 import Flex from "@casumo/cmp-flex";
 import Text from "@casumo/cmp-text";
 import Modal from "@casumo/cmp-modal";
-import { ButtonPrimary } from "@casumo/cmp-button";
 import { LockIcon, PlayIcon, TimeLockedIcon } from "@casumo/cmp-icons";
 import * as A from "Types/apollo";
 import { stringToHTML } from "Utils";
@@ -46,120 +45,110 @@ export const AccountWarmUp = ({
     </Flex.Item>
   );
 
-  const PlayGameButton = game => (
-    <ButtonPrimary
-      size="sm"
-      className="u-margin-y--xlg u-padding-y--md u-padding-x--lg u-width--full"
-      onClick={() => launchGame({ slug: game.slug })}
-    >
-      <span className="u-margin-right--md">
-        {config.content?.play_button_text}
-      </span>
-      <PlayIcon size="sm" className="c-account-warm-up__button-icon" />
-    </ButtonPrimary>
-  );
+  const playButtonConfig = {
+    text: config.content?.play_button_text,
+    icon: PlayIcon,
+    action: () => {
+      acceptModal();
+      launchGame({ slug: config.reelRace.game.slug });
+    },
+  };
 
-  const DismissButton = () => (
-    <ButtonPrimary
-      size="sm"
-      className="u-margin-y--xlg u-padding-y--md u-padding-x--lg u-width--full"
-      onClick={acceptModal}
-    >
-      <span className="u-margin-left">
-        {config.content?.dismiss_button_text}
-      </span>
-    </ButtonPrimary>
-  );
+  const dismissButtonConfig = {
+    text: config.content?.dismiss_button_text,
+    action: () => {
+      acceptModal();
+    },
+  };
 
   return (
     <Modal
       className={cx(rootClassName, classNames)}
       closeIcon={{ action: closeModal }}
-      bigTitle={config.content?.title}
+      primaryButton={config.reelRace ? playButtonConfig : dismissButtonConfig}
     >
-      <Text
-        className="u-padding-y-md u-text-align-left"
-        dangerouslySetInnerHTML={stringToHTML(config.content?.content)}
-      ></Text>
-      <Flex direction="vertical" className="u-margin-left--xlg">
-        {/* <div className="o-flex--horizontal">
-          <Text className="u-padding u-text-align-left o-flex__block">
-            {config.content?.verification_status_label}
-          </Text>
-        </div> */}
-        <Flex className={cx(`${rootClassName}__time-remaining-title`)}>
-          <div className="t-border-r--circle t-background-grey-5 u-height--lg u-margin-top u-padding--sm">
-            <TimeLockedIcon />
-          </div>
-          <Text className="u-padding u-text-align-left o-flex__block">
-            {config.content?.days_left_title}
-          </Text>
-        </Flex>
-
-        {timeRemaining && (
-          <Flex
-            spacing="md"
-            justify="center"
-            className={cx(
-              `${rootClassName}__time-remaining`,
-              "u-padding--md t-border-r--md t-background-grey-5"
-            )}
-          >
-            <Flex.Item>
-              <Flex direction="vertical" align="center" justify="center">
-                <Text size="md" className="u-font-weight-bold">
-                  {timeRemaining.days}
-                </Text>
-                <Text size="xs" className="t-color-grey-50">
-                  Days
-                </Text>
-              </Flex>
-            </Flex.Item>
-            <Flex.Item>:</Flex.Item>
-            <Flex.Item>
-              <Flex direction="vertical" align="center" justify="center">
-                <Text size="md" className="u-font-weight-bold">
-                  {timeRemaining.hours}
-                </Text>
-                <Text size="xs" className="t-color-grey-50">
-                  Hours
-                </Text>
-              </Flex>
-            </Flex.Item>
-            <Flex.Item>:</Flex.Item>
-            <Flex.Item>
-              <Flex direction="vertical" align="center" justify="center">
-                <Text size="md" className="u-font-weight-bold">
-                  {timeRemaining.minutes}
-                </Text>
-                <Text size="xs" className="t-color-grey-50">
-                  Minutes
-                </Text>
-              </Flex>
-            </Flex.Item>
-            <Text
-              size="xs"
-              tag="div"
-              className={cx(
-                `${rootClassName}__days-left`,
-                "u-padding--sm u-font-weight-bold t-background-grey-20 t-border-r--sm"
-              )}
-            >
-              {config.content?.days_left_label}
-              <LockIcon
-                size="sm"
-                className="u-margin-left--sm"
-                style={{ width: "10px", height: "11px" }}
-              />
+      <Flex
+        direction="vertical"
+        spacing="md"
+        className={cx(`${rootClassName}__content`, "u-padding-y-md")}
+      >
+        <Text tag="h3" className="u-padding u-font-lg u-margin-top--lg">
+          {config.content?.title}
+        </Text>
+        <Text
+          className="u-padding u-text-align-left"
+          dangerouslySetInnerHTML={stringToHTML(config.content?.content)}
+        ></Text>
+        <Flex direction="vertical" className="u-margin-left--3xlg">
+          <Flex className={cx(`${rootClassName}__time-remaining-title`)}>
+            <div className="t-border-r--circle t-background-grey-5 u-height--xlg u-margin-top u-padding--sm">
+              <TimeLockedIcon className="t-color-grey-50" />
+            </div>
+            <Text className="u-padding u-text-align-left o-flex__block">
+              {config.content?.days_left_title}
             </Text>
           </Flex>
-        )}
+
+          {timeRemaining && (
+            <Flex
+              spacing="md"
+              justify="center"
+              className={cx(
+                `${rootClassName}__time-remaining`,
+                "u-padding--md t-border-r--md t-background-grey-5"
+              )}
+            >
+              <Flex.Item>
+                <Flex direction="vertical" align="center" justify="center">
+                  <Text size="md" className="u-font-weight-bold">
+                    {timeRemaining.days}
+                  </Text>
+                  <Text size="xs" className="t-color-grey-50">
+                    Days
+                  </Text>
+                </Flex>
+              </Flex.Item>
+              <Flex.Item>:</Flex.Item>
+              <Flex.Item>
+                <Flex direction="vertical" align="center" justify="center">
+                  <Text size="md" className="u-font-weight-bold">
+                    {timeRemaining.hours}
+                  </Text>
+                  <Text size="xs" className="t-color-grey-50">
+                    Hours
+                  </Text>
+                </Flex>
+              </Flex.Item>
+              <Flex.Item>:</Flex.Item>
+              <Flex.Item>
+                <Flex direction="vertical" align="center" justify="center">
+                  <Text size="md" className="u-font-weight-bold">
+                    {timeRemaining.minutes}
+                  </Text>
+                  <Text size="xs" className="t-color-grey-50">
+                    Minutes
+                  </Text>
+                </Flex>
+              </Flex.Item>
+              <Text
+                size="xs"
+                tag="div"
+                className={cx(
+                  `${rootClassName}__days-left`,
+                  "u-padding--sm u-font-weight-bold t-background-grey-20 t-border-r--sm"
+                )}
+              >
+                {config.content?.days_left_label}
+                <LockIcon
+                  size="sm"
+                  className="u-margin-left--sm"
+                  style={{ width: "10px", height: "11px" }}
+                />
+              </Text>
+            </Flex>
+          )}
+        </Flex>
       </Flex>
-      {config.reelRace ? (
-        <PlayGameButton game={config.reelRace.game} />
-      ) : (
-        <DismissButton />
-      )}
     </Modal>
   );
 };
