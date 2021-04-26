7bf78ca6d1667849cba43cb47d60826a16648a51
chogarcia <choishere@gmail.com>
2021-04-26 11:46:31 +0200

    fix(sports): fix type errors

--- src/features/sports/components/FreebetNotification/FreebetNotificationContainer.tsx
+++ src/features/sports/components/FreebetNotification/FreebetNotificationContainer.tsx
@@ -8,6 +8,7 @@ import {
   getLatestValuable,
 } from "Models/valuables";
 import { FreebetNotification } from "./FreebetNotification";
+import type { PlayerValuableFreeBet } from "Src/types/apollo";
 
 // We are persisting the users action of hiding the notification to localStorage.
 export const IS_HIDDEN_STORAGE_KEY = "isFreebetNotificationHidden";
@@ -37,21 +38,21 @@ const FREEBET_QUERY = gql`
 
 export const FreebetNotificationContainer = () => {
   const { data, loading } = useQuery(FREEBET_QUERY);
-  const [isHidden, setIsHidden] = useState();
+  const [isHidden, setIsHidden] = useState<boolean>();
   const valuables = R.pathOr([], ["player", "valuables"], data);
   // Only display it for locked free bets.
   // This is something that we would like to change to support both, but as we are short on time we would like go in small steps.
   // We have put it here in order to not pollute the generic the FreebetNotification component unnecessarily.
-  const lockedFreebets = getValuablesByState([VALUABLE_STATES.LOCKED])(
+  const lockedFreebets: PlayerValuableFreeBet[] = getValuablesByState([VALUABLE_STATES.LOCKED])(
     valuables
   );
-  const latestLockedFreebet = getLatestValuable(lockedFreebets);
+  // @ts-expect-error  Type '{}' is missing the following properties from type 'PlayerValuableFreeBet': backgroundImage, content, currency, expirationTimeInHours, and 12 more.ts(2740)
+  const latestLockedFreebet: PlayerValuableFreeBet = getLatestValuable(lockedFreebets);
 
   if (
     isHidden ||
     loading ||
     !latestLockedFreebet ||
-    // @ts-expect-error ts-migrate(2339) FIXME: Property 'id' does not exist on type 'unknown'.
     getPersistedIsHidden(latestLockedFreebet?.id)
   ) {
     return null;
@@ -59,28 +60,17 @@ export const FreebetNotificationContainer = () => {
 
   return (
     <FreebetNotification
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'backgroundImage' does not exist on type ... Remove this comment to see the full error message
       backgroundImage={latestLockedFreebet.backgroundImage}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'currency' does not exist on type 'unknow... Remove this comment to see the full error message
       currency={latestLockedFreebet.currency}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'expiryDate' does not exist on type 'unkn... Remove this comment to see the full error message
       expiryDate={latestLockedFreebet.expiryDate}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'market' does not exist on type 'unknown'... Remove this comment to see the full error message
       market={latestLockedFreebet.market}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'valuableState' does not exist on type 'u... Remove this comment to see the full error message
       valuableState={latestLockedFreebet.valuableState}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'valuableType' does not exist on type 'un... Remove this comment to see the full error message
       valuableType={latestLockedFreebet.valuableType}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'title' does not exist on type 'unknown'.
       title={latestLockedFreebet.title}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'content' does not exist on type 'unknown... Remove this comment to see the full error message
       description={latestLockedFreebet.content}
-      // @ts-expect-error ts-migrate(2339) FIXME: Property 'caveat' does not exist on type 'unknown'... Remove this comment to see the full error message
       caveat={latestLockedFreebet.caveat}
       onClose={() => {
-        // @ts-expect-error ts-migrate(2345) FIXME: Argument of type 'boolean' is not assignable to pa... Remove this comment to see the full error message
         setIsHidden(true);
-        // @ts-expect-error ts-migrate(2339) FIXME: Property 'id' does not exist on type 'unknown'.
         persistIsHidden(latestLockedFreebet.id);
       }}
     />
