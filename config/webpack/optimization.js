bcea3170bea9317b81799f531accec1e03d3acba
Dave Calleja <dave.calleja@casumo.com>
2021-04-28 09:28:45 +0200

    re-added terser plugin

--- config/webpack/optimization.js
+++ config/webpack/optimization.js
@@ -1,4 +1,8 @@
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const safePostCssParser = require("postcss-safe-parser");
+
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
 
 module.exports = env => ({
   moduleIds: env.production ? "hashed" : false,
@@ -19,5 +23,64 @@ module.exports = env => ({
     },
   },
   minimize: env.production,
-  minimizer: [new CssMinimizerPlugin()],
+  minimizer: [
+    new TerserPlugin({
+      terserOptions: {
+        parse: {
+          // we want terser to parse ecma 8 code. However, we don't want it
+          // to apply any minfication steps that turns valid ecma 5 code
+          // into invalid ecma 5 code. This is why the 'compress' and 'output'
+          // sections only apply transformations that are ecma 5 safe
+          // https://github.com/facebook/create-react-app/pull/4234
+          ecma: 8,
+        },
+        compress: {
+          ecma: 5,
+          warnings: false,
+          // Disabled because of an issue with Uglify breaking seemingly valid code:
+          // https://github.com/facebook/create-react-app/issues/2376
+          // Pending further investigation:
+          // https://github.com/mishoo/UglifyJS2/issues/2011
+          comparisons: false,
+          // Disabled because of an issue with Terser breaking valid code:
+          // https://github.com/facebook/create-react-app/issues/5250
+          // Pending futher investigation:
+          // https://github.com/terser-js/terser/issues/120
+          inline: 2,
+        },
+        mangle: {
+          safari10: true,
+        },
+        output: {
+          ecma: 5,
+          comments: false,
+          // Turned on because emoji and regex is not minified properly using default
+          // https://github.com/facebook/create-react-app/issues/2488
+          ascii_only: true,
+        },
+      },
+      // Use multi-process parallel running to improve the build speed
+      // Default number of concurrent runs: os.cpus().length - 1
+      parallel: true,
+      // Enable file caching
+      cache: true,
+      sourceMap: shouldUseSourceMap,
+    }),
+    // This is only used in production mode
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorOptions: {
+        parser: safePostCssParser,
+        map: shouldUseSourceMap
+          ? {
+              // `inline: false` forces the sourcemap to be output into a
+              // separate file
+              inline: false,
+              // `annotation: true` appends the sourceMappingURL to the end of
+              // the css file, helping the browser find the sourcemap
+              annotation: true,
+            }
+          : false,
+      },
+    }),
+  ],
 });
