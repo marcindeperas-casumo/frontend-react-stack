e71dfffbebaf784846c7a53f44edc0e65711f5fd
Michał Ignaszewski <michal.ignaszewski@casumo.com>
2021-04-29 11:33:35 +0200

    missing param

--- src/features/sports/components/SportsYouWon/SportsYouWonQuery.ts
+++ src/features/sports/components/SportsYouWon/SportsYouWonQuery.ts
@@ -3,6 +3,7 @@ import { gql } from "@apollo/client";
 export const BET_DETAILS_QUERY = gql`
   query BetDetailsQuery($combinationRef: BigInt!) {
     betDetails(combinationRef: $combinationRef) {
+      username
       payout
       currency
       status
