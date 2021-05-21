bb7841ffe6a8df0ee435b19af1d588a3de38765d
Michał Ignaszewski <michal.ignaszewski@casumo.com>
2021-05-21 09:08:42 +0200

    Adding cms config to sports jackpots

--- src/features/sports/components/SportsJackpots/SportsJackpots.tsx
+++ src/features/sports/components/SportsJackpots/SportsJackpots.tsx
@@ -1,16 +1,38 @@
 import * as React from "react";
 import "./SportsJackpots.scss";
+import { useTranslations } from "Utils/hooks";
+import { SportsJackpotsTranslations } from "Features/sports/components/SportsJackpots/SportsJackpots.types";
+import { isTestEnv } from "Utils";
+import { isMobile, isTablet } from "Components/ResponsiveLayout";
 
-const data = {
-  background_desktop:
-    "https://cms.casumo.com/wp-content/uploads/2021/05/Desktop.png",
-};
+const CMS_SLUG_CONFIG = "sports.sports-jackpots-component-config-page";
+const CMS_SLUG_JACKPOTS = "sports-jackpots";
 
 export const SportsJackpots = () => {
+  const t = useTranslations<SportsJackpotsTranslations>(CMS_SLUG_CONFIG);
+
+  if (
+    !t ||
+    (t && isTestEnv() && t.enable_for_test === false) ||
+    (t && !isTestEnv() && t.enable_for_prod === false)
+  ) {
+    return null;
+  }
+
+  const backgroundImage = () => {
+    if (isMobile()) {
+      return t.background_mobile.url;
+    }
+    if (isTablet()) {
+      return t.background_tablet.url;
+    }
+    return t.background_desktop.url;
+  };
+
   return (
     <div
-      className="c-sports-jackpots"
-      style={{ backgroundImage: `url('${data.background_desktop}'` }}
+      className="c-sports-jackpots bg-center bg-gradient-to-b"
+      style={{ backgroundImage: `url('${backgroundImage()}'` }}
     >
       aaaa
     </div>
