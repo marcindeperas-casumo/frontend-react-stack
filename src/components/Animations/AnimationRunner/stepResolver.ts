1a2c4bdb317767600c2e9424672e3dd4a7de0750
lukKowalski <lukasz.kowalski@casumo.com>
2021-06-07 15:13:35 +0200

    added coment in stepResolver

--- src/components/Animations/AnimationRunner/stepResolver.ts
+++ src/components/Animations/AnimationRunner/stepResolver.ts
@@ -1,6 +1,7 @@
 import { ClipTest } from "../AnimationClips/ClipTest/ClipTest";
 import { AnimationClipProps, AvailableAnimationClipsProps } from "../constants";
 
+// Map all new clips here so they will be avaliable to compose into animation
 const animationClipsMap = {
   test: ClipTest,
 };
