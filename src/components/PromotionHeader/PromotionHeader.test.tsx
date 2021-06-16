013812037d331aae7a9734b1777d1dd5015cb0b1
Christian Ciantar <christian.ciantar@casumo.com>
2021-06-16 10:46:59 +0200

    fix tests

--- src/components/PromotionHeader/PromotionHeader.test.tsx
+++ src/components/PromotionHeader/PromotionHeader.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 import PromotionHeader from "Components/PromotionHeader";
 
 describe("PromotionHeader", () => {
@@ -14,18 +14,14 @@ describe("PromotionHeader", () => {
   };
 
   test("render a Media component if there is a badge", () => {
-    const rendered = mount(<PromotionHeader {...mockPromotionWithBadge} />);
-    expect(
-      rendered.children().find("PromotionHeaderTextWithBadge").length
-    ).toBe(1);
-    expect(rendered.children().find("PromotionHeaderText").length).toBe(0);
+    const rendered = shallow(<PromotionHeader {...mockPromotionWithBadge} />);
+    expect(rendered.find("PromotionTitleTextWithBadge").length).toBe(1);
+    expect(rendered.find("PromotionTitleText").length).toBe(0);
   });
 
   test("render a div if there isn't a badge", () => {
-    const rendered = mount(<PromotionHeader {...mockPromotion} />);
-    expect(
-      rendered.children().find("PromotionHeaderTextWithBadge").length
-    ).toBe(0);
-    expect(rendered.children().find("PromotionHeaderText").length).toBe(1);
+    const rendered = shallow(<PromotionHeader {...mockPromotion} />);
+    expect(rendered.find("PromotionTitleTextWithBadge").length).toBe(0);
+    expect(rendered.find("PromotionTitleText").length).toBe(1);
   });
 });
