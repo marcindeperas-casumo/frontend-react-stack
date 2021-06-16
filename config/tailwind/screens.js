106a0038d89a97126686d6a48242d1ec0f63d666
Christian Ciantar <christian.ciantar@casumo.com>
2021-06-16 10:55:16 +0200

    cleanup

--- config/tailwind/screens.js
+++ config/tailwind/screens.js
@@ -6,10 +6,3 @@ module.exports = {
   "desktop": "1024px",
 };
 /* eslint-enable prettier/prettier */
-
-// module.exports = {
-//   mobile: { min: "0", max: "374px" },
-//   phablet: { min: "375px", max: "767px" },
-//   tablet: { min: "768px", max: "1023px" },
-//   desktop: { min: "1024px" },
-// };
