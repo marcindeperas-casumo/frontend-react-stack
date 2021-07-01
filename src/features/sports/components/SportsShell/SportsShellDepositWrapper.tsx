35ffd9f81b05230f2e9e64b6c7cc821b64b0aa2d
Shaun Grech <shaun.grech@casumo.com>
2021-07-01 16:08:10 +0200

    feat: prettified the function

--- src/features/sports/components/SportsShell/SportsShellDepositWrapper.tsx
+++ src/features/sports/components/SportsShell/SportsShellDepositWrapper.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { Deposit } from "Components/Payments/Deposit";
 import { Mobile } from "Components/ResponsiveLayout";
 
-export const SportsShellDepositWrapper = () => {
-  return (
-    <Mobile>
-      <div className="pt-sm px-sm">
-        <Deposit />
-      </div>
-    </Mobile>
-  );
-};
+export const SportsShellDepositWrapper = () => (
+  <Mobile>
+    <div className="pt-sm px-sm">
+      <span>Hello</span>
+      <Deposit />
+    </div>
+  </Mobile>
+);
