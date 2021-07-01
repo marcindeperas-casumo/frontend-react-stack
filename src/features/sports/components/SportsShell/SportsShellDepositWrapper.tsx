69fcc0ccdc18a9d100c69f797b0e2af05193659f
Shaun Grech <shaun.grech@casumo.com>
2021-07-01 15:58:49 +0200

    feat: removed unsed references

--- src/features/sports/components/SportsShell/SportsShellDepositWrapper.tsx
+++ src/features/sports/components/SportsShell/SportsShellDepositWrapper.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Deposit } from "Components/Payments/Deposit";
 import { Mobile } from "Components/ResponsiveLayout";
-import { hasMadeFirstDepositSelector } from "Models/handshake";
 
 export const SportsShellDepositWrapper = () => {
-  const hasMadeFirstDeposit = useSelector(hasMadeFirstDepositSelector);
-
   return (
     <Mobile>
       <div className="pt-sm px-sm">
