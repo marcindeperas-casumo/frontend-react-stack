b43de9bfc9c4552930682235c0bc1016212f3963
bogdandumitriu <bogdan.dumitriu@casumo.com>
2021-07-12 16:03:26 +0200

    WIP-Add ticker jackpot

---
+++ src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonJackpotsFooterWidgetDesktop.stories.tsx
@@ -0,0 +1,12 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import MockStore from "Components/MockStore";
+import { BlueRibbonJackpotsFooterWidget } from "./BlueRibbonJackpotsFooterWidget";
+import { normalizedPots } from "./__mocks__/jackpots";
+
+const stories = storiesOf("BlueRibbon", module);
+stories.add("JackpotsFooterWidgetDesktop", () => (
+  <MockStore>
+    <BlueRibbonJackpotsFooterWidget normalizedPots={normalizedPots} />
+  </MockStore>
+));
---
+++ src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonJackpotsFooterWidgetDesktop.tsx
@@ -0,0 +1,130 @@
+import Flex from "@casumo/cmp-flex";
+import Text from "@casumo/cmp-text";
+import { useInterval } from "react-use";
+import * as React from "react";
+import classNames from "classnames";
+import { useSelector } from "react-redux";
+import { useLocale } from "Utils/hooks";
+import { currencySelector } from "Models/handshake";
+import { formatCurrency } from "Utils";
+import { useGameModelContext } from "Components/GamePage/Contexts";
+import type { PotObject } from "./blueRibbonConsts";
+import "./blueRibbonJackpotsFooterWidget.scss";
+
+export function BlueRibbonJackpotsFooterWidget({
+  normalizedPots,
+}: {
+  normalizedPots: Array<PotObject>;
+}) {
+  const locale = useLocale();
+  const currency = useSelector(currencySelector);
+  const [visibleSection, setVisibleSection] = React.useState(0);
+
+  const formattedPotValue = pot => {
+    return formatCurrency({
+      currency,
+      locale,
+      value: pot.value,
+    });
+  };
+
+  useInterval(() => {
+    if (normalizedPots.length > 2) {
+      setVisibleSection(x => 1 - x);
+    }
+  }, 5000);
+
+  const { gameProviderModel } = useGameModelContext();
+  React.useEffect(() => {
+    setTimeout(() => {
+      gameProviderModel.fitToParentSize();
+    });
+  }, [gameProviderModel]);
+
+  const jackpotsRows =
+    normalizedPots.length > 2
+      ? [normalizedPots.slice(0, 2), normalizedPots.slice(2, 4)]
+      : [normalizedPots];
+
+  return (
+    <div className="u-overflow--hidden bg-grey-90 o-flex-align--center o-flex-justify--center c-br-footer-widget__container-direction">
+      <Flex
+        direction="vertical"
+        className="u-overflow--hidden bg-grey-70 bg-opacity-50 c-br-footer-widget__container-height c-br-footer-widget__container-width c-br-footer-widget__container-border-r"
+      >
+        <Flex
+          direction="vertical"
+          className={classNames("c-br-footer-widget__animation-transition", {
+            "c-br-footer-widget__transform-animation--reset": !visibleSection,
+            "c-br-footer-widget__transform-animation": visibleSection,
+          })}
+        >
+          {jackpotsRows.map((row, i) => (
+            <Flex
+              key={i}
+              direction="horizontal"
+              align="center"
+              justify="center"
+              spacing="lg"
+              className={classNames(
+                "o-flex__item--no-shrink o-flex--wrap c-br-footer-widget__animation-transition c-br-footer-widget__jackpot-padding",
+                visibleSection === i ? "t-opacity--100" : "t-opacity--0"
+              )}
+            >
+              {row.map((pot, idx) => (
+                <Flex.Item key={idx}>
+                  <PotItem
+                    className="o-flex--1"
+                    pot={pot}
+                    formattedValue={formattedPotValue(pot)}
+                  />
+                </Flex.Item>
+              ))}
+            </Flex>
+          ))}
+        </Flex>
+      </Flex>
+    </div>
+  );
+}
+
+type TPotItemProps = {
+  formattedValue: string;
+  pot: PotObject;
+  className: string;
+};
+
+const PotItem = ({ formattedValue, pot, className }: TPotItemProps) => {
+  const { icon, shortName } = pot;
+  return (
+    <Flex
+      direction="horizontal"
+      align="center"
+      justify="center"
+      spacing="sm"
+      className={className}
+    >
+      <img
+        className="t-border-r--circle"
+        width={16}
+        height={16}
+        alt={`${shortName} icon`}
+        src={icon}
+      />
+      <Text
+        size="xs"
+        tag="span"
+        className="u-font-weight-bold u-line-height--1 text-white"
+      >
+        <Text
+          size="2xs"
+          tag="span"
+          className="u-text-transform-uppercase text-grey-50 u-margin-x--sm"
+        >
+          {shortName}
+        </Text>
+        {formattedValue}
+      </Text>
+    </Flex>
+  );
+};
