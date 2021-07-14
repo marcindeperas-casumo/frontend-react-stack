e33fd346c8c9f98faa1ea23cfc3df1c2b1aee870
lukKowalski <lukasz.kowalski@casumo.com>
2021-07-14 13:31:57 +0200

    transition step with animation, ready to be put in amination runner

---
+++ src/components/Animations/AnimationClips/CasumoJackpot/TransitionStep/TransitionStep.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import OverlayImage from "./transitionOverlayWheel.svg";
+
+import "./TransitionStep.scss";
+
+const duration = 1200;
+
+export const TransitionStep = ({ onShowNext, onTransition }) => {
+  const [size, setSize] = React.useState({ width: 0, height: 0 });
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      onTransition();
+    }, duration / 2);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const updateSize = () => {
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  React.useEffect(() => {
+    updateSize();
+
+    window.addEventListener("resize", updateSize);
+
+    return () => {
+      window.removeEventListener("resize", updateSize);
+    };
+  }, []);
+
+  const width = Math.hypot(size.width, size.height) * 2;
+
+  return (
+    <div className="c-transition-layer o-position--absolute u-width--full u-height--full u-overflow--hidden">
+      <div
+        style={{
+          width,
+          height: width,
+          left: `-${width / 2}px`,
+          top: `calc(100% - ${width / 2}px)`,
+        }}
+        className="o-position--absolute"
+      >
+        <OverlayImage
+          style={{ animationDuration: `${duration}ms` }}
+          onAnimationEnd={onShowNext}
+          className="c-rotate-layer"
+        />
+      </div>
+    </div>
+  );
+};
