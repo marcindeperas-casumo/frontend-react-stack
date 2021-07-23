156c03aab327923273479105473e19d68ffd2837
lukKowalski <lukasz.kowalski@casumo.com>
2021-07-23 09:18:43 +0200

    updated to BR notifications after refactoring of useSubscription hook

--- src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonChristmasCampaignNotifications/BlueRibbonChristmasCampaignNotificationsContainer.tsx
+++ src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonChristmasCampaignNotifications/BlueRibbonChristmasCampaignNotificationsContainer.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import * as R from "ramda";
 import { useFetch } from "Utils/hooks";
-import { useGameModelContext } from "Components/GamePage/Contexts";
 import { urls } from "../blueRibbonConsts";
 import { BlueRibbonChristmasCampaignNotifications } from "./BlueRibbonChristmasCampaignNotifications";
 
 export function BlueRibbonChristmasCampaignNotificationsContainer() {
-  const { pauseGame, resumeGame } = useGameModelContext();
   const { response } = useFetch(urls.handshake);
   const available = R.propOr(false, "available", response);
 
@@ -14,10 +12,5 @@ export function BlueRibbonChristmasCampaignNotificationsContainer() {
     return null;
   }
 
-  return (
-    <BlueRibbonChristmasCampaignNotifications
-      pauseGame={pauseGame}
-      resumeGame={resumeGame}
-    />
-  );
+  return <BlueRibbonChristmasCampaignNotifications />;
 }
