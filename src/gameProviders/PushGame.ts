9fb5daba1288e4dfa144434a83b070c06319f1ec
Michał Mokijewski <hello@naf.gl>
2021-09-01 22:52:18 +0200

    Fix Push Gaming integration
    - extractEventId was incorrectly overwritten (it was extracting undefined from
      everything)
    - gameReady event was incorrectly used for GAME_ROUND_START (it happens only
      once and not at the beginning of every round, this event is needed for proper
      idle status)
    - targetDomain was incorrectly overwritten (default is correct value, incorrect
      value was filtering out all events, even those that we should handle)

--- src/gameProviders/PushGame.ts
+++ src/gameProviders/PushGame.ts
@@ -2,14 +2,15 @@ import type { GameProviderModelProps } from "./types";
 import { BaseIframeGame } from "./BaseIframeGame";
 import { appendToGameUrl } from "./utils";
 
-export const PUSH_GAME_EVENT_TYPE = Object.freeze({
+export const PUSH_GAME_EVENT_TYPE = {
   DISABLE: "disable",
   ENABLE: "enable",
   GO_TO_LOBBY: "gotoLobby",
   GO_TO_ACCOUNT: "gotoAccount",
   GAME_READY: "gameReady",
+  GAME_ANIMATION_STARTED: "gamePlayAnimationStarted",
   GAME_ANIMATION_COMPLETE: "gameAnimationComplete",
-});
+} as const;
 
 export type PushGameEventTypeLiteral = ValueOf<typeof PUSH_GAME_EVENT_TYPE>;
 
@@ -24,7 +25,7 @@ export const PUSH_GAME_COMMANDS = {
 };
 
 export const PUSH_GAME_EVENTS = {
-  GAME_ROUND_START: PUSH_GAME_EVENT_TYPE.GAME_READY,
+  GAME_ROUND_START: PUSH_GAME_EVENT_TYPE.GAME_ANIMATION_STARTED,
   GAME_ROUND_END: PUSH_GAME_EVENT_TYPE.GAME_ANIMATION_COMPLETE,
 };
 
@@ -43,7 +44,6 @@ export class PushGame extends BaseIframeGame {
     this.api.commands.resume = PUSH_GAME_COMMANDS.RESUME;
     this.api.events.onGameRoundStart = PUSH_GAME_EVENTS.GAME_ROUND_START;
     this.api.events.onGameRoundEnd = PUSH_GAME_EVENTS.GAME_ROUND_END;
-    this.targetDomain = props.origin || window.location.origin;
   }
 
   get componentProps() {
@@ -73,9 +73,5 @@ export class PushGame extends BaseIframeGame {
     if (message.data.method === PUSH_GAME_EVENT_TYPE.GO_TO_LOBBY) {
       this.goToLobby();
     }
-  }
-
-  extractEventId(data: any) {
-    return data.method;
   }
 }
