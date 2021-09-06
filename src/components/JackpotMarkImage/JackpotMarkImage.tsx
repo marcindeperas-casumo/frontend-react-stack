8f20ecbeef01bfc5667f2370199bf7bf766d55ad
lukKowalski <lukasz.kowalski@casumo.com>
2021-09-06 10:57:32 +0200

    JackpotMarkImage component

---
+++ src/components/JackpotMarkImage/JackpotMarkImage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type TProps = {
+  url: string;
+  type: "thumb" | "tile";
+};
+
+const THUMB_SIZE = "25px";
+const TILE_SIZE = "25px";
+
+const styles = {
+  thumb: { width: THUMB_SIZE, top: "10px", left: "10px" },
+  tile: { width: TILE_SIZE, top: "-10px", left: "-10px" },
+};
+
+export const JackpotMarkImage = ({ url, type }: TProps) => {
+  return (
+    <div style={styles[type]} className="o-position--absolute">
+      <img src={url} />
+    </div>
+  );
+};
