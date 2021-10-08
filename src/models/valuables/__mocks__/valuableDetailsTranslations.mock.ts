1a3c08096fa87eadc77c258749dd1711f6ba0eac
Christian Ciantar <christian.ciantar@casumo.com>
2021-10-08 17:21:05 +0200

    fix type errors

--- src/models/valuables/__mocks__/valuableDetailsTranslations.mock.ts
+++ src/models/valuables/__mocks__/valuableDetailsTranslations.mock.ts
@@ -15,5 +15,10 @@ export default ({
   "hour_plural": "{{value}} Hours",
   "day_singular": "{{value}} Day",
   "day_plural": "{{value}} Days",
-  "termsAndConditionsContent": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget vehicula ante, eget tincidunt lectus. Sed aliquet sapien dolor, eu pretium quam pellentesque sed. Praesent est orci, efficitur nec mi ut, efficitur blandit ligula. Etiam vestibulum sapien a sapien commodo lobortis ac et mauris. Sed feugiat varius tellus, id pharetra risus faucibus nec."
+  "termsAndConditionsTitle": "Lorem ipsum dolor sit amet",
+  "generalTermsAndConditionsTitle": "Ut eget vehicula ante, eget tincidunt lectus",
+  "marketSpecificTermsAndConditionsTitle": "Sed aliquet sapien dolor, eu pretium quam pellentesque sed",
+  "termsAndConditionsContent": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget vehicula ante, eget tincidunt lectus. Sed aliquet sapien dolor, eu pretium quam pellentesque sed. Praesent est orci, efficitur nec mi ut, efficitur blandit ligula. Etiam vestibulum sapien a sapien commodo lobortis ac et mauris. Sed feugiat varius tellus, id pharetra risus faucibus nec.",
+  "generalTermsAndConditionsContent": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget vehicula ante, eget tincidunt lectus. Sed aliquet sapien dolor, eu pretium quam pellentesque sed. Praesent est orci, efficitur nec mi ut, efficitur blandit ligula. Etiam vestibulum sapien a sapien commodo lobortis ac et mauris. Sed feugiat varius tellus, id pharetra risus faucibus nec.",
+  "marketSpecificTermsAndConditionsContent": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget vehicula ante, eget tincidunt lectus. Sed aliquet sapien dolor, eu pretium quam pellentesque sed. Praesent est orci, efficitur nec mi ut, efficitur blandit ligula. Etiam vestibulum sapien a sapien commodo lobortis ac et mauris. Sed feugiat varius tellus, id pharetra risus faucibus nec."
 });
