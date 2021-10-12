a86df8085ee91e4fb9c73a4c05a6b63eb50f6722
Mariusz <mariusz.klinger@casumo.com>
2021-10-12 12:42:45 +0200

    currency rates recalculated

--- src/components/GamePage/GamePageNotifications/LowBalanceNotification/lowBalance.constants.ts
+++ src/components/GamePage/GamePageNotifications/LowBalanceNotification/lowBalance.constants.ts
@@ -7,12 +7,12 @@ type TLowBalancesThresholds = {
 
 export const LOW_BALANCES_THRESHOLDS: TLowBalancesThresholds = {
   EUR: 10,
-  GBP: 10,
-  DKK: 10,
-  NOK: 10,
-  SEK: 10,
-  CAD: 10,
-  NZD: 10,
-  INR: 10,
-  USD: 10,
+  GBP: 8.49,
+  DKK: 74.41,
+  NOK: 98.78,
+  SEK: 101.09,
+  CAD: 14.41,
+  NZD: 16.61,
+  INR: 873,
+  USD: 11.56,
 };
