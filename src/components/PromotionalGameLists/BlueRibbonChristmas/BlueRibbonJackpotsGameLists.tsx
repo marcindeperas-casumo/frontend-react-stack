4e038bfa02320fdb2a10982d45c851535fb49331
Bogdan Dumitriu <87020741+bogdandumitriu@users.noreply.github.com>
2021-10-15 16:19:08 +0200

    Revert "Revert "Tcas 1377 jackpot onboarding component""

--- src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonJackpotsGameLists.tsx
+++ src/components/PromotionalGameLists/BlueRibbonChristmas/BlueRibbonJackpotsGameLists.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@apollo/client";
 import * as React from "react";
 import * as R from "ramda";
+import Flex from "@casumo/cmp-flex";
 import * as A from "Types/apollo";
+import logger from "Services/logger";
 import { GameListHorizontalWithWidget } from "Components/GameListHorizontal/GameListHorizontalWithWidget";
 import { GameListQuery } from "Components/GameListHorizontal/GameListHorizontalDefault/GameListHorizontalDefault.graphql";
 import { useTranslatedUrl, useTranslations } from "Utils/hooks";
@@ -9,6 +11,7 @@ import { ROUTE_IDS } from "Src/constants";
 import { BlueRibbonJackpotsWidget } from "./BlueRibbonJackpotsWidget";
 import { useBlueRibbonSDKAnonymous } from "./useBlueRibbonSDK";
 import { useComposedJackpotConfigBySlug } from "./useComposedJackpot";
+import { BlueRibbonJackpotsOnboardingWidget } from "./BlueRibbonJackpotsOnboardingWidget";
 
 export function BlueRibbonJackpotsGameLists(props: { jackpot_slug: string }) {
   const { composedJackpot } = useComposedJackpotConfigBySlug({
@@ -18,8 +21,18 @@ export function BlueRibbonJackpotsGameLists(props: { jackpot_slug: string }) {
   const t = useTranslations<{ more_link: string }>(
     "built-pages.top-lists-translations"
   );
+  const [onboardingVisible, setOnboardingVisible] = React.useState(true);
+  const userViewedJackpotOnboardingOffer = localStorage.getItem(
+    "JackpotOnboardingOfferPresented"
+  );
+
   const jackpotConfigs = useTranslations<{
     jackpot_image: string;
+    jackpot_onboarding_cta_link: string;
+    jackpot_onboarding_background: string;
+    jackpot_onboarding_title: string;
+    jackpot_onboarding_body: string;
+    jackpot_onboarding_button_copy: string;
   }>(`jackpots-configs.${props.jackpot_slug}`);
 
   useBlueRibbonSDKAnonymous();
@@ -42,6 +55,17 @@ export function BlueRibbonJackpotsGameLists(props: { jackpot_slug: string }) {
     return null;
   }
 
+  const onCloseOnboardingWidget = () => {
+    try {
+      localStorage.setItem("JackpotOnboardingOfferPresented", "true");
+    } catch (error) {
+      logger.error("JackpotOnboardingOfferPresented local storage error", {
+        error,
+      });
+    }
+    setOnboardingVisible(false);
+  };
+
   return (
     <GameListHorizontalWithWidget
       gamesInColumn={composedJackpot.pots.length > 2 ? 3 : 2}
@@ -52,11 +76,28 @@ export function BlueRibbonJackpotsGameLists(props: { jackpot_slug: string }) {
         text: t.more_link,
       }}
       Widget={() => (
-        <BlueRibbonJackpotsWidget
-          composedPots={composedJackpot.pots}
-          widgetColor={composedJackpot.widgetColor}
-          jackpotLogo={jackpotConfigs.jackpot_image}
-        />
+        <Flex direction="horizontal">
+          <BlueRibbonJackpotsWidget
+            composedPots={composedJackpot.pots}
+            widgetColor={composedJackpot.widgetColor}
+            jackpotLogo={jackpotConfigs.jackpot_image}
+            explainerPageUrl={jackpotConfigs.jackpot_onboarding_cta_link}
+          />
+          {onboardingVisible && !userViewedJackpotOnboardingOffer && (
+            <BlueRibbonJackpotsOnboardingWidget
+              content={{
+                backgroundImage: jackpotConfigs.jackpot_onboarding_background,
+                title: jackpotConfigs.jackpot_onboarding_title,
+                body: jackpotConfigs.jackpot_onboarding_body,
+                cta: jackpotConfigs.jackpot_onboarding_button_copy,
+                ctaLink: jackpotConfigs.jackpot_onboarding_cta_link,
+              }}
+              composedPots={composedJackpot.pots}
+              widgetColor={composedJackpot.widgetColor}
+              onClose={onCloseOnboardingWidget}
+            />
+          )}
+        </Flex>
       )}
     />
   );
