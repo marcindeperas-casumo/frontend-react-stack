0bd562ede0645009fe7c1956420fdf2d9c354154
bogdandumitriu <bogdan.dumitriu@casumo.com>
2021-10-25 16:15:26 +0200

    format improvement

--- src/components/GamePage/Hooks/useNetFinancialPositionValue.ts
+++ src/components/GamePage/Hooks/useNetFinancialPositionValue.ts
@@ -7,15 +7,13 @@ import {
 import { winAnimationSelector } from "Models/blueribbonJackpots/jackpots.selectors";
 
 export const useNetFinancialPositionValue = () => {
-  const [netFinancialPosition, setNetFinancialPosition] = useState("0");
+  const [netFinancialPosition, setNetFinancialPosition] = useState(0);
   const netWinning = useSelector(playerNetWinningsUpdateSelector);
   const netLoss = useSelector(playerNetLossesUpdateSelector);
   const winAnimationRunning = useSelector(winAnimationSelector);
 
   useEffect(() => {
-    setNetFinancialPosition(
-      Math.abs(Number(Number(netWinning) - Number(netLoss))).toFixed(2)
-    );
+    setNetFinancialPosition(Number(Number(netWinning) - Number(netLoss)));
   }, [netWinning, netLoss, winAnimationRunning]);
 
   return netFinancialPosition;
