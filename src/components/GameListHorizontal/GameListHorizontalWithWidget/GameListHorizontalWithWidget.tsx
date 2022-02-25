f36a2bf8c323c45e15ba820f4bd2dd842e6c55bf
Bogdan Dumitriu <87020741+bogdandumitriu@users.noreply.github.com>
2022-02-25 15:47:03 +0100

    TCAS-1364-Replace item renderer scrollable component (#1918)
    * Replace item renderer scrollable component
    
    * lint

--- src/components/GameListHorizontal/GameListHorizontalWithWidget/GameListHorizontalWithWidget.tsx
+++ src/components/GameListHorizontal/GameListHorizontalWithWidget/GameListHorizontalWithWidget.tsx
@@ -1,10 +1,7 @@
-import Scrollable from "@casumo/cmp-scrollable";
 import * as React from "react";
 import * as R from "ramda";
 import * as A from "Types/apollo";
-import { ScrollableListTitleRow } from "Components/ScrollableListTitleRow";
 import { ScrollableListPaginated } from "Components/ScrollableListPaginated";
-import { Desktop, MobileAndTablet } from "Components/ResponsiveLayout";
 import { GameRow, GameRowText } from "Components/GameRow";
 import {
   topListWidgetWidth,
@@ -36,33 +33,7 @@ export const GameListHorizontalWithWidget = ({
 }: Props) => {
   const columns = R.splitEvery(gamesInColumn, games);
 
-  const mobileItemRenderer = (i: number) => {
-    if (i === 0) {
-      return <JackpotWidget />;
-    }
-
-    if (i === 1 && JackpotOnboardingWidget) {
-      return <JackpotOnboardingWidget />;
-    }
-
-    return columns[i - 2].map(game => (
-      <div
-        key={game.id}
-        className="u-padding-bottom"
-        style={{ width: topListWidgetWidth }}
-      >
-        <GameRow
-          game={game}
-          className="bg-white t-border-r--md t-elevation--10"
-          renderText={() => (
-            <GameRowText name={game.name} description={game.gameStudio} />
-          )}
-        />
-      </div>
-    ));
-  };
-
-  const desktopItemRenderer = ({ style, columnIndex, key }) => {
+  const itemRenderer = ({ style, columnIndex, key }) => {
     return (
       <div key={key} style={style}>
         {columnIndex === 1 && JackpotOnboardingWidget && (
@@ -90,33 +61,20 @@ export const GameListHorizontalWithWidget = ({
       </div>
     );
   };
-
   return (
     <div className={`o-wrapper ${topMarginClasses} ${xPaddingClasses}`}>
-      <MobileAndTablet>
-        {name && <ScrollableListTitleRow title={name} seeMore={seeMore} />}
-        <Scrollable
-          numberOfItems={
-            /* +1 because widget takes up one column, and is not inside columns array */
-            columns.length + 1
-          }
-          itemRenderer={mobileItemRenderer}
-        />
-      </MobileAndTablet>
-      <Desktop>
-        <ScrollableListPaginated
-          title={name}
-          itemCount={
-            /* +1 because widget takes up one column, and is not inside columns array */
-            columns.length + 1
-          }
-          itemRenderer={desktopItemRenderer}
-          tileHeight={
-            gamesInColumn < 3 ? topListWidgetHeightTwoRows : topListWidgetHeight
-          }
-          seeMore={seeMore}
-        />
-      </Desktop>
+      <ScrollableListPaginated
+        title={name}
+        itemCount={
+          /* +1 because widget takes up one column, and is not inside columns array */
+          columns.length + 1
+        }
+        itemRenderer={itemRenderer}
+        tileHeight={
+          gamesInColumn < 3 ? topListWidgetHeightTwoRows : topListWidgetHeight
+        }
+        seeMore={seeMore}
+      />
     </div>
   );
 };
